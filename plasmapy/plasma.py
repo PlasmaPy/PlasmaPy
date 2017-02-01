f67ac0588dbd0e203773444e6593b8711c2f1460
Drew Leonard <andy.j.leonard@gmail.com>
2017-02-01 16:23:27 +0000

    Initial variable arrays and some docstrings

--- plasmapy/plasma.py
+++ plasmapy/plasma.py
@@ -9,11 +9,54 @@ import numpy as np
 import astropy.units as u
 
 class Plasma():
+    """Core class for describing and calculating plasma parameters.
+
+
+
+    Attributes
+    ----------
+    x : `astropy.units.Quantity`
+        x-coordinates within the plasma domain. Equal to the `domain_x` input parameter.
+    y : `astropy.units.Quantity`
+        y-coordinates within the plasma domain. Equal to the `domain_y` input parameter.
+    z : `astropy.units.Quantity`
+        z-coordinates within the plasma domain. Equal to the `domain_z` input parameter.
+    grid : `astropy.units.Quantity`
+        (3, x, y, z) array containing the values of each coordinate at every point in the domain.
+    domain_shape : tuple
+        Shape of the plasma domain.
+    density : `astropy.units.Quantity`
+        (x, y, z) array of mass density at every point in the domain.
+    momentum : `astropy.units.Quantity`
+        (3, x, y, z) array of the momentum vector at every point in the domain.
+    pressure : `astropy.units.Quantity`
+        (x, y, z) array of pressure at every point in the domain.
+    magnetic_field : `astropy.units.Quantity`
+        (3, x, y, z) array of the magnetic field vector at every point in the domain.
+
+        
+    Parameters
+    ----------
+    domain_x : `astropy.units.Quantity`
+        1D array of x-coordinates for the plasma domain. Must have units convertable to length.
+    domain_y : `astropy.units.Quantity`
+        1D array of y-coordinates for the plasma domain. Must have units convertable to length.
+    domain_z : `astropy.units.Quantity`
+        1D array of z-coordinates for the plasma domain. Must have units convertable to length.
+    """
     @u.quantity_input(domain_x=u.m, domain_y=u.m, domain_z=u.m)
     def __init__(self, domain_x, domain_y, domain_z):
+        # Define domain sizes
         self.x = domain_x
         self.y = domain_y
         self.z = domain_z
 
         x, y, z = self.x.si.value, self.y.si.value, self.z.si.value
         self.grid = np.meshgrid(x, y, z, indexing='ij') * u.m
+        self.domain_shape = (len(self.x), len(self.y), len(self.z))
+
+        # Initiate core plasma variables
+        self.density = np.zeros(self.domain_shape) * u.kg / u.m**3
+        self.momentum = np.zeros((3, *self.domain_shape)) * u.kg / (u.m**2 * u.s)
+        self.pressure = np.zeros(self.domain_shape) * u.Pa
+        self.magnetic_field = np.zeros((3, *self.domain_shape)) * u.T
--- plasmapy/tests/tests.py
+++ plasmapy/tests/tests.py
@@ -9,7 +9,21 @@ def test_plasma_defaults():
                                 domain_y=np.linspace(0, 1, 64)*u.m,
                                 domain_z=np.linspace(0, 1, 64)*u.m)
 
+    # Basic grid setup
     assert test_plasma.x.shape == (64,)
     assert test_plasma.y.shape == (64,)
     assert test_plasma.z.shape == (64,)
     assert test_plasma.grid.shape == (3, 64, 64, 64)
+
+    # Core variable units and shapes
+    assert test_plasma.density.shape == (64, 64, 64)
+    assert test_plasma.density.si.unit == u.kg / u.m**3
+
+    assert test_plasma.momentum.shape == (3, 64, 64, 64)
+    assert test_plasma.momentum.si.unit == u.kg / (u.m**2 * u.s)
+
+    assert test_plasma.pressure.shape == (64, 64, 64)
+    assert test_plasma.pressure.si.unit == u.Pa
+
+    assert test_plasma.magnetic_field.shape == (3, 64, 64, 64)
+    assert test_plasma.magnetic_field.si.unit == u.T
