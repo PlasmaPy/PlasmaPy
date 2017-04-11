f61ee7223d039f7810fd396b810353af693ca806
Nick Murphy <namurphy@cfa.harvard.edu>
2017-04-11 19:02:00 -0400

    Trying to use Astropy constants functionality but it is not working

---
+++ plasmapy/constants/__init__.py
@@ -0,0 +1,13 @@
+"""
+Contains physical constants for use in PlasmaPy.  
+
+
+
+
+This package depends heavily on and adapts code from astropy.units and
+astropy.constants, so see licenses/Astropy_LICENSE.md.
+
+
+"""
+
+from . import si
