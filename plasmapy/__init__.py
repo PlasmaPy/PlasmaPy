b3cd4688b0504e5446af7dfb1970cee4d070a86e
Nick Murphy <namurphy@cfa.harvard.edu>
2017-04-12 14:25:47 -0400

    Fixing plasmapy.constants; suppressing Astropy.constants.Constant error messages

--- plasmapy/__init__.py
+++ plasmapy/__init__.py
@@ -10,4 +10,4 @@ from warnings import warn
 if sys.version_info[:2] < (3, 6):
     warn("PlasmaPy does not support Python 3.5 and below")
 
-from . import analytic_functions
+from . import analytic_functions, constants
--- plasmapy/constants/__init__.py
+++ plasmapy/constants/__init__.py
@@ -10,4 +10,6 @@ astropy.constants, so see licenses/Astropy_LICENSE.md.
 
 """
 
-from . import si
+from .si import h, hbar, k_B, c, G, g0, m_p, m_n, m_e, u, sigma_sb, \
+    e, eps0, N_A, R, Ryd, a0, muB, atm, mu0, sigma_T, au, pc, kpc, \
+    L_sun, M_sun, R_sun, M_earth, R_earth, pi
