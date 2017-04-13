072233cdce6d43e3e6408c8c932d939f72dfc348
Nick Murphy <namurphy@cfa.harvard.edu>
2017-04-13 16:47:41 -0400

    Changing constants subpackage to simply import Astropy values

--- plasmapy/constants/__init__.py
+++ plasmapy/constants/__init__.py
@@ -1,15 +1,9 @@
-"""
-Contains physical constants for use in PlasmaPy.  
+"""Physical and mathematical constants for use within PlasmaPy"""
 
+from numpy import pi
 
+from astropy.constants.si import h, hbar, k_B, c, G, g0, m_p, m_n, m_e, \
+    u, sigma_sb, e, eps0, N_A, R, Ryd, a0, muB, mu0, sigma_T, \
+    au, pc, kpc, L_sun, M_sun, R_sun, M_earth, R_earth
 
-
-This package depends heavily on and adapts code from astropy.units and
-astropy.constants, so see licenses/Astropy_LICENSE.md.
-
-
-"""
-
-from .si import h, hbar, k_B, c, G, g0, m_p, m_n, m_e, u, sigma_sb, \
-    e, eps0, N_A, R, Ryd, a0, muB, atm, mu0, sigma_T, au, pc, kpc, \
-    L_sun, M_sun, R_sun, M_earth, R_earth, pi
+from astropy.constants import atmosphere
