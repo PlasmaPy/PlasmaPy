42453685296259697046930a34552cb07fa0a699
Nick Murphy <namurphy@users.noreply.github.com>
2017-04-13 16:50:44 -0400

    Deleting si.py
    Several constants are now imported from astropy.constants in __init__.py, so si.py is unnecessary.

--- plasmapy/constants/si.py
+++
@@ -1,9 +0,0 @@
-"""
-Physical constants in SI units.
-"""
-
-from numpy import pi
-
-from astropy.constants import h, hbar, k_B, c, G, g0, m_p, m_n, m_e, u, \
-    sigma_sb, e, eps0, N_A, R, Ryd, a0, muB, atmosphere, mu0, sigma_T, \
-    au, pc, kpc, L_sun, M_sun, R_sun, M_earth, R_earth
