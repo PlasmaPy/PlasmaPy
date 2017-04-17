7fb7c704bd6e021c0b8052088d536559a4b26197
Nick Murphy <namurphy@cfa.harvard.edu>
2017-04-17 16:35:04 -0400

    Adding parentheses around long import for readability

--- plasmapy/constants/__init__.py
+++ plasmapy/constants/__init__.py
@@ -2,8 +2,9 @@
 
 from numpy import pi
 
-from astropy.constants.si import h, hbar, k_B, c, G, g0, m_p, m_n, m_e, \
-    u, sigma_sb, e, eps0, N_A, R, Ryd, a0, muB, mu0, sigma_T, \
-    au, pc, kpc, L_sun, M_sun, R_sun, M_earth, R_earth
+from astropy.constants.si import (h, hbar, k_B, c, G, g0, m_p, m_n, m_e, 
+                                  u, sigma_sb, e, eps0, N_A, R, Ryd, a0, 
+                                  muB, mu0, sigma_T, au, pc, kpc, L_sun, 
+                                  M_sun, R_sun, M_earth, R_earth)
 
 from astropy.constants import atmosphere
