edf673203a0ee76966c9e57030243519566a7379
Nick Murphy <namurphy@cfa.harvard.edu>
2017-05-01 22:00:19 -0400

    Import statements for atomic routines

--- plasmapy/constants/__init__.py
+++ plasmapy/constants/__init__.py
@@ -2,9 +2,20 @@
 
 from numpy import pi
 
-from astropy.constants.si import (h, hbar, k_B, c, G, g0, m_p, m_n, m_e, 
-                                  u, sigma_sb, e, eps0, N_A, R, Ryd, a0, 
-                                  muB, mu0, sigma_T, au, pc, kpc, L_sun, 
+from astropy.constants.si import (h, hbar, k_B, c, G, g0, m_p, m_n, m_e,
+                                  u, sigma_sb, e, eps0, N_A, R, Ryd, a0,
+                                  muB, mu0, sigma_T, au, pc, kpc, L_sun,
                                   M_sun, R_sun, M_earth, R_earth)
 
 from astropy.constants import atmosphere
+
+from .atomic import (element_symbol, isotope_symbol, atomic_number,
+                     mass_number, element_name, standard_atomic_weight,
+                     isotope_mass, ion_mass, nuclear_binding_energy, half_life,
+                     energy_from_nuclear_reaction, is_isotope_stable)
+
+from .elements import Elements
+
+from .isotopes import Isotopes
+
+from . import tests
