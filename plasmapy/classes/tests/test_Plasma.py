d76bf053827af66b11203224388935bb9310acb8
Drew Leonard <andy.j.leonard@gmail.com>
2017-05-24 20:36:07 +0100

    Changes to plasma test functions
    Change pressure to energy, fix grid inconsistancies and pep8ify

--- plasmapy/classes/tests/test_Plasma.py
+++ plasmapy/classes/tests/test_Plasma.py
@@ -4,23 +4,24 @@ import astropy.units as u
 
 from plasmapy.classes import plasma
 
+
 @pytest.mark.parametrize('grid_dimensions, expected_size', [
-    ((100, 1, 1), 100), # Test 1D setup
-    ((128, 128, 1), 16384), # 2D
-    ((64, 64, 64), 262144), # 3D
+    ((100, 1, 1), 100),  # Test 1D setup
+    ((128, 128, 1), 16384),  # 2D
+    ((64, 64, 64), 262144),  # 3D
 ])
 def test_Plasma_setup(grid_dimensions, expected_size):
     """Function to test basic setup of the Plasma object.
 
-    Tests that a Plasma object initiated with a particular specification behaves in the 
-    correct way.
+    Tests that a Plasma object initiated with a particular specification
+    behaves in the correct way.
 
     Parameters
     ----------
     grid_dimensions : tuple of ints
-        Grid size of the Plasma object to test. Must be a tuple of length 3, indicating 
-        length of the grid in x, y, and z directions respectively. Directions not needed 
-        should have a length of 1.
+        Grid size of the Plasma object to test. Must be a tuple of length 3,
+        indicating length of the grid in x, y, and z directions respectively.
+        Directions not needed should have a length of 1.
 
     expected_size : int
         Product of grid dimensions.
@@ -48,8 +49,8 @@ def test_Plasma_setup(grid_dimensions, expected_size):
     assert test_plasma.momentum.size == 3 * expected_size
     assert test_plasma.momentum.si.unit == u.kg / (u.m**2 * u.s)
 
-    assert test_plasma.pressure.size == expected_size
-    assert test_plasma.pressure.si.unit == u.Pa
+    assert test_plasma.energy.size == expected_size
+    assert test_plasma.energy.si.unit == u.J / u.m**3
 
     assert test_plasma.magnetic_field.size == 3 * expected_size
     assert test_plasma.magnetic_field.si.unit == u.T
@@ -59,17 +60,18 @@ def test_Plasma_setup(grid_dimensions, expected_size):
 def test_Plasma_derived_vars():
     """Function to test derived variables of the Plasma class.
 
-    Tests the shapes, units and values of variables derived from core variables.
+    Tests the shapes, units and values of variables derived from core
+    variables.
     The core variables are set with arbitrary uniform values.
     """
-    test_plasma = plasma.Plasma(domain_x=np.linspace(0, 1, 64)*u.m,
-                                domain_y=np.linspace(0, 1, 64)*u.m,
+    test_plasma = plasma.Plasma(domain_x=np.linspace(0, 1, 8)*u.m,
+                                domain_y=np.linspace(0, 1, 8)*u.m,
                                 domain_z=np.linspace(0, 1, 1)*u.m)
 
     # Set an arbitrary uniform values throughout the plasma
     test_plasma.density = 2.0 * np.ones((8, 8)) * u.kg / u.m**3
     test_plasma.momentum = 10.0 * np.ones((3, 8, 8)) * u.kg / (u.m**2 * u.s)
-    test_plasma.pressure = np.ones((8, 8)) * u.Pa
+    test_plasma.energy = np.ones((8, 8)) * u.J / u.m**3
     test_plasma.magnetic_field = 0.01 * np.ones((3, 8, 8)) * u.T
 
     # Test derived variable units and shapes
