2ab5cdccab5d14ce909edfd82eab585336522ee4
Colby Haggerty <colbych@udel.edu>
2017-05-31 20:00:25 -0400

    Made code pep8 acceptable
    Also I am adding this commit so I can understand how to update
    a pull request

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-#sys.path.insert(0, '/Users/haggerty/Programing/PlasmaPy/plasmapy/classes')
-
 
 # -- General configuration ------------------------------------------------
 
@@ -154,6 +152,3 @@ texinfo_documents = [
      author, 'PlasmaPy', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
