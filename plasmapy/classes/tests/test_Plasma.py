7c5b1e3c072fd63678fdf4b4b66c05e4a1bf5794
Drew Leonard <andy.j.leonard@gmail.com>
2017-06-01 12:44:42 +0100

    Tweak simulation test to make it make sense

--- plasmapy/classes/tests/test_Plasma.py
+++ plasmapy/classes/tests/test_Plasma.py
@@ -95,10 +95,11 @@ def test_Plasma_simulation():
     test_plasma = plasma.Plasma(domain_x=np.linspace(0, 1, 8)*u.m,
                                 domain_y=np.linspace(0, 1, 8)*u.m,
                                 domain_z=np.linspace(0, 1, 8)*u.m)
+    test_plasma.density = np.ones((8, 8, 8)) * u.kg / u.m**3
     test_plasma.simulate(max_its=10)
 
-    assert (test_plasma.density == np.zeros((8, 8)) * u.kg / u.m**3).all()
+    assert (test_plasma.density == np.ones((8, 8, 8)) * u.kg / u.m**3).all()
     assert (test_plasma.momentum
-            == np.zeros((8, 8))* u.kg / (u.m**2 * u.s)).all()
-    assert (test_plasma.energy == np.zeros((8, 8)) * u.J / u.m**3).all()
-    assert (test_plasma.magnetic_field == np.zeros((8, 8)) * u.T).all()
+            == np.zeros((8, 8, 8)) * u.kg / (u.m**2 * u.s)).all()
+    assert (test_plasma.energy == np.zeros((8, 8, 8)) * u.J / u.m**3).all()
+    assert (test_plasma.magnetic_field == np.zeros((8, 8, 8)) * u.T).all()
