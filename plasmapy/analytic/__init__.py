428c18e3172379038d494bf97b7a15e7fc48db7b
Nick Murphy <namurphy@cfa.harvard.edu>
2017-06-19 16:58:50 -0400

    Move plasma parameters/tests to physics subpackage

--- plasmapy/analytic/__init__.py
+++ plasmapy/analytic/__init__.py
@@ -1,13 +1,4 @@
 from .analytic import (plasma_dispersion_func,
                        plasma_dispersion_func_deriv)
 
-from .parameters import (Alfven_speed, ion_sound_speed,
-                         electron_thermal_speed, ion_thermal_speed,
-                         electron_gyrofrequency, ion_gyrofrequency,
-                         electron_gyroradius, ion_gyroradius,
-                         electron_plasma_frequency, ion_plasma_frequency,
-                         Debye_length, Debye_number,
-                         ion_inertial_length, electron_inertial_length,
-                         magnetic_pressure, magnetic_energy_density)
-
 from . import tests
