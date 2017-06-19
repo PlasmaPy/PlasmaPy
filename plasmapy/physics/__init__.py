44f232e179a2fe152ef6a7aa9e6e5cd52a4f201e
Nick Murphy <namurphy@cfa.harvard.edu>
2017-06-19 17:13:02 -0400

    Comment that physics is a tentative subpackage name

--- plasmapy/physics/__init__.py
+++ plasmapy/physics/__init__.py
@@ -1,3 +1,6 @@
+# 'physics' is a tentative name for this subpackage.  Another
+# possibility is 'plasma'.  The organization is to be decided by v0.1.
+
 from .parameters import (Alfven_speed,
                          ion_sound_speed,
                          electron_thermal_speed,
