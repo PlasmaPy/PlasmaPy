71ec642b5c1752cd137d51dde202a0ba4d6167f3
Drew Leonard <andy.j.leonard@gmail.com>
2017-06-21 10:01:11 +0100

    Add Riemann shock test to examples script

--- plasmapy/examples/basic_simulation.py
+++ plasmapy/examples/basic_simulation.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from plasmapy import Plasma
 
+savedir = Path("/home/drew/PlasmaPy/plasmapy/tests/test_output")
+if not savedir.exists():
+    savedir.mkdir()
+
 
 def gaussian(x, mean=0.0, std=1.0, amp=1.0):
     """Simple function to return a Gaussian distribution"""
@@ -16,6 +20,56 @@ def gaussian(x, mean=0.0, std=1.0, amp=1.0):
     return f
 
 
+def riemann_shock():
+    """
+    Run simulation for the Riemann shock tube problem.
+    """
+
+    print('riemann_shock()')
+
+    # Define simulation grid and coordinates
+    print('- Initiating Simulation... ', end='')
+    riemann = Plasma(domain_x=np.linspace(0, 1, 128)*u.m,
+                     domain_y=[0]*u.m, domain_z=[0]*u.m)
+    grid = riemann.domain_shape
+    x = riemann.grid[0]
+    print('Success')
+
+    # Define initial parameter values - only in perturbed component
+    print('- Setting initial conditions... ', end='')
+    density = np.zeros(grid) * u.kg / u.m**3
+    energy = np.zeros(grid) * u.J / u.m**3
+
+    discont = 0.5 * u.m
+    density[x < discont] = 1.0 * density.unit
+    density[x > discont] = 0.125 * density.unit
+    energy[x < discont] = 2.5 * energy.unit
+    energy[x > discont] = 0.25 * energy.unit
+
+    # Set simulation parameters to values defined above
+    riemann.density = density
+    riemann.energy = energy
+    print('Success')
+
+    for maxt in np.arange(0, 0.21, 0.02):
+        print('- Running Simulation... ', end='')
+        riemann.simulate(max_time=maxt*u.s)
+        print("Simulation complete")
+
+        fig, ax = plt.subplots(2, 2)
+        ax = ax.flatten()
+        ax[0].plot(x, riemann.density)
+        ax[0].set_ylim(0, 1.2)
+        ax[1].plot(x, riemann.velocity[0, :, ...])
+        ax[1].set_ylim(0, 1.1)
+        ax[2].plot(x, riemann.energy)
+        ax[3].plot(x, riemann.pressure)
+        ax[3].set_ylim(0, 1.2)
+        plt.savefig(
+            str(savedir/"riemann_shock_{:.4f}".format(maxt)).replace('.', '_'))
+        plt.close()
+
+
 def test_mhd_waves():
     """
     """
@@ -25,6 +79,7 @@ def test_mhd_waves():
     waves = Plasma(domain_x=np.linspace(-0.5, 0.5, 128)*u.m,
                    domain_y=np.linspace(-0.5, 0.5, 128)*u.m,
                    domain_z=np.linspace(0, 1, 1)*u.m)
+    # TODO Make these vaiable names make more sense
     grid = waves.domain_shape
     x, y, z = waves.grid
     r = np.sqrt(x**2 + y**2)
@@ -49,8 +104,9 @@ def test_mhd_waves():
         fig, ax = plt.subplots()
         plt.imshow(waves.density.value, cmap='plasma')
         plt.colorbar()
-        savedir = Path("/home/drew/PlasmaPy/plasmapy/tests/test_output")
-        if not savedir.exists():
-            savedir.mkdir()
         plt.savefig(str(savedir/"mhd_waves_{}".format(max_i)))
         plt.close()
+
+
+if __name__== '__main__':
+    riemann_shock()
