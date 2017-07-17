bf620196610207ba9a950ac726e63057eb56390c
Nick Murphy <namurphy@cfa.harvard.edu>
2017-07-17 18:52:54 -0400

    PEP8ify

--- plasmapy/analytic/tests/__init__.py
+++ plasmapy/analytic/tests/__init__.py
@@ -1,2 +1,2 @@
-from .test_dispersion import (test_plasma_dispersion_func, 
+from .test_dispersion import (test_plasma_dispersion_func,
                               test_plasma_dispersion_func_deriv)
