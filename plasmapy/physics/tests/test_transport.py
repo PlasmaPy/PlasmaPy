f1e199bc5ebf7ad16225646c4abaad5e8f9c0491
Nick Murphy <namurphy@cfa.harvard.edu>
2017-08-10 13:41:48 -0400

    Tests for Coulomb_logarithm from Spitzer (1962)

--- plasmapy/physics/tests/test_transport.py
+++ plasmapy/physics/tests/test_transport.py
@@ -10,4 +10,23 @@ from ..transport import (Coulomb_logarithm)
 
 
 def test_Coulomb_logarithm():
-    Coulomb_logarithm()
+
+    n_e = np.array([1e9, 1e9, 1e24])*u.cm**-3
+    T = np.array([1e2, 1e7, 1e8])*u.K
+    Lambda = np.array([5.97, 21.66, 6.69])
+    particles = ('e', 'p')
+
+    for i in range(3):
+        assert np.isclose(Coulomb_logarithm(n_e[i], T[i], particles),
+                          Lambda[i], atol=0.01)
+
+    assert np.isclose(Coulomb_logarithm(1e9*u.cm**-3, 1e2*u.K, ('e', 'p')),
+                      5.97, atol=0.01)
+
+    assert np.isclose(Coulomb_logarithm(1e9*u.cm**-3, 1e7*u.K, ('e', 'p')),
+                      21.6, atol=0.1)
+
+    assert np.isclose(Coulomb_logarithm(1e24*u.cm**-3, 1e8*u.K, ('e', 'p')),
+                      6.69, atol=0.01)
+
+    
