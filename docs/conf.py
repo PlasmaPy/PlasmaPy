9765b06953d58afa3e0066667c26327cbdf50af8
Nick Murphy <namurphy@cfa.harvard.edu>
2017-09-29 18:07:12 -0400

    Update author information

--- docs/conf.py
+++ docs/conf.py
@@ -134,7 +134,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'PlasmaPy.tex', 'PlasmaPy Documentation',
-     'Nicholas Murphy \\& PlasmaPy Community', 'manual'),
+     'PlasmaPy Community', 'manual'),
 ]
 
 
