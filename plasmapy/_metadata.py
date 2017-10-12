7ee6f4184fe66567b93ab8ae0a2a5f88a0f2e7d3
Nick Murphy <namurphy@cfa.harvard.edu>
2017-10-12 16:26:32 -0400

    Update versioning in _metadata.py
    Now using get_git_devstr in astropy_helpers to append the commit
    number to the version number for development versions.  For example,
    if we are at commit number 454 and working toward version 0.1, then
    plasmapy.__version__ will be '0.1.dev454'.

--- plasmapy/_metadata.py
+++ plasmapy/_metadata.py
@@ -5,9 +5,27 @@
 # Name
 name = 'plasmapy'
 
-# The version scheme is being discussed at:
-#    https://github.com/PlasmaPy/PlasmaPy/issues/25
-version = '0.1.dev2'
+import ah_bootstrap
+
+from astropy_helpers.git_helpers import get_git_devstr
+
+# PlasmaPy uses Semantic Versioning of the form: MAJOR.MINOR.PATCH
+#
+#  - The MAJOR version changes when there are backwards incompatible changes
+#  - The MINOR version changes when backwards compatible functionality is added
+#  - The PATCH version changes when the public API remains the same
+#
+# During initial development releases (with MAJOR = 0), backwards compatibility
+# does not need to be maintained when MINOR is incremented.
+#
+# While a new version is being developed, '.dev' followed by the commit number
+# will be appended to the version string.
+
+version = '0.1.dev'
+
+release = 'dev' in version
+if release:
+    version += get_git_devstr(False)
 
 # Long description / docstring
 description = """
