895009117e47bb09d16a8047c3d0aa9312ae627b
Dominik Stanczak <stanczakdominik@gmail.com>
2017-10-19 07:41:43 +0200

    Fix visibility of atomic.electric_charge

--- plasmapy/atomic/__init__.py
+++ plasmapy/atomic/__init__.py
@@ -3,7 +3,7 @@ from .atomic import (atomic_symbol, isotope_symbol, atomic_number,
                      mass_number, element_name, standard_atomic_weight,
                      isotope_mass, ion_mass, half_life, is_isotope_stable,
                      known_isotopes, common_isotopes, stable_isotopes,
-                     isotopic_abundance, charge_state)
+                     isotopic_abundance, charge_state, electric_charge)
 
 from .nuclear import (nuclear_binding_energy, nuclear_reaction_energy)
 
