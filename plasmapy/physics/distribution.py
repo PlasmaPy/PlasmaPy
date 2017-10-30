caac7e3395b48e687eb7d4f842a30ed561525e8a
Pawel Marek Kozlowski <pawel@pmkozlowski.com>
2017-10-29 23:21:51 -0600

    Added and modified 1D and 3D Maxwellian speed and velocity distribution functions. These function also include optional drift velocities/speeds. I've included tests for these new functions, but the 3D tests fails because scipy's QUADPACK based triple integral doesn't play nice with units.

--- plasmapy/physics/distribution.py
+++ plasmapy/physics/distribution.py
@@ -8,6 +8,7 @@ from astropy.units import (UnitConversionError, UnitsError, quantity_input,
 from ..constants import (m_p, m_e, c, mu0, k_B, e, eps0, pi, e)
 from ..atomic import (ion_mass, charge_state)
 from ..atomic.atomic import _is_electron as is_electron
+from .parameters import thermal_speed
 import numpy as np
 
 from ..utils import _check_quantity, check_relativistic, check_quantity
@@ -15,7 +16,8 @@ from ..utils import _check_quantity, check_relativistic, check_quantity
 
 @units.quantity_input
 def Maxwellian_1D(v: units.m/units.s,
-                  T: units.K, particle="e",
+                  T, 
+                  particle="e",
                   V_drift=0*units.m/units.s):
     r"""Returns the probability at the velocity `v` in m/s
     to find a particle `particle` in a plasma of temperature `T`
@@ -33,11 +35,14 @@ def Maxwellian_1D(v: units.m/units.s,
         Representation of the particle species(e.g., 'p' for protons, 'D+'
         for deuterium, or 'He-4 +1' for $He_4^{+1}$ : singly ionized helium-4),
         which defaults to electrons.
+        
+    V_drift: Quantity, optional
+        The drift velocity in units convertible to m/s
 
     Returns
     -------
     f : Quantity
-        probability in Velocity^-1, normized so that: :math:`\int_{-\infty}^{+\infty} f(v) dv = 1`
+        probability in Velocity^-1, normalized so that: :math:`\int_{-\infty}^{+\infty} f(v) dv = 1`
 
     Raises
     ------
@@ -58,7 +63,9 @@ def Maxwellian_1D(v: units.m/units.s,
     mass m, velocity v, a drift velocity V and with temperature T is:
 
     .. math::
-        f = \sqrt{\frac{m}{2 \pi k_B T}} e^{-\frac{m}{2 k_B T} (v-V)^2}
+    	f = \sqrt{\frac{m}{2 \pi k_B T}} e^{-\frac{m}{2 k_B T} (v-V)^2}
+    	f = (\pi * v_Th^2)^{-1/2} e^{-(v - v_{drift})^2 / v_Th^2}
+    	where v_Th = \sqrt(2 k_B T / m) is the thermal speed
 
     Examples
     --------
@@ -68,25 +75,286 @@ def Maxwellian_1D(v: units.m/units.s,
     >>> Maxwellian_1D(v=v, T= 30000*u.K, particle='e',V_drift=0*u.m/u.s)
     <Quantity 5.916329687405701e-07 s / m>
     """
+    # convert temperature to Kelvins
+    T = T.to(units.K, equivalencies=units.temperature_energy())
+    # get thermal velocity and thermal velocity squared
+    vTh = thermal_speed(T, particle=particle, method="most_probable")
+    vThSq = vTh ** 2
+    # Get particle velocity squared
+    vSq = (v - V_drift) ** 2
+    # calculating distribution function
+    try:
+        coeff = (vThSq * np.pi) ** (-1 / 2)
+        expTerm = np.exp(-vSq / vThSq)
+        distFunc = coeff * expTerm
+    except Exception:
+        raise ValueError("Unable to get distribution function.")
+    return distFunc.to(units.s/units.m)
 
-    # pass to Kelvin
+@units.quantity_input
+def Maxwellian_velocity_3D(vx: units.m/units.s,
+                           vy: units.m/units.s,
+                           vz: units.m/units.s,
+                           T, 
+                           particle="e",
+                           vx_drift=0*units.m/units.s,
+                           vy_drift=0*units.m/units.s,
+                           vz_drift=0*units.m/units.s):
+    r"""Return the probability of finding a particle with velocity components
+    `v_x`, `v_y`, and `v_z`in m/s in an equilibrium plasma of temperature 
+    `T` which follows the 3D Maxwellian distribution function. This 
+    function assumes Cartesian coordinates.
 
-    if T.unit.is_equivalent(units.K):
-        T = T.to(units.K, equivalencies=units.temperature_energy())
+    Parameters
+    ----------
+    vx: Quantity
+        The velocity in x-direction units convertible to m/s
+        
+    vy: Quantity
+        The velocity in y-direction units convertible to m/s
+        
+    vz: Quantity
+        The velocity in z-direction units convertible to m/s
 
-    # Get mass
+    T: Quantity
+        The temperature, preferably in Kelvin
 
-    if is_electron(particle):
-        m_s = m_e
+    particle: string, optional
+        Representation of the particle species(e.g., 'p' for protons, 'D+'
+        for deuterium, or 'He-4 +1' for $He_4^{+1}$ : singly ionized helium-4),
+        which defaults to electrons.
+        
+    vx_drift: Quantity
+        The drift velocity in x-direction units convertible to m/s
+        
+    vy_drift: Quantity
+        The drift velocity in y-direction units convertible to m/s
+        
+    vz_drift: Quantity
+        The drift velocity in z-direction units convertible to m/s
 
-    else:
-        try:
-            m_s = ion_mass(particle)
-        except Exception:
-            raise ValueError("Unable to find ion mass in Maxwellian_1D")
+    Returns
+    -------
+    f : Quantity
+        probability in Velocity^-1, normalized so that:
+        $\iiint_{0}^{\infty} f(\vec{v}) d\vec{v} = 1}
 
-    T = k_B*T
+    Raises
+    ------
+    TypeError
+        The parameter arguments are not Quantities and
+        cannot be converted into Quantities.
 
-    f = np.sqrt((m_s/(2*pi*T)))*np.exp(-m_s/(2*T)*(v-V_drift)**2)
+    UnitConversionError
+        If the parameters is not in appropriate units.
 
-    return f.to(units.s/units.m)
+    ValueError
+        If the temperature is negative, or the particle mass or charge state
+        cannot be found.
+
+    Notes
+    -----
+    In one dimension, the Maxwellian speed distribution function describing
+    the distribution of particles with speed v in a plasma with temperature T
+    is given by:
+
+    .. math::
+    f = (\pi * v_Th^2)^{-3/2} \exp(-(\vec{v} - \vec{v_{drift}})^2 / v_Th^2)
+    where v_Th = \sqrt(2 k_B T / m) is the thermal speed
+
+    See also
+    --------
+    Maxwellian_1D
+
+    Example
+    -------
+    
+    
+    """
+    # convert temperature to Kelvins
+    T = T.to(units.K, equivalencies=units.temperature_energy())
+    # get thermal velocity and thermal velocity squared
+    vTh = thermal_speed(T, particle=particle, method="most_probable")
+    # accounting for thermal velocity in 3D
+    vTh3D = np.sqrt(3) * vTh
+    vThSq = vTh3D ** 2
+    # Get particle velocity squared
+    vSq = ((vx-vx_drift) ** 2 + (vy-vy_drift) ** 2 + (vz-vz_drift) ** 2)
+    # calculating distribution function
+    try:
+        coeff = (vThSq * np.pi) ** (-3 / 2)
+        expTerm = np.exp(-vSq / vThSq)
+        distFunc = coeff * expTerm
+    except Exception:
+        raise ValueError("Unable to get distribution function.")
+    return distFunc.to((units.s/units.m) ** 3)
+
+@units.quantity_input
+def Maxwellian_speed_1D(v: units.m/units.s,
+                        T, 
+                        particle="e",
+                        v_drift=0*units.m/units.s):
+    r"""Return the probability of finding a particle with speed `v` in m/s
+     in an equilibrium plasma of temperature `T` which follows the 
+     Maxwellian distribution function.
+
+    Parameters
+    ----------
+    v: Quantity
+        The speed in units convertible to m/s
+
+    T: Quantity
+        The temperature, preferably in Kelvin
+
+    particle: string, optional
+        Representation of the particle species(e.g., 'p' for protons, 'D+'
+        for deuterium, or 'He-4 +1' for $He_4^{+1}$ : singly ionized helium-4),
+        which defaults to electrons.
+    
+    v_drift: Quantity
+        The drift speed in units convertible to m/s
+
+    Returns
+    -------
+    f : Quantity
+        probability in speed^-1, normalized so that:
+        $\int_{0}^{\infty} f(v) dv = 1}
+
+    Raises
+    ------
+    TypeError
+        The parameter arguments are not Quantities and
+        cannot be converted into Quantities.
+
+    UnitConversionError
+        If the parameters is not in appropriate units.
+
+    ValueError
+        If the temperature is negative, or the particle mass or charge state
+        cannot be found.
+
+    Notes
+    -----
+    In one dimension, the Maxwellian speed distribution function describing
+    the distribution of particles with speed v in a plasma with temperature T
+    is given by:
+
+    .. math::
+    f(v) = 4 \pi v^2 (\pi * v_Th^2)^{-3/2} \exp(-(v - v_{drift})^2 / v_Th^2)
+    where v_Th = \sqrt(2 k_B T / m) is the thermal speed
+
+
+    Example
+    -------
+    
+    """
+    # convert temperature to Kelvins
+    T = T.to(units.K, equivalencies=units.temperature_energy())
+    # get thermal velocity and thermal velocity squared
+    vTh = thermal_speed(T, particle=particle, method="most_probable")
+    vThSq = vTh ** 2
+    # square of particle velocity [m^2/s^2]
+    vSq = (v - v_drift) ** 2
+    # calculating distribution function
+    try:
+        coeff1 = (np.pi * vThSq) ** (-3 / 2)
+        coeff2 = 4 * np.pi * vSq
+        expTerm = np.exp(-vSq / vThSq)
+        distFunc = coeff1 * coeff2 * expTerm
+    except Exception:
+        raise ValueError("Unable to get distribution function.")
+    return distFunc.to(units.s/units.m)
+
+@units.quantity_input
+def Maxwellian_speed_3D(vx: units.m/units.s,
+                        vy: units.m/units.s,
+                        vz: units.m/units.s,
+                        T, 
+                        particle="e",
+                        vx_drift=0*units.m/units.s,
+                        vy_drift=0*units.m/units.s,
+                        vz_drift=0*units.m/units.s):
+    r"""Return the probability of finding a particle with speed components
+    `v_x`, `v_y`, and `v_z`in m/s in an equilibrium plasma of temperature 
+    `T` which follows the 3D Maxwellian distribution function. This 
+    function assumes Cartesian coordinates.
+
+    Parameters
+    ----------
+    vx: Quantity
+        The speed in x-direction units convertible to m/s
+        
+    vy: Quantity
+        The speed in y-direction units convertible to m/s
+        
+    vz: Quantity
+        The speed in z-direction units convertible to m/s
+
+    T: Quantity
+        The temperature, preferably in Kelvin
+
+    particle: string, optional
+        Representation of the particle species(e.g., 'p' for protons, 'D+'
+        for deuterium, or 'He-4 +1' for $He_4^{+1}$ : singly ionized helium-4),
+        which defaults to electrons.
+        
+    vx_drift: Quantity
+        The drift speed in x-direction units convertible to m/s
+        
+    vy_drift: Quantity
+        The drift speed in y-direction units convertible to m/s
+        
+    vz_drift: Quantity
+        The drift speed in z-direction units convertible to m/s
+        
+    Returns
+    -------
+    f : Quantity
+        probability in speed^-1, normalized so that:
+        $\iiint_{0}^{\infty} f(\vec{v}) d\vec{v} = 1}
+
+    Raises
+    ------
+    TypeError
+        The parameter arguments are not Quantities and
+        cannot be converted into Quantities.
+
+    UnitConversionError
+        If the parameters is not in appropriate units.
+
+    ValueError
+        If the temperature is negative, or the particle mass or charge state
+        cannot be found.
+
+    Notes
+    -----
+    In one dimension, the Maxwellian speed distribution function describing
+    the distribution of particles with speed v in a plasma with temperature T
+    is given by:
+
+    .. math::
+    f = 4 \pi \vec{v}^2 (\pi * v_Th^2)^{-3/2} \exp(-(\vec{v} - \vec{v_{drift}})^2 / v_Th^2)
+    where v_Th = \sqrt(2 k_B T / m) is the thermal speed
+
+    See also
+    --------
+    Maxwellian_speed_1D
+
+    Example
+    -------
+    
+    """
+    # convert temperature to Kelvins
+    T = T.to(units.K, equivalencies=units.temperature_energy())
+    # Get particle speed and drift speed
+    v = np.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
+    v_drift = np.sqrt(vx_drift ** 2 + vy_drift ** 2 + vz_drift ** 2)
+    # calculating distribution function
+    try:
+        distFunc = Maxwellian_speed_1D(v=v,
+                                       T=T, 
+                                       particle=particle,
+                                       v_drift=v_drift)
+    except Exception:
+        raise ValueError("Unable to get distribution function.")
+    return distFunc.to(units.s/units.m)
--- plasmapy/physics/tests/test_distribution.py
+++ plasmapy/physics/tests/test_distribution.py
@@ -3,11 +3,16 @@
 import numpy as np
 import pytest
 from astropy import units as u
+import scipy.integrate as spint
 
 
 from ...constants import (m_p, m_e, c, mu0, k_B, e, eps0, pi, e)
 from ...atomic import (ion_mass, charge_state)
-from ..distribution import (Maxwellian_1D)
+from ..distribution import (Maxwellian_1D,
+                            Maxwellian_speed_1D,
+                            Maxwellian_velocity_3D,
+                            Maxwellian_speed_3D)
+from ..parameters import thermal_speed
 
 T_e = 30000*u.K
 V_drift = 1000000*u.m/u.s
@@ -42,3 +47,84 @@ def test_Maxwellian_1D():
 
     with pytest.raises(ValueError):
         Maxwellian_1D(1*u.m/u.s, T=1*u.K, particle='XXX')
+
+def test_Maxwellian_speed_1D():
+    r"""test the 1D Maxwellian speed distribution function"""
+    T = 1.0 * u.eV
+    particle = 'H'
+    # get thermal velocity and thermal velocity squared
+    vTh = thermal_speed(T, particle=particle, method="most_probable")
+    # setting up integration from 0 to 10*vTh
+    xData1D = np.arange(0, 10.01, 0.01) * vTh
+    yData1D = Maxwellian_speed_1D(v=xData1D,T=T,particle=particle)
+    # integrating, this should be close to 1
+    integ = spint.trapz(y=yData1D, x=xData1D)
+    print("1D integral {0}".format(integ))
+    exceptStr = "Integral of distribution function should be 1."
+    assert np.isclose(integ.value, 1), exceptStr
+    
+    
+def test_Maxwellian_velocity_3D():
+    r"""test the 3D Maxwellian velocity distribution function"""
+    T = 1.0 * u.eV
+    particle = 'H'
+    # get thermal velocity and thermal velocity squared
+    vTh = thermal_speed(T, particle=particle, method="most_probable")
+    # defining a closure with partially applied arguments
+    def velDist3D(vx, vy, vz):
+        velDist = Maxwellian_velocity_3D(vx=vx,
+                                         vy=vy,
+                                         vz=vz,
+                                         T=T, 
+                                         particle=particle)
+        return velDist
+    # setting up integration from -10*vTh to 10*vTh, which is close to Inf
+    infApprox = 10 * vTh
+    # if I recall correctly, astropy.units may have trouble with
+    # accounting for units in integrals, so this step currently fails when
+    # tplquad tries to convert a quantity with units to a scalar.
+    # integrating, this should be close to 1
+    integ = spint.tplquad(velDist3D,
+                          -infApprox,
+                          infApprox,
+                          lambda z: -infApprox,
+                          lambda z: infApprox,
+                          lambda z, y: -infApprox,
+                          lambda z, y: infApprox,
+                          epsabs=1.49e-08*u.m/u.m, 
+                          epsrel=1.49e-08*u.m/u.m)
+    exceptStr = "Integral of distribution function should be 1."
+    assert np.isclose(integ.value, 1), exceptStr
+    
+def test_Maxwellian_speed_3D():
+    r"""test the 3D Maxwellian speed distribution function"""
+    T = 1.0 * u.eV
+    particle = 'H'
+    # get thermal velocity and thermal velocity squared
+    vTh = thermal_speed(T, particle=particle, method="most_probable")
+    # defining a closure with partially applied arguments so that we
+    # can do a triple integral over vx, vy, vz
+    def speedDist3D(vx, vy, vz):
+        speedDist = Maxwellian_speed_3D(vx=vx,
+                                        vy=vy,
+                                        vz=vz,
+                                        T=T, 
+                                        particle=particle)
+        return speedDist
+    # setting up integration from 0 to 10*vTh, which is close to Inf
+    infApprox = 10 * vTh
+    # if I recall correctly, astropy.units may have trouble with
+    # accounting for units in integrals, so this step currently fails when
+    # tplquad tries to convert a quantity with units to a scalar.
+    # integrating, this should be close to 1
+    integ = spint.tplquad(speedDist3D,
+                          0*u.m/u.s,
+                          infApprox,
+                          lambda z: 0*u.m/u.s,
+                          lambda z: infApprox,
+                          lambda z, y: 0*u.m/u.s,
+                          lambda z, y: infApprox,
+                          epsabs=1.49e-08*u.m/u.m, 
+                          epsrel=1.49e-08*u.m/u.m)
+    exceptStr = "Integral of distribution function should be 1."
+    assert np.isclose(integ.value, 1), exceptStr
