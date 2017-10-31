Fixed 3D distribution function test failures due to tplquad not playing nicely with astropy.units. This was done by applying units to inputs and stripping units of output from partially applied distribution function within the test.

plasmapy/physics/distribution.py
@@ -1,6 +1,6 @@
 """Functions to deal with distribution : generate, fit, calculate"""
 
-from astropy import units
+from astropy import units as u
 
 from astropy.units import (UnitConversionError, UnitsError, quantity_input,
                            Quantity)
@@ -14,14 +14,14 @@ import numpy as np
 from ..utils import _check_quantity, check_relativistic, check_quantity
 
 
-@units.quantity_input
-def Maxwellian_1D(v: units.m/units.s,
+@u.quantity_input
+def Maxwellian_1D(v: u.m/u.s,
                   T, 
                   particle="e",
-                  V_drift=0*units.m/units.s):
+                  V_drift=0*u.m/u.s):
     r"""Returns the probability at the velocity `v` in m/s
-    to find a particle `particle` in a plasma of temperature `T`
-    following the Maxwellian distribution function.
+     to find a particle `particle` in a plasma of temperature `T`
+     following the Maxwellian distribution function.
 
     Parameters
     ----------
@@ -76,7 +76,7 @@ def Maxwellian_1D(v: units.m/units.s,
     <Quantity 5.916329687405703e-07 s / m>
     """
     # convert temperature to Kelvins
-    T = T.to(units.K, equivalencies=units.temperature_energy())
+    T = T.to(u.K, equivalencies=u.temperature_energy())
     # get thermal velocity and thermal velocity squared
     vTh = thermal_speed(T, particle=particle, method="most_probable")
     vThSq = vTh ** 2
@@ -89,17 +89,17 @@ def Maxwellian_1D(v: units.m/units.s,
         distFunc = coeff * expTerm
     except Exception:
         raise ValueError("Unable to get distribution function.")
-    return distFunc.to(units.s/units.m)
+    return distFunc.to(u.s/u.m)
 
-@units.quantity_input
-def Maxwellian_velocity_3D(vx: units.m/units.s,
-                           vy: units.m/units.s,
-                           vz: units.m/units.s,
+@u.quantity_input
+def Maxwellian_velocity_3D(vx: u.m/u.s,
+                           vy: u.m/u.s,
+                           vz: u.m/u.s,
                            T, 
                            particle="e",
-                           Vx_drift=0*units.m/units.s,
-                           Vy_drift=0*units.m/units.s,
-                           Vz_drift=0*units.m/units.s):
+                           Vx_drift=0*u.m/u.s,
+                           Vy_drift=0*u.m/u.s,
+                           Vz_drift=0*u.m/u.s):
     r"""Return the probability of finding a particle with velocity components
     `v_x`, `v_y`, and `v_z`in m/s in an equilibrium plasma of temperature 
     `T` which follows the 3D Maxwellian distribution function. This 
@@ -184,7 +184,7 @@ def Maxwellian_velocity_3D(vx: units.m/units.s,
     
     """
     # convert temperature to Kelvins
-    T = T.to(units.K, equivalencies=units.temperature_energy())
+    T = T.to(u.K, equivalencies=u.temperature_energy())
     # get thermal velocity and thermal velocity squared
     vTh = thermal_speed(T, particle=particle, method="most_probable")
     # accounting for thermal velocity in 3D
@@ -199,13 +199,13 @@ def Maxwellian_velocity_3D(vx: units.m/units.s,
         distFunc = coeff * expTerm
     except Exception:
         raise ValueError("Unable to get distribution function.")
-    return distFunc.to((units.s/units.m) ** 3)
+    return distFunc.to((u.s/u.m) ** 3)
 
-@units.quantity_input
-def Maxwellian_speed_1D(v: units.m/units.s,
+@u.quantity_input
+def Maxwellian_speed_1D(v: u.m/u.s,
                         T, 
                         particle="e",
-                        V_drift=0*units.m/units.s):
+                        V_drift=0*u.m/u.s):
     r"""Return the probability of finding a particle with speed `v` in m/s
      in an equilibrium plasma of temperature `T` which follows the 
      Maxwellian distribution function.
@@ -266,7 +266,7 @@ def Maxwellian_speed_1D(v: units.m/units.s,
     
     """
     # convert temperature to Kelvins
-    T = T.to(units.K, equivalencies=units.temperature_energy())
+    T = T.to(u.K, equivalencies=u.temperature_energy())
     # get thermal velocity and thermal velocity squared
     vTh = thermal_speed(T, particle=particle, method="most_probable")
     vThSq = vTh ** 2
@@ -280,17 +280,17 @@ def Maxwellian_speed_1D(v: units.m/units.s,
         distFunc = coeff1 * coeff2 * expTerm
     except Exception:
         raise ValueError("Unable to get distribution function.")
-    return distFunc.to(units.s/units.m)
+    return distFunc.to(u.s/u.m)
 
-@units.quantity_input
-def Maxwellian_speed_3D(vx: units.m/units.s,
-                        vy: units.m/units.s,
-                        vz: units.m/units.s,
+@u.quantity_input
+def Maxwellian_speed_3D(vx: u.m/u.s,
+                        vy: u.m/u.s,
+                        vz: u.m/u.s,
                         T, 
                         particle="e",
-                        Vx_drift=0*units.m/units.s,
-                        Vy_drift=0*units.m/units.s,
-                        Vz_drift=0*units.m/units.s):
+                        Vx_drift=0*u.m/u.s,
+                        Vy_drift=0*u.m/u.s,
+                        Vz_drift=0*u.m/u.s):
     r"""Return the probability of finding a particle with speed components
     `v_x`, `v_y`, and `v_z`in m/s in an equilibrium plasma of temperature 
     `T` which follows the 3D Maxwellian distribution function. This 
@@ -374,7 +374,7 @@ def Maxwellian_speed_3D(vx: units.m/units.s,
     
     """
     # convert temperature to Kelvins
-    T = T.to(units.K, equivalencies=units.temperature_energy())
+    T = T.to(u.K, equivalencies=u.temperature_energy())
     # Get particle speed and drift speed
     v = np.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
     V_drift = np.sqrt(Vx_drift ** 2 + Vy_drift ** 2 + Vz_drift ** 2)
@@ -386,4 +386,4 @@ def Maxwellian_speed_3D(vx: units.m/units.s,
                                        V_drift=V_drift)
     except Exception:
         raise ValueError("Unable to get distribution function.")
-    return distFunc.to(units.s/units.m)
+    return distFunc.to(u.s/u.m)

plasmapy/physics/tests/test_distribution.py
@@ -71,14 +71,17 @@ def test_Maxwellian_velocity_3D():
     vTh = thermal_speed(T, particle=particle, method="most_probable")
     # defining a closure with partially applied arguments
     def velDist3D(vx, vy, vz):
-        velDist = Maxwellian_velocity_3D(vx=vx,
-                                         vy=vy,
-                                         vz=vz,
+        # tplquad passes values without units, so we add units so that
+        # Maxwellian_velocity_3D can handle them
+        velDist = Maxwellian_velocity_3D(vx=vx*u.m/u.s,
+                                         vy=vy*u.m/u.s,
+                                         vz=vz*u.m/u.s,
                                          T=T, 
                                          particle=particle)
-        return velDist
+        # return value stripped of units so tplquad can handle it
+        return velDist.si.value
     # setting up integration from -10*vTh to 10*vTh, which is close to Inf
-    infApprox = 10 * vTh
+    infApprox = (10 * vTh).si.value
     # if I recall correctly, astropy.units may have trouble with
     # accounting for units in integrals, so this step currently fails when
     # tplquad tries to convert a quantity with units to a scalar.
@@ -90,10 +93,16 @@ def test_Maxwellian_velocity_3D():
                           lambda z: infApprox,
                           lambda z, y: -infApprox,
                           lambda z, y: infApprox,
-                          epsabs=1.49e-08*u.m/u.m, 
-                          epsrel=1.49e-08*u.m/u.m)
-    exceptStr = "Integral of distribution function should be 1."
-    assert np.isclose(integ.value, 1), exceptStr
+                          epsabs=0.0, 
+                          epsrel=5.0e-1)
+    # value returned from tplquad is (integral, error), we just need the 1st
+    integVal = integ[0]
+    exceptStr = ("Integral of distribution function should be 1 "
+                 f"and not {integVal}.")
+    assert np.isclose(integVal,
+                      1,
+                      rtol=1e-3,
+                      atol=0.0), exceptStr
     
 def test_Maxwellian_speed_3D():
     r"""test the 3D Maxwellian speed distribution function"""
@@ -104,26 +113,32 @@ def test_Maxwellian_speed_3D():
     # defining a closure with partially applied arguments so that we
     # can do a triple integral over vx, vy, vz
     def speedDist3D(vx, vy, vz):
-        speedDist = Maxwellian_speed_3D(vx=vx,
-                                        vy=vy,
-                                        vz=vz,
+        # tplquad passes values without units, so we add units so that
+        # Maxwellian_speed_3D can handle them
+        speedDist = Maxwellian_speed_3D(vx=vx*u.m/u.s,
+                                        vy=vy*u.m/u.s,
+                                        vz=vz*u.m/u.s,
                                         T=T, 
                                         particle=particle)
-        return speedDist
+        # return value stripped of units so tplquad can handle it
+        return speedDist.si.value
     # setting up integration from 0 to 10*vTh, which is close to Inf
-    infApprox = 10 * vTh
-    # if I recall correctly, astropy.units may have trouble with
-    # accounting for units in integrals, so this step currently fails when
-    # tplquad tries to convert a quantity with units to a scalar.
-    # integrating, this should be close to 1
+    infApprox = (10 * vTh).si.value
+    # integral should be close to 1
     integ = spint.tplquad(speedDist3D,
-                          0*u.m/u.s,
+                          0,
                           infApprox,
-                          lambda z: 0*u.m/u.s,
+                          lambda z: 0,
                           lambda z: infApprox,
-                          lambda z, y: 0*u.m/u.s,
+                          lambda z, y: 0,
                           lambda z, y: infApprox,
-                          epsabs=1.49e-08*u.m/u.m, 
-                          epsrel=1.49e-08*u.m/u.m)
-    exceptStr = "Integral of distribution function should be 1."
-    assert np.isclose(integ.value, 1), exceptStr
+                          epsabs=5.0e-1, 
+                          epsrel=5.0e-1)
+    # value returned from tplquad is (integral, error), we just need the 1st
+    integVal = integ[0]
+    exceptStr = ("Integral of distribution function should be 1 "
+                 f"and not {integVal}")
+    assert np.isclose(integVal,
+                      1,
+                      rtol=-3,
+                      atol=0.0), exceptStr