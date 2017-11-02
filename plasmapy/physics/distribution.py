44e885eaabbe4d3d0380cde250639379030d28d9
Pawel Marek Kozlowski <pawel@pmkozlowski.com>
2017-11-02 05:01:08 -0600

    Fixed problem with drift velocities getting multiplied by units when 0*u.m/m.s was passed as an argument. This made the units squared, which then caused failed unit conversions. Also fixed docstring values.

--- plasmapy/physics/distribution.py
+++ plasmapy/physics/distribution.py
@@ -1,5 +1,5 @@
 """Functions to deal with distribution : generate, fit, calculate"""
-
+import astropy as astropy
 from astropy import units as u
 
 from astropy.units import (UnitConversionError, UnitsError, quantity_input,
@@ -90,116 +90,6 @@ def Maxwellian_1D(v: u.m/u.s,
     except Exception:
         raise ValueError("Unable to get distribution function.")
     return distFunc.to(u.s/u.m)
-
-#@u.quantity_input
-#def Maxwellian_velocity_3D(vx: u.m/u.s,
-#                           vy: u.m/u.s,
-#                           vz: u.m/u.s,
-#                           T, 
-#                           particle="e",
-#                           Vx_drift=0*u.m/u.s,
-#                           Vy_drift=0*u.m/u.s,
-#                           Vz_drift=0*u.m/u.s):
-#    r"""Return the probability of finding a particle with velocity components
-#    `v_x`, `v_y`, and `v_z`in m/s in an equilibrium plasma of temperature 
-#    `T` which follows the 3D Maxwellian distribution function. This 
-#    function assumes Cartesian coordinates.
-#
-#    Parameters
-#    ----------
-#    vx: Quantity
-#        The velocity in x-direction units convertible to m/s
-#        
-#    vy: Quantity
-#        The velocity in y-direction units convertible to m/s
-#        
-#    vz: Quantity
-#        The velocity in z-direction units convertible to m/s
-#
-#    T: Quantity
-#        The temperature, preferably in Kelvin
-#
-#    particle: string, optional
-#        Representation of the particle species(e.g., 'p' for protons, 'D+'
-#        for deuterium, or 'He-4 +1' for $He_4^{+1}$ : singly ionized helium-4),
-#        which defaults to electrons.
-#        
-#    Vx_drift: Quantity
-#        The drift velocity in x-direction units convertible to m/s
-#        
-#    Vy_drift: Quantity
-#        The drift velocity in y-direction units convertible to m/s
-#        
-#    Vz_drift: Quantity
-#        The drift velocity in z-direction units convertible to m/s
-#
-#    Returns
-#    -------
-#    f : Quantity
-#        probability in Velocity^-1, normalized so that:
-#        $\iiint_{0}^{\infty} f(\vec{v}) d\vec{v} = 1}
-#
-#    Raises
-#    ------
-#    TypeError
-#        The parameter arguments are not Quantities and
-#        cannot be converted into Quantities.
-#
-#    UnitConversionError
-#        If the parameters is not in appropriate units.
-#
-#    ValueError
-#        If the temperature is negative, or the particle mass or charge state
-#        cannot be found.
-#
-#    Notes
-#    -----
-#    In one dimension, the Maxwellian speed distribution function describing
-#    the distribution of particles with speed v in a plasma with temperature T
-#    is given by:
-#
-#    .. math::
-#    f = (\pi * v_Th^2)^{-3/2} \exp(-(\vec{v} - \vec{V_{drift}})^2 / v_Th^2)
-#    where v_Th = \sqrt(2 k_B T / m) is the thermal speed
-#
-#    See also
-#    --------
-#    Maxwellian_1D
-#
-#    Example
-#    -------
-#    >>> from plasmapy.physics import Maxwellian_velocity_3D
-#    >>> from astropy import units as u
-#    >>> v=1*u.m/u.s
-#    >>> Maxwellian_velocity_3D(vx=v,
-#    ... vy=v,
-#    ... vz=v,
-#    ... T=30000*u.K,
-#    ... particle='e',
-#    ... Vx_drift=0*u.m/u.s,
-#    ... Vy_drift=0*u.m/u.s,
-#    ... Vz_drift=0*u.m/u.s)
-#    <Quantity 3.985430307328086e-20 s3 / m3>
-#    
-#    
-#    """
-#    # convert temperature to Kelvins
-#    T = T.to(u.K, equivalencies=u.temperature_energy())
-#    # get thermal velocity and thermal velocity squared
-#    vTh = thermal_speed(T, particle=particle, method="most_probable")
-#    # accounting for thermal velocity in 3D
-#    vTh3D = np.sqrt(3) * vTh
-#    vThSq = vTh3D ** 2
-#    # Get square of relative particle velocity
-#    vSq = ((vx-Vx_drift) ** 2 + (vy-Vy_drift) ** 2 + (vz-Vz_drift) ** 2)
-#    # calculating distribution function
-#    try:
-#        coeff = (vThSq * np.pi) ** (-3 / 2)
-#        expTerm = np.exp(-vSq / vThSq)
-#        distFunc = coeff * expTerm
-#    except Exception:
-#        raise ValueError("Unable to get distribution function.")
-#    return distFunc.to((u.s/u.m) ** 3)
 
 def Maxwellian_velocity_3D(vx,
                            vy,
@@ -301,7 +191,7 @@ def Maxwellian_velocity_3D(vx,
     ... Vx_drift=0*u.m/u.s,
     ... Vy_drift=0*u.m/u.s,
     ... Vz_drift=0*u.m/u.s)
-    <Quantity 3.985430307328086e-20 s3 / m3>
+    <Quantity 3.985430307328085e-20 s3 / m3>
     
     
     """
@@ -314,11 +204,14 @@ def Maxwellian_velocity_3D(vx,
         # catching case where drift velocities have default values, they
         # need to be assigned units
         if Vx_drift == 0:
-            Vx_drift = Vx_drift * u.m/u.s
+            if not isinstance(Vx_drift, astropy.units.quantity.Quantity):
+                Vx_drift = Vx_drift * u.m/u.s
         if Vy_drift == 0:
-            Vy_drift = Vy_drift * u.m/u.s
+            if not isinstance(Vy_drift, astropy.units.quantity.Quantity):
+                Vy_drift = Vy_drift * u.m/u.s
         if Vz_drift == 0:
-            Vz_drift = Vz_drift * u.m/u.s
+            if not isinstance(Vz_drift, astropy.units.quantity.Quantity):
+                Vz_drift = Vz_drift * u.m/u.s
         # checking units of drift velocities
         Vx_drift = Vx_drift.to(u.m/u.s)
         Vy_drift = Vy_drift.to(u.m/u.s)
@@ -433,7 +326,8 @@ def Maxwellian_speed_1D(v,
         # catching case where drift velocity has default value, and
         # needs to be assigned units
         if V_drift == 0:
-            V_drift = V_drift * u.m/u.s
+            if not isinstance(V_drift, astropy.units.quantity.Quantity):
+                V_drift = V_drift * u.m/u.s
         # checking drift velocity units
         V_drift = V_drift.to(u.m/u.s)
         # convert temperature to Kelvins
@@ -565,7 +459,7 @@ def Maxwellian_speed_3D(vx,
     ... Vx_drift=0*u.m/u.s,
     ... Vy_drift=0*u.m/u.s,
     ... Vz_drift=0*u.m/u.s)
-    <Quantity 7.80707263422481e-18 s / m>
+    <Quantity 1.7623854373113508e-53 s3 / m3>
     
     """
     if units == "units":
@@ -577,11 +471,14 @@ def Maxwellian_speed_3D(vx,
         # catching case where drift velocities have default values, they
         # need to be assigned units
         if Vx_drift == 0:
-            Vx_drift = Vx_drift * u.m/u.s
+            if not isinstance(Vx_drift, astropy.units.quantity.Quantity):
+                Vx_drift = Vx_drift * u.m/u.s
         if Vy_drift == 0:
-            Vy_drift = Vy_drift * u.m/u.s
+            if not isinstance(Vy_drift, astropy.units.quantity.Quantity):
+                Vy_drift = Vy_drift * u.m/u.s
         if Vz_drift == 0:
-            Vz_drift = Vz_drift * u.m/u.s
+            if not isinstance(Vz_drift, astropy.units.quantity.Quantity):
+                Vz_drift = Vz_drift * u.m/u.s
         Vx_drift = Vx_drift.to(u.m/u.s)
         Vy_drift = Vy_drift.to(u.m/u.s)
         Vz_drift = Vz_drift.to(u.m/u.s)
