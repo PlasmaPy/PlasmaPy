2add5eb17a13970ce8ffdf9e9ac39bd6c4bb41a5
Pawel Marek Kozlowski <pawel@pmkozlowski.com>
2017-11-03 04:16:12 -0600

    Moved parameters for initializing tests into setup_method() section for each Test class.

--- plasmapy/physics/tests/test_distribution.py
+++ plasmapy/physics/tests/test_distribution.py
@@ -14,59 +14,60 @@ from ..distribution import (Maxwellian_1D,
                             Maxwellian_speed_3D)
 from ..parameters import thermal_speed
 
-T_e = 30000*u.K
-V_drift = 1000000*u.m/u.s
 
-start = -5000
-stop = - start
-dv = 10000 * u.m/u.s
-
-v_vect = np.arange(start, stop, dtype='float64') * dv
 
 
 # test class for Maxwellian_1D (velocity) function:
 class Test_Maxwellian_1D(object):
+    def setup_method(self):
+        """initializing parameters for tests """
+        self.T_e = 30000*u.K
+        self.V_drift = 1000000*u.m/u.s
+        self.start = -5000
+        self.stop = - self.start
+        self.dv = 10000 * u.m/u.s
+        self.v_vect = np.arange(self.start, self.stop, dtype='float64') * self.dv
     def test_max_noDrift(self):
         """
         Checks maximum value of distribution function is in expected place,
         when there is no drift applied.
         """
-        max_index = Maxwellian_1D(v_vect, 
-                                  T=T_e, 
+        max_index = Maxwellian_1D(self.v_vect, 
+                                  T=self.T_e, 
                                   particle='e', 
                                   V_drift=0*u.m/u.s
                                   ).argmax()
-        assert np.isclose(v_vect[max_index].value, 0.0)
+        assert np.isclose(self.v_vect[max_index].value, 0.0)
     def test_max_drift(self):
         """
         Checks maximum value of distribution function is in expected place,
         when there is drift applied.
         """
-        max_index = Maxwellian_1D(v_vect,
-                                  T=T_e,
+        max_index = Maxwellian_1D(self.v_vect,
+                                  T=self.T_e,
                                   particle='e',
-                                  V_drift=V_drift
+                                  V_drift=self.V_drift
                                   ).argmax()
-        assert np.isclose(v_vect[max_index].value, V_drift.value)
+        assert np.isclose(self.v_vect[max_index].value, self.V_drift.value)
     def test_norm(self):
         """
         Tests whether distribution function is normalized, and integrates to 1.
         """
         # integral of the distribution over v_vect
-        integral = (Maxwellian_1D(v_vect,
+        integral = (Maxwellian_1D(self.v_vect,
                                   T=30000*u.K,
-                                  particle='e')).sum()*dv
+                                  particle='e')).sum()*self.dv
         assert np.isclose(integral, 1.0)
     def test_std(self):
         """
         Tests standard deviation of function?
         """
-        std = (Maxwellian_1D(v_vect,
-                             T=T_e,
-                             particle='e')*v_vect**2*dv).sum()
+        std = (Maxwellian_1D(self.v_vect,
+                             T=self.T_e,
+                             particle='e')*self.v_vect**2*self.dv).sum()
         std = np.sqrt(std)
         T_distri = (std**2/k_B*m_e).to(u.K)
-        assert np.isclose(T_distri.value, T_e.value)
+        assert np.isclose(T_distri.value, self.T_e.value)
     def test_valErr(self):
         """
         Tests whether ValueError is raised when invalid particle name
@@ -79,17 +80,23 @@ class Test_Maxwellian_1D(object):
 
 # test class for Maxwellian_speed_1D function
 class Test_Maxwellian_speed_1D(object):
+    def setup_method(self):
+        """initializing parameters for tests """
+        self.T = 1.0 * u.eV
+        self.particle = 'H'
+        # get thermal velocity and thermal velocity squared
+        self.vTh = thermal_speed(self.T,
+                                 particle=self.particle,
+                                 method="most_probable")
     def test_norm(self):
         """
         Tests whether distribution function is normalized, and integrates to 1.
         """
-        T = 1.0 * u.eV
-        particle = 'H'
-        # get thermal velocity and thermal velocity squared
-        vTh = thermal_speed(T, particle=particle, method="most_probable")
         # setting up integration from 0 to 10*vTh
-        xData1D = np.arange(0, 10.01, 0.01) * vTh
-        yData1D = Maxwellian_speed_1D(v=xData1D,T=T,particle=particle)
+        xData1D = np.arange(0, 10.01, 0.01) * self.vTh
+        yData1D = Maxwellian_speed_1D(v=xData1D,
+                                      T=self.T,
+                                      particle=self.particle)
         # integrating, this should be close to 1
         integ = spint.trapz(y=yData1D, x=xData1D)
         exceptStr = "Integral of distribution function should be 1."
@@ -98,15 +105,20 @@ class Test_Maxwellian_speed_1D(object):
     
 # test class for Maxwellian_velocity_3D function
 class Test_Maxwellian_velocity_3D(object):
+    def setup_method(self):
+        """initializing parameters for tests """
+        self.T = 1.0 * u.eV
+        self.particle = 'H'
+        # get thermal velocity and thermal velocity squared
+        self.vTh = thermal_speed(self.T,
+                                 particle=self.particle,
+                                 method="most_probable")
     def test_norm(self):
         """
         Tests whether distribution function is normalized, and integrates to 1.
         """
-        T = 1.0 * u.eV
-        particle = 'H'
-        # get thermal velocity and thermal velocity squared
-        vTh = thermal_speed(T, particle=particle, method="most_probable")
-        vTh = vTh.si.value
+        # converting vTh to unitless
+        vTh = self.vTh.si.value
         # setting up integration from -10*vTh to 10*vTh, which is close to Inf
         infApprox = (10 * vTh)
         # integrating, this should be close to 1
@@ -117,7 +129,13 @@ class Test_Maxwellian_velocity_3D(object):
                               lambda z: infApprox,
                               lambda z, y: -infApprox,
                               lambda z, y: infApprox,
-                              args=(T,particle,0,0,0, vTh, "unitless"),
+                              args=(self.T,
+                                    self.particle,
+                                    0,
+                                    0,
+                                    0,
+                                    vTh,
+                                    "unitless"),
                               epsabs=1e0,
                               epsrel=1e0,
                               )
@@ -132,15 +150,20 @@ class Test_Maxwellian_velocity_3D(object):
 
 # test class for Maxwellian_speed_3D function
 class Test_Maxwellian_speed_3D(object):
+    def setup_method(self):
+        """initializing parameters for tests """
+        self.T = 1.0 * u.eV
+        self.particle = 'H'
+        # get thermal velocity and thermal velocity squared
+        self.vTh = thermal_speed(self.T,
+                                 particle=self.particle,
+                                 method="most_probable")
     def test_norm(self):
         """
         Tests whether distribution function is normalized, and integrates to 1.
         """
-        T = 1.0 * u.eV
-        particle = 'H'
-        # get thermal velocity and thermal velocity squared
-        vTh = thermal_speed(T, particle=particle, method="most_probable")
-        vTh = vTh.si.value
+        # converting vTh to unitless
+        vTh = self.vTh.si.value
         # setting up integration from 0 to 10*vTh, which is close to Inf
         infApprox = (10 * vTh)
         # integral should be close to 1
@@ -151,7 +174,13 @@ class Test_Maxwellian_speed_3D(object):
                               lambda z: infApprox,
                               lambda z, y: 0,
                               lambda z, y: infApprox,
-                              args=(T,particle,0,0,0, vTh, "unitless"),
+                              args=(self.T,
+                                    self.particle,
+                                    0,
+                                    0,
+                                    0,
+                                    vTh,
+                                    "unitless"),
                               epsabs=1e0,
                               epsrel=1e0,
                               )
@@ -163,3 +192,19 @@ class Test_Maxwellian_speed_3D(object):
                           1,
                           rtol=1e-3,
                           atol=0.0), exceptStr
+    def test_units_no_vTh(self):
+        """
+        Tests distribution function with units, but not passing vTh.
+        """
+    def test_units_vTh(self):
+        """
+        Tests distribution function with units and passing vTh.
+        """
+    def test_unitless_no_vTh(self):
+        """
+        Tests distribution function without units, and not passing vTh.
+        """
+    def test_unitless_vTh(self):
+        """
+        Tests distribution function without units, and with passing vTh.
+        """
