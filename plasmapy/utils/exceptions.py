57df4e9437488b6ddb5bbf847d8561dc5dc18b04
samurai688 <sam.langendorf@gmail.com>
2017-11-22 22:46:25 -0700

    make RelativityWarning a PhysicsWarning

--- plasmapy/utils/exceptions.py
+++ plasmapy/utils/exceptions.py
@@ -6,13 +6,13 @@ Custom Errors and Warning names to improve readability
 """
 
 class PhysicsError(Exception):
-    """Error for using a deeply troubling physics value"""
+    """Error for using a worrisome physics value"""
     pass
 
 class PhysicsWarning(Warning):
     """Warning for using a mildly worrisome physics value"""
     pass
 
-class RelativityWarning(Warning):
+class RelativityWarning(PhysicsWarning):
     """Warning for approaching or exceeding the speed of light"""
     pass
