4b1ae6c2b60c10ea86617491aac63efebec0524c
samurai688 <sam.langendorf@gmail.com>
2017-11-22 20:47:11 -0700

    commit with errors.py actually added

---
+++ plasmapy/classes/errors.py
@@ -0,0 +1,10 @@
+"""
+plasmapy.errors
+===============
+
+Custom Errors and Warning names to improve readability
+"""
+
+class RelativityWarning(Warning):
+    """Warning for approaching or exceeding the speed of light"""
+    pass
