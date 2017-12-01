70ecbb23137f4321e666362918831c807e7c0ff1
Pawel Marek Kozlowski <pawel@pmkozlowski.com>
2017-12-01 13:14:36 -0700

    Added 1D and 3D kappa velocity distribution functions.

--- plasmapy/physics/distribution.py
+++ plasmapy/physics/distribution.py
@@ -1,8 +1,9 @@
 """Functions to deal with distribution : generate, fit, calculate"""
 import astropy as astropy
 from astropy import units as u
-from .parameters import thermal_speed
+from .parameters import (thermal_speed, kappa_thermal_speed)
 import numpy as np
+from scipy.special import gamma
 
 
 def Maxwellian_1D(v,
@@ -547,6 +548,299 @@ def Maxwellian_speed_3D(vx,
                              units=units)
     # multiplying probabilities in each axis to get 3D probability
     distFunc = fx * fy * fz
+    if units == "units":
+        return distFunc.to((u.s/u.m)**3)
+    elif units == "unitless":
+        return distFunc
+
+#%% kappas
+
+def kappa_velocity_1D(v,
+                      T,
+                      kappa,
+                      particle="e",
+                      V_drift=0,
+                      vTh=np.nan,
+                      units="units"):
+    r"""Returns the probability at the velocity `v` in m/s
+     to find a particle `particle` in a plasma of temperature `T`
+     following the Kappa distribution function. The slope of the
+     tail of the Kappa distribution function is set by 'kappa', which
+     must be greater than 1/2.
+
+    Parameters
+    ----------
+    v: Quantity
+        The velocity in units convertible to m/s
+
+    T: Quantity
+        The temperature in Kelvin
+    
+    kappa: Quantity
+        The kappa parameter is a dimensionless number which sets the slope
+        of the energy spectrum of suprathermal particles forming the tail
+        of the Kappa velocity distribution function. Kappa must be greater
+        than 3/2.
+        
+    particle: string, optional
+        Representation of the particle species(e.g., 'p' for protons, 'D+'
+        for deuterium, or 'He-4 +1' for $He_4^{+1}$ : singly ionized helium-4),
+        which defaults to electrons.
+        
+    V_drift: Quantity, optional
+        The drift velocity in units convertible to m/s
+        
+    vTh: Quantity, optional
+        Thermal velocity (most probable) in m/s. This is used for
+        optimization purposes to avoid re-calculating vTh, for example
+        when integrating over velocity-space.
+        
+    units: string, optional
+        Selects whether to run function with units and unit checks (when
+        equal to "units") or to run as unitless (when equal to "unitless").
+        The unitless version is substantially faster for intensive
+        computations.
+
+    Returns
+    -------
+    f : Quantity
+        probability in Velocity^-1, normalized so that: :math:`\int_{-\infty}^{+\infty} f(v) dv = 1`
+
+    Raises
+    ------
+    TypeError
+        The parameter arguments are not Quantities and
+        cannot be converted into Quantities.
+
+    UnitConversionError
+        If the parameters is not in appropriate units.
+
+    ValueError
+        If the temperature is negative, or the particle mass or charge state
+        cannot be found.
+
+    Notes
+    -----
+    In one dimension, the Kappa velocity distribution function describing
+    the distribution of particles with speed v in a plasma with temperature T
+    and suprathermal parameter kappa is given by:
+
+    .. math::
+    f = A_\kappa \left(1 + \frac{(\vec{v} - \vec{V_{drift}})^2}{\kappa v_Th,\kappa^2}\right)^{-(\kappa + 1)}
+    where v_Th,\kappa is the kappa thermal speed
+    and A_\kappa = \frac{1}{2 \pi (\kappa v_Th,\kappa^2)^{1/2} \frac{\Gamma(\kappa + 1)}{\Gamma(\kappa - 1/2) \Gamma(3/2)} is the normalization constant
+
+    Examples
+    --------
+    >>> from plasmapy.physics import kappa_velocity_1D
+    >>> from astropy import units as u
+    >>> v=1*u.m/u.s
+    >>> kappa_velocity_1D(v=v, T=30000*u.K, kappa=4, particle='e',V_drift=0*u.m/u.s)
+    <Quantity 5.916329687405703e-07 s / m>
+    """
+    # must have kappa > 3/2 for distribution function to be valid
+    if kappa <= 3/2:
+        raise ValueError(f"Must have kappa > 3/2, instead of {kappa}.")
+    if units == "units":
+        # unit checks and conversions
+        # checking velocity units
+        v = v.to(u.m/u.s)
+        # catching case where drift velocities have default values, they
+        # need to be assigned units
+        if V_drift == 0:
+            if not isinstance(V_drift, astropy.units.quantity.Quantity):
+                V_drift = V_drift * u.m/u.s
+        # checking units of drift velocities
+        V_drift = V_drift.to(u.m/u.s)
+        # convert temperature to Kelvins
+        T = T.to(u.K, equivalencies=u.temperature_energy())
+        if np.isnan(vTh):
+            # get thermal velocity and thermal velocity squared
+            vTh = kappa_thermal_speed(T,
+                                      kappa,
+                                      particle=particle)
+        elif not np.isnan(vTh):
+            # check units of thermal velocity
+            vTh = vTh.to(u.m/u.s)
+    elif np.isnan(vTh) and units == "unitless":
+        # assuming unitless temperature is in Kelvins
+        vTh = (kappa_thermal_speed(T*u.K,
+                                   kappa,
+                                   particle=particle)).si.value
+    # Get thermal velocity squared
+    vThSq = vTh ** 2
+    # Get square of relative particle velocity
+    vSq = (v - V_drift) ** 2
+    # calculating distribution function
+    expTerm = (1 + vSq / (kappa * vThSq)) ** (-(kappa + 1))
+    coeff1 = 1 / (2 * np.pi * (kappa * vThSq) ** (1 / 2))
+    coeff2 = gamma(kappa + 1) / (gamma(kappa - 1/2) * gamma(3/2))
+    distFunc = coeff1 * coeff2 * expTerm
+    if units == "units":
+        return distFunc.to(u.s/u.m)
+    elif units == "unitless":
+        return distFunc
+
+def kappa_velocity_3D(vx,
+                      vy,
+                      vz,
+                      T,
+                      kappa,
+                      particle="e",
+                      Vx_drift=0,
+                      Vy_drift=0,
+                      Vz_drift=0,
+                      vTh=np.nan,
+                      units="units"):
+    r"""Return the probability of finding a particle with velocity components
+    `v_x`, `v_y`, and `v_z`in m/s in a suprathermal plasma of temperature 
+    `T` and parameter 'kappa' which follows the 3D Kappa distribution function. 
+    This function assumes Cartesian coordinates.
+
+    Parameters
+    ----------
+    vx: Quantity
+        The velocity in x-direction units convertible to m/s
+        
+    vy: Quantity
+        The velocity in y-direction units convertible to m/s
+        
+    vz: Quantity
+        The velocity in z-direction units convertible to m/s
+
+    T: Quantity
+        The temperature, preferably in Kelvin
+        
+    kappa: Quantity
+        The kappa parameter is a dimensionless number which sets the slope
+        of the energy spectrum of suprathermal particles forming the tail
+        of the Kappa velocity distribution function. Kappa must be greater
+        than 3/2.
+
+    particle: string, optional
+        Representation of the particle species(e.g., 'p' for protons, 'D+'
+        for deuterium, or 'He-4 +1' for $He_4^{+1}$ : singly ionized helium-4),
+        which defaults to electrons.
+        
+    Vx_drift: Quantity, optional
+        The drift velocity in x-direction units convertible to m/s
+        
+    Vy_drift: Quantity, optional
+        The drift velocity in y-direction units convertible to m/s
+        
+    Vz_drift: Quantity, optional
+        The drift velocity in z-direction units convertible to m/s
+    
+    vTh: Quantity, optional
+        Thermal velocity (most probable) in m/s. This is used for
+        optimization purposes to avoid re-calculating vTh, for example
+        when integrating over velocity-space.
+        
+    units: string, optional
+        Selects whether to run function with units and unit checks (when
+        equal to "units") or to run as unitless (when equal to "unitless").
+        The unitless version is substantially faster for intensive
+        computations.
+
+    Returns
+    -------
+    f : Quantity
+        probability in Velocity^-1, normalized so that:
+        $\iiint_{0}^{\infty} f(\vec{v}) d\vec{v} = 1}
+
+    Raises
+    ------
+    TypeError
+        The parameter arguments are not Quantities and
+        cannot be converted into Quantities.
+
+    UnitConversionError
+        If the parameters is not in appropriate units.
+
+    ValueError
+        If the temperature is negative, or the particle mass or charge state
+        cannot be found.
+
+    Notes
+    -----
+    In three dimensions, the Kappa velocity distribution function describing
+    the distribution of particles with speed v in a plasma with temperature T
+    and suprathermal parameter kappa is given by:
+
+    .. math::
+    f = A_\kappa \left(1 + \frac{(\vec{v} - \vec{V_{drift}})^2}{\kappa v_Th,\kappa^2}\right)^{-(\kappa + 1)}
+    where v_Th,\kappa is the kappa thermal speed
+    and A_\kappa = \frac{1}{2 \pi (\kappa v_Th,\kappa^2)^{3/2} \frac{\Gamma(\kappa + 1)}{\Gamma(\kappa - 1/2) \Gamma(3/2)} is the normalization constant
+
+    See also
+    --------
+    kappa_velocity_1D
+    kappa_thermal_speed
+
+    Example
+    -------
+    >>> from plasmapy.physics import kappa_velocity_3D
+    >>> from astropy import units as u
+    >>> v=1*u.m/u.s
+    >>> kappa_velocity_3D(vx=v,
+    ... vy=v,
+    ... vz=v,
+    ... T=30000*u.K,
+    ... kappa=4,
+    ... particle='e',
+    ... Vx_drift=0*u.m/u.s,
+    ... Vy_drift=0*u.m/u.s,
+    ... Vz_drift=0*u.m/u.s)
+    <Quantity 3.985430307328085e-20 s3 / m3>
+    """
+    # must have kappa > 3/2 for distribution function to be valid
+    if kappa <= 3/2:
+        raise ValueError(f"Must have kappa > 3/2, instead of {kappa}.")
+    if units == "units":
+        # unit checks and conversions
+        # checking velocity units
+        vx = vx.to(u.m/u.s)
+        vy = vy.to(u.m/u.s)
+        vz = vz.to(u.m/u.s)
+        # catching case where drift velocities have default values, they
+        # need to be assigned units
+        if Vx_drift == 0:
+            if not isinstance(Vx_drift, astropy.units.quantity.Quantity):
+                Vx_drift = Vx_drift * u.m/u.s
+        if Vy_drift == 0:
+            if not isinstance(Vy_drift, astropy.units.quantity.Quantity):
+                Vy_drift = Vy_drift * u.m/u.s
+        if Vz_drift == 0:
+            if not isinstance(Vz_drift, astropy.units.quantity.Quantity):
+                Vz_drift = Vz_drift * u.m/u.s
+        # checking units of drift velocities
+        Vx_drift = Vx_drift.to(u.m/u.s)
+        Vy_drift = Vy_drift.to(u.m/u.s)
+        Vz_drift = Vz_drift.to(u.m/u.s)
+        # convert temperature to Kelvins
+        T = T.to(u.K, equivalencies=u.temperature_energy())
+        if np.isnan(vTh):
+            # get thermal velocity and thermal velocity squared
+            vTh = kappa_thermal_speed(T,
+                                      kappa,
+                                      particle=particle)
+        elif not np.isnan(vTh):
+            # check units of thermal velocity
+            vTh = vTh.to(u.m/u.s)
+    elif np.isnan(vTh) and units == "unitless":
+        # assuming unitless temperature is in Kelvins
+        vTh = (kappa_thermal_speed(T*u.K,
+                                   kappa,
+                                   particle=particle)).si.value
+    # getting square of thermal velocity
+    vThSq = vTh ** 2
+    # Get square of relative particle velocity
+    vSq = ((vx-Vx_drift) ** 2 + (vy-Vy_drift) ** 2 + (vz-Vz_drift) ** 2)
+    # calculating distribution function
+    expTerm = (1 + vSq / (kappa * vThSq)) ** (-(kappa + 1))
+    coeff1 = 1 / (2 * np.pi * (kappa * vThSq) ** (3 / 2))
+    coeff2 = gamma(kappa + 1) / (gamma(kappa - 1/2) * gamma(3/2))
+    distFunc = coeff1 * coeff2 * expTerm
     if units == "units":
         return distFunc.to((u.s/u.m)**3)
     elif units == "unitless":
