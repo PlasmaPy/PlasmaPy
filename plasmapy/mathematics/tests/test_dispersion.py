7cefda4dcbc6f949792cb601760ed3fa27f222d2
Nick Murphy <namurphy@cfa.harvard.edu>
2017-12-19 21:30:17 -0500

    Extend tests for plasma_dispersion_func
    I added a test against a power series expansion that I think is valid
    around 0 + 0j, and added tests for the first five zeros of the panda
    dispersion function.
    
    I increased the accuracy of many of these tests by using mpmath to do
    this at greater precision:
    
        from mpmath import j, sqrt, exp, erf, findroot
        def Z(w):
            return j * sqrt(pi) * exp(-w**2) * (1 + erf(j * w))
    
    To find the roots at greater precision than in the appendix of Plasma
    Waves by Swanson (2nd edition), I did:
    
        findroot(Z, 4.106_107_271 - 3.725_948_279j)

--- plasmapy/mathematics/tests/test_dispersion.py
+++ plasmapy/mathematics/tests/test_dispersion.py
@@ -6,29 +6,28 @@ from astropy import units as u
 from ..mathematics import plasma_dispersion_func, plasma_dispersion_func_deriv
 
 
-# (zeta, expected, atol)
+# (zeta, expected)
 plasma_dispersion_func_table = [
-    (0, 1j * np.sqrt(np.pi), 1e-8*(1 + 1j)),
-    (1, -1.076_159_01 + 0.652_049_33j, 1e-8*(1 + 1j)),
-    (1j, 0.757_872_156j, 1e-8*(1 + 1j)),
-    (1.2 + 4.4j, -0.054_246_146 + 0.207_960_589j, 1e-8*(1 + 1j)),
-    (9.2j, plasma_dispersion_func(9.2j * u.dimensionless_unscaled), 0),
-    (5.4 - 3.1j, -0.139_225 - 0.0820_678j, 2e-6*(1 + 1j)),
-    (9.9 - 10j, 2.013_79 - 25.901_3j, 1e-4*(1 + 1j)), 
-    (5 + 7j, -0.066_816 + 0.094_803_5j, 2e-5 * (1 + 1j)),
-    (4.5 - 10j, -0.136_750e36 - 0.685_390e35j, 6e29 * (1 + 1j)),
+    (0, 1j * np.sqrt(np.pi)),
+    (1, -1.076_159_013_825_536_8 + 0.652_049_332_173_292_2j),
+    (1j, 0.757_872_156_141_311_87j),
+    (1.2 + 4.4j, -0.054_246_146_372_377_471 + 0.207_960_589_336_958_13j),
+    (9.2j, plasma_dispersion_func(9.2j * u.dimensionless_unscaled)),
+    (5.4 - 3.1j, -0.139_224_873_051_713_11 - 0.082_067_822_640_155_802j),
+    (9.9 - 10j, 2.013_835_257_947_027_6 - 25.901_274_737_989_727j),
+    (4.5 - 10j, -1.367_495_046_340_094_7e35 - 6.853_923_234_842_270_6e34j),
     ]
 
 
-@pytest.mark.parametrize('zeta, expected, atol', plasma_dispersion_func_table)
-def test_plasma_dispersion_func(zeta, expected, atol):
+@pytest.mark.parametrize('zeta, expected', plasma_dispersion_func_table)
+def test_plasma_dispersion_func(zeta, expected):
     r"""Test the implementation of plasma_dispersion_func against
     exact results, quantities calculated by Fried & Conte (1961),
     symmetry properties, and analytic results."""
 
     Z0 = plasma_dispersion_func(zeta)
 
-    assert np.isclose(Z0, expected, atol=atol, rtol=0), \
+    assert np.isclose(Z0, expected, atol=1e-12*(1 + 1j), rtol=1e-12), \
         (f"plasma_disperion_func({zeta}) equals {Z0} instead of the "
          f"expected approximate result of {expected}.  The difference between "
          f"the actual and expected results is {Z0 - expected}.")
@@ -58,6 +57,51 @@ def test_plasma_dispersion_func(zeta, expected, atol):
              f"the two results is {Z3 - Z4}.")
 
 
+def test_plasma_dispersion_func_power_series_expansion():
+    """Test plasma_dispersion_func against a power series expansion of
+    the plasma dispersion function."""
+
+    zeta_array = np.array(
+        [[0.1356 + 0.114j, -0.204 - 0.0012j],
+         [-0.131 + 0.131j, 0.1313 - 0.125j],
+         [-0.334 - 0.712j, 0.12411 + 0j],
+         [0.1278 + 0.928j, 0 + 0j]],
+        dtype=np.complex128)
+
+    Z1 = plasma_dispersion_func(zeta_array)
+
+    Z2 = np.zeros_like(zeta_array)
+
+    for n in range(0, 200):
+        Z2 += (1j * zeta_array)**n / np.math.gamma(n/2 + 1)
+
+    Z2 = 1j * np.sqrt(np.pi) * Z2
+
+    assert np.allclose(Z1, Z2, atol=1e-10 * (1 + 1j), rtol=1e-10), \
+        ("plasma_dispersion_func is returning values that are inconsistent "
+         "with the power series expansion given by equation B.3 from Plasma "
+         "Waves by D. G. Swanson (2nd edition, 2003).  The results from "
+         f"plasma_dispersion_func are:\n\n{Z1}\n\n"
+         f"whereas the results from the power series expansion are:\n\n{Z2}\n")
+
+
+def test_plasma_dispersion_func_zeros():
+    """Test the zeros of the plasma dispersion function."""
+    zeros = np.array([
+            1.991_466_842_833_879_6 - 1.354_810_128_112_006_2j,
+            2.691_149_024_251_438_8 - 2.177_044_906_089_615_9j,
+            3.235_330_868_352_816_5 - 2.784_387_613_230_428_2j,
+            3.697_309_702_468_468_4 - 3.287_410_789_389_848_6j,
+            4.106_107_284_682_632_1 - 3.725_948_719_445_790_4j],
+            dtype=np.complex128)
+
+    for zero in zeros:
+        Z = plasma_dispersion_func(zero)
+        assert np.isclose(Z, 0 + 0j, atol=1e-15 * (1 + 1j)), \
+            ("A zero of the plasma dispersion function is expected at zeta = "
+             f"{zero}, but plasma_dispersion_func({zero}) is equal to {Z}.")
+
+
 # zeta, expected
 plasma_disp_deriv_table = [
     (0, -2),
@@ -74,8 +118,9 @@ plasma_disp_deriv_table = [
 
 @pytest.mark.parametrize('zeta, expected', plasma_disp_deriv_table)
 def test_plasma_dispersion_func_deriv(zeta, expected):
-    r"""Test the implementation of plasma_dispersion_func_deriv against
-    tabulated results from Fried & Conte (1961)."""
+    r"""Test the implementation of plasma_dispersion_func_deriv
+    against tabulated results and an analytical relationship from
+    Fried & Conte (1961)."""
 
     Z_deriv = plasma_dispersion_func_deriv(zeta)
 
@@ -92,8 +137,8 @@ def test_plasma_dispersion_func_deriv(zeta, expected):
     assert np.isclose(Z_deriv, Z_deriv_characterization, rtol=1e-15), \
         (f"The relationship that Z'(zeta) = -2 * [1 + zeta * Z(zeta)] is not "
          f"met for zeta = {zeta}, where Z'(zeta) = {Z_deriv} and "
-         f"-2 * [1 + zeta * Z(zeta)] = {Z_deriv_characterization}."
-         )
+         f"-2 * [1 + zeta * Z(zeta)] = {Z_deriv_characterization}.")
+
 
 # zeta, expected_error
 plasma_disp_func_errors_table = [
