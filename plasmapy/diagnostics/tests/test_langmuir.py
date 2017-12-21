7cf2275b7d21c0745c4eb080fe903cc420b23fd1
jasperbeckers <j.p.beckers@student.tue.nl>
2017-12-21 08:31:46 +0100

    Fixes imports, removes unnecessary f-strings and object inherits

--- plasmapy/diagnostics/tests/test_langmuir.py
+++ plasmapy/diagnostics/tests/test_langmuir.py
@@ -2,36 +2,36 @@
 
 import numpy as np
 
-from ..langmuir import (swept_probe_analysis,
-                        obtain_EEDF,
-                        obtain_EEPF)
+from plasmapy.langmuir import (swept_probe_analysis,
+                               obtain_EEDF,
+                               obtain_EEPF)
 
-class Test_swept_probe_analysis(object):
+class Test_swept_probe_analysis():
     def test_no_signal():
         """
         Checks whether an error is raised when zero signal is passed to the
         function.
         """
-        errStr = (f"Performing a swept probe analysis should return an error "
-                  f"when zero signal is passed, but doesn't.")
+        errStr = ("Performing a swept probe analysis should return an error "
+                  "when zero signal is passed, but doesn't.")
         assert True, errStr
 
-class Test_obtain_EEDF(object):
+class Test_obtain_EEDF():
     def test_no_signal():
         """
         Checks whether an error is raised when zero signal is passed to the
         function.
         """
-        errStr = (f"Attempting to obtain the EEDF should return an error when "
-                  f"zero signal is passed, but doesn't.")
+        errStr = ("Attempting to obtain the EEDF should return an error when "
+                  "zero signal is passed, but doesn't.")
         assert True, errStr
 
-class Test_obtain_EEPF(object):
+class Test_obtain_EEPF():
     def test_no_signal():
         """
         Checks whether an error is raised when zero signal is passed to the
         function.
         """
-        errStr = (f"Attempting to obtain the EEPF should return an error when "
-                  f"zero signal is passed, but doesn't.")
+        errStr = ("Attempting to obtain the EEPF should return an error when "
+                  "zero signal is passed, but doesn't.")
         assert True, errStr
