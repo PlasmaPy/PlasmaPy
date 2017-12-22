f2c1361aacc5cdd75297bf05598897f56e8777c1
Nick Murphy <namurphy@cfa.harvard.edu>
2017-12-22 17:12:26 -0500

    Update __init__.py
    This commit updates __init__.py so that it uses the helper functions
    within utils/import_helpers.py.  This also adds __name__, __doc__, and
    __version__ for the plasmapy module.
    
    I also added and modified code from Astropy's __init__.py that cleans
    up the top-level namespace - see licenses/Astropy_LICENSE.md for
    Astropy's BSD 3-clause license..

--- plasmapy/__init__.py
+++ plasmapy/__init__.py
@@ -1,6 +1,14 @@
-import sys
-import warnings
-import importlib
+from .utils import import_helpers
+
+__name__ = 'plasmapy'
+
+__doc__ = ('A community-developed and community-driven open source core '
+           'Python package for plasma physics.')
+
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
 
 __minimum_python_version__ = '3.6'
 
@@ -9,52 +17,8 @@ __minimum_versions__ = {
     'astropy': '2.0',
     }
 
-
-def _split_version(version):
-    """Separate a string including digits separated by periods into a
-    tuple of integers."""
-    return tuple(int(ver) for ver in version.split('.'))
-
-
-def _check_python(minimum_python_version):
-    """Raises an ImportError if the version of Python is not at least
-    the version given by a string representing the minimum version
-    number."""
-    required_python = _split_version(minimum_python_version)
-    current_python = sys.version_info
-
-    if current_python < required_python:
-        raise ImportError(f"PlasmaPy requires Python {minimum_python_version} "
-                          "or newer.") from None
-
-
-def _check_versions(minimum_versions):
-    """Raises an ImportError if a dependent package is not installed
-    and at the required version number, or provides a warning if the
-    version of the dependent package cannot be found."""
-
-    for module_name in minimum_versions.keys():
-        minimum_version = minimum_versions[module_name]
-
-        try:
-            module = importlib.import_module(module_name)
-            module_version = module.__version__
-        except ImportError:
-            raise ImportError(f"Unable to import {module_name} while "
-                              "importing PlasmaPy.") from None
-        except AttributeError:
-            warnings.warn(f"{module_name}.__version__ was not found while "
-                          "importing PlasmaPy", UserWarning)
-        else:
-            if minimum_version > module_version:
-                raise ImportError(
-                    f"{module_name} {minimum_version} or newer is required "
-                    "for PlasmaPy. The currently installed version is "
-                    f"{module_version}.") from None
-
-
-_check_python(__minimum_python_version__)
-_check_versions(__minimum_versions__)
+import_helpers.check_python(__minimum_python_version__)
+import_helpers.check_versions(__minimum_versions__)
 
 try:
     from .classes import Plasma
@@ -65,4 +29,34 @@ try:
     from . import physics
     from . import utils
 except ImportError as exc:
-    raise ImportError("Unable to load PlasmaPy subpackages.") from exc
+    raise ImportError('Unable to load PlasmaPy subpackages.') from exc
+
+# Clean up the top-level namespace by deleting everything that (1)
+# isn't listed in keep, (2) is a dunder, and (3) isn't a submodule of
+# this package.  This was adapted from astropy's __init__.py.
+
+__keep__ = ['Plasma']
+
+from types import ModuleType as __module_type__
+
+for varname in dir():
+
+    in_keep = varname in __keep__
+
+    is_dunder = varname.startswith('__') and varname.endswith('__')
+
+    # When using relative imports like ``from .. import config``, the
+    # ``config`` variable is automatically created in the namespace of
+    # whatever module ``..`` resolves to (in this case astropy).  This
+    # happens a few times just in the module setup above.  This allows
+    # the cleanup to keep any public submodules of the package.
+
+    is_subpackage = (varname[0] != '_' and
+                     isinstance(locals()[varname], __module_type__) and
+                     locals()[varname].__name__.startswith(__name__ + '.'))
+
+    if not (in_keep or is_dunder or is_subpackage):
+        del locals()[varname]
+
+del (import_helpers, __minimum_python_version__, __minimum_versions__,
+     varname, __module_type__, __keep__, in_keep, is_dunder, is_subpackage)
