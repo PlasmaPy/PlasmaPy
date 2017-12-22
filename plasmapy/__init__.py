35543efb3a2d7798988579b35438338f1e2b23d1
Nick Murphy <namurphy@cfa.harvard.edu>
2017-12-22 17:55:57 -0500

    Remove loop for cleaning up top-level namespace
    Coveralls showed that nothing was being deleted in the loop to clean
    up our top-level namespace, so it presently looks best to just use a
    del statement for three variables instead.  At some point we may want
    to implement something like this again.  Also, it's snowing!

--- plasmapy/__init__.py
+++ plasmapy/__init__.py
@@ -8,13 +8,16 @@ __doc__ = ('A community-developed and community-driven open source core '
 try:
     from .version import version as __version__
 except ImportError:
-    __version__ = ''
+    __version__ = '0.1.0dev0'
 
 __minimum_python_version__ = '3.6'
+
+# The following information is duplicated in requirements/base.txt
 
 __minimum_versions__ = {
     'numpy': '1.13',
     'astropy': '2.0',
+    'scipy': '0.19',
     }
 
 import_helpers.check_python(__minimum_python_version__)
@@ -31,32 +34,7 @@ try:
 except ImportError as exc:
     raise ImportError('Unable to load PlasmaPy subpackages.') from exc
 
-# Clean up the top-level namespace by deleting everything that (1)
-# isn't listed in keep, (2) is a dunder, and (3) isn't a submodule of
-# this package.  This was adapted from astropy's __init__.py.
+# A more extensive and thoughtful method for cleaning up our top-level
+# namespace is in Astropy's __init__.py (see also pull request #210).
 
-__keep__ = ['Plasma']
-
-from types import ModuleType as __module_type__
-
-for varname in dir():
-
-    in_keep = varname in __keep__
-
-    is_dunder = varname.startswith('__') and varname.endswith('__')
-
-    # When using relative imports like ``from .. import config``, the
-    # ``config`` variable is automatically created in the namespace of
-    # whatever module ``..`` resolves to (in this case astropy).  This
-    # happens a few times just in the module setup above.  This allows
-    # the cleanup to keep any public submodules of the package.
-
-    is_subpackage = (varname[0] != '_' and
-                     isinstance(locals()[varname], __module_type__) and
-                     locals()[varname].__name__.startswith(__name__ + '.'))
-
-    if not (in_keep or is_dunder or is_subpackage):
-        del locals()[varname]
-
-del (import_helpers, __minimum_python_version__, __minimum_versions__,
-     varname, __module_type__, __keep__, in_keep, is_dunder, is_subpackage)
+del (import_helpers, __minimum_python_version__, __minimum_versions__)
