f2cfa1a190d66b01ae33948745dda1c3c0dbf22c
Nick Murphy <namurphy@cfa.harvard.edu>
2017-12-26 21:31:24 -0500

    Add check_versions to utils/__init__.py

--- plasmapy/utils/__init__.py
+++ plasmapy/utils/__init__.py
@@ -2,3 +2,5 @@ from .checks import (_check_quantity,
                      _check_relativistic,
                      check_quantity,
                      check_relativistic)
+
+from .import_helpers import check_versions
