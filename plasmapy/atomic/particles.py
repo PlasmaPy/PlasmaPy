8096255dd7ba15f80f158e1198296839b4a995f4
Nick Murphy <namurphy@cfa.harvard.edu>
2017-12-29 17:07:03 -0500

    Add code to construct _Particles dictionary
    Instead of manually inputting data (which can lead to errors and
    duplication of code), this commit adds a function that initializes the
    dictionary used to store information about particles and antiparticles.
    
    I have some loops like `for thing in everything`.  I originally used
    `for particle in particles` but this doesn't account for some of the
    things being antiparticles.  It would be helpful to have a more
    specific name for the set containing both particles and antiparticles.

--- plasmapy/atomic/particles.py
+++ plasmapy/atomic/particles.py
@@ -1,200 +1,113 @@
 from astropy import units as u, constants as const
 import numpy as np
 
-Particles = {
+"""Next we go through the process of creating the Particles dictionary."""
 
-    "e-": {
-        "name": "electron",
-        "mass": const.m_e,
-        "charge": -1,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "antimatter": False,
-        "generation": 1,
-        "class": "lepton",
-        "lepton number": 1,
-        "baryon number": 0,
-    },
 
-    "e+": {
-        "name": "positron",
-        "mass": const.m_e,
-        "charge": 1,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "antimatter": True,
-        "generation": 1,
-        "class": "antilepton",
-        "lepton number": -1,
-        "baryon number": 0,
-    },
+def _create_Particles_dict():
 
-    "mu-": {
-        "name": "muon",
-        "mass": 1.883_531_594e-28 * u.kg,
-        "charge": -1,
-        "spin": 1/2,
-        "half-life": 2.1969811e-6 * u.s,
-        "antimatter": False,
-        "generation": 2,
-        "class": "lepton",
-        "lepton number": 1,
-        "baryon number": 0,
-    },
+    leptons = ['e-', 'mu-', 'tau-', 'nu_e', 'nu_mu', 'nu_tau']
+    antileptons = ['e+', 'mu+', 'tau+', 'anti_nu_e',
+                   'anti_nu_mu', 'anti_nu_tau']
 
-    "mu+": {
-        "name": "antimuon",
-        "mass": 1.883_531_594e-28 * u.kg,
-        "charge": 1,
-        "spin": 1/2,
-        "half-life": 2.1969811e-6 * u.s,
-        "antimatter": True,
-        "generation": 2,
-        "class": "antilepton",
-        "lepton number": -1,
-        "baryon number": 0,
-    },
+    baryons = ['p', 'n']
+    antibaryons = ['p-', 'antineutron']
 
-    "tau-": {
-        "name": "tau",
-        "mass": 3.167_47e-27 * u.kg,
-        "charge": -1,
-        "spin": 1/2,
-        "half-life": 2.906e-13 * u.s,
-        "antimatter": False,
-        "generation": 3,
-        "class": "lepton",
-        "lepton number": 1,
-        "baryon number": 0,
-    },
+    everything = leptons + antileptons + baryons + antibaryons
 
-    "tau+": {
-        "name": "antitau",
-        "mass": 3.167_47e-27 * u.kg,
-        "charge": 1,
-        "spin": 1/2,
-        "half-life": 2.906e-13 * u.s,
-        "antimatter": True,
-        "generation": 3,
-        "class": "antilepton",
-        "lepton number": -1,
-        "baryon number": 0,
-    },
+    particles = leptons + antileptons + baryons + antibaryons
+    antiparticles = antileptons + antibaryons
 
-    "nu_e": {
-        "name": "electron neutrino",
-        "charge": 0,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "antimatter": False,
-        "generation": 1,
-        "class": "lepton",
-        "lepton number": 1,
-        "baryon number": 0,
-    },
+    fermions = leptons + antileptons + baryons + antibaryons
+    bosons = []
 
-    "anti_nu_e": {
-        "name": "electron antineutrino",
-        "charge": 0,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "antimatter": True,
-        "generation": 1,
-        "class": "antilepton",
-        "lepton number": -1,
-        "baryon number": 0,
-    },
+    neutrinos = [lepton for lepton in leptons if 'nu' in lepton]
+    antineutrinos = [antilepton for antilepton in antileptons
+                     if 'nu' in antilepton]
 
-    "nu_mu": {
-        "name": "muon neutrino",
-        "charge": 0,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "antimatter": False,
-        "generation": 2,
-        "class": "lepton",
-        "lepton number": 1,
-        "baryon number": 0,
-    },
+    symbols_and_names = [
+        ('e-', 'electron'),
+        ('e+', 'positron'),
+        ('mu-', 'muon'),
+        ('mu+', 'antimuon'),
+        ('tau-', 'tau'),
+        ('tau+', 'antitau'),
+        ('nu_e', 'electron neutrino'),
+        ('anti_nu_e', 'electron antineutrino'),
+        ('nu_mu', 'muon neutrino'),
+        ('anti_nu_mu', 'muon antineutrino'),
+        ('nu_tau', 'tau neutrino'),
+        ('anti_nu_tau', 'tau antineutrino'),
+        ('p', 'proton'),
+        ('p-', 'antiproton'),
+        ('n', 'neutron'),
+        ('antineutron', 'antineutron'),
+    ]
 
-    "anti_nu_mu": {
-        "name": "muon antineutrino",
-        "charge": 0,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "antimatter": True,
-        "generation": 2,
-        "class": "antilepton",
-        "lepton number": -1,
-        "baryon number": 0,
-    },
+    Particles = dict()
 
-    "nu_tau": {
-        "name": "tau neutrino",
-        "charge": 0,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "antimatter": False,
-        "generation": 3,
-        "class": "lepton",
-        "lepton number": 1,
-        "baryon number": 0,
-    },
+    for thing in everything:
+        Particles[thing] = dict()
 
-    "anti_nu_tau": {
-        "name": "tau antineutrino",
-        "charge": 0,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "antimatter": True,
-        "generation": 3,
-        "class": "antilepton",
-        "lepton number": -1,
-        "baryon number": 0,
-    },
+    for symbol, name in symbols_and_names:
+        Particles[symbol]['name'] = name
 
-    "p": {
-        "name": "proton",
-        "mass": const.m_p,
-        "charge": 1,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "class": "baryon",
-        "lepton number": 0,
-        "baryon number": 1,
-    },
+    for fermion in fermions:
+        Particles[fermion]['spin'] = 0.5
 
-    "p-": {
-        "name": "antiproton",
-        "mass": const.m_p,
-        "charge": -1,
-        "spin": 1/2,
-        "half-life": np.inf * u.s,
-        "class": "antibaryon",
-        "lepton number": 0,
-        "baryon number": -1,
-    },
+    for boson in bosons:
+        Particles[boson]['spin'] = 0
 
-    "n": {
-        "name": "neutron",
-        "mass": const.m_n,
-        "charge": 0,
-        "spin": 1,
-        "half-life": 881.5 * u.s,
-        "class": "baryon",
-        "lepton number": 0,
-        "baryon number": 1,
-    },
+    for lepton in leptons:
+        Particles[lepton]['class'] = 'lepton'
+        Particles[lepton]['lepton number'] = 1
+        Particles[lepton]['baryon number'] = 0
 
-    "antineutron": {
-        "name": "antineutron",
-        "mass": const.m_n,
-        "charge": 0,
-        "spin": 1,
-        "half-life": 881.5 * u.s,
-        "class": "antibaryon",
-        "lepton number": 0,
-        "baryon number": -1,
-    },
+    for antilepton in antileptons:
+        Particles[antilepton]['class'] = 'antilepton'
+        Particles[antilepton]['lepton number'] = -1
+        Particles[antilepton]['baryon number'] = 0
 
-}
+    for baryon in baryons:
+        Particles[baryon]['class'] = 'baryon'
+        Particles[baryon]['lepton number'] = 0
+        Particles[baryon]['baryon number'] = 1
+
+    for antibaryon in antibaryons:
+        Particles[antibaryon]['class'] = 'antibaryon'
+        Particles[antibaryon]['lepton number'] = 0
+        Particles[antibaryon]['baryon number'] = -1
+
+    for thing in leptons + antileptons:
+        if 'e' in thing:
+            Particles[thing]['generation'] = 1
+        elif 'mu' in thing:
+            Particles[thing]['generation'] = 2
+        elif 'tau' in thing:
+            Particles[thing]['generation'] = 3
+
+    for thing in leptons + antileptons:
+        if 'nu' not in thing:
+            if 'e' in thing:
+                Particles[thing]['mass'] = const.m_e
+            elif 'mu' in thing:
+                Particles[thing]['mass'] = 1.883_531_594e-28 * u.kg
+                Particles[thing]['half-life'] = 2.1969811e-6 * u.s
+            elif 'tau' in thing:
+                Particles[thing]['mass'] = 3.167_47e-27 * u.kg
+                Particles[thing]['half-life'] = 2.906e-13 * u.s
+
+    for thing in ['p', 'p-']:
+        Particles[thing]['mass'] = const.m_p
+
+    for thing in ['n', 'antineutron']:
+        Particles[thing]['mass'] = const.m_n
+        Particles[thing]['half-life'] = 881.5 * u.s
+
+    for thing in everything:
+        if 'half-life' not in Particles[thing].keys():
+            Particles[thing]['half-life'] = np.inf * u.s
+
+    return Particles
+
+_Particles = _create_Particles_dict()
