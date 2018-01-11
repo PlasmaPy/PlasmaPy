1dbd33ab7a5493b7324ce4f9397d7ab97c250de0
Siddharth <think.siddharth@gmail.com>
2018-01-11 18:43:48 -0500

    Modified __init__.py for physics

--- plasmapy/physics/__init__.py
+++ plasmapy/physics/__init__.py
@@ -2,8 +2,8 @@
 # possibility is 'plasma'.  The organization is to be decided by v0.1.
 
 from .parameters import *
-from .quantum import *
-from .relativity import *
-from .transport import *
-from .distribution import *
-from .dielectric import *
+from .quantum import deBroglie_wavelength, thermal_deBroglie_wavelength, Fermi_energy, Thomas_Fermi_length
+from .relativity import Lorentz_factor
+from .transport import Coulomb_logarithm
+from .distribution import Maxwellian_1D, Maxwellian_velocity_3D,Maxwellian_speed_1D, Maxwellian_speed_3D,kappa_velocity_3D
+from .dielectric import cold_plasma_permittivity_LRP, cold_plasma_permittivity_SDP
