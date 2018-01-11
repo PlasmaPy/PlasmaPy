d086a066f51f144b01fee3cde3843389b28396ba
Drew Leonard <andy.j.leonard@gmail.com>
2018-01-11 17:10:50 +0000

    Minor addition to docs config - does useful things apparently

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,7 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+default_role = 'obj'
 
 # -- Options for HTML output ----------------------------------------------
 
