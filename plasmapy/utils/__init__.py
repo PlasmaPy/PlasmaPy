c421fb7ca70508e81af4bc8a0fc3b0aee7cd22bd
Dominik Stanczak <stanczakdominik@gmail.com>
2018-01-13 17:26:19 +0100

    add Nick style () import

--- plasmapy/utils/__init__.py
+++ plasmapy/utils/__init__.py
@@ -1,4 +1,4 @@
 from .checks import check_quantity, check_relativistic, _check_quantity, _check_relativistic
-from .exceptions import PlasmaPyError, PhysicsError, RelativityError, AtomicError, MissingAtomicDataError, NoChargeInfoError, IonError, IsotopeError, ElementError, ParticleError,
-                        PlasmaPyWarning, PhysicsWarning, RelativityWarning, AtomicWarning, MissingAtomicDataWarning
+from .exceptions import (PlasmaPyError, PhysicsError, RelativityError, AtomicError, MissingAtomicDataError, NoChargeInfoError, IonError, IsotopeError, ElementError, ParticleError,
+                        PlasmaPyWarning, PhysicsWarning, RelativityWarning, AtomicWarning, MissingAtomicDataWarning)
 from .import_helpers import check_versions
