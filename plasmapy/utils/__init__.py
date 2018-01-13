6b5cd722c0a29fbaa5ffacbcd225bccdad001940
Dominik Stanczak <stanczakdominik@gmail.com>
2018-01-13 17:19:56 +0100

    remove star imports from utils

--- plasmapy/utils/__init__.py
+++ plasmapy/utils/__init__.py
@@ -1,3 +1,4 @@
-from .checks import *
-from .exceptions import *
-from .import_helpers import *
+from .checks import check_quantity, check_relativistic, _check_quantity, _check_relativistic
+from .exceptions import PlasmaPyError, PhysicsError, RelativityError, AtomicError, MissingAtomicDataError, NoChargeInfoError, IonError, IsotopeError, ElementError, ParticleError,
+                        PlasmaPyWarning, PhysicsWarning, RelativityWarning, AtomicWarning, MissingAtomicDataWarning
+from .import_helpers import check_versions
