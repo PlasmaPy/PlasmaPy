212c3286885752fa89bc659545ad9388880b4c85
Siddharth <think.siddharth@gmail.com>
2018-01-12 22:15:34 -0500

    Fixed __init__.py for math & atomic

--- plasmapy/mathematics/__init__.py
+++ plasmapy/mathematics/__init__.py
@@ -1,3 +1,3 @@
-from .mathematics import *
+from .mathematics import plasma_dispersion_func, plasma_dispersion_func_deriv
 
 from . import tests
--- plasmapy/physics/__init__.py
+++ plasmapy/physics/__init__.py
@@ -1,7 +1,7 @@
 # 'physics' is a tentative name for this subpackage.  Another
 # possibility is 'plasma'.  The organization is to be decided by v0.1.
 
-from .parameters import *
+from .parameters import Alfven_speed, ion_sound_speed, thermal_speed, kappa_thermal_speed, gyrofrequency, gyroradius, plasma_frequency, Debye_length, Debye_number, inertial_length, magnetic_pressure,magnetic_energy_density, upper_hybrid_frequency, lower_hybrid_frequency
 from .quantum import deBroglie_wavelength, thermal_deBroglie_wavelength, Fermi_energy, Thomas_Fermi_length
 from .relativity import Lorentz_factor
 from .transport import Coulomb_logarithm
