212883ea06e493ed263d9334391d193f85d14cf3
Nick Murphy <namurphy@cfa.harvard.edu>
2018-01-16 23:16:31 -0500

    Update atomic/__init__.py
    Changed formatting of the import statements and removed functions that
    should not be part of the public API.

--- plasmapy/atomic/__init__.py
+++ plasmapy/atomic/__init__.py
@@ -1,5 +1,19 @@
 """Physical and mathematical constants for use within PlasmaPy."""
-from .atomic import atomic_symbol, isotope_symbol, atomic_number, is_isotope_stable, half_life, mass_number, element_name, standard_atomic_weight, isotope_mass, ion_mass, known_isotopes, common_isotopes, stable_isotopes, isotopic_abundance, charge_state, electric_charge
+from .atomic import (atomic_symbol,
+                     isotope_symbol,
+                     atomic_number,
+                     is_isotope_stable,
+                     half_life,
+                     mass_number,
+                     element_name,
+                     standard_atomic_weight,
+                     isotope_mass,
+                     ion_mass,
+                     known_isotopes,
+                     common_isotopes,
+                     stable_isotopes,
+                     isotopic_abundance,
+                     charge_state,
+                     electric_charge)
+
 from .nuclear import nuclear_binding_energy, nuclear_reaction_energy
-from .elements import atomic_symbols, atomic_symbols_dict, Elements
-from .isotopes import Iso
