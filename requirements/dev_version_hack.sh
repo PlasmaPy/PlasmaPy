dbe2b2cf1fb5852c081662dced0047a9de6f3baa
Pawel Kozlowski <pawel@pmkozlowski.com>
2018-02-15 23:58:35 -0700

    remove print to stderr and add end DEBUG

--- requirements/dev_version_hack.sh
+++ requirements/dev_version_hack.sh
@@ -9,8 +9,11 @@ fi
 if [[ -z $PYTHON_VERSION ]]; then
     # fetches python --version output
     version="$(python --version 2>&1)"
-    >&2 echo "Current version is $version"
+    echo "Current version is $version"
     # binds output of python --version to PYTHON_VERSION
     PYTHON_VERSION="$version"
-    >&2 echo "PYTHON_VERSION is now $PYTHON_VERSION"
+    echo "PYTHON_VERSION is now $PYTHON_VERSION"
 fi
+
+# disables debug if it was set
+set +x
