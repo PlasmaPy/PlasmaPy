836c453cb8fc92e0d3b3a87b8cc0b2a591b625de
Nick Murphy <namurphy@cfa.harvard.edu>
2018-02-21 00:46:30 -0500

    Begin creating general test function
    The goal of this function is to take a tuple with a function; args and
    kwargs for that function; and the expected result, warning, or exception
    and then test that it has the appropriate behavior.  This function is
    likely to be helpful for functions where the args, kwargs, and result
    can be represented straightforwardly.  This function should generally
    be used in conjunction with @pytest.mark.parametrize.

--- plasmapy/utils/__init__.py
+++ plasmapy/utils/__init__.py
@@ -20,3 +20,5 @@ from .exceptions import (PlasmaPyError,
                          MissingAtomicDataWarning)
 
 from .import_helpers import check_versions
+
+from .pytest_helpers import run_test_of_function
--- plasmapy/utils/pytest_helpers.py
+++ plasmapy/utils/pytest_helpers.py
@@ -1,7 +1,47 @@
 import pytest
-from typing import Callable, Tuple, Dict, Any
+from typing import Callable, Dict, Tuple, Any
+import inspect
+''
 
-
-def run_function_test(f: Callable, args: Any, kwargs: Dict, expected: Any):
-    """..."""
+def _function_call_string(f: Callable, args: Tuple, kwargs: Dict) -> str:
     ...
+
+
+def run_test_of_function(f: Callable, args: Any, kwargs: Dict, expected: Any):
+    """Tests a function or class to make sure it returns the expected result,
+    raises the appropriate exception, or issues an appropriate warning.
+
+    Parameters
+    ----------
+
+    f: `function` or `class`
+        The callable to be tested.
+
+    args: `tuple` or `object`
+        The positional arguments to `f`.
+
+    kwargs: `dict`
+        The keyword arguments to `f`.
+
+    expected: `object`
+        The expected result, exception, or warning from `f(*args, **kwargs)`.
+        This may also be a tuple of length two
+    """
+
+    if not isinstance(args, tuple):
+        args = (args)
+
+    call_string = _function_call_string(f, args, kwargs)
+
+    if inspect.isclass(expected) and issubclass(expected, Exception):
+
+        article = 'a'
+
+        missing_exception_errmsg = (
+            f"When testing {f.__module__}, the command\n\n"
+            f"    {call_string}\n\n"
+            f"did not raise {article} {expected.__name__} as expected. "
+        )
+
+        with pytest.raises(expected, message=(missing_exception_errmsg)):
+            f(*args, **kwargs)
