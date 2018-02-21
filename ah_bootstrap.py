e75ef3d8795eb44e789d5f42663ecec51272cdce
Nick Murphy <namurphy@cfa.harvard.edu>
2018-02-21 09:10:11 -0500

    Copy ah_bootstrap file to top directory

--- ah_bootstrap.py
+++ ah_bootstrap.py
@@ -47,9 +47,14 @@ except ImportError:
     from configparser import ConfigParser, RawConfigParser
 
 
-_str_types = (str, bytes)
-_text_type = str
-PY3 = True
+if sys.version_info[0] < 3:
+    _str_types = (str, unicode)
+    _text_type = unicode
+    PY3 = False
+else:
+    _str_types = (str, bytes)
+    _text_type = str
+    PY3 = True
 
 
 # What follows are several import statements meant to deal with install-time
