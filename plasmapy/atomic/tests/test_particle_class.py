dfec25399f16c65cebfd4816149c9e5be7950b67
Nick Murphy <namurphy@cfa.harvard.edu>
2018-03-02 18:40:10 -0500

    Update test of Particle.is_electron
    By using the @property decorator, we can retrieve this as, e.g.,
    is_electron rather than is_electron().

--- plasmapy/atomic/tests/test_particle_class.py
+++ plasmapy/atomic/tests/test_particle_class.py
@@ -518,4 +518,4 @@ def test_particle_half_life_string():
 
 @pytest.mark.parametrize("p, is_one", [(Particle('e-'), True), (Particle('p+'), False)])
 def test_particle_is_electron(p, is_one):
-    assert p.is_electron() == is_one
+    assert p.is_electron == is_one
