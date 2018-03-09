ec71b552a7b6d23e9375f65a2d03f2d61bf6bab3
Nick Murphy <namurphy@cfa.harvard.edu>
2018-03-09 12:51:54 -0500

    Test the test helper function
    These tests are presently failing since the needed functionality has
    not been added yet.

--- plasmapy/utils/tests/test_pytest_helpers.py
+++ plasmapy/utils/tests/test_pytest_helpers.py
@@ -1,9 +1,8 @@
 import pytest
 
-from ..pytest_helpers import (
-    call_string,
-    run_test,
-)
+from ..pytest_helpers import call_string, run_test
+
+from ..exceptions import PlasmaPyWarning, PlasmaPyError
 
 
 def f(*args, **kwargs):
@@ -29,3 +28,64 @@ def test_call_string(f, args, kwargs, expected):
     """Tests that _function_call_string returns a string that is
     equivalent to the function call."""
     assert expected == call_string(f, args, kwargs)
+
+
+def raise_exception(x, y=None):
+    raise PlasmaPyError("I'm sorry, Dave. I'm afraid I can't do that.")
+
+
+def warn_warning(x, y=None):
+    warnings.warn("Danger, Will Robinson!", PlasmaPyWarning)
+    return 42
+
+
+def adams_number(x, y=None):
+    return 42
+
+
+f_args_kwargs_expected_shouldpass = [
+
+    (adams_number, 0, {'y': 1}, 42, True),
+    (adams_number, (0,), {'y': 1}, 42, True),
+    (adams_number, (0, 1), {}, 42, True),
+
+    (adams_number, 0, {'y': 1}, 6 * 9, False),
+    (adams_number, (0,), {'y': 1}, 6 * 9, False),
+    (adams_number, (0, 1), {}, 6 * 9, False),
+
+    (raise_exception, 0, {'y': 1}, PlasmaPyError, True),
+    (raise_exception, (0,), {'y': 1}, PlasmaPyError, True),
+    (raise_exception, (0, 1), {}, PlasmaPyError, True),
+
+    (raise_exception, 0, {'y': 1}, TypeError, False),
+    (raise_exception, (0,), {'y': 1}, TypeError, False),
+    (raise_exception, (0, 1), {}, TypeError, False),
+
+    (warn_warning, 0, {'y': 1}, PlasmaPyWarning, True),
+    (warn_warning, (0,), {'y': 1}, PlasmaPyWarning, True),
+    (warn_warning, (0, 1), {}, PlasmaPyWarning, True),
+
+    (warn_warning, 0, {'y': 1}, (42, UserWarning), False),
+    (warn_warning, (0,), {'y': 1}, (42, UserWarning), False),
+    (warn_warning, (0, 1), {}, (42, UserWarning), False),
+
+]
+
+@pytest.mark.parametrize(
+    "f, args, kwargs, expected, should_pass",
+    f_args_kwargs_expected_shouldpass,
+)
+def test_passing_tests(f, args, kwargs, expected, should_pass):
+    if should_pass:
+        run_test(f, args, kwargs, expected)
+    else:
+        with pytest.raises(Exception, message=
+            f"run_test did not raise an exception with:\n"
+            f"f        = {f.__name__}\n"
+            f"args     = {args}\n"
+            f"kwargs   = {kwargs}\n"
+            f"expected = {expected}"
+        ):
+            run_test(f, args, kwargs, expected)
+
+
