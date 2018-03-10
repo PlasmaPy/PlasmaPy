895b618882bab72d6308d895673ab96c6d963029
Nick Murphy <namurphy@cfa.harvard.edu>
2018-03-09 20:23:43 -0500

    Fix some bugs in test helper function

--- plasmapy/utils/pytest_helpers.py
+++ plasmapy/utils/pytest_helpers.py
@@ -55,10 +55,7 @@ def call_string(f: Callable, args: Any=tuple(), kwargs: Dict={}) -> str:
     return f"{f.__name__}({args_and_kwargs})"
 
 
-def run_test(f: Callable,
-             args: Any = (),
-             kwargs: Dict = {},
-             expected_outcome: Any = None):
+def run_test(func: Callable, args: Any = (), kwargs: Dict = {}, expected_outcome: Any = None):
     """
     Test that a function or class returns the expected result, raises
     the expected exception, or issues an expected warning for the
@@ -66,20 +63,27 @@ def run_test(f: Callable,
 
     Parameters
     ----------
-    f: callable
+    func: callable
         The callable to be tested.
 
     args: tuple or object
-        The positional arguments to `f`.
+        The positional arguments to `func`.
 
     kwargs: dict
-        The keyword arguments to `f`.
+        The keyword arguments to `func`.
 
     expected_outcome: object
         The expected result, exception, or warning from
-        `f(*args, **kwargs)`. This may also be a tuple of length two
+        `func(*args, **kwargs)`. This may also be a tuple of length two
         that contains the expected result as the first item and the
         expected warning as the second item.
+
+    Examples
+    --------
+    >>> from warnings import warn
+    >>> run_test(lambda: 42, tuple(), dict(), 42)
+    >>> run_test(lambda: raise ValueError, tuple(), dict(), ValueError)
+    >>> run_test(lambda: warn("", UserWarning), tuple(), dict(), UserWarning)
 
     """
 
@@ -89,7 +93,7 @@ def run_test(f: Callable,
         exception `ex`.
         """
         article = 'an' if ex.__name__[0] in 'aeiouAEIOU' else 'a'
-        return f"{article} {ex.__name__[0]}"
+        return f"{article} {ex.__name__}"
 
     if not isinstance(args, tuple):
         args = (args,)
@@ -97,7 +101,9 @@ def run_test(f: Callable,
     # We will want to include a string that can reproduce the test for
     # all error messages.
 
-    call_str = call_string(f, args, kwargs)
+    call_str = call_string(func, args, kwargs)
+
+    # print(f"call_str = {call_str}")
 
     # There are many possibilities for expected outcomes that we must
     # keep track of, including exceptions being raised and warnings
@@ -105,29 +111,34 @@ def run_test(f: Callable,
 
     expected = defaultdict(lambda: None)
 
-    if inspect.isclass(expected):
-        if issubclass(expected, Exception):
-            expected['exception'] = expected_outcome
-        elif issubclass(expected, Warning):
+    if inspect.isclass(expected_outcome):
+        subclass_of_Exception = issubclass(expected_outcome, Exception)
+        subclass_of_Warning = issubclass(expected_outcome, Warning)
+
+        if subclass_of_Warning:
             expected['warning'] = expected_outcome
-# TODO       elif issubclass(expected, u.Unit):
-# TODO            expected['unit'] = expected_outcome
+        if subclass_of_Exception and not subclass_of_Warning:
+            expected['exception'] = expected_outcome
 
     # If a warning is issued, then there may also be an expected result.
 
     if isinstance(expected_outcome, tuple):
-        if len(expected_outcome) > 2:
-            raise RuntimeError
-        tuple_contains_warning = inspect.isclass(expected_outcome[1])
-        expected['warning'] = expected_outcome[1] if tuple_contains_warning else None
-        expected['result'] = expected_outcome[0] if tuple_contains_warning else None
-        if not tuple_contains_warning:
-            raise RuntimeError
+        if len(expected_outcome) > 2 or not inspect.isclass(expected_outcome[1]):
+            raise ValueError
+        if issubclass(expected_outcome[1], Warning):
+            expected['result'] = expected_outcome[0]
+            expected['warning'] = expected_outcome[1]
 
-    if not expected['exception'] and not expected['result']:
+        else:
+            raise ValueError
+
+    if expected['exception'] is None and expected['warning'] is None:
         expected['result'] = expected_outcome
 
-    expected['type'] = type(expected['result'])
+    expected['type'] = type(expected['result']) if expected['result'] is not None else None
+
+    for key in expected.keys():
+        print(f"expected[{key}] = {expected[key]}")
 
     # First we go through all of the possibilities for when an exception
     # is expected to be raised.  If no exception is raised, then we want
@@ -141,44 +152,41 @@ def run_test(f: Callable,
 
         exception = expected['exception']
 
-        print(exception)
-
         try:
-            result = f(*args, **kwargs)
+            result = func(*args, **kwargs)
         except exception:
             pass
         except Exception as exc:
             unexpected_exception = exc.__reduce__()[0]
-            raise UnexpectedExceptionError(
-                f"Running the command:\n\n"
-                f"  {call_str}\n\n"
-                f"did not raise {exc_str(exception)} as expected, "
-                f"but instead raised {exc_str(unexpected_exception)}.\n"
-            ) from exc
+            raise UnexpectedExceptionError(f"\nRunning the command:\n\n"
+                                           f"  {call_str}\n\n"
+                                           f"did not raise {exc_str(exception)} as expected, "
+                                           f"but instead raised {exc_str(unexpected_exception)}.\n") from exc
         else:
-            raise MissingExceptionError(
-                f"Running the command:\n\n"
-                f"  {call_str}\n\n"
-                f"did not raise a {exc_str(exception)} as "
-                f"expected, but instead returned the value:\n\n"
-                f"  {result}\n") from exc
+            raise MissingExceptionError(f"\nRunning the command:\n\n"
+                                        f"  {call_str}\n\n"
+                                        f"did not raise {exc_str(exception)} as "
+                                        f"expected, but instead returned the value:\n\n"
+                                        f"  {result}\n")
     else:
 
-        print(expected)
-
         try:
-            if expected['warning']:
-                with pytest.warns(expected['warning']):
-                    result = f(*args, **kwargs)
-            else:
-                result = f(*args, **kwargs)
+            with pytest.warns(expected['warning']):
+                result = func(*args, **kwargs)
         except pytest.raises.Exception as exc:
-            raise MissingWarningError(
-                f"Running the command:\n\n"
-                f"  {call_str}\n\n"
-                f"was supposed to issue a {exc_str(expected['warning'])}, "
-                f"but instead returned the value:\n\n"
-                f"  {result}\n") from exc
+            raise MissingWarningError(f"\nRunning the command:\n"
+                                      f"  {call_str}\n"
+                                      f"should issue {exc_str(expected['warning'])}, "
+                                      f"but instead returned the value:\n"
+                                      f"  {result}\n")
+        except Exception as exc:
 
-        if expected['result'] is not None and result != expected['result']:
-            raise RunTestError("")
+            raise UnexpectedExceptionError(f"Running the command {call_str} "
+                                           f"unexpectedly raised {exc_str(exc.__reduce__()[0])} "
+                                           f"instead of returning the expected value of {expected['result']}.")
+
+        if expected['result'] is not None:
+            assert result == expected['result'], (
+                f"Running the command {call_str} yielded a result of "
+                f"{result} instead of returning the expected value of "
+                f"{expected['result']}.")
