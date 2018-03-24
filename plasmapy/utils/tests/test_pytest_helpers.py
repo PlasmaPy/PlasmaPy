3c16615edf56bd3cd8013eeada1a25a7117c68be
Nick Murphy <namurphy@cfa.harvard.edu>
2018-03-23 22:03:06 -0400

    PEP8ify test helper tests

--- plasmapy/utils/tests/test_pytest_helpers.py
+++ plasmapy/utils/tests/test_pytest_helpers.py
@@ -357,8 +357,6 @@ def test_run_test_equivalent_calls(inputs, error):
 
 
 def test_run_test_equivalent_calls_types():
-
     run_test_equivalent_calls(return_arg, 1, 1.0, require_same_type=False)
-
     with pytest.raises(UnexpectedResultError):
         run_test_equivalent_calls(return_arg, 1, 1.0)
