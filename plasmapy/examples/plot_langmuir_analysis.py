faa7cb08ef485a9daa1b093a3ce8dde32f6aa23b
Dominik Stanczak <stanczakdominik@gmail.com>
2018-04-17 21:41:42 +0200

    Improve formatting of example

--- plasmapy/examples/plot_langmuir_analysis.py
+++ plasmapy/examples/plot_langmuir_analysis.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 """
-Analyzing Langmuir
+Langmuir probe data analysis
 =========================
 
 Let's analyze a few Langmuir probe characteristics using the
@@ -14,7 +14,6 @@ import numpy as np
 import os
 
 ######################################################
-
 # The first characteristic we analyze is a simple single-probe measurement in
 # a low (ion) temperature, low density plasma with a cylindrical probe. This
 # allows us to utilize OML theory implemented in `swept_probe_analysis`.
@@ -23,7 +22,7 @@ import os
 
 # Load the bias and current values stored in the .p pickle file.
 path = os.path.join("langmuir_samples", "Beckers2017.p")
-[bias, current] = pickle.load(open(path, 'rb'))
+bias, current = pickle.load(open(path, 'rb'))
 
 # Create the Characteristic object, taking into account the correct units
 characteristic = Characteristic(np.array(bias) * u.V,
@@ -35,6 +34,7 @@ probe_diameter = 1.57 * u.mm
 probe_area = (probe_length * np.pi * probe_diameter +
               np.pi * 0.25 * probe_diameter**2)
 
+######################################################
 # Perform the analysis. Since the plasma is in Helium a ion mass number of 4
 # is entered. The results are visualized and the obtained EEDF is also shown.
 print(swept_probe_analysis(characteristic,
@@ -42,12 +42,12 @@ print(swept_probe_analysis(characteristic,
                            visualize=True,
                            plot_EEDF=True))
 
+######################################################
 # The cyan and yellow lines indicate the fitted electron and ion currents,
 # respectively. The green line is the sum of these and agrees nicely with the
 # raw data. This indicates a succesfull analysis.
 
 ######################################################
-
 # The next characteristic is also obtained from a low relatively ion
 # temperature and density plasma, in Argon.
 
@@ -57,6 +57,7 @@ path = os.path.join("langmuir_samples", "Pace2015.p")
 characteristic = Characteristic(np.array(bias) * u.V,
                                 np.array(current) * 1e3 * u.mA)
 
+######################################################
 # Initially the electrons are assumed to be Maxwellian. To check this the fit
 # of the electron growth region will be plotted.
 swept_probe_analysis(characteristic,
@@ -65,6 +66,7 @@ swept_probe_analysis(characteristic,
                      bimaxwellian=False,
                      plot_electron_fit=True)
 
+######################################################
 # It can be seen that this plasma is slightly bi-Maxwellian, as there are two
 # distinct slopes in the exponential section. The analysis is now performed
 # with bimaxwellian set to True, which yields improved results.
@@ -75,6 +77,7 @@ print(swept_probe_analysis(characteristic,
                            visualize=True,
                            plot_electron_fit=True))
 
+######################################################
 # The probe current resolution of the raw data is relatively poor, but the
 # analysis still performs well in the ion current region. The bi-Maxwellian
 # properties are not significant but do make a difference. Check this analysis
@@ -84,7 +87,6 @@ print(swept_probe_analysis(characteristic,
 # close.
 
 ######################################################
-
 # This Helium plasma is fully bi-Maxwellian.
 
 # Import probe data and calculate probe surface area.
@@ -97,6 +99,7 @@ probe_diameter = 1.57 * u.mm
 probe_area = (probe_length * np.pi * probe_diameter +
               np.pi * 0.25 * probe_diameter**2)
 
+######################################################
 # `plot_electron_fit` is set to True to check the bi-Maxwellian properties.
 # The fit converges nicely to the two slopes of the electron growth region.
 print(swept_probe_analysis(characteristic,
