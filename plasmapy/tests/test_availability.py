b9c75e85a110f53e0f1302c52e0b23789f913a35
Nick Murphy <namurphy@cfa.harvard.edu>
2018-04-23 22:10:29 -0400

    Test consistency between requirements files
    I added several more tests to make sure that the requirements files
    are consistent with each other.  These tests may be picky, but if
    all the tests are passing then the files are very consistent.

--- plasmapy/tests/test_availability.py
+++ plasmapy/tests/test_availability.py
@@ -1,5 +1,6 @@
 """
-Test for common discrepancies and inconsistencies in requirements files.
+Test for common discrepancies and inconsistencies in code related to
+availability and testing.
 """
 
 import pytest
@@ -9,19 +10,19 @@ from ..utils import RunTestError
 from ..import_helpers import requirements_dir
 from os.path import dirname
 
-
 filenames = [
-            'automated-code-tests.txt',
-            'automated-documentation-tests.txt',
-            'environment.txt',
-            'environment.yml',
-            'requirements.txt',
+    'automated-code-tests.txt',
+    'automated-documentation-tests.txt',
+    'environment.txt',
+    'environment.yml',
+    'requirements.txt',
 ]
 
 Requirements = namedtuple('Requirements', [
     'file',
     'readlines',
     'lines',
+    'packages',
 ])
 
 files = {}
@@ -29,8 +30,24 @@ files = {}
 for filename in filenames:
     file = open(requirements_dir + filename, 'rt')
     readlines = file.readlines()
-    lines = {line.strip('\n') for line in readlines}
-    files[filename] = Requirements(file, readlines, lines)
+    lines = {line.strip('\n').rstrip(' ') for line in readlines}
+
+    if filename[-4:] == '.txt':
+
+        packages = {
+            line.split('(')[0].strip() if '(' in line else line.strip() for line in lines
+        }
+
+    elif filename[-4:] == '.yml':
+
+        packages = set()
+        for line in lines:
+            if ':' not in line and 'python' not in line and 'plasmapy' not in line and line != "":
+                packages |= {line.replace(' - ', '').strip()}
+    else:
+        packages = None
+
+    files[filename] = Requirements(file, readlines, lines, packages)
 
 
 @pytest.mark.parametrize('filename', filenames)
@@ -47,9 +64,59 @@ def test_requirements_issubset_automated_code_tests():
     """
     missing_lines = files['requirements.txt'].lines - files['automated-code-tests.txt'].lines
     if missing_lines:
-        raise RunTestError(
+        raise Exception(
             f"The following lines from requirements.txt are not in "
             f"automated-code-tests.txt: {missing_lines}")
+
+
+def test_requirements_issubset_automated_documentation_tests():
+    """
+    Test that all of the packages in requirements.txt are also in
+    automated-code-tests.txt.
+    """
+    missing_packages = \
+        files['requirements.txt'].packages - files['automated-documentation-tests.txt'].packages
+    if missing_packages:
+        raise Exception(
+            f"The following packages from requirements.txt are not in "
+            f"automated-documentation-tests.txt: {missing_packages}")
+
+
+def test_requirements_issubset_environment_txt():
+    """
+    Test that all of the packages in requirements.txt are also in
+    environment.txt.
+    """
+    missing_packages = files['requirements.txt'].packages - files['environment.txt'].packages
+    if missing_packages:
+        raise Exception(
+            f"The following packages from requirements.txt are missing "
+            f"from environment.txt: {missing_packages}")
+
+
+def test_requirements_issubset_environment_yml():
+    """
+    Test that all of the packages in requirements.yml are also in
+    environment.yml.
+    """
+    missing_packages = files['requirements.txt'].packages - files['environment.yml'].packages
+    if missing_packages:
+        raise Exception(
+            f"The following packages from requirements.txt are missing "
+            f" from environment.yml: {missing_packages}")
+
+
+def test__environment_txt_and_yml_files():
+    """
+    Test that environment.txt and environment.yml have the same
+    packages.
+    """
+    symmetric_difference = files['environment.txt'].packages ^ files['environment.yml'].packages
+    if symmetric_difference:
+        raise Exception(
+            f"The following packages are in one but not both of the "
+            f"environment.txt and environment.yml requirements files: "
+            f"{symmetric_difference}")
 
 
 def test_no_formatted_string_literals():
