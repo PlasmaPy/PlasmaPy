7d069185c127b8d1cc1e7f014b1442a980605435
Nick Murphy <namurphy@cfa.harvard.edu>
2018-04-23 20:59:15 -0400

    Use f-strings in test_import_helpers.py

--- plasmapy/tests/test_import_helpers.py
+++ plasmapy/tests/test_import_helpers.py
@@ -2,7 +2,6 @@
 
 import pytest
 import numpy
-
 from ..import_helpers import check_versions, split_version
 
 
@@ -12,10 +11,7 @@ def _create_distinct_versions(current_version, time):
 
     major, minor, *extra = split_version(current_version)
 
-    if len(extra) == 1:
-        patch = extra[0]
-    else:
-        patch = 0
+    patch = extra[0] if len(extra) == 1 else 0
 
     if time == 'older':
         increment = -1
@@ -23,28 +19,25 @@ def _create_distinct_versions(current_version, time):
         increment = +1
 
     different_versions = [
-        str(major) + '.' + str(minor + increment),
-        str(major + increment) + '.' + str(minor),
+        f"{major}.{minor + increment}",
+        f"{major + increment}.{minor}",
         ]
 
     if time == 'after' or patch > 0:
-        different_versions.append(
-            str(major) + '.' + str(minor) + '.' + str(patch + increment))
+        different_versions.append(f"{major}.{minor}.{patch + increment}")
 
     if time == 'before':
         different_versions.append(current_version)
-        different_versions.append(str(major) + '.' + str(minor) + '.0')
+        different_versions.append(f"{major}.{minor}.0")
 
     return different_versions
 
 
-@pytest.mark.parametrize('newer_version',
-                         _create_distinct_versions(numpy.__version__, 'newer'))
+@pytest.mark.parametrize('newer_version', _create_distinct_versions(numpy.__version__, 'newer'))
 def test_check_versions_newer(newer_version):
     """Test that check_versions will raise an ImportError when the
     minimum version for NumPy is newer than the current version.
     """
-
     newer_minimum_versions = {'numpy': newer_version}
     with pytest.raises(ImportError):
         check_versions(newer_minimum_versions)
@@ -60,7 +53,6 @@ def test_check_versions_older(older_version):
     """Test that check_versions will not raise an ImportError when the minimum
     version is the same as or older than the current version.
     """
-
     older_minimum_versions = {'numpy': older_version}
     try:
         check_versions(older_minimum_versions)
