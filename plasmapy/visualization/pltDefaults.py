ee77d473b705eacf1aa5f8b49db9c0d2e35e4d97
Pawel Marek Kozlowski <pawel@pmkozlowski.com>
2018-05-04 14:51:22 -0600

    Adding try-except blocks to catch missing fonts. Adding stix font as fallback setting.

--- plasmapy/visualization/pltDefaults.py
+++ plasmapy/visualization/pltDefaults.py
@@ -11,8 +11,32 @@ plotFont = {'family' : 'serif',
 plt.rc('font', **plotFont)
 plt.rc('lines', linewidth=2)
 
+
+validMathTextFonts = ['dejavusans',
+                      'dejavuserif',
+                      'cm',
+                      'stix',
+                      'stixsans',
+                      'custom']
+
 # setting latex rendered fonts to be same as regular fonts
-matplotlib.rcParams['mathtext.fontset'] = 'dejavuserif'
-matplotlib.rcParams['mathtext.rm'] = 'DejaVu Serif'
-matplotlib.rcParams['mathtext.it'] = 'DejaVu Serif:italic'
-matplotlib.rcParams['mathtext.bf'] = 'DejaVu Serif:bold'
+try:
+    matplotlib.rcParams['mathtext.fontset'] = 'dejavuserif'
+    matplotlib.rcParams['mathtext.rm'] = 'DejaVu Serif'
+    matplotlib.rcParams['mathtext.it'] = 'DejaVu Serif:italic'
+    matplotlib.rcParams['mathtext.bf'] = 'DejaVu Serif:bold'
+except:
+    print("Couldn't load dejavuserif fonts for plot defaults."
+          "Falling back to stix fonts.")
+    try:
+        matplotlib.rcParams['mathtext.fontset'] = 'stix'
+        matplotlib.rcParams['font.family'] = 'STIXGeneral'
+    except:
+        print("Couldn't load stix fonts for plot defaults.")
+
+
+# comparing fonts
+plt.title(r"cm-3 $\rm cm^{-3}$")
+plt.show()
+
+
