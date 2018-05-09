f1d97860a4b61f40a84d9dfedcf6e4015b218ced
Nick Murphy <namurphy@cfa.harvard.edu>
2018-05-09 13:35:20 -0400

    Create plasmapy._citation_bibtex private attribute
    I decided to put this as a private attribute rather than a dunder
    attribute like __citation__ because dunder notation should be reserved
    for dunder methods and private attribute notation should be used for
    library-specific things.

--- plasmapy/__init__.py
+++ plasmapy/__init__.py
@@ -15,13 +15,13 @@ __name__ = "plasmapy"
 __doc__ = ("A community-developed and community-driven open source "
            "core Python package for plasma physics.")
 
+
 class UnsupportedPythonError(Exception):
     pass
 
 
 if sys.version_info < tuple((int(val) for val in "3.6".split('.'))):
     raise UnsupportedPythonError("plasmapy does not support Python < {}".format(3.6))
-
 
 if not _ASTROPY_SETUP_:
     # For egg_info test builds to pass, put package imports here.
@@ -33,3 +33,22 @@ if not _ASTROPY_SETUP_:
     from . import physics
     from . import utils
 
+_citation_bibtex = """@misc{plasmapy_community_2018_1238132,
+  author       = {PlasmaPy Community and
+                  Murphy, Nicholas A. and
+                  Leonard, Andrew J. and
+                  Sta\'nczak, Dominik and
+                  Kozlowski, Pawel M. and
+                  Langendorf, Samuel J. and
+                  Haggerty, Colby C. and
+                  Beckers, Jasper P. and
+                  Mumford, Stuart J. and
+                  Parashar, Tulasi N. and
+                  Huang, Yi-Min},
+  title        = {{PlasmaPy: an open source community-developed 
+                   Python package for plasma physics}},
+  month        = apr,
+  year         = 2018,
+  doi          = {10.5281/zenodo.1238132},
+  url          = {https://doi.org/10.5281/zenodo.1238132}
+}"""
