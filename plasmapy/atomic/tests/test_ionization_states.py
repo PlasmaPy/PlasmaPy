a7973b7c109c5a97de6df478af241fb4975558e7
Nick Murphy <namurphy@cfa.harvard.edu>
2018-05-14 21:42:46 -0400

    Rework tests of IonizationStates

--- plasmapy/atomic/tests/test_ionization_states.py
+++ plasmapy/atomic/tests/test_ionization_states.py
@@ -2,9 +2,10 @@ import pytest
 import numpy as np
 import astropy.units as u
 from ..ionization_states import IonizationState, IonizationStates
-from ...utils import AtomicError, RunTestError, InvalidIsotopeError
+from ...utils import AtomicError, RunTestError, InvalidIsotopeError, run_test
 from ...atomic import (
     atomic_number,
+    mass_number,
     atomic_symbol,
     isotope_symbol,
     Particle,
@@ -251,20 +252,142 @@ class Test_IonizationStates:
 
     @classmethod
     def setup_class(cls):
-        cls.abundances = {'H': 1.0, 'He': 0.1}
 
-    def test_dict_inputs(self):
-        inputs = {'H': [0.1, 0.9], 'He': (0.2, 0.3, 0.5)}
-        instance = IonizationStates(inputs, abundances=self.abundances)
-        for key in inputs.keys():
-            expected = inputs[key]
-            actual = instance.ionic_fractions[key]
-            assert np.allclose(actual, expected), "Incorrect ionic fractions."
+        H_number_densities = np.array([0.1, 0.9]) * u.m ** -3
 
-    def test_dict_quantity_inputs(self):
-        inputs = {'He': np.array([1, 9]) * u.m ** -3, 'H': np.array([2, 3, 5]) * u.cm ** -3}
-        instance = IonizationStates(inputs, abundances=self.abundances)
-        for key in inputs.keys():
-            expected = inputs[key].value / np.sum(inputs[key].value)
-            actual = instance.ionic_fractions[key]
-            assert np.allclose(actual, expected), "Incorrect ionic fractions."
+        cls.tests = {
+
+            'basic': {
+                'inputs': {'H': [0.1, 0.9], 'helium': (0.2, 0.3, 0.5)},
+                'T_e': 1e6 * u.K,
+                'tol': 1e-15,
+                'abundances': {'hydrogen': 1.0, 'He': 0.1},
+            },
+
+            'quantities': {
+                'inputs': {'H': H_number_densities},
+                'abundances': {'H': 1.0}
+            },
+
+            'just H': {
+                'inputs': {'H': [0.1, 0.9]},
+            }
+
+        }
+
+        cls.instances = {}
+
+    tests = ['basic', 'quantities', 'just H']
+
+    @pytest.mark.parametrize('test', tests)
+    def test_instantiation(self, test):
+        try:
+            self.instances[test] = IonizationStates(**self.tests[test])
+        except Exception as exc:
+            raise AtomicError(
+                f"Cannot create IonizationStates instance for "
+                f"test='{test}'") from exc
+
+    @pytest.mark.parametrize('test', tests)
+    def test_keys(self, test):
+        input_keys = self.tests[test]['inputs'].keys()
+        particles = [Particle(input_key) for input_key in input_keys]
+        expected_keys = [p.particle for p in particles]
+        actual_keys = [key for key in self.instances[test].ionic_fractions.keys()]
+        elements = self.instances[test].elements
+
+        assert actual_keys == expected_keys == elements, (
+            f"For test='{test}', the following should be equal:\n"
+            f"  actual_keys = {actual_keys}\n"
+            f"expected_keys = {expected_keys}\n"
+            f"     elements = {elements}"
+        )
+
+    @pytest.mark.parametrize('test', tests)
+    def test_element_sorting(self, test):
+        elements = self.instances[test].elements
+        before_sorting = []
+        for element in elements:
+            atomic_numb = atomic_number(element)
+            try:
+                mass_numb = mass_number(element)
+            except InvalidIsotopeError:
+                mass_numb = 0
+            before_sorting.append((atomic_numb, mass_numb))
+        after_sorting = sorted(before_sorting)
+
+        assert before_sorting == after_sorting, (
+            f"Elements/isotopes are not sorted for test='{test}':\n" 
+            f"  before_sorting = {before_sorting}\n"
+            f"   after_sorting = {after_sorting}\n"
+            f"where above is (atomic_number, mass_number if isotope else 0)")
+
+    @pytest.mark.parametrize('test', tests)
+    def test_ionic_fractions(self, test):
+
+        errmsg = ""
+
+        elements = self.instances[test].elements
+        input_keys = self.tests[test]["inputs"].keys()
+        for element, input_key in zip(elements, input_keys):
+
+            expected = np.array(self.tests[test]["inputs"][input_key])
+
+            if isinstance(expected, u.Quantity):
+                expected = np.array(expected.value / np.sum(expected.value))
+
+            #if not isinstance(expected, np.ndarray)
+
+            actual = self.instances[test].ionic_fractions[element]
+
+            if not np.allclose(actual, expected):
+                errmsg += (
+                    f"\n\nThere is a discrepancy in ionic fractions for "
+                    f"({test}, {element}, {input_key})\n"
+                    f"  expected = {expected}\n"
+                    f"    actual = {actual}")
+            if not isinstance(actual, np.ndarray) or isinstance(actual, u.Quantity):
+                raise AtomicError(
+                    f"\n\nNot a numpy.ndarray: ({test}, {element})")
+
+        if errmsg:
+            raise AtomicError(errmsg)
+
+
+#    @pytest.mark.parametrize('key', ['H', 'helium'])
+#    def test_get_item(self, key):
+
+#        try:
+#            actual = self.instance[key]
+#        except Exception as exc:
+#            raise AtomicError(f"Unable to get {key} in IonizationStates instance.")
+
+#        if not isinstance(actual, IonizationState):
+#            raise TypeError(f"Not getting an IonizationState instance for key='{key}'.")
+
+#        expected = self.inputs[key]
+
+#        assert np.allclose(actual.ionic_fractions, expected), (
+#            f"The resulting ionic fractions of {actual.ionic_fractions} "
+#            f"do not equal the expected values of {expected}")
+
+IE = collections.namedtuple("IE", ["inputs", "expected_exception"])
+
+tests_for_exceptions = {
+    'wrong type': IE({"inputs": None}, TypeError),
+    'not normalized': IE({"inputs": {'He': [0.4, 0.5, 0.0]}, "tol": 1e-9}, AtomicError),
+    'negative ionfrac': IE({"inputs": {'H': [-0.1, 1.1]}}, AtomicError),
+    'ion': IE({"inputs": {'H': [0.1, 0.9], 'He+': [0.0, 0.9, 0.1]}}, AtomicError),
+}
+
+@pytest.mark.parametrize('test', tests_for_exceptions.keys())
+def test_execeptions(test):
+    """
+    Test that appropriate exceptions are raised for inappropriate inputs
+    to IonizationStates.
+    """
+    run_test(
+        IonizationStates,
+        kwargs=tests_for_exceptions[test].inputs,
+        expected_outcome=tests_for_exceptions[test].expected_exception,
+    )
