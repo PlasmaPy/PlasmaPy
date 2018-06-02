2d5ee51d000944e4d09f627d17d78759cd5bed96
Dominik Stańczak <stanczakdominik@gmail.com>
2018-06-02 14:09:39 +0200

    Add force drift

--- plasmapy/physics/drifts.py
+++ plasmapy/physics/drifts.py
@@ -19,7 +19,35 @@ def ExB_drift(E: u.V/u.m, B: u.T) -> u.m/u.s:
     v: ~astropy.units.Quantity
         Velocity, in m/s
 
+    References
+    ----------
+    Bellan, Fundamentals of Plasma Physics, 3.57
+
     """
+
     # np.cross drops units right now, thus this hack
     cross = np.cross(E.si.value, B.si.value) * E.unit * B.unit
     return (cross / (B*B).sum(-1)).to(u.m/u.s)
+
+@utils.check_quantity(F={'units':u.N}, B={'units':u.T}, q={'units':u.C})
+def force_drift(F: u.N, B: u.T, q: u.C):
+    """
+
+    Parameters
+    ----------
+    F :
+    B :
+    q :
+
+    Returns
+    -------
+    v: ~astropy.units.Quantity
+        Velocity, in m/s
+
+    References
+    ----------
+    Bellan, Fundamentals of Plasma Physics, 3.58
+
+    """
+    cross = np.cross(F.si.value, B.si.value) * F.unit * B.unit
+    return (cross / (q * (B*B).sum(-1))).to(u.m/u.s)
--- plasmapy/physics/tests/test_drifts.py
+++ plasmapy/physics/tests/test_drifts.py
@@ -30,3 +30,36 @@ class Test_ExB_drift:
         result = drifts.ExB_drift(2*E, 3*B)
         assert_quantity_allclose(result, (2/3)*u.Quantity([[[0,0,1]]],
                                                           unit=u.m/u.s))
+
+
+class Test_force_drift:
+    def test_force_x_B_1d_arrays(self):
+        F = u.Quantity([1,0,0], unit=u.N)
+        B = u.Quantity([0,1,0], unit=u.T)
+        q = 1*u.C
+        result = drifts.force_drift(2*F, 3*B, q)
+        assert_quantity_allclose(result, (2/3)*u.Quantity([0,0,1], u.m/u.s))
+
+    def test_force_B_2d_array(self):
+        F = u.Quantity([[1,0,0],
+                        [1,0,0],
+                        [1,0,0]], unit=u.N)
+        B = u.Quantity([[0,1,0],
+                        [0,1,0],
+                        [0,1,0]], unit=u.T)
+        q = 1*u.C
+
+        result = drifts.force_drift(2*F, 3*B, q)
+        assert_quantity_allclose(result, (2/3)*u.Quantity([[0,0,1],
+                                                           [0,0,1],
+                                                           [0,0,1],
+                                                           ], unit=u.m/u.s))
+
+    def test_force_B_3d_array(self):
+        F = u.Quantity([[[1,0,0]]], unit=u.N)
+        B = u.Quantity([[[0,1,0]]], unit=u.T)
+        q = 1*u.C
+
+        result = drifts.force_drift(2*F, 3*B, q)
+        assert_quantity_allclose(result, (2/3)*u.Quantity([[[0,0,1]]],
+                                                          unit=u.m/u.s))
