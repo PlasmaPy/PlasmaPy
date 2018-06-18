c33796bfa5e67d1c8dca3eb22220f32cf45db367
ritiek <ritiekmalhotra123@gmail.com>
2018-06-18 19:27:08 +0530

    Fetch data from HDF5 when accessed

--- plasmapy/classes/sources/openpmd_hdf5.py
+++ plasmapy/classes/sources/openpmd_hdf5.py
@@ -1,9 +1,28 @@
 import h5py
+import numpy as np
+import astropy.units as u
 
 from plasmapy.classes import GenericPlasma
 from plasmapy.utils import OpenPMDError
 
 import os
+
+
+class _ElectricField:
+    def __init__(self, E):
+        self.E = E
+
+    @property
+    def x(self):
+        return np.array(self.E['x']) * u.V / u.m
+
+    @property
+    def y(self):
+        return np.array(self.E['y']) * u.V / u.m
+
+    @property
+    def z(self):
+        return np.array(self.E['z']) * u.V / u.m
 
 
 class HDF5Reader(GenericPlasma):
@@ -20,19 +39,33 @@ class HDF5Reader(GenericPlasma):
         if not openPMD:
             raise OpenPMDError("Input HDF5 file does not go on with "
                                "standards defined by OpenPMD")
+
+        self.subname = tuple(h5['data'])[0]
         self.h5 = h5
 
     @property
     def electric_field(self):
-        raise NotImplementedError
+        path = 'data/' + self.subname + '/fields/E'
+        if path in self.h5:
+            return _ElectricField(self.h5[path])
+        else:
+            raise AttributeError
 
     @property
     def charge_density(self):
-        raise NotImplementedError
+        path = 'data/' + self.subname + '/fields/rho'
+        if path in self.h5:
+            return np.array(self.h5[path]) * u.C / u.m**3
+        else:
+            raise AttributeError
 
     @property
     def x(self):
-        raise NotImplementedError
+        path = 'data/' + self.subname + '/fields/E/x'
+        if path in self.h5:
+            return self.h5[path]
+        else:
+            raise AttributeError
 
     @property
     def y(self):
@@ -40,7 +73,11 @@ class HDF5Reader(GenericPlasma):
 
     @property
     def z(self):
-        raise NotImplementedError
+        path = 'data/' + self.subname + '/fields/E/z'
+        if path in self.h5:
+            return self.h5[path]
+        else:
+            raise AttributeError
 
     @classmethod
     def is_datasource_for(cls, **kwargs):
