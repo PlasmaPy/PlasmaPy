2c50465ff73823871943bc0dbcfe64e4a2edb824
Nick Murphy <namurphy@cfa.harvard.edu>
2018-07-16 17:21:46 -0400

    Add roman.py from roman package into utils
    This file is copyright 2001 by Mark Pilgrim and is able to be
    modified and redistributed under the Python 2.1.1 license.

---
+++ plasmapy/utils/roman/__init__.py
@@ -0,0 +1,10 @@
+"""Utilities to convert integers to Roman numerals and vice versa."""
+
+from .roman import (
+    toRoman,
+    fromRoman,
+    RomanError,
+    OutOfRangeError,
+    NotIntegerError,
+    InvalidRomanNumeralError,
+)
