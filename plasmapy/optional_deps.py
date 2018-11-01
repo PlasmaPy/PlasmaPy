00a2de4cc3dc8e464716b3a47b912d1521c3f09a
Dominik Stańczak <stanczakdominik@gmail.com>
2018-11-01 08:30:39 +0100

    Review fixes for optional dependencies
    * add conda_channel setting to import template
    * make the template name more descriptive
    * add message about link rot

--- plasmapy/optional_deps.py
+++ plasmapy/optional_deps.py
@@ -1,9 +1,12 @@
 """ Useful error messages for optional dependencies that aren't found. """
 from typing import Optional
 
-def _template(pkgname: str,
-              url: str,
-              library: Optional[str] = None) -> ImportError:
+
+def _optional_import_error_template(pkgname: str,
+                                    url: str,
+                                    library: Optional[str] = None,
+                                    conda_channel: Optional[str] = None,
+                                    ) -> ImportError:
     """
     Simple template to prevent text duplication.
 
@@ -15,6 +18,8 @@ def _template(pkgname: str,
         Link to install page for given package
     library : str (optional)
         Full package name
+    conda_channel: str (optional)
+        set to "conda-forge" to add -c conda-forge to the conda install line
 
     Returns
     -------
@@ -22,29 +27,35 @@ def _template(pkgname: str,
 
     """
     library = pkgname if library is None else library
+    conda_channel = f"-c {conda_channel} " if conda_channel is not None else ""
+
     template = f"""
     {library} could not be found. Try either
 
-        conda install {pkgname}
+        conda install {conda_channel}{pkgname}
         
     on Anaconda environments or
         
         pip install {pkgname}
 
     in general. In case of trouble refer to
-    {url}"""
+    
+        {url} 
+    
+    (link active as of 2018.10.31 - please report dead links on GitHub!)"""
 
     return ImportError(template)
 
-h5py_import_error = _template("h5py",
+
+h5py_import_error = _optional_import_error_template("h5py",
                               "http://docs.h5py.org/en/latest/build.html")
 
-mpl_import_error = _template("matplotlib",
+mpl_import_error = _optional_import_error_template("matplotlib",
                              "https://matplotlib.org/users/installing.html")
 
-mpmath_import_error = _template("mpmath",
+mpmath_import_error = _optional_import_error_template("mpmath",
                                 "http://mpmath.org/doc/current/setup.html#download-and-installation")
 
-lmfit_import_error = _template("lmfit",
+lmfit_import_error = _optional_import_error_template("lmfit",
                                "https://lmfit.github.io/lmfit-py/installation.html")
 
