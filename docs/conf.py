d19b54bf5f90896e9c37c4c8eacf097044ac3c83
Nick Murphy <namurphy@cfa.harvard.edu>
2018-12-31 15:03:00 -0500

    Update copyright years in licenses

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'PlasmaPy'
-copyright = '2015-2018, PlasmaPy Community'
+copyright = '2015-2019, PlasmaPy Community'
 author = 'PlasmaPy Community'
 
 # The version info for the project you're documenting, acts as replacement for
