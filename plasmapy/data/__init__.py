acfc5273b36f27e6dc3f199f828d9734ab45f132
Dominik Stańczak <stanczakdominik@gmail.com>
2019-03-16 15:14:12 +0100

    Add note on source of test files

---
+++ plasmapy/data/__init__.py
@@ -0,0 +1,6 @@
+"""Data used for testing PlasmaPy, example datasets, etc.
+
+- test/data*.h5 files come from
+  https://github.com/openPMD/openPMD-example-datasets/tree/b4f87b817629b99a048026a2724a5b265810d8be
+
+"""
