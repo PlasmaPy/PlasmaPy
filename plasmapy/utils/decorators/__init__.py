131a3a75c9f98a8b37bcf761c87c49456523ffba
rocco8773 <eteverson@gmail.com>
2019-06-11 16:52:45 -0700

    remove import of helpers module

--- plasmapy/utils/decorators/__init__.py
+++ plasmapy/utils/decorators/__init__.py
@@ -3,5 +3,4 @@ A module to contain various decorators used to build readable and useful code.
 """
 __all__ = ['preserve_signature']
 
-from . import helpers
 from .helpers import preserve_signature
