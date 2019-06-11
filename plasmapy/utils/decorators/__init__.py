d83b0a52529adca7ee4e3ffb45278c49e4eab19a
rocco8773 <eteverson@gmail.com>
2019-06-11 16:28:08 -0700

    add `preserve_signature` to decorator package level imports

--- plasmapy/utils/decorators/__init__.py
+++ plasmapy/utils/decorators/__init__.py
@@ -1,3 +1,7 @@
 """
 A module to contain various decorators used to build readable and useful code.
 """
+__all__ = ['preserve_signature']
+
+from . import helpers
+from .helpers import preserve_signature
