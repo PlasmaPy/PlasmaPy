6d331a54447793ca01a532ea83649be05ad9dd9c
rocco8773 <eteverson@gmail.com>
2019-06-14 17:03:09 -0700

    add `check_relativistic` and `check_quantity` to package level namespace of plasmapy.utils.decorators

--- plasmapy/utils/decorators/__init__.py
+++ plasmapy/utils/decorators/__init__.py
@@ -1,6 +1,7 @@
 """
 A module to contain various decorators used to build readable and useful code.
 """
-__all__ = ['preserve_signature']
+__all__ = ['check_relativistic', 'check_quantity', 'preserve_signature']
 
 from .helpers import preserve_signature
+from .checks import (check_relativistic, check_quantity)
