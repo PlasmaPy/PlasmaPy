88620f8ecc459cc8c0bcaafb4742f94041e3699d
rocco8773 <eteverson@gmail.com>
2019-06-18 16:28:19 -0700

    add decorator `check_values` to `plasmapy.utils.decorators` package level imports

--- plasmapy/utils/decorators/__init__.py
+++ plasmapy/utils/decorators/__init__.py
@@ -1,7 +1,8 @@
 """
 A module to contain various decorators used to build readable and useful code.
 """
-__all__ = ['check_relativistic', 'check_quantity', 'preserve_signature']
+__all__ = ['check_relativistic', 'check_quantity', 'check_values',
+           'preserve_signature']
 
 from .helpers import preserve_signature
-from .checks import (check_relativistic, check_quantity)
+from .checks import (check_relativistic, check_quantity, check_values)
