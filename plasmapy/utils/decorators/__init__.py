826c32009e76a0399826d655f95b526fa8d328f4
rocco8773 <eteverson@gmail.com>
2019-06-19 16:34:52 -0700

    expose `CheckValues` to plasmapy.utils.decorators package level imports

--- plasmapy/utils/decorators/__init__.py
+++ plasmapy/utils/decorators/__init__.py
@@ -2,7 +2,7 @@
 A module to contain various decorators used to build readable and useful code.
 """
 __all__ = ['check_relativistic', 'check_quantity', 'check_values',
-           'preserve_signature']
+           'preserve_signature', 'CheckValues']
 
 from .helpers import preserve_signature
-from .checks import (check_relativistic, check_quantity, check_values)
+from .checks import (check_relativistic, check_quantity, check_values, CheckValues)
