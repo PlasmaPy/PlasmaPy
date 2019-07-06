9473c3ab7440c77008bb3f7f1a6b9868c0c2f000
rocco8773 <eteverson@gmail.com>
2019-07-06 12:39:39 -0700

    add `CheckUnits` to module level imports of plasmapy.utils.decorators and to the __all__

--- plasmapy/utils/decorators/__init__.py
+++ plasmapy/utils/decorators/__init__.py
@@ -2,7 +2,13 @@
 A module to contain various decorators used to build readable and useful code.
 """
 __all__ = ['check_relativistic', 'check_quantity', 'check_values',
-           'preserve_signature', 'CheckValues']
+           'preserve_signature', 'CheckUnits', 'CheckValues']
 
 from .helpers import preserve_signature
-from .checks import (check_relativistic, check_quantity, check_values, CheckValues)
+from .checks import (
+    check_relativistic,
+    check_quantity,
+    check_values,
+    CheckUnits,
+    CheckValues,
+)
