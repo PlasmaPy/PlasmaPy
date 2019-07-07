a2a87768cc749e95f9c968a7353d80d220cfe99b
rocco8773 <eteverson@gmail.com>
2019-07-06 17:07:09 -0700

    add module level import of `ValidateQuantities` to plasmapy.utils.decorators; include in __all__

--- plasmapy/utils/decorators/__init__.py
+++ plasmapy/utils/decorators/__init__.py
@@ -2,7 +2,7 @@
 A module to contain various decorators used to build readable and useful code.
 """
 __all__ = ['check_relativistic', 'check_quantity', 'check_values',
-           'preserve_signature', 'CheckUnits', 'CheckValues']
+           'preserve_signature', 'CheckUnits', 'CheckValues', 'ValidateQuantities']
 
 from .helpers import preserve_signature
 from .checks import (
@@ -12,3 +12,4 @@ from .checks import (
     CheckUnits,
     CheckValues,
 )
+from .validators import ValidateQuantities
