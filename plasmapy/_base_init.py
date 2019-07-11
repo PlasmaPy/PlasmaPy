cf6e6920b477eb4e280e37594a37d73dd10c60ae
Dominik Stańczak <stanczakdominik@gmail.com>
2019-07-11 09:23:58 +0200

    Remove more unnecessary stuff

--- plasmapy/_base_init.py
+++ plasmapy/_base_init.py
@@ -3,57 +3,39 @@
 __all__ = ['__version__', '__githash__']
 
 # this indicates whether or not we are in the package's setup.py
-try:
-    _ASTROPY_SETUP_
-except NameError:
-    from sys import version_info
-    if version_info[0] >= 3:
-        import builtins
-    else:
-        import __builtin__ as builtins
-    builtins._ASTROPY_SETUP_ = False
+from sys import version_info
+import builtins
 
-try:
-    from .version import version as __version__
-except ImportError:
-    __version__ = ''
-try:
-    from .version import githash as __githash__
-except ImportError:
-    __githash__ = ''
+from .version import version as __version__
+from .version import githash as __githash__
 
 
-if not _ASTROPY_SETUP_:  # noqa
-    import os
-    from warnings import warn
-    from astropy.config.configuration import (
-        update_default_config,
-        ConfigurationDefaultMissingError,
-        ConfigurationDefaultMissingWarning)
+import os
+from warnings import warn
+from astropy.config.configuration import (
+    update_default_config,
+    ConfigurationDefaultMissingError,
+    ConfigurationDefaultMissingWarning)
 
-    # Create the test function for self test
-    from astropy.tests.helper import TestRunner
-    test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
-    __all__ += ['test']
+# Create the test function for self test
+from astropy.tests.helper import TestRunner
+test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
+__all__ += ['test']
 
-    # add these here so we only need to cleanup the namespace at the end
-    config_dir = None
-
-    if not os.environ.get('ASTROPY_SKIP_CONFIG_UPDATE', False):
-        config_dir = os.path.dirname(__file__)
-        config_template = os.path.join(config_dir, __package__ + ".cfg")
-        if os.path.isfile(config_template):
-            try:
-                update_default_config(
-                    __package__, config_dir, version=__version__)
-            except TypeError as orig_error:
-                try:
-                    update_default_config(__package__, config_dir)
-                except ConfigurationDefaultMissingError as e:
-                    wmsg = (e.args[0] +
-                            " Cannot install default profile. If you are "
-                            "importing from source, this is expected.")
-                    warn(ConfigurationDefaultMissingWarning(wmsg))
-                    del e
-                except Exception:
-                    raise orig_error
+config_dir = os.path.dirname(__file__)
+config_template = os.path.join(config_dir, __package__ + ".cfg")
+if os.path.isfile(config_template):
+    try:
+        update_default_config(
+            __package__, config_dir, version=__version__)
+    except TypeError as orig_error:
+        try:
+            update_default_config(__package__, config_dir)
+        except ConfigurationDefaultMissingError as e:
+            wmsg = (e.args[0] +
+                    " Cannot install default profile. If you are "
+                    "importing from source, this is expected.")
+            warn(ConfigurationDefaultMissingWarning(wmsg))
+            del e
+        except Exception:
+            raise orig_error
