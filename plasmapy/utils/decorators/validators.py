19f8df7a317cef50cd7bf9f88281af220fcb1dd2
rocco8773 <eteverson@gmail.com>
2019-07-15 17:21:33 -0700

    `ValidateQuantities`: inherit from `CheckValues` and `CheckUnits`; implement value checking; all for validation of the wrapped function's returned variable;

--- plasmapy/utils/decorators/validators.py
+++ plasmapy/utils/decorators/validators.py
@@ -10,11 +10,11 @@ import warnings
 from astropy import units as u
 from plasmapy.utils.decorators.checks import (CheckValues, CheckUnits)
 from plasmapy.utils.decorators.helpers import preserve_signature
-from plasmapy.utils.exceptions import ImplicitUnitConversionWarning
+from plasmapy.utils.exceptions import (ImplicitUnitConversionWarning, PlasmaPyWarning)
 from typing import (Any, Dict, List, Union)
 
 
-class ValidateQuantities(CheckUnits):
+class ValidateQuantities(CheckUnits, CheckValues):
     """
     A decorator class to "validate" (i.e. control convert) the units (and values) of
     input/output arguments to a function.  (Validating of function arguments `*args`
@@ -22,11 +22,23 @@ class ValidateQuantities(CheckUnits):
 
     """
 
-    def __init__(self, equivalencies: Union[None, List] = None,
-                 **validations: Dict[str, Any]):
+    def __init__(self, validations_on_return=None, **validations: Dict[str, Any]):
 
-        super().__init__(equivalencies, **validations)
+        checks_on_return = validations.pop('checks_on_return', None)
+        if checks_on_return is not None:
+            if 'checks_on_return' in validations:
+                raise TypeError(f"keyword argument 'checks_on_return' is not allowed, "
+                                f"use 'validations_on_return' to set validations "
+                                f"on the return variable")
+
         self._validations = validations
+
+        checks = validations.copy()
+        if validations_on_return is not None:
+            self._validations['validations_on_return'] = validations_on_return
+            checks['checks_on_return'] = validations_on_return
+
+        super().__init__(**checks)
 
     def __call__(self, f):
         self.f = f
@@ -39,38 +51,87 @@ class ValidateQuantities(CheckUnits):
             bound_args = wrapped_sign.bind(*args, **kwargs)
             bound_args.apply_defaults()
 
-            # check and convert units
-            unit_validations = self._get_unit_validations(bound_args)
+            # get conditioned validations
+            validations = self._get_validations(bound_args)
 
-            for arg_name in unit_validations:
-                arg = self._validate_unit(bound_args.arguments[arg_name],
-                                          arg_name,
-                                          **unit_validations[arg_name])
+            # validate (input) argument units and values
+            for arg_name in validations:
+                # skip check of output/return
+                if arg_name == 'checks_on_return':
+                    continue
+
+                # validate argument & update for conversion
+                arg = self._validate_quantity(bound_args.arguments[arg_name],
+                                              arg_name,
+                                              **validations[arg_name])
                 bound_args.arguments[arg_name] = arg
 
-            return CheckValues(**self._validations)(f)(**bound_args.arguments)
+            # call function
+            _return = f(**bound_args.arguments)
+
+            # validate output
+            if 'checks_on_return' in validations:
+                _return = self._validate_quantity(_return, 'checks_on_return',
+                                                  **validations['checks_on_return'])
+
+            return _return
         return wrapper
 
-    def _get_unit_validations(
+    def _get_validations(
             self,
             bound_args: inspect.BoundArguments) -> Dict[str, Dict[str, Any]]:
         """
         Blah
         """
-        unit_validations = self._get_checks(bound_args)
+        unit_checks = self._get_unit_checks(bound_args)
+        value_checks = self._get_value_checks(bound_args)
 
-        # some extra conditioning
-        for arg_name in unit_validations:
+        # ensure both dictionaries have the exact same keys
+        # key_set = set(list(value_validations.keys()) + list(validations))
+        # if len(key_set - set(value_validations.keys())) != 0 \
+        #         or len(key_set - set(validations.keys())) != 0:
+        #     raise TypeError
+
+        # combine all validations
+        key_set = set(list(unit_checks.keys()) + list(value_checks.keys()))
+        validations = {}
+        for arg_name in key_set:
+            # add unit checks to validations
+            try:
+                validations[arg_name] = unit_checks[arg_name].copy()
+            except KeyError:
+                # no unit checks were defined, but value checks were
+                validations[arg_name] = value_checks[arg_name]
+
+                # TODO: improve warning string
+                warnings.warn(u.UnitsWarning(f"No units defined for argument"))
+
+                # jump to next for-loop iteration
+                continue
+
             # augment 'none_shall_pass' (if needed)
             try:
-                unit_validations[arg_name]['none_shall_pass'] = \
+                # if 'none_shall_pass' was in the original passed-in validations,
+                # then override the value determined by CheckUnits
+                validations[arg_name]['none_shall_pass'] = \
                     self._validations[arg_name]['none_shall_pass']
             except KeyError:
+                # 'none_shall_pass' was not in the original passed-in validations, so
+                # rely on the value determined by CheckUnits
+                pass
+            else:
+                del value_checks[arg_name]['none_shall_pass']
+
+            # update the validations dictionary
+            try:
+                validations[arg_name].update(value_checks[arg_name])
+            except KeyError:
+                # no value checks were defined
                 pass
 
-        return unit_validations
+        return validations
 
-    def _validate_unit(self, arg, arg_name, **validations: Dict[str, Any]):
+    def _validate_quantity(self, arg, arg_name, **validations: Dict[str, Any]):
 
         # initialize TypeError message
         typeerror_msg = (
@@ -87,7 +148,9 @@ class ValidateQuantities(CheckUnits):
         # add units to arg if possible
         # * a None value will be taken care of by `_check_unit`
         #
-        if arg is not None and not isinstance(arg, u.Quantity):
+        if 'units' not in validations:
+            pass
+        elif arg is not None and not isinstance(arg, u.Quantity):
             if len(validations['units']) != 1:
                 raise TypeError(typeerror_msg)
             else:
@@ -103,24 +166,30 @@ class ValidateQuantities(CheckUnits):
                         raise TypeError(typeerror_msg)
 
                     warnings.warn(u.UnitsWarning(
-                        f"No units are specified for {arg_name} = {arg} "
-                        f"in {self.f.__name__}. Assuming units of "
+                        f"No units are specified for {arg_name} = {arg.value} "
+                        f"in {self.f.__name__}(). Assuming units of "
                         f"{validations['units'][0]}. To silence this warning, "
                         f"explicitly pass in an astropy Quantity "
                         f"(e.g. 5. * astropy.units.cm) "
                         f"(see http://docs.astropy.org/en/stable/units/)"
                     ))
 
-        # check units
-        arg, unit, equiv = self._check_unit(arg, arg_name, **validations)
+        if 'units' in validations:
+            # check units
+            arg, unit, equiv = self._check_unit(arg, arg_name, **validations)
 
-        # convert quantity
-        if not validations['pass_equivalent_units']:
-            if arg.unit != unit:
-                arg.to(unit, equivalencies=equiv)
+            # convert quantity
+            if arg is None:
+                pass
+            elif unit is not None and unit != arg.unit:
+                arg = arg.to(unit, equivalencies=equiv)
 
                 # if non-standard conversion then warn
                 if equiv is not None:
                     warnings.warn(ImplicitUnitConversionWarning)
+
+        # check value
+        if all([key in validations for key in self._CheckValues__check_defaults]):
+            self._check_value(arg, arg_name, **validations)
 
         return arg
