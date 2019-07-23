3cbeca65abcca2c19ddd289ee09a823b4d078feb
rocco8773 <eteverson@gmail.com>
2019-07-23 15:53:07 -0700

    still trying to get TravisCI to see ImplicitUnitConversionWarning within ValidateQuantities._validate_quantity

--- plasmapy/utils/decorators/validators.py
+++ plasmapy/utils/decorators/validators.py
@@ -167,10 +167,10 @@ class ValidateQuantities(CheckUnits, CheckValues):
         # convert quantity
         if arg is not None and unit is not None:
             # if non-standard conversion then warn
-            if not arg.unit.is_equivalent(unit):
-                warnings.warn(ImplicitUnitConversionWarning(
-                    f"{err_msg} has a non-standard unit conversion..."
-                    f"converting {arg.unit} to {unit}"))
+            # if not arg.unit.is_equivalent(unit):
+            warnings.warn(ImplicitUnitConversionWarning(
+                f"{err_msg} has a non-standard unit conversion..."
+                f"converting {arg.unit} to {unit}"))
 
             arg = arg.to(unit, equivalencies=equiv)
         elif err is not None:
