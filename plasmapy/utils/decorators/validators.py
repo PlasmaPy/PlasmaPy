de6a86440ff29a1d37c80f13363d9850ef5bb7a6
rocco8773 <eteverson@gmail.com>
2019-07-23 16:55:13 -0700

    the different astropy unit registries was causing the discrepancy between tests locally vs TravisCI...setting equivalencies=None to avoid the discrepancy

--- plasmapy/utils/decorators/validators.py
+++ plasmapy/utils/decorators/validators.py
@@ -167,14 +167,10 @@ class ValidateQuantities(CheckUnits, CheckValues):
         # convert quantity
         if arg is not None and unit is not None:
             # if non-standard conversion then warn
-            # if not arg.unit.is_equivalent(unit):
-            is_standard = arg.unit.is_equivalent(unit, equivalencies=None)
-            warnings.warn(u.UnitsWarning(
-                f"The unit conversion is standard: {is_standard}...{arg.unit} to {unit}"))
-            # if True:
-            #     warnings.warn(ImplicitUnitConversionWarning(
-            #         f"{err_msg} has a non-standard unit conversion..."
-            #         f"converting {arg.unit} to {unit}"))
+            if not arg.unit.is_equivalent(unit, equivalencies=None):
+                warnings.warn(ImplicitUnitConversionWarning(
+                    f"{err_msg} has a non-standard unit conversion..."
+                    f"converting {arg.unit} to {unit}"))
 
             arg = arg.to(unit, equivalencies=equiv)
         elif err is not None:
