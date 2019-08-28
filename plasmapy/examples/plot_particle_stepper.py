88acb97b3a461483d6572f5adea244a1bdcc9ccc
Dominik Stańczak <stanczakdominik@gmail.com>
2019-08-28 19:45:52 +0200

    PEP8

--- plasmapy/examples/plot_particle_stepper.py
+++ plasmapy/examples/plot_particle_stepper.py
@@ -36,7 +36,7 @@ plasma.electric_field[1, :, :, :] = np.ones((10, 10, 10)) * E0
 # Calculate the timestep. We'll take one proton `p`, take its gyrofrequency, invert that
 # to get to the gyroperiod, and resolve that into 10 steps for higher accuracy.
 
-freq = gyrofrequency(B0, 'p').to(u.Hz, equivalencies = u.dimensionless_angles())
+freq = gyrofrequency(B0, 'p').to(u.Hz, equivalencies=u.dimensionless_angles())
 gyroperiod = (1/freq).to(u.s)
 steps_to_gyroperiod = 10
 timestep = gyroperiod / steps_to_gyroperiod
@@ -45,35 +45,35 @@ timestep = gyroperiod / steps_to_gyroperiod
 # Initialize the trajectory calculation.
 
 number_steps = steps_to_gyroperiod * int(2 * np.pi)
-particle_trajectory = ParticleTracker(plasma, 'p', 1, 1, timestep, number_steps)
+trajectory = ParticleTracker(plasma, 'p', 1, 1, timestep, number_steps)
 
 ############################################################
 # We still have to initialize the particle's velocity. We'll limit ourselves to
 # one in the x direction, parallel to the magnetic field B -
 # that way, it won't turn in the z direction.
 
-particle_trajectory.v[0][0] = 1 * (u.m / u.s)
+trajectory.v[0][0] = 1 * (u.m / u.s)
 
 ############################################################
 # Run the pusher and plot the trajectory versus time.
 
-particle_trajectory.run()
-particle_trajectory.plot_time_trajectories()
+trajectory.run()
+trajectory.plot_time_trajectories()
 
 ############################################################
 # Plot the shape of the trajectory in 3D.
 
-particle_trajectory.plot_trajectories()
+trajectory.plot_trajectories()
 
 ############################################################
 # As a test, we calculate the mean velocity in the z direction from the
 # velocity and position
 
-vmean = particle_trajectory.velocity_history[:, :, 2].mean()
+vmean = trajectory.velocity_history[:, :, 2].mean()
 print(f"The calculated drift velocity is {vmean:.4f} to compare with the"
       f"theoretical E0/B0 = {E0/B0:.4f}")
 
 ############################################################
 # and from position:
-Vdrift = particle_trajectory.position_history[-1, 0, 2] / (particle_trajectory.NT * particle_trajectory.dt)
+Vdrift = trajectory.position_history[-1, 0, 2] / (trajectory.NT * trajectory.dt)
 print(f"The calculated drift velocity from position is {Vdrift:.4f}")
