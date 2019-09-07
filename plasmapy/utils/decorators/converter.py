28fb871d2678a78c06ef5c665b6b365d9abeebbd
diego diaz <diego7319@gmail.com>
2019-09-06 22:01:39 -0500

    change in decorator

--- plasmapy/utils/decorators/converter.py
+++ plasmapy/utils/decorators/converter.py
@@ -3,11 +3,23 @@ Decorator to convert units of functions in /physics methods
 """
 __all__ = ["angular_freq_to_hz"]
 
+import functools
+import inspect
 from astropy import units as u
 from plasmapy.utils.decorators import preserve_signature
 
 
 def angular_freq_to_hz(fn):
+
+    fn.__doc__ += """
+    Parameters
+    ------------
+    to_hz: bool
+        Set `True` to to convert function output from angular frequency to Hz
+    """
+
+    @preserve_signature
+    @functools.wraps(fn)
     def wrapper(*args, to_hz=False, **kwargs):
         _result = fn(*args, **kwargs)
         if to_hz:
