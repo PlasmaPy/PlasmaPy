ef76647e9d483f4f518f2a4a22d8044ac03b638d
Brian Goodall <goodab@uw.edu>
2019-09-12 15:59:04 -0700

    Removed custom skip code

--- plasmapy/conftest.py
+++ plasmapy/conftest.py
@@ -1,4 +1,4 @@
-# Force MPL to use non-gui backends for testing.
+    # Force MPL to use non-gui backends for testing.
 import pytest
 
 try:
@@ -22,21 +22,3 @@ def pytest_configure(config):
         ("slow: mark test as slow to run. Test can be skipped with --not-slow or exclusively "
         "executed with --slow.")
     )
-
-def pytest_collection_modifyitems(config, items):
-    skip_condtion = None
-    if config.getoption("--not-slow") and config.getoption("--slow"):
-        # User wants to run both the not-slow tests and the slow tests, which is the same as running
-        #   with no options
-        pass
-    elif config.getoption("--not-slow"):
-        # Skip slow tests
-        skip_mark = pytest.mark.skip(reason="Test is marked slow.")
-        skip_condition = lambda x: "slow" in x.keywords
-    elif config.getoption("--slow"):
-        skip_mark = pytest.mark.skip(reason="Test isn't marked slow.")
-        skip_condition = lambda x: "slow" not in x.keywords
-    if skip_condition is not None:
-        for item in items:
-            if skip_condition(item):
-                item.add_marker(skip_mark)
