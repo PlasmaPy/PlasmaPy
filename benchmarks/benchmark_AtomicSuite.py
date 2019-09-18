aa7d627cdae924d015bbc2ef2ad9c65b99b56aee
diego diaz <diego7319@gmail.com>
2019-09-17 19:01:17 -0500

    fixed pep8

--- benchmarks/benchmark_AtomicSuite.py
+++ benchmarks/benchmark_AtomicSuite.py
@@ -29,13 +29,11 @@ class PhysicsSuite:
     def setup(self):
         pass
 
-
     def time_atomic_number(self):
         atomic_number("H")
         atomic_number("tritium")
         atomic_number("alpha")
         atomic_number("oganesson")
-
 
     def time_mass_number(self):
         mass_number("H-1")
@@ -43,13 +41,11 @@ class PhysicsSuite:
         mass_number("tritium")
         mass_number("alpha")
 
-
     def time_standard_atomic_weight(self):
         standard_atomic_weight("H")
         standard_atomic_weight("lead")
         standard_atomic_weight("Au")
         standard_atomic_weight("nitrogen")
-
 
     def time_particle_mass(self):
         particle_mass("Au")
@@ -57,13 +53,11 @@ class PhysicsSuite:
         particle_mass(2)
         particle_mass("90")
 
-
     def time_isotopic_abundance(self):
         isotopic_abundance('Pb-208')
         isotopic_abundance('hydrogen', 1)
         isotopic_abundance('hydrogen', 3)
         isotopic_abundance('hydrogen', 2)
-
 
     def time_integer_charge(self):
         integer_charge('Fe-56 2+')
@@ -71,13 +65,11 @@ class PhysicsSuite:
         integer_charge('H+')
         integer_charge('N-14++')
 
-
     def time_electric_charge(self):
         electric_charge('p+')
         electric_charge('O-')
         electric_charge('N-')
         electric_charge('He-')
-
 
     def time_is_stable(self):
         is_stable("H-1")
@@ -85,13 +77,11 @@ class PhysicsSuite:
         is_stable("e-")
         is_stable("tau+")
 
-
     def time_half_life(self):
         half_life('T')
         half_life('n')
         half_life('H-1')
         half_life('C-14')
-
 
     def time_known_isotopes(self):
         known_isotopes('H')
@@ -99,13 +89,11 @@ class PhysicsSuite:
         known_isotopes()[0:10]
         known_isotopes()[4:15]
 
-
     def time_common_isotopes(self):
         common_isotopes('H')
         common_isotopes('Fe')
         common_isotopes('Fe', most_common_only=True)
         common_isotopes()[0:7]
-
 
     def time_stable_isotopes(self):
         stable_isotopes('beryllium')
@@ -113,13 +101,11 @@ class PhysicsSuite:
         stable_isotopes(118)
         stable_isotopes('U', unstable=True)[:5]
 
-
     def time_reduced_mass(self):
         reduced_mass('p+', 'e-')
         reduced_mass(5.4e-27 * u.kg, 8.6e-27 * u.kg)
         reduced_mass(6.4e-10 * u.kg, 8.6e-11 * u.kg)
         reduced_mass(1.4e-10 * u.kg, 2.6e-40 * u.kg)
-
 
     def time_periodic_table_period(self):
         periodic_table_period(5)
@@ -127,20 +113,17 @@ class PhysicsSuite:
         periodic_table_period("Au")
         periodic_table_period("nitrogen")
 
-
     def time_periodic_table_group(self):
         periodic_table_group(18)
         periodic_table_group(24)
         periodic_table_group("Al")
         periodic_table_group("neon")
 
-
     def time_periodic_table_block(self):
         periodic_table_block(66)
         periodic_table_block("Tl")
         periodic_table_block("thallium")
         periodic_table_block("francium")
-
 
     def time_periodic_table_category(self):
         periodic_table_category(82)
