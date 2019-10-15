d21dde29b7696ca62e2f5f959f928b175ff024ec
Dominik Stańczak <stanczakdominik@gmail.com>
2019-10-15 13:04:51 +0200

    further improvements to skippable hypothesis test

--- plasmapy/simulation/tests/test_particletracker_hypothesis.py
+++ plasmapy/simulation/tests/test_particletracker_hypothesis.py
@@ -4,32 +4,33 @@ try:
 except ImportError:
     import pytest
     pytestmark = pytest.mark.skip("Optional hypothesis test")
+else:
 
-from astropy import units as u
-from plasmapy.classes.sources.analyticalplasma import AnalyticalPlasma
-from plasmapy.simulation import ParticleTracker
-from plasmapy.physics.parameters import gyrofrequency
-import numpy as np
+    from astropy import units as u
+    from plasmapy.classes.sources.analyticalplasma import AnalyticalPlasma
+    from plasmapy.simulation import ParticleTracker
+    from plasmapy.physics.parameters import gyrofrequency
+    import numpy as np
 
-def magnetic_field(r):
-    return u.Quantity([[4, 0, 0]]*len(r), u.T)
+    def magnetic_field(r):
+        return u.Quantity([[4, 0, 0]]*len(r), u.T)
 
-# precomputed for efficiency
-E_unit = u.V / u.m
-def electric_field(r):
-    return u.Quantity([[0, 2, 0]]*len(r), E_unit)
+    # precomputed for efficiency
+    E_unit = u.V / u.m
+    def electric_field(r):
+        return u.Quantity([[0, 2, 0]]*len(r), E_unit)
 
-N = 20
-@given(arrays(dtype='float', shape=(N, 2)))
-@settings(deadline=None)
-def test_no_underflow(velocity):
-    plasma = AnalyticalPlasma(magnetic_field, electric_field)
-    freq = gyrofrequency(4 * u.T, 'p').to(u.Hz, equivalencies=u.dimensionless_angles())
-    gyroperiod = (1/freq).to(u.s)
-    steps_to_gyroperiod = 10
-    timestep = gyroperiod / steps_to_gyroperiod
-    number_steps = 5 * steps_to_gyroperiod * int(2 * np.pi)
+    N = 20
+    @given(arrays(dtype='float', shape=(N, 2)))
+    @settings(deadline=None)
+    def test_no_underflow(velocity):
+        plasma = AnalyticalPlasma(magnetic_field, electric_field)
+        freq = gyrofrequency(4 * u.T, 'p').to(u.Hz, equivalencies=u.dimensionless_angles())
+        gyroperiod = (1/freq).to(u.s)
+        steps_to_gyroperiod = 10
+        timestep = gyroperiod / steps_to_gyroperiod
+        number_steps = 5 * steps_to_gyroperiod * int(2 * np.pi)
 
-    trajectory = ParticleTracker(plasma, 'p', N, 1, timestep/100, number_steps)
-    trajectory._v[:, :2] = velocity
-    trajectory.run()
+        trajectory = ParticleTracker(plasma, 'p', N, 1, timestep/100, number_steps)
+        trajectory._v[:, :2] = velocity
+        trajectory.run()
