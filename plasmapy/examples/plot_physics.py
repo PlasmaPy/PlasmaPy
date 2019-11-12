94a179927b079c119901c3ea1a37e4eddfe85e08
Bryan Luu <bryanluu30794@gmail.com>
2019-11-12 11:55:42 -0800

    Add some real data points of plasma devices. Added a grid to the plots.

--- plasmapy/examples/plot_physics.py
+++ plasmapy/examples/plot_physics.py
@@ -57,4 +57,28 @@ plt.scatter(
     formulary.plasma_frequency(electron_concentration))
 plt.xlabel("Electron Concentration (m^-3)")
 plt.ylabel("Langmuir Wave Plasma Frequency (rad/s)")
+plt.grid()
+plt.show()
+
+############################################################
+# Let's compare our data to real-world data of plasma devices
+
+# Our 'Data'
+plt.scatter(electron_concentration, formulary.plasma_frequency(electron_concentration))
+
+# IRT1 Tokamak Data
+# http://article.sapub.org/pdf/10.5923.j.jnpp.20110101.03.pdf
+n_e = 1.2e19 / u.m**3
+T_e = 136.8323 * u.eV
+f1 = plt.scatter(n_e, formulary.plasma_frequency(n_e))
+
+# Wendelstein 7-X Stellerator Data
+# https://nucleus.iaea.org/sites/fusionportal/Shared%20Documents/FEC%202016/fec2016-preprints/preprint0541.pdf
+n_e = 3e19 / u.m**3
+T_e = 6 * u.keV
+plt.scatter(n_e, formulary.plasma_frequency(n_e))
+
+plt.xlabel("Electron Concentration (m^-3)")
+plt.ylabel("Langmuir Wave Plasma Frequency (rad/s)")
+plt.grid()
 plt.show()
