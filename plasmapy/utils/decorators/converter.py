815cd755c7213d52198d1c6d77c8798062bcf3af
Erik Everson <eteverson@gmail.com>
2019-12-13 12:32:44 -0800

    add sphinx syntax to docstring
    add sphinx syntax to docstring to properly highlight key words; PEP8 function 2-spaces below imports

--- plasmapy/utils/decorators/converter.py
+++ plasmapy/utils/decorators/converter.py
@@ -9,13 +9,15 @@ import inspect
 from astropy import units as u
 from plasmapy.utils.decorators import preserve_signature
 
+
 def angular_freq_to_hz(fn):
     """
     A decorator that adds to a function the ability to convert the function's return from
-    angular frequency (rad/s or ω) to frequency (Hz).
+    angular frequency (rad/s) to frequency (Hz).
 
-    A kwarg to_hz is added to the function's signature, with a default value of False.
-    The keyword is also added to the function's docstring under the Other Parameters heading.
+    A kwarg `to_hz` is added to the function's signature, with a default value of `False`.
+    The keyword is also added to the function's docstring under the **"Other Parameters"**
+    heading.
 
     Parameters
     ----------
