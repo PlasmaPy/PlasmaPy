00e11b529b09df5e2ab336eeed115126c30712f9
Nick Murphy <namurphy@cfa.harvard.edu>
2019-12-13 22:06:05 -0500

    Create pytest helper classes to hold test inputs
    This commit creates three classes to be used to store the callable and
    arguments, and then call it.  These classes include all of the
    processing to handle and validate positional and keyword arguments,
    and include additional checks like making sure that functions are
    indeed functions.  The goal is to hide all of this low-level stuff
    from the higher-level test runner functionality during the refactoring
    of run_test.
    
    The three variations of the result test pattern are: (1) calling a
    function with args & kwargs, (2) calling a class instance method, and
    (3) accessing a class instance attribute to make sure that the result
    matches the correct value.  The classes corresponding to this include
    FunctionTestInputs, ClassMethodTestInputs, and
    ClassAttributeTestInputs.
    
    The lines that say `__tracebackhide__ = True` are instructions for
    pytest to consider this a test runner function and not include
    traceback information when doing the error report.
    
    An alternative to the `call` method on these classes is to use
    functools.partial, which binds arguments to a callable and creates a
    new function-like thing.  I think the code is more understandable to
    spell out how the function, method, or attribute is being called prior
    to a test.

--- plasmapy/utils/pytest_helpers/__init__.py
+++ plasmapy/utils/pytest_helpers/__init__.py
@@ -1,9 +1,27 @@
 from plasmapy.utils.pytest_helpers.pytest_helpers import (
-    run_test, run_test_equivalent_calls, assert_can_handle_nparray,
+    run_test,
+    run_test_equivalent_calls,
+    assert_can_handle_nparray,
 )
-from .error_messages import call_string
-from .exceptions import (
-    InconsistentTypeError, UnexpectedResultError, UnexpectedExceptionError, RunTestError,
-    IncorrectResultError, MissingExceptionError, MissingWarningError, InvalidTestError,
+from plasmapy.utils.pytest_helpers.error_messages import call_string
+
+from plasmapy.utils.pytest_helpers.exceptions import (
+    InconsistentTypeError,
+    UnexpectedResultError,
+    UnexpectedExceptionError,
+    RunTestError,
+    IncorrectResultError,
+    MissingExceptionError,
+    MissingWarningError,
+    InvalidTestError,
 )
-from .expected import ExpectedTestOutcome
+
+from plasmapy.utils.pytest_helpers.expected import ExpectedTestOutcome
+
+from plasmapy.utils.pytest_helpers.inputs import (
+    AbstractTestInputs,
+    GenericClassTestInputs,
+    ClassMethodTestInputs,
+    ClassAttributeTestInputs,
+    FunctionTestInputs,
+)
--- plasmapy/utils/pytest_helpers/inputs.py
+++ plasmapy/utils/pytest_helpers/inputs.py
@@ -1,38 +1,50 @@
-import abc
-from typing import Tuple, List, Dict, Union, Callable
+from abc import ABC, abstractmethod
+from typing import Tuple, List, Dict, Union, Callable, Optional, Any, NoReturn, AnyStr
 import inspect
 
+from plasmapy.utils.pytest_helpers import InvalidTestError
 
-class TestInputs:
-    def __init__(
-            self,
-            cls_or_func,
-            args,
-            kwargs,
-            attribute,
-            method,
-            method_args,
-            method_kwargs,
-    ):
-        self.factory = choose_factory
+__all__ = [
+    'AbstractTestInputs',
+    'FunctionTestInputs',
+    'GenericClassTestInputs',
+    'ClassAttributeTestInputs',
+    'ClassMethodTestInputs',
+]
 
 
-
-
-
-def _validate_args(args):
+def _validate_args(args: Any) -> Union[Tuple, List]:
     """
     If the argument is a `tuple` or `list`, then return the argument.
-    Otherwise, return a `tuple` containing the argument.
+    Otherwise, return a `tuple` containing the argument.  If the input
+    is `None`, then this function will return an empty `tuple`.
     """
-    return args if isinstance(args, (tuple, list)) else (args,)
+    if isinstance(args, (tuple, list)):
+        return args
+    elif args is None:
+        return tuple()
+    else:
+        return (args,)
 
 
-def _validate_kwargs(kwargs: Dict[str]) -> Dict[str]:
+def _validate_kwargs(kwargs: Optional[Dict[str, Any]]) -> Dict[str, Any]:
     """
-    Check that the provided keyword arguments are valid, and if so
-    """
+    Check that the argument is a valid `dict` to represent keyword
+    arguments to be provided to some function.  If the input is `None`,
+    then this function will return an empty `dict`.  This function does
+    not check that
 
+    Raises
+    ------
+    TypeError
+        If the input is not a `dict`.
+
+    ValueError
+        If the `dict` contains a key that is not a string.
+
+    """
+    if kwargs is None:
+        return dict()
     if not isinstance(kwargs, dict):
         raise TypeError("kwargs must be represented as a dictionary.")
     for key in kwargs.keys():
@@ -41,116 +53,313 @@ def _validate_kwargs(kwargs: Dict[str]) -> Dict[str]:
     return kwargs
 
 
-@abc.ABC
-class AbstractTestInputs:
-    @abc.abstractmethod
+class AbstractTestInputs(ABC):
+    @abstractmethod
     def call(self):
-        ...
+        pass
 
 
 class FunctionTestInputs(AbstractTestInputs):
-    def __init__(self, function, args=tuple(), kwargs = {}):
-        self._info = {}
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
+    """
+    A class to contain a function to be tested, along with the
+    positional and keyword arguments to be provided to the function
+    during the test.
+
+    Parameters
+    ----------
+    function
+        The function to be tested.
+
+    args : optional
+        The positional arguments to be provided to the function that is
+        being tested.  If this is a `tuple` or `list`, then the
+        arguments will be each of the items in the collection. If there
+        is only one positional argument, then it may be inputted as is
+        without being put into a `tuple` or `list`.
+
+    kwargs : `dict`, optional
+        The keyword arguments to be provided to the function that is
+        being tested. If provided, the keys of this `dict` must be
+        strings.
+
+    """
+    def __init__(
+            self,
+            function: Callable,
+            args: Optional[Union[Tuple, List, Any]] = None,
+            kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        try:
+            self._info = dict()
+            self.function = function
+            self.args = args
+            self.kwargs = kwargs
+        except Exception as exc:
+            # TODO: Make sure that repr is what we want here and below
+            raise InvalidTestError(
+                f"Unable to instantiate with function = {repr(function)}, "
+                f"args = {repr(args)}, and kwargs = {repr(kwargs)}."
+            ) from exc
 
     @property
-    def function(self) -> callable:
+    def function(self) -> Callable:
+        """The function to be tested."""
         __tracebackhide__ = False
         return self._info['function']
 
     @property
-    def args(self):
+    def args(self) -> Union[Tuple, List]:
+        """The positional arguments to be provided to the function that is being tested."""
         return self._info['args']
 
     @property
-    def kwargs(self):
+    def kwargs(self) -> Dict[str, Any]:
+        """The keyword arguments to be provided to the function that is being tested."""
         return self._info['kwargs']
 
     @function.setter
-    def function(self, provided_function):
+    def function(self, provided_function: Callable):
         if not inspect.isfunction(provided_function):
             raise TypeError("The provided function is not a function.")
+        self._info['function'] = provided_function
 
     @args.setter
-    def args(self, provided_args):
-        is_tuple_or_list = isinstance(provided_args, (tuple, list))
-        self._args = provided_args if is_tuple_or_list else (provided_args,)
+    def args(self, provided_args: Any):
+        self._info['args'] = _validate_args(provided_args)
 
     @kwargs.setter
-    def kwargs(self, provided_kwargs):
-        if not isinstance(kwargs, dict):
-            raise TypeError("Keyword arguments should be a dictionary with strings as keys")
+    def kwargs(self, provided_kwargs: Optional[Dict[str, Any]]):
+        self._info['kwargs'] = _validate_kwargs(provided_kwargs)
 
-    def __call__(self):
+    def call(self):
+        """
+        Call the function to be tested with the provided positional and
+        keyword arguments.
+        """
         __tracebackhide__ = True
         return self.function(*self.args, **self.kwargs)
 
 
-class AbstractClassTestInputs(AbstractTestInputs):
+class GenericClassTestInputs(AbstractTestInputs):
+
+    _info = dict()
 
     @property
     def cls(self):
+        """The class to be tested."""
         return self._info['cls']
 
     @property
-    def cls_args(self):
+    def cls_args(self) -> Union[Tuple, List]:
+        """The positional arguments to be passed to the class during instantiation."""
         return self._info['cls_args']
 
     @property
-    def cls_kwargs(self):
+    def cls_kwargs(self) -> Dict[str, Any]:
+        """The keyword arguments to be passed to the class during instantiation."""
         return self._info['cls_kwargs']
 
     @cls.setter
     def cls(self, provided_cls):
         if inspect.isclass(provided_cls):
-            self.cls = provided_cls
+            self._info['cls'] = provided_cls
         else:
             raise TypeError("Expecting a class.")
 
     @cls_args.setter
-    def cls_args(self, provided_cls_args):
+    def cls_args(self, provided_cls_args: Any):
         self._info['cls_args'] = _validate_args(provided_cls_args)
 
     @cls_kwargs.setter
-    def cls_kwargs(self, provided_cls_kwargs):
+    def cls_kwargs(self, provided_cls_kwargs: Optional[Dict[str, Any]]):
         self._info['cls_kwargs'] = _validate_kwargs(provided_cls_kwargs)
 
 
-class ClassAttributeTestInputs(ClassTestInputs):
-    def __init__(self, cls, cls_args, cls_kwargs, attribute):
-        self._info = {}
-        self.cls = cls
-        self.cls_args = cls_args
-        self.cls_kwargs = cls_kwargs
-        self.attribute = attribute
+class ClassAttributeTestInputs(GenericClassTestInputs):
+    """
+    Contains a class to be tested, positional and keyword arguments to
+    be provided to the class during instantiation, the name of the
+    class method to be tested, and positional and keyword arguments to
+    be provided to the method during the test.
+
+    Parameters
+    ----------
+    cls
+        The class containing the method to be tested.
+
+    attribute : str
+        The name of the attribute contained in ``cls`` to be tested.
+
+    cls_args : optional
+        The positional arguments to be provided to the class that is
+        being tested during instantiation. If this is a `tuple` or
+        `list`, then the arguments will be each of the items in the
+        collection. If there is only one positional argument, then it
+        may be inputted as is without being put into a `tuple` or `list`.
+
+    cls_kwargs : `dict`, optional
+        The keyword arguments to be provided to the class that is
+        being tested during instantiation. If provided, the keys of
+        this `dict` must be strings.
+
+    Raises
+    ------
+    ~plasmapy.utils.pytest_helpers.InvalidTestError
+        If this class cannot be instantiated.
+
+    """
+    def __init__(
+            self,
+            cls,
+            attribute: AnyStr,
+            cls_args: Any = None,
+            cls_kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        try:
+            self._info = {}
+            self.cls = cls
+            self.cls_args = cls_args
+            self.cls_kwargs = cls_kwargs
+            self.attribute = attribute
+        except Exception as exc:
+            # TODO: Make sure that the input variables are being represented correctly here
+            raise InvalidTestError(
+                f"Unable to instantiate ClassAttributeTestInputs for "
+                f"class {cls.__name__} with cls_args = {repr(cls_args)}, "
+                f"cls_kwargs = {repr(cls_kwargs)}, and attribute = "
+                f"{repr(attribute)}."
+            ) from exc
 
     @property
-    def attribute(self):
+    def attribute(self) -> AnyStr:
+        """The name of the attribute to be tested."""
         return self._info['attribute']
 
     @attribute.setter
-    def attribute(self, provided_attribute):
-        if not isinstance(provided_attribute, str):
-            raise TypeError("Expecting a string")
-        if not hasattr(self.cls, provided_attribute):
-            raise ValueError(f"No attribute named {provided_attribute}.")
+    def attribute(self, attribute_name: AnyStr):
+        if not isinstance(attribute_name, str):
+            raise TypeError("Expecting the name of a class attribute as a string.")
+        if not hasattr(self.cls, attribute_name):
+            raise ValueError(f"No attribute named {attribute_name}.")
         else:
-            self._info['attribute'] = provided_attribute
+            self._info['attribute'] = attribute_name
 
-    @property
     def call(self):
         __tracebackhide__ = True
-        return self.cls(*self.cls_args, **self.cls_kwargs)
+        instance = self.cls(*self.cls_args, **self.cls_kwargs)
+        return getattr(instance, self.attribute)
 
 
-class ClassMethodTestInputs(AbstractTestInputs):
-    def __init__(self, cls, cls_args, cls_kwargs, method, method_args, method_kwargs):
-        self.info = {}
-        self.cls = cls
-        self.cls_args = cls_args
-        self.cls_kwargs = cls_kwargs
-        self.method = method
-        self.method_args = method_args
-        self.method_kwargs = method_kwargs
+class ClassMethodTestInputs(GenericClassTestInputs):
+    """
+    Contains a class to be tested, positional and keyword arguments to
+    be provided to the class during instantiation, the name of the
+    class method to be tested, and positional and keyword arguments to
+    be provided to the method during the test.
+
+    Parameters
+    ----------
+    cls
+        The class containing the method to be tested.
+
+    method : str
+        The name of the method contained in ``cls`` to be tested.
+
+    cls_args : optional
+        The positional arguments to be provided to the class that is
+        being tested during instantiation.  If this is a `tuple` or
+        `list`, then the arguments will be each of the items in the
+        collection. If there is only one positional argument, then it
+        may be inputted as is without being put into a `tuple` or `list`.
+
+    cls_kwargs : `dict`, optional
+        The keyword arguments to be provided to the function that is
+        being tested. If provided, the keys of this `dict` must be
+        strings.
+
+    method_args : optional
+        The positional arguments to be provided to the method that is
+        being tested.  If this is a `tuple` or `list`, then the
+        arguments will be each of the items in the collection. If there
+        is only one positional argument, then it may be inputted as is
+        without being put into a `tuple` or `list`.
+
+    method_kwargs : `dict`, optional
+        The keyword arguments to be provided to the method that is being
+        tested. If provided, the keys of this `dict` must be strings.
+
+    Raises
+    ------
+    ~plasmapy.utils.pytest_helpers.InvalidTestError
+        If this class cannot be instantiated.
+
+    """
+
+    def __init__(
+            self,
+            cls,
+            method: AnyStr,
+            cls_args=None,
+            cls_kwargs: Optional[Dict[str, Any]] = None,
+            method_args=None,
+            method_kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        try:
+            self._info = {}
+            self.cls = cls
+            self.cls_args = cls_args
+            self.cls_kwargs = cls_kwargs
+            self.method = method
+            self.method_args = method_args
+            self.method_kwargs = method_kwargs
+        except Exception as exc:
+            # TODO: Test that error message is representing things correctly
+            raise InvalidTestError(
+                f"Unable to instantiate ClassMethodTestInputs for class "
+                f"{cls.__name__} with cls_args = {cls_args}, cls_kwargs = "
+                f"{cls_kwargs}, method = {method}, method_args = "
+                f"{method_args}, and method_kwargs = {method_kwargs}."
+            ) from exc
+
+    @property
+    def method(self):
+        """The name of the method to be tested."""
+        return self._info['method']
+
+    @property
+    def method_args(self) -> Union[Tuple, List]:
+        """The positional arguments to be provided to the method being tested."""
+        return self._info['method_args']
+
+    @property
+    def method_kwargs(self) -> Dict[str, Any]:
+        """The keyword arguments to be provided to the method being tested."""
+        return self._info['method_kwargs']
+
+    @method.setter
+    def method(self, method_name: AnyStr):
+        if not isinstance(method_name, str):
+            raise TypeError("Expecting the name of a method as a string.")
+        elif not hasattr(self.cls, method_name):
+            raise ValueError(f"No method named {method_name} in class {self.cls.__name__}.")
+        actual_method = getattr(self.cls, method_name)
+        if callable(actual_method):
+            self._info['method'] = method_name
+        else:
+            raise ValueError(
+                f"Expecting the attribute {method_name} in "
+                f"{self.cls.__name__} to be callable.")
+
+    @method_args.setter
+    def method_args(self, provided_args):
+        self._info['method_args'] = _validate_args(provided_args)
+
+    @method_kwargs.setter
+    def method_kwargs(self, provided_kwargs: Optional[Dict[str, Any]]):
+        self._info['method_kwargs'] = _validate_kwargs(provided_kwargs)
+
+    def call(self):
+        __tracebackhide__ = True
+        instance = self.cls(*self.cls_args, **self.cls_kwargs)
+        method = getattr(instance, self.method)
+        return method(*self.method_args, **self.method_kwargs)
