80b7e1b805226994e7a5eac5ae6dc4b4e58be0d4
rocco8773 <eteverson@gmail.com>
2019-12-14 00:12:06 -0800

    add discovery of namespace packages and add them to setup's entry_points

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+import importlib
 import os
+import pkgutil
 import sys
-from itertools import chain
-import builtins
+import plasmapy.addons
 
+from itertools import chain
 from setuptools import setup
 from setuptools.config import read_configuration
 
@@ -23,6 +25,25 @@ exclude_keys = ("tests", "docs", "dev")
 ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
 # Concatenate all the values together for 'all'
 extras['all'] = list(chain.from_iterable(ex_extras.values()))
+
+
+# Determine entry_points for namespace pacakge plasmapy.addons
+# - this needs to be so that the namespace is actually populated
+# - copied from https://packaging.python.org/guides/creating-and-discovering-plugins/
+def iter_namespace(ns_pkg):
+    # Specifying the second argument (prefix) to iter_modules makes the
+    # returned name an absolute name instead of a relative one. This allows
+    # import_module to work without having to do additional modification to
+    # the name.
+    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
+
+
+discovered_plugins = {
+    name: importlib.import_module(name)
+    for finder, name, ispkg
+    in iter_namespace(plasmapy.addons)
+}
+extras['entry_points'] = discovered_plugins
 
 # Get configuration information from all of the various subpackages.
 # See the docstring for setup_helpers.update_package_files for more
