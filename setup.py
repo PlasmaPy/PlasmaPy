69f95261df965174a4f281382f919a5b17241877
rocco8773 <eteverson@gmail.com>
2019-12-14 00:48:52 -0800

    add 'plasmapy.addons' to entry_points

--- setup.py
+++ setup.py
@@ -27,23 +27,26 @@ ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
 extras['all'] = list(chain.from_iterable(ex_extras.values()))
 
 
-# Determine entry_points for namespace pacakge plasmapy.addons
-# - this needs to be so that the namespace is actually populated
-# - copied from https://packaging.python.org/guides/creating-and-discovering-plugins/
-def iter_namespace(ns_pkg):
-    # Specifying the second argument (prefix) to iter_modules makes the
-    # returned name an absolute name instead of a relative one. This allows
-    # import_module to work without having to do additional modification to
-    # the name.
-    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
-
-
-discovered_plugins = {
-    name: importlib.import_module(name)
-    for finder, name, ispkg
-    in iter_namespace(plasmapy.addons)
-}
-extras['entry_points'] = discovered_plugins
+# # Determine entry_points for namespace pacakge plasmapy.addons
+# # - this needs to be so that the namespace is actually populated
+# # - copied from https://packaging.python.org/guides/creating-and-discovering-plugins/
+# def iter_namespace(ns_pkg):
+#     # Specifying the second argument (prefix) to iter_modules makes the
+#     # returned name an absolute name instead of a relative one. This allows
+#     # import_module to work without having to do additional modification to
+#     # the name.
+#     return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
+#
+#
+# discovered_plugins = {
+#     name: importlib.import_module(name)
+#     for finder, name, ispkg
+#     in iter_namespace(plasmapy.addons)
+# }
+# discovered_addons_list = [
+#     f'{name} = '
+# ]
+extras['entry_points'] = {'plasmapy.addons': []}
 
 # Get configuration information from all of the various subpackages.
 # See the docstring for setup_helpers.update_package_files for more
