31bcefceb1f805e75fa5bb3f5534c165342d78d5
rocco8773 <eteverson@gmail.com>
2019-12-15 23:49:29 -0800

    remove manual setting of packages [going back to setup.cfg]

--- setup.py
+++ setup.py
@@ -23,8 +23,9 @@ ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
 # Concatenate all the values together for 'all'
 extras['all'] = list(chain.from_iterable(ex_extras.values()))
 
-packages = find_packages() + ['plasmapy.addons', ]
+# packages = find_packages() + ['plasmapy.addons', ]
+
 # Get configuration information from all of the various subpackages.
 # See the docstring for setup_helpers.update_package_files for more
 # details.
-setup(extras_require=extras, use_scm_version=True, packages=packages)
+setup(extras_require=extras, use_scm_version=True)
