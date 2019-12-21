c342f0b3aae8842e592e6964461ff1e120abecf8
Nick Murphy <namurphy@cfa.harvard.edu>
2019-12-20 21:03:18 -0500

    Refine and expand ActualTestOutputs

--- plasmapy/utils/pytest_helpers/actual.py
+++ plasmapy/utils/pytest_helpers/actual.py
@@ -1,23 +1,175 @@
 import pytest
 import warnings
-import copy
+from typing import List
+from plasmapy.utils.pytest_helpers.inputs import AbstractTestInputs
 
 __all__ = ["ActualTestOutcome"]
 
 
+class _MockException(Exception):
+    pass
+
+
+class _MockWarning(Warning):
+    pass
+
+
 class ActualTestOutcome:
     def __init__(self, inputs):
-        self.inputs = inputs
-        try:
-            with pytest.warns(Warning) as warnings_record:
-                self.result = self.inputs.call()
-                self.warnings_record = copy.copy(warnings_record)
-                self.warning_was_issued = bool(self.warnings_record)
-                warnings.warn(Warning)
-        except Exception as exception_info:
-            self.exception_was_raised = True
-            self.warning_was_issued = False
-            self.exception_info = copy.copy(exception_info)
+        """
+        A class to record the actual outcome of a test.
+
+        Parameters
+        ----------
+        inputs : instance of subclass of `plasmapy.utils.pytest_helpers.inputs.AbstractTestInputs`
+
+        """
+
+        if not isinstance(inputs, AbstractTestInputs):
+            raise TypeError(
+                "Expecting an instance of a subclass of AbstractTestInputs,"
+                "such as FunctionTestInputs, ClassAttributeTestInputs, or "
+                "ClassMethodTestInputs.")
+
+        with pytest.warns(Warning) as warnings_record:
+            warnings.warn('So we can exit pytest.warns context manager', _MockWarning)
+            with pytest.raises(Exception) as exception_info:
+                self._value = inputs.call()
+                raise _MockException('So we can exit pytest.raises context manager')
+
+        self._exception_info = exception_info
+
+        # Remove _MockWarning from the list of warnings before storing it
+        warnings_record.pop()
+        self._warnings_record = warnings_record
+
+    @property
+    def value_was_returned(self) -> bool:
+        """
+        `True` if the `callable` being tested returned a value or
+        `None`, and `False` otherwise.
+        """
+        return hasattr(self, '_value')
+
+    @property
+    def exception_was_raised(self) -> bool:
+        """
+        `True` if an exception was raised by the `callable` being
+        tested, and `False` otherwise.
+        """
+        return self._exception_info.type is not _MockException
+
+    @property
+    def warning_was_issued(self) -> bool:
+        """
+        `True` if a warning was issued by the `callable` being tested,
+        and `False` otherwise.
+        """
+        return bool(self._warnings_record.list)
+
+    @property
+    def exception_info(self):
+        """
+        The ``ExceptionInfo`` instance created while using the `pytest.raises`
+        context manager.
+
+        Raises
+        ------
+        RuntimeError
+            If no exception was raised during the test.
+
+        """
+        if self.exception_was_raised:
+            return self._exception_info
         else:
-            self.exception_was_raised = False
-            self.exception_info = None
+            raise RuntimeError("No exception was raised.")
+
+    @property
+    def exception_type(self):
+        """
+        Return the type of exception that was raised.
+
+        Raises
+        ------
+        RuntimeError
+            If no exception was raised during the test.
+
+        """
+        return self.exception_info.type
+
+    @property
+    def exception_message(self):
+        """
+        The error message of the exception that was raised during the
+        test.
+
+        Raises
+        ------
+        RuntimeError
+            If no exception was raised during the test.
+
+        """
+        return str(self.exception_info.value)
+
+    @property
+    def warnings_record(self):
+        """
+        The ``WarningsRecorder`` instance created by the `pytest.warns`
+        context manager.
+
+        Raises
+        ------
+        RuntimeError
+            If no warning was issued during the test.
+
+        """
+        if self.warning_was_issued:
+            return self._warnings_record
+        else:
+            raise RuntimeError("Warnings information is not available")
+
+    @property
+    def warning_types(self) -> List:
+        """
+        A `tuple` containing the warnings that were issued during the
+        test, corresponding to the ``warning_messages`` attribute.
+
+        Raises
+        ------
+        RuntimeError
+            If no warning was issued during the test.
+        """
+        return [warning.category for warning in self.warnings_record]
+
+    @property
+    def warning_messages(self) -> List[str]:
+        """
+        A `tuple` containing the warning messages that were issued
+        during the test, corresponding to the ``warning_types``
+        attribute.
+
+        Raises
+        ------
+        RuntimeError
+            If no warning was issued during the test.
+
+        """
+        return [str(warning.message.args[0]) for warning in self.warnings_record]
+
+    @property
+    def value(self):
+        """
+        The value returned by the function, class, or class method that
+        is being tested.
+
+        Raises
+        ------
+        RuntimeError
+            If no value was returned during the test, for example if
+            the test raised an exception.
+
+        """
+        if self.value_was_returned:
+            return self._value
+        else:
+            raise RuntimeError("No value was returned.")
--- plasmapy/utils/pytest_helpers/tests/test_actual.py
+++ plasmapy/utils/pytest_helpers/tests/test_actual.py
@@ -9,23 +9,33 @@ def return_something():
 
 
 def issue_warning():
-    warnings.warn(UserWarning)
+    warnings.warn("warning message", UserWarning)
     return 24
 
 
 def raise_exception():
-    raise RuntimeError
+    raise SyntaxError("exception message")
 
 
 @pytest.mark.parametrize("function, attribute, expected", [
+
     (return_something, 'warning_was_issued', False),
     (return_something, 'exception_was_raised', False),
-    (return_something, 'result', 42),
+    (return_something, 'value', 42),
+    (return_something, 'value_was_returned', True),
+
     (issue_warning, 'warning_was_issued', True),
     (issue_warning, 'exception_was_raised', False),
-    (issue_warning, 'result', 24),
+    (issue_warning, 'value', 24),
+    (issue_warning, 'value_was_returned', True),
+    (issue_warning, 'warning_messages', ['warning message', ]),
+    (issue_warning, 'warning_types', [UserWarning]),
+
     (raise_exception, 'warning_was_issued', False),
     (raise_exception, 'exception_was_raised', True),
+    (raise_exception, 'value_was_returned', False),
+    (raise_exception, 'exception_message', 'exception message'),
+    (raise_exception, 'exception_type', SyntaxError),
 ])
 def test_actual_test_outcome(function, attribute, expected):
     inputs = FunctionTestInputs(function)
@@ -36,3 +46,27 @@ def test_actual_test_outcome(function, attribute, expected):
             f" function {function.__name__} was {getattr(actual, attribute)}, "
             f"instead of the expected value of {expected}.",
         )
+
+
+expected_exception = RuntimeError
+
+
+@pytest.mark.parametrize("function, attribute, expected_exception", [
+    (return_something, 'exception_info', expected_exception),
+    (return_something, 'warnings_record', expected_exception),
+    (return_something, 'exception_type', expected_exception),
+
+    (issue_warning, 'exception_info', expected_exception),
+    (issue_warning, 'exception_type', expected_exception),
+    (issue_warning, 'exception_message', expected_exception),
+
+    (raise_exception, 'warnings_record', expected_exception),
+    (raise_exception, 'value', expected_exception),
+    (raise_exception, 'warning_messages', expected_exception),
+    (raise_exception, 'warning_types', expected_exception),
+])
+def test_actual_test_outcome_errors(function, attribute, expected_exception):
+    inputs = FunctionTestInputs(function)
+    actual = ActualTestOutcome(inputs)
+    with pytest.raises(expected_exception):
+        getattr(actual, attribute)
