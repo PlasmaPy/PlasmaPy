7f451e6779846ca2ad0941d8cd5063b9a744c18c
Nick Murphy <namurphy@cfa.harvard.edu>
2020-01-09 19:07:05 -0500

    Create a CompareValues class
    Separate responsibilities out of the CompareActualExpected class that
    generates error messages.

--- plasmapy/utils/pytest_helpers/comparators.py
+++ plasmapy/utils/pytest_helpers/comparators.py
@@ -1,5 +1,12 @@
 """Contains test comparators: tools that automate comparisons."""
 
+from numbers import Number
+from typing import Union, Tuple, Any, Optional
+
+from astropy import units as u
+
+from plasmapy.utils.pytest_helpers import InvalidTestError
+
 from plasmapy.utils.pytest_helpers.expected import ExpectedTestOutcome
 from plasmapy.utils.pytest_helpers.actual import ActualTestOutcome
 
@@ -8,15 +15,221 @@ from plasmapy.utils.pytest_helpers.error_messages import (
     _string_together_warnings_for_printing,
 )
 
-__all__ = ['CompareActualExpected']
+
+__all__ = ["CompareActualExpected"]
+
 
 # TODO: Test that error messages are working correctly!  This is going to
 #       be hard to manually check.
 
+class CompareValues:
+    """
+    Compares the properties of two values.
+
+    Parameters
+    ----------
+    this
+        The first `object` to be compared.
+
+    that
+        The second `object` to be compared.
+
+    rtol : number or dimensionless `~astropy.units.Quantity`, optional, keyword-only
+        The relative tolerance to be supplied to `~astropy.units.isclose`
+        or `~astropy.units.allclose`.  Defaults to ``1e-6``.
+
+    atol : number or `~astropy.units.Quantity`, optional, keyword-only
+        The absolute tolerance to be supplied to `astropy.units.isclose`
+        or `~astropy.units.allclose`.  If ``atol`` is a
+        `~astropy.units.Quantity`, then it must have the same units as
+        ``this`` and ``that``.  Defaults to zero in the appropriate units.
+    """
+
+    def __init__(
+            self,
+            this: Any,
+            that: Any,
+            *,
+            rtol: Union[Number, u.Quantity] = 1e-6,
+            atol: Optional[Union[Number, u.Quantity]] = None,
+    ):
+
+        self._this = this
+        self._that = that
+
+        try:
+            self.rtol = rtol
+            self.atol = atol
+        except Exception as exc:  # TODO: make more specific
+            raise InvalidTestError(
+                "Cannot perform comparison with invalid values for the "
+                "relative and/or absolute tolerances for comparison."
+            ) from exc
+
+    @property
+    def values(self) -> Tuple[Any, Any]:
+        """
+        The two values that are being compared.
+        """
+
+        return (self._this, self._that)
+
+    @property
+    def types(self) -> Tuple[type, type]:
+        """
+        The types of the two values that are being compared.
+        """
+
+        return (type(self._this), type(self._that))
+
+    @property
+    def rtol(self) -> Union[Number, u.Quantity]:
+        """
+        The relative tolerance to be used by `~astropy.units.isclose` or
+        `~astropy.units.allclose`.
+
+        If ``rtol`` is a `~astropy.units.Quantity`, then it must be
+        dimensionless.
+        """
+
+        return self._rtol
+
+    @property
+    def atol(self) -> Optional[Union[Number, u.Quantity]]:
+        """
+        The absolute tolerance to be used by `~astropy.units.isclose` or
+        `~astropy.units.allclose`.
+
+        If the objects being compared are two `~astropy.units.Quantity`
+        instances, then ``atol`` must have consistent dimensions.
+        """
+
+        return self._atol
+
+    @rtol.setter
+    def rtol(self, relative_tolerance):
+        self._rtol = relative_tolerance
+        raise NotImplementedError
+
+    @atol.setter
+    def atol(self, absolute_tolerance):
+        self._atol = absolute_tolerance
+        raise NotImplementedError
+
+
+    @property
+    def are_identical(self) -> bool:
+        """
+        Return `True` if the two values refer to the same identical
+        object, and `False` otherwise.
+        """
+
+        return self._this is self._that
+
+    @property
+    def are_equal(self) -> bool:
+        """
+        Return `True` if the two values evaluate as equal to each other,
+        and `False` otherwise (including if the .
+        """
+
+        try:
+            return self._this == self._that
+        except Exception:
+            return False
+
+    @property
+    def have_same_types(self) -> bool:
+        """
+        Return `True` if the two values are of the same `type`, and
+        `False` otherwise.
+        """
+
+        return self.types[0] is self.types[1]
+
+    @property
+    def are_quantities(self) -> bool:
+        """
+        `True` if each `object` being compared is a `Quantity`, and
+        `False` otherwise.
+        """
+
+        return isinstance(self._this, u.Quantity) and isinstance(self._that, u.Quantity)
+
+    @property
+    def are_quantity_and_unit(self) -> bool:
+        """
+        `True` if the first `object` being compared is a `~astropy.units.Quantity`
+        instance and the second `object` being compared is an instance of
+        (a subclass of) `~astropy.units.UnitBase`, and `False` otherwise.
+        """
+
+        return isinstance(self._this, u.Quantity) and isinstance(self._that, u.UnitBase)
+
+    @property
+    def units_are_consistent(self) -> bool:
+        """
+        If the objects being compared are both `~astropy.units.Quantity`
+        instances, then return `True` if the units are identical and
+        `False` otherwise.
+
+        If the first object is a `~astropy.units.Quantity` and the second
+        object is a `~astropy.units.UnitBase`
+
+        Return `True` if the two objects being compared are both
+        `~astropy.units.Quantity` instances with identical units, or if
+        the first object is a `~Quantity.`
+
+        This attribute will raise a `u.UnitsError` if the values being
+        compared are a `
+        """
+
+        if self.are_quantities:
+            return self._this.unit == self._that.unit
+        elif self.are_quantity_and_unit:
+            return self._this.unit == self._that
+        else:
+            raise u.UnitsError(
+                "The `units_are_consistent` attribute of CompareValues "
+                "may only be used when comparing a Quantity with a Quantity, "
+                "or a Quantity with a Unit (in that order)."
+            )
+
+
+    @property
+    def are_close(self):
+        """
+        `True` if
+        """
+        return u.allclose(self._this, self._that, rtol=self.rtol, atol=self.atol)
+
+
+    def __bool__(self):
+        if self.are_identical:
+            return True
+        elif self.are_equal and self.have_same_types:
+            return True
+        elif self.are_quantity_and_unit:
+            return
+
 
 class CompareActualExpected:
+    """
+    A class to compare the actual and expected results of a test, and
+    generate an appropriate error message if necessary.
 
-    def __init__(self, actual: ActualTestOutcome, expected: ExpectedTestOutcome):
+
+    """
+
+
+    def __init__(
+        self,
+        actual: ActualTestOutcome,
+        expected: ExpectedTestOutcome,
+        *,
+        rtol=1e-9,
+        atol=0.0,
+    ):
 
         if not isinstance(actual, ActualTestOutcome):
             raise TypeError("Expecting an instance of ActualTestOutcome")
@@ -47,6 +260,59 @@ class CompareActualExpected:
         return self._expected
 
     @property
+    def rtol(self) -> Union[Number, u.Quantity]:
+        """
+        The relative tolerance to be used in `~astropy.units.isclose` or
+        `astropy.units.allclose`.  If ``rtol`` is a `~astropy.units.Quantity`
+        instance, then it must be dimensionless.
+        """
+        return self._rtol
+
+    @property
+    def atol(self) -> Union[Number, u.Quantity]:
+        """
+        The absolute tolerance to be used in `~astropy.units.isclose`
+        or `~astropy.units.allclose`.
+        """
+        return self._atol
+
+    @rtol.setter
+    def rtol(self, relative_tolerance):
+        if isinstance(relative_tolerance, u.Quantity):
+            if relative_tolerance.unit == u.dimensionless_unscaled:
+                self._rtol = relative_tolerance
+            else:
+                raise u.UnitsError("`rtol` should be dimensionless if it is a Quantity")
+        elif isinstance(relative_tolerance, Number):
+            self._rtol = relative_tolerance
+        else:
+            raise TypeError(f"Invalid value of `rtol`: {relative_tolerance}")
+
+    @atol.setter
+    def atol(self, absolute_tolerance):
+
+        # if it is a quantity, then it must have the same units as the expected value,
+        # and otherwise it should raise an InvalidTestError
+        #
+        raise NotImplementedError
+
+#        is_a_quantity = isinstance(absolute_tolerance, u.Quantity)
+#        expecting_a_value = self.expected.expecting_a_value
+#        expecting_a_quantity = isinstance()
+
+#        if expecting_a_value:
+
+
+#        expecting_a_quantity = self.expected.expecting_a_value and self.ex
+
+#        expecting_a_quantity = isinstance(self.expected.expecting_a_value)
+
+#        if isinstance(u.Quantity):
+
+
+#            if isinstance(self.expected.expected_value)
+
+    @property
     def test_passed(self) -> bool:
         """
         Returns `True` if the actual outcome matches the expected outcome,
@@ -62,11 +328,26 @@ class CompareActualExpected:
         If the test passed, then return an empty string.
         """
 
-        return ' '.join(self._error_messages_list)
+        return " ".join(self._error_messages_list)
 
     def _add_errmsg(self, errmsg):
         """Add an error message to the list of error messages."""
         self._error_messages_list.append(errmsg)
+
+    @property
+    def _subject(self):
+        """
+        Return an appropriate subject for the first sentence in the
+        error message.  The call string should be included in each error
+        message if and only if it is the first error message.
+        """
+        is_first_error = not self._error_messages_list
+        return (
+            f"The command {self.actual.call_string}"
+            if is_first_error
+            else "This command"
+        )
+
 
     def _make_unexpected_warnings_errmsg(self):
         """
@@ -75,16 +356,18 @@ class CompareActualExpected:
         """
 
         _string_together_warnings_for_printing(
-            self.actual.warning_types,
-            self.actual.warning_messages,
+            self.actual.warning_types, self.actual.warning_messages,
         )
 
         is_first_error = not self._error_messages_list
-        subject = f"The command {self.actual.call_string}" if is_first_error else "This command"
+        subject = (
+            f"The command {self.actual.call_string}"
+            if is_first_error
+            else "This command"
+        )
 
         warnings_for_printing = _string_together_warnings_for_printing(
-            self.actual.warning_types,
-            self.actual.warning_messages,
+            self.actual.warning_types, self.actual.warning_messages,
         )
 
         number_of_warnings = len(self.actual.warning_types)
@@ -161,9 +444,33 @@ class CompareActualExpected:
 
         expected_value = self.expected.expected_value
         actual_value = self.actual.value
+        both_values = (expected_value, actual_value)
+
+        are_same_object = expected_value is actual_value
+        test_is_unit_check = isinstance(expected_value, u.Unit)
+
+        both_are_quantities = all([
+            isinstance(expected_value, u.Quantity),
+            isinstance(actual_value, u.Quantity),
+        ])
+
+        try:
+            are_equal = expected_value == actual_value
+        except Exception:
+            are_equal = False
+        finally:
+            same_type = type(expected_value) is type(actual_value)
+
+        if are_equal and same_type:
+            return
+
+
+
+
+
 
         if actual_value is not expected_value and actual_value != expected_value:
-            self._add_errmsg('_make_value_mismatch_errmsg_if_necessary')
+            self._add_errmsg("_make_value_mismatch_errmsg_if_necessary")
 
     def _make_missing_warning_errmsg(self):
         """
@@ -172,7 +479,11 @@ class CompareActualExpected:
         """
 
         is_first_error = not self._error_messages_list
-        subject = f"The command {self.actual.call_string}" if is_first_error else "This command "
+        subject = (
+            f"The command {self.actual.call_string}"
+            if is_first_error
+            else "This command "
+        )
 
         self._add_errmsg(
             f"{subject} did not raise {_exc_str(self.expected.expected_warning)}"
@@ -181,8 +492,8 @@ class CompareActualExpected:
 
     def _make_warning_mismatch_errmsg_if_necessary(self):
         """
-        Compose an error message for tests where the expected warnings
-        were not issued but the
+        Compose an error message for tests where the expected warning
+        was not issued but different warning(s) were.
         """
 
         expected_warning = self.expected.expected_warning
@@ -196,7 +507,11 @@ class CompareActualExpected:
         if expected_warning in actual_warnings:
             return
 
-        subject = f"The command {self.actual.call_string}" if is_first_error else "This command"
+        subject = (
+            f"The command {self.actual.call_string}"
+            if is_first_error
+            else "This command"
+        )
 
         errmsg = (
             f"{subject} was expected to issue a {_exc_str(expected_warning)}, "
--- plasmapy/utils/pytest_helpers/tests/test_comparators.py
+++ plasmapy/utils/pytest_helpers/tests/test_comparators.py
@@ -1,10 +1,13 @@
 import warnings
 import pytest
+import numpy as np
+from astropy import units as u
 
 from plasmapy.utils.pytest_helpers.actual import ActualTestOutcome
 from plasmapy.utils.pytest_helpers.expected import ExpectedTestOutcome
 from plasmapy.utils.pytest_helpers.inputs import FunctionTestInputs
 from plasmapy.utils.pytest_helpers.comparators import CompareActualExpected
+from plasmapy.utils.pytest_helpers.exceptions import InvalidTestError
 
 
 class SampleException(Exception):
@@ -23,12 +26,20 @@ class SampleWarningSubclass(SampleWarning):
     pass
 
 
-def return_42():
+def return_42() -> int:
     return 42
 
 
-def issue_sample_warning_and_return_42():
-    warnings.warn('warning message', SampleWarning)
+def return_42_meters() -> u.Quantity:
+    return 42.0 * u.m
+
+
+def return_np_array(*args) -> np.array:
+    return np.array(args)
+
+
+def issue_sample_warning_and_return_42() -> int:
+    warnings.warn("warning message", SampleWarning)
     return 42
 
 
@@ -40,8 +51,11 @@ def return_sum_of_two_args_and_kwargs(arg1, arg2, *, kw1=None, kw2=None):
     return arg1 + arg2 + kw1 + kw2
 
 
-class SampleClass:
+def return_none():
+    return None
 
+
+class SampleClass:
     def __init__(self, *args, **kwargs):
         pass
 
@@ -49,7 +63,7 @@ class SampleClass:
         return arg + kwarg
 
     @property
-    def forty(self):
+    def forty(self) -> int:
         return 40
 
 
@@ -65,40 +79,137 @@ nokwargs = {}
         (raise_sample_exception, noargs, nokwargs, SampleExceptionSubclass, False),
         (raise_sample_exception, noargs, nokwargs, 42, False),
         (raise_sample_exception, noargs, nokwargs, SampleWarning, False),
-
         (return_42, noargs, nokwargs, 42, True),
         (return_42, noargs, nokwargs, 6 * 9, False),
+        (return_42, noargs, nokwargs, 42.0, False),
         (return_42, noargs, nokwargs, SampleException, False),
         (return_42, noargs, nokwargs, (SampleWarning, 42), False),
         (return_42, noargs, nokwargs, (42, SampleWarning), False),
-
+        (return_42_meters, noargs, nokwargs, 42.0 * u.m, True),
+        (return_42_meters, noargs, nokwargs, 42.00000000000001 * u.m, True),
+        (return_42_meters, noargs, nokwargs, 4200.0 * u.cm, False),
+        (return_42_meters, noargs, nokwargs, 42 * u.kg, False),
+        (return_42_meters, noargs, nokwargs, 6 * 9 * u.m, False),
+        (return_42_meters, noargs, nokwargs, u.m, True),
+        (return_42_meters, noargs, nokwargs, u.cm, False),
+        (return_none, noargs, nokwargs, None, True),
+        (return_none, noargs, nokwargs, False, False),
         (issue_sample_warning_and_return_42, noargs, nokwargs, SampleWarning, True),
-        (issue_sample_warning_and_return_42, noargs, nokwargs, SampleWarningSubclass, False),
+        (
+            issue_sample_warning_and_return_42,
+            noargs,
+            nokwargs,
+            SampleWarningSubclass,
+            False,
+        ),
         (issue_sample_warning_and_return_42, noargs, nokwargs, Warning, False),
-
-        (issue_sample_warning_and_return_42, noargs, nokwargs, (SampleWarning, 42), True),
-        (issue_sample_warning_and_return_42, noargs, nokwargs, (42, SampleWarning), True),
-        (issue_sample_warning_and_return_42, noargs, nokwargs, (SampleWarning, 6 * 9), False),
-        (issue_sample_warning_and_return_42, noargs, nokwargs, (6 * 9, SampleWarning), False),
-
-        (issue_sample_warning_and_return_42, noargs, nokwargs, (SampleWarningSubclass, 42), False),
-        (issue_sample_warning_and_return_42, noargs, nokwargs, (42, SampleWarningSubclass), False),
+        (
+            issue_sample_warning_and_return_42,
+            noargs,
+            nokwargs,
+            (SampleWarning, 42),
+            True,
+        ),
+        (
+            issue_sample_warning_and_return_42,
+            noargs,
+            nokwargs,
+            (42, SampleWarning),
+            True,
+        ),
+        (
+            issue_sample_warning_and_return_42,
+            noargs,
+            nokwargs,
+            (SampleWarning, 6 * 9),
+            False,
+        ),
+        (
+            issue_sample_warning_and_return_42,
+            noargs,
+            nokwargs,
+            (6 * 9, SampleWarning),
+            False,
+        ),
+        (
+            issue_sample_warning_and_return_42,
+            noargs,
+            nokwargs,
+            (SampleWarningSubclass, 42),
+            False,
+        ),
+        (
+            issue_sample_warning_and_return_42,
+            noargs,
+            nokwargs,
+            (42, SampleWarningSubclass),
+            False,
+        ),
         (issue_sample_warning_and_return_42, noargs, nokwargs, (Warning, 42), False),
         (issue_sample_warning_and_return_42, noargs, nokwargs, (42, Warning), False),
-
         # if unexpected warnings should cause a failure
         (issue_sample_warning_and_return_42, noargs, nokwargs, 42, False),
-
-        (return_sum_of_two_args_and_kwargs, (2, 3), {'kw1': 5, 'kw2': 7}, 17, True),
-        (return_sum_of_two_args_and_kwargs, (2, 3), {'kw1': 5, 'kw2': 7}, 42, False),
-        (return_sum_of_two_args_and_kwargs, (2, 3), {'kw1': 5, 'kw2': 7}, Warning, False),
-        (return_sum_of_two_args_and_kwargs, (2, 3), {'kw1': 5, 'kw2': 7}, Exception, False),
-        (return_sum_of_two_args_and_kwargs, (2, 3), {'kw1': 5, 'kw2': 7}, (Warning, 17), False),
-        (return_sum_of_two_args_and_kwargs, (2, 3), {'kw1': 5, 'kw2': 7}, (17, Warning), False),
-        (return_sum_of_two_args_and_kwargs, (2, 3), {'kw1': 5, 'kw2': 7}, '17', False),
-
-    ])
+        (return_sum_of_two_args_and_kwargs, (2, 3), {"kw1": 5, "kw2": 7}, 17, True),
+        (return_sum_of_two_args_and_kwargs, (2, 3), {"kw1": 5, "kw2": 7}, 42, False),
+        (
+            return_sum_of_two_args_and_kwargs,
+            (2, 3),
+            {"kw1": 5, "kw2": 7},
+            Warning,
+            False,
+        ),
+        (
+            return_sum_of_two_args_and_kwargs,
+            (2, 3),
+            {"kw1": 5, "kw2": 7},
+            Exception,
+            False,
+        ),
+        (
+            return_sum_of_two_args_and_kwargs,
+            (2, 3),
+            {"kw1": 5, "kw2": 7},
+            (Warning, 17),
+            False,
+        ),
+        (
+            return_sum_of_two_args_and_kwargs,
+            (2, 3),
+            {"kw1": 5, "kw2": 7},
+            (17, Warning),
+            False,
+        ),
+        (return_sum_of_two_args_and_kwargs, (2, 3), {"kw1": 5, "kw2": 7}, "17", False),
+    ],
+)
 def test_comparator_actual_expected(function, args, kwargs, expected, test_should_pass):
+    """
+    Test that `CompareActualExpected` correctly determines whether or
+    not a test passes.
+
+    Parameters
+    ----------
+    function
+        The sample function to be tested
+
+    args
+        A `tuple` or `list` of positional arguments
+
+    kwargs
+        A dictionary of keyword arguments
+
+    expected
+        The expected outcome of the test to be provided to
+        `CompareActualExpected`.  The value of this ``expected`` will
+        sometimes be correct and sometimes  be incorrect in order to
+        perform test this class.
+
+    test_should_pass
+        `True` if ``function(*args, **kwargs)`` should result in
+        ``expected``, and `False` otherwise.  Equivalently, this
+        argument should be `True` if ``expected`` is correct and
+        `False` if ``expected`` is incorrect.
+    """
 
     test_information = (
         "\n\n"
@@ -122,20 +233,27 @@ def test_comparator_actual_expected(function, args, kwargs, expected, test_shoul
     try:
         comparison = CompareActualExpected(actual, expected)
     except Exception:
-        pytest.fail(
-            f"Unable to instantiate CompareActualExpected."
-            f"{test_information}")
+        raise InvalidTestError(
+            f"Unable to instantiate CompareActualExpected." f"{test_information}"
+        )
 
     test_information += f"\ncomparison.test_passed = {comparison.test_passed}"
 
     if comparison.error_message:
         test_information += (
-            f"\n\n"
-            f"Presumably incorrect error message: "
-            + comparison.error_message
+            f"\n\n" f"Presumably incorrect error message: " + comparison.error_message
         )
 
     if test_should_pass and not comparison.test_passed:
         pytest.fail("Test should have passed, but did not." + test_information)
+
     if not test_should_pass and comparison.test_passed:
         pytest.fail("Test should not have passed, but did." + test_information)
+
+
+def test_comparator_atol():
+    raise NotImplementedError
+
+
+def test_comparator_rtol():
+    raise NotImplementedError
