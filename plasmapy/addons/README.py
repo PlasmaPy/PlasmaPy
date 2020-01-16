0804be7ab2c3a1cc623419a3b353b13a471d1df3
Erik Everson <eteverson@gmail.com>
2020-01-15 21:37:52 -0800

    updated about() s.t. it can read the README.md when installed inside the plasmapy package

--- plasmapy/addons/README.py
+++ plasmapy/addons/README.py
@@ -1,3 +1,7 @@
+from pkg_resources import resource_filename
+
+
 def about():
-    with open('README.md', encoding='utf-8') as f:
+    fname = resource_filename('plasmapy', 'addons/README.md')
+    with open(fname, encoding='utf-8') as f:
         print(f.read())
