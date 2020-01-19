b0914404c6d7f52fe11db2c699fb26ca39d70f5d
Dominik Stańczak <stanczakdominik@gmail.com>
2020-01-19 19:05:24 +0100

    Fix up shapes for AnalyticalPlasma

--- plasmapy/classes/sources/analyticalplasma.py
+++ plasmapy/classes/sources/analyticalplasma.py
@@ -4,6 +4,7 @@ Defines the core Plasma class used by PlasmaPy to represent plasma properties.
 import astropy.units as u
 
 from plasmapy.classes import GenericPlasma
+import numpy as np
 
 __all__ = [
     "AnalyticalPlasma"
@@ -23,8 +24,21 @@ class AnalyticalPlasma(GenericPlasma):
         The most basic description is composition (ion), temperature,
         density, and ionization.
         """
-        self.interpolate_B = lambda r: u.Quantity(len(r) * [magnetic_field(r)])  # TODO I think this may be off...
-        self.interpolate_E = lambda r: u.Quantity(len(r) * [electric_field(r)])
+        test_array = np.zeros((4, 3))
+        if magnetic_field(test_array).shape != test_array.shape:
+            self._interpolate_B = lambda r: np.array([magnetic_field(ri) for ri in r])
+        else: 
+            self._interpolate_B = magnetic_field
+        if electric_field(test_array).shape != test_array.shape:
+            self._interpolate_E = lambda r: np.array([electric_field(ri) for ri in r])
+        else: 
+            self._interpolate_E = electric_field
+
+    def interpolate_B(self, r):
+        return u.Quantity(self._interpolate_B(r), u.T, copy = False)
+
+    def interpolate_E(self, r):
+        return u.Quantity(self._interpolate_E(r), u.V / u.m, copy = False)
 
     @classmethod
     def is_datasource_for(cls, **kwargs):
