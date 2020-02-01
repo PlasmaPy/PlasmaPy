cef28b9a6b9fe0f0f6751ad30044e12cfa45755f
Jakub Polak <jakubpolak95@gmail.com>
2020-02-01 20:28:31 +0100

    Add changelog 743

--- plasmapy/formulary/magnetostatics.py
+++ plasmapy/formulary/magnetostatics.py
@@ -49,12 +49,6 @@ class MagneticDipole(MagnetoStatics):
 
     @validate_quantities
     def __init__(self, moment: u.A * u.m**2, p0: u.m):
-        print(p0)
-        print(type(p0))
-
-        print(p0.unit)
-        print(moment.unit)
-
         self.moment = moment.value
         self._moment_u = moment.unit
         self.p0 = p0.value
