07479619a17efbffa75a8f4609986a39ca958e81
Diego Diaz <diego7319@users.noreply.github.com>
2020-02-05 12:44:25 -0500

    Freqto hz test doc (#756)
    * multiline¿
    
    * -added changelog.
    -Fix E122
    
    * -Try 1 fix E122 PEP8

--- plasmapy/utils/decorators/tests/test_converters.py
+++ plasmapy/utils/decorators/tests/test_converters.py
@@ -8,8 +8,11 @@ def test_conversion():
     def test_function():
         return 2 * np.pi * u.rad / u.s
 
-    assert test_function().unit == (u.rad / u.s)
-    assert test_function().value == 2 * np.pi
-    assert test_function(to_hz=True).unit == u.Hz
-    assert test_function(to_hz=True).value == 1
-
+    assert test_function().unit == (u.rad / u.s), \
+        f'Unit expected is {(u.rad / u.s)} instead of {test_function().unit}'
+    assert test_function().value == 2 * np.pi, \
+        f'Value expected is {2 * np.pi} instead of {test_function().value}'
+    assert test_function(to_hz=True).unit == u.Hz, \
+        f'Unit expected is {u.Hz} instead of {test_function(to_hz=True).unit}'
+    assert test_function(to_hz=True).value == 1, \
+        f'Value expected is {1} instead of {test_function(to_hz=True).value}'
