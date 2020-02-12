8f2e5b220d5b8349892f4f6969585806c0539c20
Nick Murphy <namurphy@cfa.harvard.edu>
2020-02-12 18:52:58 -0500

    Reformat data directory with black

--- plasmapy/data/test/__init__.py
+++ plasmapy/data/test/__init__.py
@@ -4,4 +4,4 @@ import glob
 import plasmapy
 
 rootdir = os.path.join(os.path.dirname(plasmapy.__file__), "data", "test")
-file_list = glob.glob(os.path.join(rootdir, '*.[!p]*'))
+file_list = glob.glob(os.path.join(rootdir, "*.[!p]*"))
