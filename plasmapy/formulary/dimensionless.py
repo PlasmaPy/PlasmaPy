cfdd4df8a431315fda3f21e5f004e1564a611da0
diego diaz <diego7319@gmail.com>
2020-03-01 00:55:37 -0500

    added tests for prandtl number and magnetic prandtl number

--- plasmapy/formulary/dimensionless.py
+++ plasmapy/formulary/dimensionless.py
@@ -8,7 +8,7 @@ For example, plasmas at high (much larger than 1) Reynolds numbers are
 highly turbulent, while turbulence is negligible at low Reynolds
 numbers.
 """
-__all__ = ["beta", "quantum_theta","magnetic_prandtl_number"]
+__all__ = ["beta", "quantum_theta", "magnetic_prandtl_number"]
 
 from astropy import constants
 from astropy import units as u
@@ -92,18 +92,64 @@ def beta(T: u.K, n: u.m ** -3, B: u.T) -> u.dimensionless_unscaled:
 
 
 @validate_quantities(
-v = {"can_be_negative": False},
-n = {"can_be_negative": False}
+    v = {"can_be_negative": False},
+    alpha = {"can_be_negative": False},
+)
+def prandtl_number(v: u.m ** 2 / u.s,
+                   alpha: u.m ** 2 / u.s) -> u.dimensionless_unscaled:
+    r"""
+    The ratio of momentum diffusivity to thermal diffusivity..
+
+    Parameters
+    ----------
+    v : ~astropy.units.Quantity
+        The momentum diffusivity (kinematic viscosity).
+    alpha : ~astropy.units.Quantity
+        The thermal diffusivity.
+
+    Returns
+    -------
+    prandtl_number: ~astropy.units.Quantity
+        Dimensionless quantity.
+
+    Notes
+    -----
+    The Prandtl number is given as [1]_:
+
+    .. math::
+        \mathrm{Pr} =  \frac{\nu}{\alpha}
+
+    Examples
+    --------
+    >>> import astropy.units as u
+    >>> prandtl_number(8e-2*u.m**2/u.s, 3e2*u.m**2/u.s)
+    <Quantity 0.00026667>
+    >>> prandtl_number(4e2*u.m**2/u.s,2e-10*u.m**2/u.s)
+    <Quantity 2.e+12>
+
+
+    References
+    ----------
+    .. [1] https://en.wikipedia.org/wiki/Prandtl_number
+
+    """
+
+    return v / alpha
+
+
+@validate_quantities(
+    v = {"can_be_negative": False},
+    n = {"can_be_negative": False},
 )
 def magnetic_prandtl_number(v: u.m ** 2 / u.s,
-                            n: u.m ** 2 / u.s ) -> u.dimensionless_unscaled:
+                            n: u.m ** 2 / u.s) -> u.dimensionless_unscaled:
     r"""
     The ratio of momentum diffusivity (viscosity) and magnetic diffusivity.
 
     Parameters
     ----------
     v : ~astropy.units.Quantity
-        The the momentum diffusivity (kinematic viscosity).
+        The momentum diffusivity (kinematic viscosity).
     n : ~astropy.units.Quantity
         The magnetic diffusivity.
 
@@ -114,7 +160,7 @@ def magnetic_prandtl_number(v: u.m ** 2 / u.s,
 
     Notes
     -----
-    The plasma dispersion function is defined as [1]_:
+    The magnetic Prandtl number is given as [1]_:
 
     .. math::
         \mathrm{Pr}_\mathrm{m} =  \frac{\nu}{\eta}
--- plasmapy/formulary/tests/test_dimensionless.py
+++ plasmapy/formulary/tests/test_dimensionless.py
@@ -1,4 +1,9 @@
-from plasmapy.formulary.dimensionless import beta, quantum_theta
+from plasmapy.formulary.dimensionless import (
+     beta,
+     quantum_theta,
+     prandtl_number,
+     magnetic_prandtl_number
+)
 
 import astropy.units as u
 import numpy as np
@@ -6,7 +11,7 @@ import numpy as np
 B = 1.0 * u.T
 n = 5e19 * u.m ** -3
 T = 1e6 * u.K
-
+v = 1e4 * (u.m ** 2 / u.s)
 
 def test_beta_dimensionless():
     # Check that beta is dimensionless
@@ -27,10 +32,27 @@ def test_beta_nan():
     assert np.isnan(out[1])
     assert out[1].unit == u.dimensionless_unscaled
 
-def test_magnetic_prandtl_number():
-    #TO DO
-    pass
 
 def test_prandtl_number():
-    #TO DO
-    pass
+    assert1 = prandtl_number(v, 1e4 * (u.m ** 2 / u.s))
+    error1 = f"""Prandtl number should be 1 when the momentum diffusivity(v) and
+              thermal diffusivity are equal(alpha) instead of {assert1}"""
+    assert2 = prandtl_number(v, 1e6 * (u.m ** 2 / u.s))
+    error2 = f"""Prandtl number should be 2 when the momentum diffusivity(v)
+             and thermal diffusivity(alpha) are 10**4 and 10**6 instead of
+             {assert2}"""
+    assert assert1 ==  1.0 , error1
+    assert assert2 == 0.01 , error2
+
+
+def test_magnetic_prandtl_number():
+    assert1 = magnetic_prandtl_number(v, 1e4 * (u.m ** 2 / u.s))
+    error1 = f"""Magnetic prandtl number should be 1 when the momentum
+             diffusivity(v) and magnetic diffusivity(n) are equal instead of
+             {assert1}"""
+    assert2 = magnetic_prandtl_number(v, 1e6 * (u.m ** 2 / u.s))
+    error2 = f"""Magnetic prandtl number should be 2 when the momentum
+             diffusivity(v) and magnetic diffusivity(n) are 10**4 and 10**6
+             instead of {assert2}"""
+    assert assert1 ==  1.0 , error1
+    assert assert2 == 0.01 , error2
