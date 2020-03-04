10287a3aeaf82fa1b1df2036e0971f1acdbd8ee8
Dominik Stańczak <stanczakdominik@gmail.com>
2020-03-04 18:43:55 +0100

    Setup files cleanup (#771)
    * Simplify version check using pkg_resources
    
    This is a follow-up simplification for #763.
    
    I picked this up while reading an xarray PR from @crusadersky:
    https://github.com/pydata/xarray/pull/3714/files
    
    * Simplify version.py file out
    
    as far as I can tell, it has no actual use if we can simply use pkg_resources
    
    * Further setup cleanup
    
    * Remove perhaps the last leftover of astropy_helpers
    
    ... a man can dream!
    
    * Add a changelog entry

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
-import os
-import sys
 from itertools import chain
-import builtins
 
 from setuptools import setup
 from setuptools.config import read_configuration
-
-# Append cwd for pip 19
-sys.path.append(os.path.abspath("."))
 
 ################################################################################
 # Programmatically generate some extras combos.
@@ -24,24 +18,7 @@ ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
 # Concatenate all the values together for 'all'
 extras["all"] = list(chain.from_iterable(ex_extras.values()))
 
-VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
-try:
-    from setuptools_scm import get_version
-    __version__ = get_version(root='..', relative_to=__file__)
-except Exception:
-    __version__ = '{version}'
-""".lstrip()
-
 # Get configuration information from all of the various subpackages.
 # See the docstring for setup_helpers.update_package_files for more
 # details.
-setup(
-    extras_require=extras,
-    use_scm_version={
-        "write_to": os.path.join("plasmapy", "version.py"),
-        "write_to_template": VERSION_TEMPLATE,
-    },
-)
+setup(extras_require=extras, use_scm_version=True)
