fbe5fb49cc03be469fe249c088bd4995425b2f20
Nick Murphy <namurphy@cfa.harvard.edu>
2020-03-19 15:32:43 -0400

    Expand tests for classes to contain test case objects
    The tests for FunctionTestCase, MethodTestCase, and AttrTestCase only need
    to check whether or not the objects are stored correctly, since all of the
    checks are done in different classes.

--- plasmapy/tests/helpers/tests/test_case_classes.py
+++ plasmapy/tests/helpers/tests/test_case_classes.py
@@ -1,31 +1,95 @@
 """Test the classes responsible for holding test cases."""
 
-from plasmapy.tests.helpers.cases import (FunctionTestCase, MethodTestCase, AttrTestCase)
-from plasmapy.tests.helpers.tests.sample_functions import return_42
+import pytest
+
+from plasmapy.tests.helpers.cases import FunctionTestCase, MethodTestCase, AttrTestCase
+from plasmapy.tests.helpers.tests.sample_functions import return_42, SampleClass2
 
 
-def test_instantiation_and_storage():
-    """..."""
+class TestFunctionTestCase:
+    """Test that `FunctionTestCase` correctly stores its attributes."""
 
-    function = return_42
-    args = (52, 24)
-    kwargs = {"x": 38}
-    expected = 42
-    atol = 1e-7
-    rtol = 1e-8
+    def setup_class(self):
 
-    case = FunctionTestCase(
-        function=function,
-        args=args,
-        kwargs=kwargs,
-        expected=expected,
-        rtol=rtol,
-        atol=atol,
-    )
+        self.function = return_42
+        self.args = (52, 24)
+        self.kwargs = {"x": 38}
+        self.expected = 42
+        self.atol = 1e-7
+        self.rtol = 1e-9
 
-    assert case.function is function
-    assert case.args == args
-    assert case.kwargs == kwargs
-    assert case.expected == expected
-    assert case.atol == atol
-    assert case.rtol == rtol
+        self.instance = FunctionTestCase(
+            expected=self.expected,
+            function=self.function,
+            args=self.args,
+            kwargs=self.kwargs,
+            atol=self.atol,
+            rtol=self.rtol,
+        )
+
+    @pytest.mark.parametrize("attr", ["expected", "args", "kwargs", "atol", "rtol"])
+    def test_that_attribute_is_stored_correctly(self, attr):
+        assert getattr(self, attr) is getattr(self.instance, attr)
+
+    def test_that_stored_function_has_correct_name(self):
+        assert self.function.__name__ == self.instance.function.__name__ == "return_42"
+
+
+class TestMethodTestCase:
+    """Test that `MethodTestCase` correctly stores its attributes."""
+
+    def setup_class(self):
+
+        self.expected = Exception
+        self.cls = SampleClass2
+        self.method = "method_name"
+        self.cls_args = (23, 32)
+        self.cls_kwargs = {"x": 835}
+        self.method_args = (32, 22)
+        self.method_kwargs = {"y": 53}
+        self.atol = 1e-23
+        self.rtol = 1e-22
+
+        self.instance = MethodTestCase(
+            expected=self.expected,
+            cls=self.cls,
+            method=self.method,
+            cls_args=self.cls_args,
+            cls_kwargs=self.cls_kwargs,
+            method_args=self.method_args,
+            method_kwargs=self.method_kwargs,
+            atol=self.atol,
+            rtol=self.rtol,
+        )
+
+    @pytest.mark.parametrize("attr", ["expected", "cls", "method", "cls_args", "cls_kwargs", "method_args", "method_kwargs", "atol", "rtol"])
+    def test_that_attribute_is_stored_correctly(self, attr):
+        assert getattr(self, attr) is getattr(self.instance, attr)
+
+        
+class TestAttrTestCase:
+    """Test that `AttrTestCase` correctly stores its attributes."""
+
+    def setup_class(self):
+
+        self.expected = Exception
+        self.cls = SampleClass2
+        self.attribute = "attr_name"
+        self.cls_args = (23, 32)
+        self.cls_kwargs = {"x": 835}
+        self.atol = 1e-23
+        self.rtol = 1e-22
+
+        self.instance = AttrTestCase(
+            expected=self.expected,
+            cls=self.cls,
+            attribute=self.attribute,
+            cls_args=self.cls_args,
+            cls_kwargs=self.cls_kwargs,
+            atol=self.atol,
+            rtol=self.rtol,
+        )
+
+    @pytest.mark.parametrize("attr", ["expected", "cls", "attribute", "cls_args", "cls_kwargs", "atol", "rtol"])
+    def test_that_attribute_is_stored_correctly(self, attr):
+        assert getattr(self, attr) is getattr(self.instance, attr)
