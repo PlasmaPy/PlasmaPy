9ae4aa6143cde1af5c61d8301e64a97938c4b09a
Nick Murphy <namurphy@cfa.harvard.edu>
2020-03-18 22:39:33 -0400

    Expand classes for storing method/attribute test cases

--- plasmapy/tests/helpers/cases.py
+++ plasmapy/tests/helpers/cases.py
@@ -28,8 +28,7 @@ class _AbstractTestCase(ABC):
 
 class FunctionTestCase(_AbstractTestCase):
     """
-    Contains the function to be tested, the arguments to be provided to
-    the function during the test, and the test's expected outcome.
+    Stores the objects needed to test a function.
 
     Parameters
     ----------
@@ -119,8 +118,58 @@ class FunctionTestCase(_AbstractTestCase):
         return self._atol
 
 
-class MethodTestCase(_AbstractTestCase):
-    """..."""
+class MethodTestCase:
+    """
+    Stores the objects needed to test a method of a class instance.
+
+    Parameters
+    ----------
+    expected
+        The expected outcome of the test, which can be an exception,
+        a warning, the resulting object, or a tuple that contains
+        a warning and the resulting object.
+
+    cls
+        The class containing the method to be tested.
+
+    method : str
+        The name of the method to be tested.
+
+    cls_args : optional, keyword-only
+        The positional arguments to be provided to ``cls`` during
+        instantiation.  If this is a `tuple` or `list`, then the
+        arguments will be each of the items in the collection. If there
+        is only one positional argument, then it may be inputted as is
+        without being put into a `tuple` or `list`.
+
+    cls_kwargs : `dict`, optional, keyword-only
+        The keyword arguments to be provided to ``cls`` during
+        instantiation. If provided, the keys of this `dict` must be
+        strings.
+
+    method_args : optional, keyword-only
+        The positional arguments to be provided to the method during the
+        test.  If this is a `tuple` or `list`, then the arguments will
+        be each of the items in the collection. If there is only one
+        positional argument, then it may be inputted as is without being
+        put into a `tuple` or `list`.
+
+    method_kwargs : `dict`, optional, keyword-only
+        The keyword arguments to be provided to the method during the
+        test. If provided, the keys of this `dict` must be
+        strings.
+
+    rtol : dimensionless number, optional, keyword-only
+        The relative tolerance to be used in `astropy.units.allclose`.
+        Defaults to ``1e-8``.  If ``rtol`` is a `~astropy.units.Quantity`
+        instance, then it must be dimensionless.
+
+    atol : number or `~astropy.units.Quantity`, optional, keyword-only
+        The absolute tolerance to be supplied to `astropy.units.isclose`
+        or `~astropy.units.allclose`.  If ``atol`` is a
+        `~astropy.units.Quantity`, then it must have the same units as
+        ``this`` and ``that``.  Defaults to zero in the appropriate units.
+    """
 
     def __init__(
         self,
@@ -137,20 +186,115 @@ class MethodTestCase(_AbstractTestCase):
         purpose: Optional[str] = None,
     ):
 
-        self.expected = expected
-        self.cls = cls
-        self.method = method
-        self.cls_args = cls_args
-        self.cls_kwargs = {} if cls_kwargs is None else cls_kwargs
-        self.method_args = method_args
-        self.method_kwargs = method_kwargs
-        self.purpose = str(purpose)
-        self.atol = atol
-        self.rtol = rtol
+        self._expected = expected
+        self._cls = cls
+        self._method = method
+        self._cls_args = cls_args
+        self._cls_kwargs = {} if cls_kwargs is None else cls_kwargs
+        self._method_args = method_args
+        self._method_kwargs = method_kwargs
+        self._purpose = str(purpose)
+        self._atol = atol
+        self._rtol = rtol
+
+    @property
+    def expected(self):
+        """The expected outcome of the test."""
+        return self._expected
+
+    @property
+    def cls(self):
+        """The class containing the method to be tested."""
+        return self._cls
+
+    @property
+    def method(self) -> str:
+        """The name of the method to be tested."""
+        return self._method
+
+    @property
+    def cls_args(self):
+        """
+        The positional arguments to be provided to ``cls`` during
+        instantiation.
+        """
+        return self._cls_args
+
+    @property
+    def cls_kwargs(self) -> Dict[str, Any]:
+        """
+        The keyword arguments to be provided to ``cls`` during
+        instantiation.
+        """
+        return self._cls_kwargs
+
+    @property
+    def method_args(self):
+        """
+        The positional arguments to be provided to the method during the
+        test.
+        """
+        return self._method_args
+
+    @property
+    def method_kwargs(self) -> Dict[str, Any]:
+        """
+        The keyword arguments to be provided to ``cls`` during
+        instantiation.
+        """
+        return self._cls_kwargs
+
+    @property
+    def rtol(self):
+        """The relative tolerance for numerical comparisons."""
+        return self._rtol
+
+    @property
+    def atol(self):
+        """The absolute tolerance for numerical comparisons."""
+        return self._atol
 
 
-class AttrTestCase(_AbstractTestCase):
-    """..."""
+class AttrTestCase:
+    """
+    Stores the objects needed to test an attribute of a class instance.
+
+    Parameters
+    ----------
+    expected
+        The expected outcome of the test, which can be an exception,
+        a warning, the resulting object, or a tuple that contains
+        a warning and the resulting object.
+
+    cls
+        The class containing the attribute to be tested.
+
+    attribute : str
+        The name of the attribute to be tested.
+
+    cls_args : optional, keyword-only
+        The positional arguments to be provided to ``cls`` during
+        instantiation.  If this is a `tuple` or `list`, then the
+        arguments will be each of the items in the collection. If there
+        is only one positional argument, then it may be inputted as is
+        without being put into a `tuple` or `list`.
+
+    cls_kwargs : `dict`, optional, keyword-only
+        The keyword arguments to be provided to ``cls`` during
+        instantiation. If provided, the keys of this `dict` must be
+        strings.
+
+    rtol : dimensionless number, optional, keyword-only
+        The relative tolerance to be used in `astropy.units.allclose`.
+        Defaults to ``1e-8``.  If ``rtol`` is a `~astropy.units.Quantity`
+        instance, then it must be dimensionless.
+
+    atol : number or `~astropy.units.Quantity`, optional, keyword-only
+        The absolute tolerance to be supplied to `astropy.units.isclose`
+        or `~astropy.units.allclose`.  If ``atol`` is a
+        `~astropy.units.Quantity`, then it must have the same units as
+        ``this`` and ``that``.  Defaults to zero in the appropriate units.
+    """
 
     def __init__(
         self,
@@ -160,16 +304,55 @@ class AttrTestCase(_AbstractTestCase):
         *,
         cls_args=(),
         cls_kwargs: Optional[Dict[str, Any]] = None,
-        purpose: Optional[str] = None,
         atol=None,
         rtol=1e-8,
     ):
 
-        self.expected = expected
-        self.cls = cls
-        self.attribute = attribute
-        self.cls_args = cls_args
-        self.cls_kwargs = {} if cls_kwargs is None else cls_kwargs
-        self.purpose = purpose
-        self.atol = atol
-        self.rtol = rtol
+        self._expected = expected
+        self._cls = cls
+        self._attribute = attribute
+        self._cls_args = cls_args
+        self._cls_kwargs = {} if cls_kwargs is None else cls_kwargs
+        self._atol = atol
+        self._rtol = rtol
+
+    @property
+    def expected(self):
+        """The expected outcome of the test."""
+        return self._expected
+
+    @property
+    def cls(self):
+        """The class containing the method to be tested."""
+        return self._cls
+
+    @property
+    def attribute(self) -> str:
+        """The name of the attribute to be tested."""
+        return self._attribute
+
+    @property
+    def cls_args(self):
+        """
+        The positional arguments to be provided to ``cls`` during
+        instantiation.
+        """
+        return self._cls_args
+
+    @property
+    def cls_kwargs(self) -> Dict[str, Any]:
+        """
+        The keyword arguments to be provided to ``cls`` during
+        instantiation.
+        """
+        return self._cls_kwargs
+
+    @property
+    def rtol(self):
+        """The relative tolerance for numerical comparisons."""
+        return self._rtol
+
+    @property
+    def atol(self):
+        """The absolute tolerance for numerical comparisons."""
+        return self._atol
