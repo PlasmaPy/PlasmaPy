fadd184a542d2de2c98233148d0d62c5b2011aad
Erik Everson <eteverson@gmail.com>
2020-04-10 22:27:56 -0700

    define _parse_readme and about functions

--- plasmapy/addons/__init__.py
+++ plasmapy/addons/__init__.py
@@ -1,7 +1,15 @@
+
+
 from pkg_resources import resource_filename
 
 
-def about():
+def _parse_readme():
     fname = resource_filename('plasmapy', 'addons/README.md')
     with open(fname, encoding='utf-8') as f:
-        print(f.read())
+        readme = f.read()
+
+    return readme
+
+
+def about():
+    print(_parse_readme())
