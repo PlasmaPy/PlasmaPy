b6d61cd3cd12621d6015b98707da77d8cd937c21
Erik Everson <eteverson@gmail.com>
2020-05-12 19:37:41 -0700

    create `AbstractProbe` class

--- plasmapy/diagnostics/__init__.py
+++ plasmapy/diagnostics/__init__.py
@@ -1,3 +1,10 @@
 """The diagnostics subpackage contains tools for experimental research.
 Currently, we have functionality for analyzing data from Langmuir probes.
 """
+__all__ = ['AbstractProbe']
+
+import abc
+
+
+class AbstractProbe(abc.ABC):
+    """Abstract class for defining probe characteristics/parameters."""
