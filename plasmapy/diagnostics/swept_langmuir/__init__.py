415a4a07b264994f753d7e4f2fd46afa264fb0da
Erik Everson <eteverson@gmail.com>
2020-05-15 11:02:19 -0700

    add XSweptLangmuirDiagnostic to plasmapy.diagnostics.swept_langmuir namespace

--- plasmapy/diagnostics/swept_langmuir/__init__.py
+++ plasmapy/diagnostics/swept_langmuir/__init__.py
@@ -2,6 +2,7 @@
 Sub-package that fully defines framework for analysis Swept Langmuir traces
 traces within the `xarray` framework.
 """
-__all__ = ['SweptLangmuirProbe']
+__all__ = ["SweptLangmuirProbe", "XSweptLangmuirDiagnostic"]
 
 from plasmapy.diagnostics.swept_langmuir.probe import SweptLangmuirProbe
+from plasmapy.diagnostics.swept_langmuir.xdiagnostic import XSweptLangmuirDiagnostic
