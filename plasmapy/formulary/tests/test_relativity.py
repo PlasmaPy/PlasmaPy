b20e47b631b403242e56569ef69c4bf785e479b9
KhalilBryant <35078079+KhalilBryant@users.noreply.github.com>
2020-05-19 18:55:45 -0400

    Add `relativistic_energy` to the Formulary (#805)
    All looks good!  @KhalilBryant Congrats and great job on your first PR! 🎉

--- plasmapy/formulary/tests/test_relativity.py
+++ plasmapy/formulary/tests/test_relativity.py
@@ -2,9 +2,10 @@
 
 import pytest
 import numpy as np
+
 from astropy import units as u
 from astropy.constants import c
-from plasmapy.formulary.relativity import Lorentz_factor
+from plasmapy.formulary.relativity import Lorentz_factor, relativistic_energy
 from plasmapy.utils.exceptions import RelativityError
 
 
@@ -38,3 +39,37 @@ def test_Lorentz_factor():
 
     with pytest.raises(u.UnitTypeError):
         Lorentz_factor(4 * u.kg)
+
+
+def test_relativistic_energy():
+    r"""Test relativistic_energy in relativity.py"""
+
+    v = 123456789 * u.m / u.s
+    m = 1 * u.kg
+    assert np.isclose(relativistic_energy(m, v).value, ((1 / np.sqrt(1 - v ** 2 / c ** 2)) * m * c ** 2).value)
+    assert relativistic_energy(m, -v) == relativistic_energy(m, v)
+
+    assert np.isclose(relativistic_energy(m, 0 * u.m / u.s).value, (m * c ** 2).value)
+    assert relativistic_energy(m, c) == np.inf
+
+    V_arr = np.array([987532.0, 299792458]) * u.m / u.s
+    Energy_arr = relativistic_energy(m, V_arr)
+    assert np.isclose(Energy_arr[0].value, ((1 / np.sqrt(1 - V_arr[0] ** 2 / c ** 2)) * m * c ** 2).value)
+    assert Energy_arr[1] == np.inf
+
+    assert relativistic_energy(2 * u.kg, 3 * u.m / u.s).unit == u.J
+
+    with pytest.raises(RelativityError):
+        relativistic_energy(m, 1.0000000001 * c)
+
+    with pytest.raises(RelativityError), pytest.warns(u.UnitsWarning):
+        relativistic_energy(1, 299792459)
+
+    with pytest.warns(u.UnitsWarning):
+        relativistic_energy(m, 2.2)
+
+    with pytest.raises(u.UnitTypeError):
+        relativistic_energy(m, 4 * u.kg)
+
+    with pytest.raises(ValueError):
+        relativistic_energy(-m, v)
