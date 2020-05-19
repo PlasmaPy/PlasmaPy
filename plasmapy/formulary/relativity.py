ba823f82a7af109c12bec5a9b5b5e86723e0fe12
Erik Everson <eteverson@gmail.com>
2020-05-19 15:45:20 -0700

    Fix RelativityError not resolving on RTD

--- plasmapy/formulary/relativity.py
+++ plasmapy/formulary/relativity.py
@@ -125,7 +125,7 @@ def relativistic_energy(m: u.kg, v: u.m / u.s) -> u.Joule:
     ValueError
         If the magnitude of `m` is negative or arguments are complex.
 
-    `RelativityError`
+    :exc:`~plasmapy.utils.exceptions.RelativityError`
         If the velocity `v` is greater than the speed of light.
 
     Warns
