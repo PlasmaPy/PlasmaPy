6a568c5657844daceae5037eb9421c8c07241e17
Erik Everson <eteverson@gmail.com>
2020-06-02 00:05:46 -0700

    move `plasmapy.data` -> `plasmapy.particles.data`

--- plasmapy/particles/data/test/__init__.py
+++ plasmapy/particles/data/test/__init__.py
@@ -3,5 +3,5 @@ import glob
 
 import plasmapy
 
-rootdir = os.path.join(os.path.dirname(plasmapy.__file__), "data", "test")
+rootdir = os.path.join(os.path.dirname(plasmapy.__file__), "particles", "data", "test")
 file_list = glob.glob(os.path.join(rootdir, "*.[!p]*"))
