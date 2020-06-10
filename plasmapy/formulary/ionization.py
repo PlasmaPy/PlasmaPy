f49e025f83e5cd8a0f638c053153bedc2948c0c4
KhalilBryant <kalelb@umich.edu>
2020-06-10 19:37:32 -0400

    update plasmapy/formulary/ionization.py

--- plasmapy/formulary/ionization.py
+++ plasmapy/formulary/ionization.py
@@ -12,7 +12,7 @@ from astropy.constants import c, a0, k_B
 from numpy import pi, exp, sqrt, log
 
 
-def Z_bal(n: u.m ** -3, T: u.K):
+def Z_bal(n: u.m ** -3, T_e: u.K):
     r"""
     Z_bal is derived from the Saha equation with the assumptions that
     the atoms are of a single species,
@@ -22,10 +22,19 @@ def Z_bal(n: u.m ** -3, T: u.K):
     The Saha equation and therefore Z_bal are more accurate when the plasma
     is at a high density and temperature.
 
+    .. math::
+
+        Z\_bal = \sqrt{\frac{k_B T_e}{E_H}} \sqrt{\ln{\frac{1}{4 n a_{0}^3}
+        (\frac{k_B T_e}{\pi E_H})^{3/2}}} - \frac{1}{2}
+
+    Where :math:`k_B` is the Boltzmann constant,
+    :math:`a_0` is the Bohr radius, and
+    :math:`E_H` is the ionization energy of Hydrogen
+
     Parameters
     ----------
 
-    T : `~astropy.units.Quantity`
+    T_e : `~astropy.units.Quantity`
         The electron temperature.
 
     n : ~astropy.units.Quantity`
@@ -39,8 +48,8 @@ def Z_bal(n: u.m ** -3, T: u.K):
     """
     E_H = 13.6 * u.eV
 
-    A = sqrt(k_B * T / E_H)
-    B = log(1 / (4 * n * a0 ** 3) * (k_B * T / (pi * E_H)) ** (3 / 2))
+    A = sqrt(k_B * T_e / E_H)
+    B = log(1 / (4 * n * a0 ** 3) * (k_B * T_e / (pi * E_H)) ** (3 / 2))
 
     Z = A * sqrt(B) - 1 / 2
     return Z
