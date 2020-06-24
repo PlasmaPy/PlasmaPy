64be5109a885483de674f06d500a5aca5037443f
Dominik Stańczak <stanczakdominik@gmail.com>
2020-06-24 10:59:17 +0200

    test func

---
+++ plasmapy/formulary/verification.py
@@ -0,0 +1,18 @@
+def function(temperature, density):
+    """
+    Checks something nonsensical
+
+    like args or some such
+    ======================
+    temperature
+        float
+
+    density
+        int
+
+    triple_product
+        dict
+
+    """
+
+    return 42
