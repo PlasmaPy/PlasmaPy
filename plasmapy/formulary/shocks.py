64ad1607e49a42ed328f5ada77f340fea3026dab
KhalilBryant <kalelb@umich.edu>
2020-07-03 12:50:55 -0400

    update formulary/shocks.py

--- plasmapy/formulary/shocks.py
+++ plasmapy/formulary/shocks.py
@@ -1,6 +1,6 @@
 """ This module gathers functions relating to shocks and the properties thereof.
 """
-__all__ = [""]
+__all__ = ["entropy_across_shock_polytropic"]
 
 
 from astropy import units as u
@@ -15,9 +15,11 @@ from numpy import pi, exp, sqrt, log
 
 validate_quantities(
     rho_1={"can_be_negative": False}, rho_2={"can_be_negative": False})
-def entropy_across_shock_polytorpic(c_v: u.J / u.K, p_1: u.Bar, p_2: u.Bar, rho_1: u.kg / u.m ** 3, rho_2: u.kg / u.m ** 3, gamma) -> u.J / u.K:
+def entropy_across_shock_polytropic(c_v: u.J / u.K, p_1: u.Bar, p_2: u.Bar, rho_1: u.kg / u.m ** 3, rho_2: u.kg / u.m ** 3, gamma) -> u.J / u.K:
     r"""
-
+    Entropy is not conserved across a shock, since a shock is
+    an irreversible, non-adiabatic process. This equation is based on the specific entropy
+    of a polytropic gas before and after the shock has passed.
 
      .. math::
 
@@ -25,4 +27,35 @@ def entropy_across_shock_polytorpic(c_v: u.J / u.K, p_1: u.Bar, p_2: u.Bar, rho_
         c_v ln\left[ \frac{p_2}{p_1} \left( \frac{\rho_1}{\rho_2} \right)^{\gamma} \right]
     Here variables indexed by 1 and 2 are upstream (pre shock)
     and downstream (post shock) respectively.
+
+    Parameters
+    ----------
+    c_v : `~astropy.units.Quantity`
+        Heat capacity a constant volume for the gas.
+
+    p_1 : `~astropy.units.Quantity`
+        Upstream pressure of the gas.
+
+    p_2 : `~astropy.units.Quantity`
+        Downstream pressure of the gas.
+
+    rho_1 : `~astropy.units.Quantity`
+        Upstream mass density of the gas.
+
+    rho_2 : `~astropy.units.Quantity`
+        Downstream mass density of the gas.
+
+    gamma : `float`
+        Polytropic index of the gas.
+
+    Returns
+    -------
+    delta_s: `~astropy.units.Quantity`
+        The change of entropy due to the shock on the gas.
+
     """
+    pressure_ratio = p_2 / p_1
+    density_ratio = (rho_1 / rho_2)
+
+    delta_s = c_v * log(pressure_ratio * density_ratio ** gamma)
+    return delta_s
