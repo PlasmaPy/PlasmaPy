f84b4035325e001cb6074d9dab2388287ae986aa
KhalilBryant <kalelb@umich.edu>
2020-07-03 18:05:37 -0400

    update formulary/shocks.py

--- plasmapy/formulary/shocks.py
+++ plasmapy/formulary/shocks.py
@@ -2,7 +2,6 @@
 """
 __all__ = ["entropy_across_shock_polytropic"]
 
-
 from astropy import units as u
 from plasmapy.utils.decorators import (
     angular_freq_to_hz,
@@ -12,10 +11,15 @@ from plasmapy.utils.decorators import (
 from astropy.constants import c, a0, k_B
 from numpy import pi, exp, sqrt, log
 
-
 validate_quantities(
-    rho_1={"can_be_negative": False}, rho_2={"can_be_negative": False})
-def entropy_across_shock_polytropic(c_v: u.J / u.K, p_1: u.Bar, p_2: u.Bar, rho_1: u.kg / u.m ** 3, rho_2: u.kg / u.m ** 3, gamma) -> u.J / u.K:
+    rho_1={"can_be_negative": False},
+    rho_2={"can_be_negative": False},
+    ds={"can_be_negative": False},
+    c_v={"can_be_negative": False})
+
+
+def entropy_across_shock_polytropic(c_v: u.J / u.K, p_1: u.Pa, p_2: u.Pa, rho_1: u.kg / u.m ** 3,
+                                    rho_2: u.kg / u.m ** 3, gamma) -> u.J / u.K:
     r"""
     Entropy is not conserved across a shock, since a shock is
     an irreversible, non-adiabatic process. This equation is based on the specific entropy
@@ -53,9 +57,25 @@ def entropy_across_shock_polytropic(c_v: u.J / u.K, p_1: u.Bar, p_2: u.Bar, rho_
     ds: `~astropy.units.Quantity`
         The change of entropy due to the shock on the gas.
 
+     Examples
+    --------
+    >>> import astropy.units as u
+    >>> c_v = 15 * u.J / u.K
+    >>> gamma = 5 / 3 #ideal gas approx
+    >>> p_1 = 101325 * u.Pa
+    >>> p_2 = 2 * p_1
+    >>> rho_1 = 2 * u.kg / u.m ** 3
+    >>> rho_2 = 3 * u.kg / u.m ** 3
+    >>> entropy_across_shock_polytropic(c_v, p_1, p_2, rho_1, rho_2, gamma)
+    <Quantity 0.26058001 J / K>
+
     """
     pressure_ratio = p_2 / p_1
     density_ratio = (rho_1 / rho_2)
 
     ds = c_v * log(pressure_ratio * density_ratio ** gamma)
     return ds
+
+
+ds_polytropic = entropy_across_shock_polytropic
+"""alias for :func:`entropy_across_shock_polytropic`"""
--- plasmapy/formulary/tests/test_shocks.py
+++ plasmapy/formulary/tests/test_shocks.py
@@ -3,5 +3,19 @@ import numpy as np
 import pytest
 from astropy.constants import c
 
+from plasmapy.formulary import entropy_across_shock_polytropic
+
 
 def test_entropy_across_shock_polytropic():
+    c_v = 15 * u.J / u.K
+    gamma = 5 / 3  # ideal gas approx
+    p_1 = 101325 * u.Pa
+    p_2 = 2 * p_1
+    rho_1 = 2 * u.kg / u.m ** 3
+    rho_2 = 3 * u.kg / u.m ** 3
+    entropy_across_shock_polytropic(c_v, p_1, p_2, rho_1, rho_2, gamma)
+
+    assert (entropy_across_shock_polytropic(c_v, p_1, p_2, rho_1, rho_2, gamma)).unit == u.J / u.K
+
+    with pytest.raises(ValueError):
+        entropy_across_shock_polytropic(c_v, p_1, 0.5 * p_1, rho_1, rho_2, gamma)
