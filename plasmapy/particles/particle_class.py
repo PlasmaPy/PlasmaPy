41157249f72c73c4209a24696e1fae02721798e6
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2020-07-09 18:30:36 +0530

    Reformatted particle_class.py

--- plasmapy/particles/particle_class.py
+++ plasmapy/particles/particle_class.py
@@ -3,7 +3,7 @@
 __all__ = [
     "AbstractParticle",
     "CustomParticle",
-    "DimensionlessParticle", 
+    "DimensionlessParticle",
     "Particle",
     "ParticleJSONDecoder",
 ]
@@ -105,20 +105,29 @@ def _category_errmsg(particle, category: str) -> str:
 
 
 class ParticleJSONDecoder(json.JSONDecoder):
-    def __init__(self, *, object_hook=None, parse_float=None,
-                 parse_int=None, parse_constant=None, strict=True,
-                 object_pairs_hook=None):
+    def __init__(
+        self,
+        *,
+        object_hook=None,
+        parse_float=None,
+        parse_int=None,
+        parse_constant=None,
+        strict=True,
+        object_pairs_hook=None,
+    ):
 
         if object_hook is None:
             object_hook = self.particle_hook
 
-        json.JSONDecoder.__init__(self,
-                                  object_hook=object_hook,
-                                  parse_float=parse_float,
-                                  parse_int=parse_int,
-                                  parse_constant=parse_constant,
-                                  strict=strict,
-                                  object_pairs_hook=object_pairs_hook)
+        json.JSONDecoder.__init__(
+            self,
+            object_hook=object_hook,
+            parse_float=parse_float,
+            parse_int=parse_int,
+            parse_constant=parse_constant,
+            strict=strict,
+            object_pairs_hook=object_pairs_hook,
+        )
 
     @staticmethod
     def particle_hook(json_dict):
