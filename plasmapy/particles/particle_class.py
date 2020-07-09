b4f8c7cb4355e66cf1d27b54e410ab56cd1f87eb
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2020-07-09 18:20:29 +0530

    Update plasmapy/particles/particle_class.py
    Co-authored-by: Erik Everson <eteverson@gmail.com>

--- plasmapy/particles/particle_class.py
+++ plasmapy/particles/particle_class.py
@@ -99,10 +99,23 @@ def _category_errmsg(particle, category: str) -> str:
 
 
 class ParticleJSONDecoder(json.JSONDecoder):
-    def __init__(self, *args, **kwargs):
-        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+    def __init__(self, *, object_hook=None, parse_float=None,
+                 parse_int=None, parse_constant=None, strict=True,
+                 object_pairs_hook=None):
 
-    def object_hook(self, json_dict):
+        if object_hook is None:
+            object_hook = self.particle_hook
+
+        json.JSONDecoder.__init__(self,
+                                  object_hook=object_hook,
+                                  parse_float=parse_float,
+                                  parse_int=parse_int,
+                                  parse_constant=parse_constant,
+                                  strict=strict,
+                                  object_pairs_hook=object_pairs_hook)
+
+    @staticmethod
+    def particle_hook(json_dict):
         """
         An `object_hook` utilized by the `json` deserialization processes to decode
         json strings into a `plasmapy` particle class (`AbstractParticle`,
