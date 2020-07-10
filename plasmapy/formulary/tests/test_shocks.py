34391105c287518370f4e0b3f417d2eb6a7b89c6
KhalilBryant <kalelb@umich.edu>
2020-07-10 14:54:35 -0400

    update formulary/test_shocks.py

--- plasmapy/formulary/tests/test_shocks.py
+++ plasmapy/formulary/tests/test_shocks.py
@@ -13,11 +13,10 @@ def test_entropy_across_shock_polytropic():
     p_2 = 2 * p_1
     rho_1 = 2 * u.kg / u.m ** 3
     rho_2 = 3 * u.kg / u.m ** 3
-    entropy_across_shock_polytropic(c_v, p_1, p_2, rho_1, rho_2, gamma)
+    val = entropy_across_shock_polytropic(c_v, p_1, p_2, rho_1, rho_2, gamma)
 
-    assert (
-        entropy_across_shock_polytropic(c_v, p_1, p_2, rho_1, rho_2, gamma)
-    ).unit == u.J / u.K
+    assert val.unit == u.J / u.K
+    assert np.isclose(val.value, 0.26058001)
 
     with pytest.raises(Exception):
         entropy_across_shock_polytropic(c_v, p_1, 0.5 * p_1, rho_1, rho_2, gamma)
