c4950782ff9556c60eb85256fb32892207d8db3a
Erik <eteverson@gmail.com>
2020-07-30 15:53:01 -0700

    move plasmapy/utils/roman/roman.py to plasmapy/utils/roman.py

--- plasmapy/utils/roman/__init__.py
+++ plasmapy/utils/roman/__init__.py
@@ -1,10 +1,2 @@
 """Utilities to convert integers to Roman numerals and vice versa."""
 
-from .roman import (
-    InvalidRomanNumeralError,
-    OutOfRangeError,
-    RomanError,
-    from_roman,
-    is_roman_numeral,
-    to_roman,
-)
