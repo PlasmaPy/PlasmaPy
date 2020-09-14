d6472d117d2825c60580379c53d8be755a9c6623
Erik <eteverson@gmail.com>
2020-09-14 12:05:52 -0700

    change name find_plasma_potential() -> find_plasma_potential_didv(); add module level docstrings; add imports; add __all__

--- plasmapy/analysis/swept_langmuir/plasma_potential.py
+++ plasmapy/analysis/swept_langmuir/plasma_potential.py
@@ -1,4 +1,12 @@
-def find_plasma_potential(i_probe: np.ndarray,v_probe: np.ndarray):
+"""Functionality for determining the plasma potential of a Langmuir sweep."""
+__all__ = ["find_plasma_potential_didv"]
+
+import numpy as np
+
+from scipy.signal import find_peaks, peak_prominences, savgol_filter
+
+
+def find_plasma_potential_didv(i_probe: np.ndarray, v_probe: np.ndarray):
     win_size = np.int(0.05 * (i_probe.shape[0]))
     if (win_size % 2) == 0:
         win_size = win_size + 1
