9430502b89ac8843a6d2b0bd819175fb38b870fa
Erik <eteverson@gmail.com>
2020-09-25 17:08:55 -0700

    add check_sweep() to plasmapy/analysis/swept_langmuir/__init__.py

--- plasmapy/analysis/swept_langmuir/__init__.py
+++ plasmapy/analysis/swept_langmuir/__init__.py
@@ -1,6 +1,7 @@
 """
 Sub-Package containing routines for analyzing swept Langmuir probe traces.
 """
-__all__ = ["find_floating_potential"]
+__all__ = ["check_sweep", "find_floating_potential"]
 
-from plasmapy.analysis.swept_langmuir.floating_potential import find_floating_potential
+from .floating_potential import find_floating_potential
+from .helpers import check_sweep
