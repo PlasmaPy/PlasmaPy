37e930a9f79a43ddce97a62cf919c949914051db
Erik <eteverson@gmail.com>
2020-10-03 17:54:13 -0700

    clean up BaseFFTests.test_basics; add test methods test_func, test_param_assignment, test_instantiation; move test_inheritance and tes_iscallable up

--- plasmapy/analysis/tests/test_fit_functions.py
+++ plasmapy/analysis/tests/test_fit_functions.py
@@ -25,6 +25,12 @@ class BaseFFTests(ABC):
     def func(x, *args):
         raise NotImplementedError
 
+    def test_inheritance(self):
+        assert issubclass(self.ff_class, self.abc)
+
+    def test_iscallable(self):
+        assert callable(self.ff_class())
+
     def test_basics(self):
         assert hasattr(self.ff_class, "_param_names")
         if self.ff_class._param_names == NotImplemented:
@@ -34,41 +40,91 @@ class BaseFFTests(ABC):
                 f"the fit parameters."
             )
 
+        # required attributes/methods
+        for name in ("curve_fit", "func", "func_err", "root_solve"):
+            assert hasattr(self.ff_class, name)
+
+        # required properties
+        for name in ("curve_fit_results", "latex_str", "params", "param_errors",
+                     "param_names", "rsq"):
+            assert hasattr(self.ff_class, name)
+            assert isinstance(getattr(self.ff_class, name), property)
+
+    def test_instantiation(self):
+        # default
         foo = self.ff_class()
 
-        assert hasattr(foo, "func")
-        assert hasattr(foo, "func_err")
+        assert isinstance(foo.param_names, tuple)
+        assert len(foo.param_names) != 0
+        assert all(isinstance(val, str) for val in foo.param_names)
 
         assert hasattr(foo, "FitParamTuple")
         assert issubclass(foo.FitParamTuple, tuple)
+        for name in foo.param_names:
+            assert hasattr(foo.FitParamTuple, name)
 
-        assert hasattr(foo, "curve_fit_results")
         assert foo.curve_fit_results is None
-
-        assert hasattr(foo, "param_names")
-        assert isinstance(foo.param_names, tuple)
-        assert all(isinstance(val, str) for val in foo.param_names)
-
-        for attr in ("params", "param_errors"):
-            assert hasattr(foo, attr)
-            assert getattr(foo, attr) is None
-            params = [1] * len(foo.param_names)
-            setattr(foo, attr, params)
-            assert getattr(foo, attr) == foo.FitParamTuple(*params)
-            assert all(hasattr(getattr(foo, attr), name)
-                       for name in foo.param_names)
-
-        assert hasattr(foo, "latex_str")
-        assert isinstance(foo.latex_str, str)
-
-        assert hasattr(foo, "rsq")
+        assert foo.params is None
+        assert foo.param_errors is None
         assert foo.rsq is None
 
-    def test_inheritance(self):
-        assert issubclass(self.ff_class, self.abc)
+        assert isinstance(foo.latex_str, str)
 
-    def test_iscallable(self):
-        assert callable(self.ff_class())
+        # assign at instantiation
+        params = [1] * len(foo.param_names)
+        foo = self.ff_class(params=params, param_errors=params)
+        assert foo.params == foo.FitParamTuple(*params)
+        assert foo.param_errors == foo.FitParamTuple(*params)
+
+        with pytest.raises(ValueError):
+            self.ff_class(params=5)
+        with pytest.raises(ValueError):
+            self.ff_class(param_errors=5)
+
+        params = [2] * len(foo.param_names)
+        params[0] = "let me in"
+        with pytest.raises(ValueError):
+            self.ff_class(params=params)
+        with pytest.raises(ValueError):
+            self.ff_class(param_errors=params)
+
+        params = [2] * len(foo.param_names)
+        params += [5]
+        with pytest.raises(ValueError):
+            self.ff_class(params=params)
+        with pytest.raises(ValueError):
+            self.ff_class(param_errors=params)
+
+    def test_param_assignment(self):
+        foo = self.ff_class()
+        params = [2] * len(foo.param_names)
+
+        # for attr in ("params", "param_errors"):
+        #     params = [1] * len(foo.param_names)
+        #     setattr(foo, attr, params)
+        #     assert getattr(foo, attr) == foo.FitParamTuple(*params)
+        #     assert all(hasattr(getattr(foo, attr), name)
+        #                for name in foo.param_names)
+
+
+    def test_func(self):
+        foo = self.ff_class()
+
+        for x in (0, 1., np.linspace(10, 30, num=20)):
+            assert np.allclose(foo.func(x, *self._test_params),
+                              self.func(x, *self._test_params))
+
+        x = [4, 5, 6]
+        assert np.allclose(foo.func(x, *self._test_params),
+                           self.func(np.array(x), *self._test_params))
+
+        with pytest.raises(ValueError):
+            foo.func("hello", *self._test_params)
+
+        with pytest.raises(ValueError):
+            params = list(self._test_params)
+            params[0] = "hello"
+            foo.func(5, *params)
 
 
 # class TestAbstractFitFunction(BaseFFTests):
