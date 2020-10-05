4640e199661f8934c90c3fecc19b6ffbdeeb3b11
pheuer <pvheuer@gmail.com>
2020-10-05 10:50:44 -0400

    Update test_grids.py

--- plasmapy/plasma/tests/test_grids.py
+++ plasmapy/plasma/tests/test_grids.py
@@ -32,18 +32,12 @@ def test_CartesianGrid_exceptions():
 
     # Wrong input
     with pytest.raises(ValueError):
-        grid = grids.CartesianGrid("str")
-
-    with pytest.raises(ValueError):
         grid = grids.CartesianGrid(1)
 
     # ****************************************
     # _make_grid
 
     # Wrong input
-    with pytest.raises(ValueError):
-        grid = grids.CartesianGrid(1, "str")
-
     # Incompatable units
     with pytest.raises(ValueError):
         grid = grids.CartesianGrid(1 * u.cm, 1 * u.eV)
