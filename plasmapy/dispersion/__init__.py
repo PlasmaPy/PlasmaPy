7500cb66b8319d88bd62e84dba11d29b7c646a15
ahmadryan <ahmadr@udel.edu>
2020-10-05 14:35:35 -0400

    new file:   plasmapy/dispersion/__init__.py 	modified:   plasmapy/formulary/__init__.py

---
+++ plasmapy/dispersion/__init__.py
@@ -0,0 +1,14 @@
+"""
+The `~plasmapy.dispersion` subpackage contains functions associated with the 
+dispersion solver and other relevant quantities
+"""
+# __all__ will be auto populated below
+__all__ = []
+
+from .dispersionfunction import *
+
+# auto populate __all__
+for obj_name in list(globals()):
+    if not (obj_name.startswith("__") or obj_name.endswith("__")):
+        __all__.append(obj_name)
+__all__.sort()
