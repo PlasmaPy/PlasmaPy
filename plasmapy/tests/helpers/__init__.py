d984feb11bde5c7305ea75f4d148a58a7ba61565
Nick Murphy <namurphy@cfa.harvard.edu>
2020-10-11 16:43:16 -0400

    Add commented-out import statements to be used in future PRs

--- plasmapy/tests/helpers/__init__.py
+++ plasmapy/tests/helpers/__init__.py
@@ -1,5 +1,14 @@
 """Test helper functionality for PlasmaPy and affiliated packages."""
 
+# This file contains several commented out import statements.  These
+# statements will be uncommented out over the course of several pull
+# requests that were each originally part of #728.  The blank lines
+# between the import statements will hopefully simplify automatic merging.
+
+# from plasmapy.tests.helpers.actual import ActualTestOutcome
+
+# from plasmapy.tests.helpers.cases import AttrTestCase, FunctionTestCase, MethodTestCase
+
 from plasmapy.tests.helpers.exceptions import (
     Failed,
     InconsistentTypeError,
@@ -9,3 +18,15 @@ from plasmapy.tests.helpers.exceptions import (
     UnexpectedExceptionError,
     UnexpectedResultError,
 )
+
+# from plasmapy.tests.helpers.expected import ExpectedTestOutcome
+
+#  from plasmapy.tests.helpers.inputs import (
+#      AbstractTestInputs,
+#      ClassAttributeTestInputs,
+#     ClassMethodTestInputs,
+#     FunctionTestInputs,
+#     GenericClassTestInputs,
+# )
+
+# from plasmapy.tests.helpers.runner import test_runner
