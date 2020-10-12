2f3b39d1f01c60807a6d65d3845e1e64b86b248f
Ramiz Qudsi <ahmadr@udel.edu>
2020-10-12 14:36:56 -0400

    Update plasmapy/dispersion/__init__.py
    Co-authored-by: Erik Everson <eteverson@gmail.com>

--- plasmapy/dispersion/__init__.py
+++ plasmapy/dispersion/__init__.py
@@ -11,9 +11,3 @@ from plasmapy.dispersion.dispersionfunction import (
     plasma_dispersion_func,
     plasma_dispersion_func_deriv,
 )
-
-# auto populate __all__
-for obj_name in list(globals()):
-    if not (obj_name.startswith("__") or obj_name.endswith("__")):
-        __all__.append(obj_name)
-__all__.sort()
