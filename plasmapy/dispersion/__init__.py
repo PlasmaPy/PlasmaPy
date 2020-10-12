5a4dbf45012d355f7a541abdca40d49fc1e491bf
Ramiz Qudsi <ahmadr@udel.edu>
2020-10-12 14:36:34 -0400

    Update plasmapy/dispersion/__init__.py
    Co-authored-by: Erik Everson <eteverson@gmail.com>

--- plasmapy/dispersion/__init__.py
+++ plasmapy/dispersion/__init__.py
@@ -2,8 +2,10 @@
 The `~plasmapy.dispersion` subpackage contains functionality associated with
 plasma dispersion relations, solvers and analytical solutions.
 """
-# __all__ will be auto populated below
-__all__ = []
+__all__ = [
+    "plasma_dispersion_func",
+    "plasma_dispersion_func_deriv",
+]
 
 from plasmapy.dispersion.dispersionfunction import (
     plasma_dispersion_func,
