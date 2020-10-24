ba6a9d565e87f16b1ce0613f343c5550d02f3a4c
Nick Murphy <namurphy@cfa.harvard.edu>
2020-10-24 13:51:52 -0400

    Revert name of exception

--- plasmapy/particles/exceptions.py
+++ plasmapy/particles/exceptions.py
@@ -29,7 +29,7 @@ class MissingParticleDataError(ParticleError):
     pass
 
 
-class ParticleChargeError(ParticleError):
+class ChargeError(ParticleError):
     """An exception for incorrect or missing charge information."""
 
     pass
