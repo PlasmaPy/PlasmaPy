e34ff0080b004d1f2f30a21f2f67ef1e6891e908
Erik <eteverson@gmail.com>
2020-11-21 19:19:42 -0800

    rework mass_density so it calculates mass density associated with particle, and not "total" mass density that includes the electron component

--- plasmapy/formulary/parameters.py
+++ plasmapy/formulary/parameters.py
@@ -53,6 +53,7 @@ from typing import Optional
 
 from plasmapy import particles
 from plasmapy.particles import Particle
+from plasmapy.particles.exceptions import InvalidParticleError
 from plasmapy.utils import PhysicsError
 from plasmapy.utils.decorators import (
     angular_freq_to_hz,
@@ -96,64 +97,93 @@ def _grab_charge(ion: Particle, z_mean=None):
 )
 def mass_density(
     density: [u.m ** -3, u.kg / (u.m ** 3)],
-    particle: Optional[Particle] = None,
-    z_mean: Optional[numbers.Real] = None,
+    particle: Particle,
+    z_ratio: Optional[numbers.Real] = 1,
 ) -> u.kg / u.m ** 3:
-    """Utility function to merge two possible inputs for particle charge.
+    """
+    Calculated the mass density from a number density.
+
+    .. math::
+
+        \\rho = \\left| \\frac{Z_{s}}{Z_{particle}} \\right| n_{s} m_{particle}
+              = | Z_{raito} | n_{s} m_{particle}
+
+    where :math:`m_{particle}` is the particle mass, :math:`n_{s}` is a number
+    denisty for plasma species :math:`s`, :math:`Z_{s}` is the integer charge of
+    species :math:`s`, and :math:`Z_{particle}` is the integer charge of
+    ``particle``.  For example, if the electron density is given for :math:`n_s`
+    and ``particle`` is a doubly ionized atom, then :math:`Z_{ratio} = -1 / 2`.
 
     **Aliases:** `rho_`
 
     Parameters
     ----------
-    density : ~astropy.units.Quantity
-        Either a particle density (number of particles per unit volume, in units
-        of 1/m^3) or a mass density (in units of kg/m^3 or equivalent).
+    density : `~astropy.units.Quantity`
+        Either a particle number density (in units of :math:`m^{-3}` or
+        equivalent) or a mass density (in units of :math:`kg/m^3` or
+        equivalent).  If ``density`` is a mass density, then it will be passed
+        through and returned.
 
-    particle : ~plasmapy.particles.Particle, optional
-        Representation of the particle species (e.g., `'p'` for protons, `'D+'`
-        for deuterium, or `'He-4 +1'` for singly ionized helium-4).  If no
-        charge state information is provided, then the particles are assumed
-        to be singly charged. A particle is required if using a number
-        density (1/m^3) for the density parameter.
+    particle : `~plasmapy.particles.Particle`
+        The particle for which the mass density is being calculated for.  Must
+        be a plasmapy `~plasmapy.particles.Particle` or a value convertible to
+        a plasmapy `~plasmapy.particles.Particle` (e.g., ``'p'`` for protons,
+        ``'D+'`` for deuterium, or ``'He-4 +1'`` for singly ionized helium-4).
 
-    z_mean : float
-        An optional float describing the average ionization of a particle
-        species.
+    z_ratio : `int`, `float`, optional
+        The ratio of the integer charges coresponding to the plasma species
+        represted by ``density`` and the ``particle``.  For example, if the
+        given ``density`` is and electron density and ``particle`` is doubly
+        ionized ``He``, then ``z_ratio = -0.5``.  Defalut is ``1``.
 
     Raises
     ------
-    ValueError
+    `ValueError`
         If the `density` has units inconvertible to either a particle density
-        or a mass density, or if you pass in a number density without a particle.
+        or a mass density.
+
+    `TypeError`
+        If ``particle`` is not of type or convertible to
+        `~plamsapy.particles.Particle`.
+
+    `TypeError`
+        If ``z_ratio`` is not of type `int` or `float`.
 
     Returns
     -------
-    ~astropy.units.Quantity
-        The mass density calculated from all the provided sources of information.
+    `~astropy.units.Quantity`
+        The mass density for the plasma specie represented by ``particle``.
 
     Examples
     -------
-    >>> from astropy import units as u
-    >>> mass_density(1 * u.m ** -3,'p')
-    <Quantity 1.67353...e-27 kg / m3>
-    >>> mass_density(4 * u.m ** -3,'D+')
-    <Quantity 1.33779...e-26 kg / m3>
-
+    >>> import astropy.units as u
+    >>> mass_density(1 * u.m ** -3, 'p')
+    <Quantity 1.67262...e-27 kg / m3>
+    >>> mass_density(4 * u.m ** -3, 'D+')
+    <Quantity 1.33743...e-26 kg / m3>
+    >>> mass_density(2.e12 * u.cm ** -3, 'He')
+    <Quantity 1.32929...e-08 kg / m3>
+    >>> mass_density(2.e12 * u.cm ** -3, 'He', z_ratio=0.5)
+    <Quantity 6.64647...e-09 kg / m3>
     """
-    # validate_quantities ensures we have units of u.kg/u.m**3 or 1/u.m**3
-    rho = density
-    if not rho.unit.is_equivalent(u.kg / u.m ** 3):
-        if particle:
-            m_i = particles.particle_mass(particle)
-            Z = _grab_charge(particle, z_mean)
-            rho = density * m_i + Z * density * m_e
-        else:
-            raise ValueError(
-                f"If passing a number density, you must pass a "
-                f"particle (not {particle}) to calculate the mass density!"
+    if density.unit.is_equivalent(u.kg / u.m ** 3):
+        return density
+
+    if not isinstance(particle, Particle):
+        try:
+            particle = Particle(particle)
+        except TypeError:
+            raise TypeError(
+                f"If passing a number density, you must pass a plasmapy Particle "
+                f"(not type {type(particle)}) to calculate the mass density!"
             )
 
-    return rho
+    if not isinstance(z_ratio, (float, np.floating, int, np.integer)):
+        raise TypeError(
+            f"Expected type int or float for keyword z_ratio, got type {type(z_ratio)}."
+        )
+
+    return abs(z_ratio) * density * particle.mass
 
 
 rho_ = mass_density
