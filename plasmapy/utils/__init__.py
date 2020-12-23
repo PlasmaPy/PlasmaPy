3d04a661e4bef24d9650c252427b7856b051094d
Nick Murphy <namurphy@cfa.harvard.edu>
2020-12-23 18:39:06 -0500

    Fix typo in comment

--- plasmapy/utils/__init__.py
+++ plasmapy/utils/__init__.py
@@ -35,6 +35,6 @@ from plasmapy.utils.exceptions import (
 try:
     from plasmapy.utils import pytest_helpers
 except ModuleNotFoundError:
-    # pytest is not a hard dependency, so only import pytest_helpers is pytest
+    # pytest is not a hard dependency, so only import pytest_helpers if pytest
     # is installed
     pass
