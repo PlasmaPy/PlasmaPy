b51b7ba87f56fc9685db29f02c9ecbc931353112
Nick Murphy <namurphy@cfa.harvard.edu>
2020-12-31 15:44:58 -0500

    Change `raise NotImplementedError` to `pass` in abstract classes

--- plasmapy/simulation/abstractions.py
+++ plasmapy/simulation/abstractions.py
@@ -56,7 +56,7 @@ class AbstractTimeDependentSimulation(AbstractSimulation):
     pass
 
 
-class AbstractNormalizations(abc.ABC):  # coverage: ignore
+class AbstractNormalizations(ABC):  # coverage: ignore
     """
     An abstract base class to represent the normalizations of systems of
     equations describing plasmas.
@@ -66,105 +66,105 @@ class AbstractNormalizations(abc.ABC):  # coverage: ignore
     This interface is unstable and is subject to change.
     """
 
-    @abc.abstractmethod
+    @abstractmethod
     def current_density(self) -> u.A / u.m ** 2:
         """The current density normalization."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def diffusivity(self) -> u.m ** 2 / u.s:
         """The normalization for diffusivity."""
-        raise NotImplementedError
+        pass
 
-    @property
+    @abstractmethod
     def dynamic_viscosity(self) -> u.Pa * u.s:
         """The normalization for dynamic viscosity."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def electric_field(self) -> u.V / u.m:
         """The electric field normalization."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def heat_flux(self) -> u.J * u.m ** -2 * u.s ** -1:
         """The normalization for heat flux."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def ion(self) -> Particle:
         """
         The ion in the plasma, which could be a positron in the case
         of a pair plasma.
         """
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def length(self) -> u.m:
         """The normalization for length."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def magnetic_field(self) -> u.T:
         """The magnetic field normalization."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def magnetic_flux(self) -> u.T * u.m:
         """The normalization for the magnetic flux or vector potential."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def mass(self) -> u.kg:
         """The normalization for mass."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def mass_density(self) -> u.kg / u.m ** 3:
         """The normalization for mass density."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def number_density(self) -> u.m ** -3:
         """The normalization for number density."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def pressure(self) -> u.Pa:
         """The normalization for pressure."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def temperature(self) -> u.K:
         """The normalization for temperature."""
-        raise NotImplementedError
+        pass
 
-    @property
+    @abstractmethod
     def thermal_conductivity(self) -> u.W / (u.K * u.m):
         """The normalization for thermal conductivity."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def time(self) -> u.s:
         """The normalization for time."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def wavenumber(self) -> u.m ** -1:
         """The normalization for inverse length."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def velocity(self) -> u.m / u.s:
         """The normalization for velocity."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def volumetric_heating_rate(self) -> u.J * u.m ** -3 * u.s ** -1:
         """The normalization for the volumetric heating rate."""
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def volumetric_rate(self) -> u.m ** -3 * u.s ** -1:
         """
         The normalization for a volumetric rate.
@@ -172,4 +172,4 @@ class AbstractNormalizations(abc.ABC):  # coverage: ignore
         This normalization is applicable to, for example, the number
         of collisions per unit volume per unit time.
         """
-        raise NotImplementedError
+        pass
