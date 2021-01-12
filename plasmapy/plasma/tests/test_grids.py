363d7f1aaddd52b0e036c5b6a440463e2d671ea3
Peter Heuer <pvheuer@gmail.com>
2021-01-12 13:36:17 -0500

    Grids (part 2) (#963)
    Co-authored-by: Dominik Stańczak <stanczakdominik@gmail.com>
    Co-authored-by: Erik Everson <eteverson@gmail.com>
    Co-authored-by: Erik <eteverson@gmail.com>

--- plasmapy/plasma/tests/test_grids.py
+++ plasmapy/plasma/tests/test_grids.py
@@ -11,7 +11,26 @@ from plasmapy.plasma import grids as grids
 
 
 def test_AbstractGrid():
-    grid = grids.AbstractGrid(-1 * u.cm, 1 * u.cm, num=(10, 20, 5))
+
+    # Create grid with single u.Quantity args
+    grid = grids.AbstractGrid(-1 * u.cm, 1 * u.cm, num=10)
+    assert grid.is_uniform_grid
+
+    # Create grid with lists of  u.Quantity args
+    grid = grids.AbstractGrid(
+        [-1 * u.cm, -1 * u.cm, -1 * u.cm],
+        [1 * u.cm, 1 * u.cm, 1 * u.cm],
+        num=[10, 10, 10],
+    )
+    assert grid.is_uniform_grid
+
+    # Create grid with arrays of u.quantities
+    grid = grids.AbstractGrid(
+        np.array([-1] * 3) * u.cm, np.array([1] * 3) * u.cm, num=[10, 10, 10]
+    )
+    assert grid.is_uniform_grid
+
+    print(grid)
 
     array = grid.grid
     units = grid.units
@@ -40,18 +59,39 @@ def test_AbstractGrid():
         grid = grids.AbstractGrid(1 * u.cm, 1 * u.eV, num=10)
 
     # Test adding a quantity
-    q1 = np.random.randn(10, 20, 5) * u.kg
-    grid.add_quantity("test quantity", q1)
+    q = np.random.randn(10, 10, 10) * u.T
+    grid.add_quantities(B_x=q)
+
+    # Test accessing a quantity using __getitem__ or directly
+    Bx = grid.ds["B_x"]
+    Bx = grid["B_x"]
+
+    # Test adding a quantity with wrong units
+    q = np.random.randn(10, 10, 10) * u.kg
+    with pytest.raises(ValueError):
+        grid.add_quantities(B_x=q)
+
+    # Testing adding a quantity with an unrecognized key name
+    with pytest.warns(UserWarning):
+        grid.add_quantities(not_a_recognized_key=q)
 
     # Test adding a quantity of incompatible size
-    q2 = np.random.randn(5, 20, 5) * u.kg
+    q = np.random.randn(5, 20, 5) * u.T
     with pytest.raises(ValueError):
-        grid.add_quantity("test quantity2", q2)
+        grid.add_quantities(B_x=q)
+
+    # Test adding multiple quantites at once
+    q = np.random.randn(10, 10, 10) * u.T
+    grid.add_quantities(B_x=q, B_y=q, B_z=q)
+
+    print(grid)
 
 
 def test_CartesianGrid():
 
-    grid = grids.CartesianGrid(-1 * u.cm, 1 * u.cm, num=10)
+    grid = grids.CartesianGrid(
+        np.array([-1, -1, -1]) * u.cm, np.array([1, 1, 1]) * u.cm, num=(10, 10, 10)
+    )
 
     x_arr, y_arr, z_arr = grid.grids
     x_axis, y_axis, z_axis = grid.ax0, grid.ax1, grid.ax2
@@ -73,43 +113,101 @@ def test_CartesianGrid():
         )
 
 
-def test_interpolators():
-    # Test interpolator
+def test_interpolate_indices():
+    # Create grid
+    grid = grids.CartesianGrid(-1 * u.cm, 1 * u.cm, num=25)
 
-    # Test interpolator
+    # One position
+    pos = np.array([0.1, -0.3, 0]) * u.cm
+    i = grid.interpolate_indices(pos)[0]
+    # Assert that nearest grid cell was found
+    pout = grid.grid[int(i[0]), int(i[1]), int(i[2])] * grid.unit
+    assert np.allclose(pos, pout, atol=0.1)
+
+    # Two positions
+    pos = np.array([[0.1, -0.3, 0], [0.1, -0.3, 0]]) * u.cm
+    i = grid.interpolate_indices(pos)[0]
+
+    # Contains out-of-bounds values (index array should contain NaNs)
+    pos = np.array([5, -0.3, 0]) * u.cm
+    i = grid.interpolate_indices(pos)[0]
+    assert np.sum(np.isnan(i)) > 0
+
+    # ***********************************************************************
+
+    # Create a non-uniform grid
+    grid = grids.NonUniformCartesianGrid(-1 * u.cm, 1 * u.cm, num=100)
+
+    # One position
+    pos = np.array([0.1, -0.3, 0]) * u.cm
+    i = grid.interpolate_indices(pos)[0]
+
+    # Assert that nearest grid cell was found
+    pout = grid.grid[int(i)] * grid.unit
+    assert np.allclose(pos, pout, atol=0.5)
+
+
+def test_nearest_neighbor_interpolator():
     # Create grid
     grid = grids.CartesianGrid(-1 * u.cm, 1 * u.cm, num=25)
     # Add some data to the grid
-    data = grid.add_quantity("pts0", grid.grids[0])
-    data = grid.add_quantity("pts1", grid.grids[1])
+    grid.add_quantities(x=grid.grids[0])
+    grid.add_quantities(y=grid.grids[1])
 
+    # One position
     pos = np.array([0.1, -0.3, 0]) * u.cm
-    pos2 = np.array([[0.1, -0.3, 0], [0.1, -0.3, 0]]) * u.cm
-
-    # Interpolate indices
-    i = grid.interpolate_indices(pos)[0]
-
-    pout = grid.grid[i[0], i[1], i[2], :] * grid.unit
-
-    # Assert that nearest grid cell was found
-    assert np.allclose(pos, pout, atol=0.1)
-
-    # Interpolate grid values using nearest-neighbor interpolator
-    pout = grid.nearest_neighbor_interpolator(pos, "pts0")
+    pout = grid.nearest_neighbor_interpolator(pos, "x")
     assert np.allclose(pos[0], pout, atol=0.1)
 
-    # Interpolate grid values using volume-weighted interpolator
-    pout = grid.volume_averaged_interpolator(pos, "pts0")
+    # Test quantity key not present in dataset
+    with pytest.raises(KeyError):
+        pout = grid.nearest_neighbor_interpolator(pos, "B_x")
+
+    # Two positions, two quantities
+    pos = np.array([[0.1, -0.3, 0], [0.1, -0.3, 0]]) * u.cm
+    pout = grid.nearest_neighbor_interpolator(pos, "x", "y")
+
+    # Contains out-of-bounds values (must handle NaNs correctly)
+    pos = np.array([5, -0.3, 0]) * u.cm
+    pout = grid.nearest_neighbor_interpolator(pos, "x")
+    assert np.allclose(pout, 0 * u.cm, atol=0.1)
+
+    # ***********************************************************************
+
+    # Create a non-uniform grid
+    grid = grids.NonUniformCartesianGrid(-1 * u.cm, 1 * u.cm, num=100)
+    grid.add_quantities(x=grid.grids[0] * u.cm)
+
+    # One position
+    pos = np.array([0.1, -0.3, 0]) * u.cm
+    pout = grid.nearest_neighbor_interpolator(pos, "x")
+    assert np.allclose(pos[0], pout, atol=0.5)
+
+
+def test_volume_averaged_interpolator():
+    # Create grid
+    grid = grids.CartesianGrid(-1 * u.cm, 1 * u.cm, num=25)
+    # Add some data to the grid
+    grid.add_quantities(x=grid.grids[0])
+    grid.add_quantities(y=grid.grids[1])
+
+    # One position
+    pos = np.array([0.1, -0.3, 0]) * u.cm
+    pout = grid.volume_averaged_interpolator(pos, "x")
     assert np.allclose(pos[0], pout, atol=0.1)
 
-    # Test using multiple arguments
-    pout, pout2 = grid.nearest_neighbor_interpolator(pos, "pts0", "pts1")
+    # Test quantity key not present in dataset
+    with pytest.raises(KeyError):
+        pout = grid.volume_averaged_interpolator(pos, "B_x")
 
-    # Test multiple grid points
-    pos = np.array([[0, 0, 0], [0.5, 0.2, 0]]) * u.cm
-    i = grid.interpolate_indices(pos)
-    pout = grid.nearest_neighbor_interpolator(pos.value, "pts0")
-    pout = grid.volume_averaged_interpolator(pos, "pts0")
+    # Two positions, two quantities
+    pos = np.array([[0.1, -0.3, 0], [0.1, -0.3, 0]]) * u.cm
+    pout = grid.volume_averaged_interpolator(pos, "x", "y")
+
+    # Contains out-of-bounds values (must handle NaNs correctly)
+    pos = np.array([5, -0.3, 0]) * u.cm
+    pout = grid.volume_averaged_interpolator(pos, "x")
+    assert np.allclose(pout, 0 * u.cm, atol=0.1)
 
 
 def test_NonUniformCartesianGrid():
@@ -123,5 +221,15 @@ def test_NonUniformCartesianGrid():
     assert grid.is_uniform_grid == False
 
     # Test assigning a quantity
-    q1 = np.random.randn(10, 10, 10) * u.kg
-    grid.add_quantity("test_quantity", q1)
+    q1 = np.random.randn(10, 10, 10) * u.kg / u.cm ** 3
+    grid.add_quantities(rho=q1)
+
+
+if __name__ == "__main__":
+    # test_AbstractGrid()
+    # test_CartesianGrid()
+    # test_interpolate_indices()
+    # test_nearest_neighbor_interpolator()
+    # test_volume_averaged_interpolator()
+    # test_NonUniformCartesianGrid()
+    pass
