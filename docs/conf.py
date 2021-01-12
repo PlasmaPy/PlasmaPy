07a24e4edb76bf4abec13c8ee3b9686d0eea7c6c
Dominik Stańczak <stanczakdominik@gmail.com>
2021-01-12 18:47:26 +0100

    Set thumbnails for grid notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -217,7 +217,11 @@ html_favicon = "./_static/icon.ico"
 
 # -- NBSphinx options
 
-nbsphinx_thumbnails = {"notebooks/*": "_images/graphic-circular.png"}
+nbsphinx_thumbnails = {
+    "notebooks/*": "_images/graphic-circular.png",
+    "notebooks/plasma/grids_cartesian": "_static/notebook_images/uniform_grid_thumbnail.png",
+    "notebooks/plasma/grids_nonuniform": "_static/notebook_images/nonuniform_grid_thumbnail.png",
+}
 
 # adapted from https://github.com/spatialaudio/nbsphinx/blob/58b8034dd9d7349c1b4ac3e7a7d6baa87ab2a6a9/doc/conf.py
 
