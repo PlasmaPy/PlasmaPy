43e3710ac49db2566f646cc14d1d33344bce72b7
Nick Murphy <namurphy@cfa.harvard.edu>
2021-01-14 14:41:26 -0500

    Remove extra line

--- plasmapy/utils/code_repr.py
+++ plasmapy/utils/code_repr.py
@@ -284,8 +284,6 @@ def method_call_string(
         A `tuple` or `list` containing positional arguments, or any
         other type of `object` if there is only one positional argument,
         to be used during instantiation of ``cls``
-
-
     cls_kwargs: `dict`, optional
         A `dict` containing the keyword arguments to be used during
         instantiation of ``cls``
