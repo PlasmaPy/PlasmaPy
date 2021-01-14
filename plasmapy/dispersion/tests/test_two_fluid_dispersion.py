f8ba5571c23e131611a60c4d0a13763be97543cb
Ramiz Qudsi <ahmadr@udel.edu>
2021-01-14 12:47:23 -0800

    edits in test file to reflect recent edits in original file

--- plasmapy/dispersion/tests/test_two_fluid_dispersion.py
+++ plasmapy/dispersion/tests/test_two_fluid_dispersion.py
@@ -7,13 +7,13 @@ from astropy import units as u
 
 from plasmapy.dispersion.two_fluid_dispersion import two_fluid_dispersion_solution
 
-k = 0.0001 * u.m ** -1
+k = 0.0001 * u.rad/u.m
 B = 8.3e-9 * u.T
-n = 5.0e6 * u.m ** -3
+n_i = 5.0e6 * u.m ** -3
 theta = np.pi / 4 * u.rad
 T_e = 1.6e6 * u.K
 T_i = 4.0e5 * u.K
-z = 1
+z_mean = 1
 
 B_neg = -1 * u.T
 n_neg = -5.0e6 * u.m ** -3
@@ -21,32 +21,31 @@ T_e_neg = -1.6e6 * u.K
 T_i_neg = -4.0e5 * u.K
 z_neg = -1
 
-k_arr = np.linspace(10 ** -7, 10 ** -2, 10000) * u.m ** -1
+k_arr = np.linspace(10 ** -7, 10 ** -2, 10000) * u.rad/u.m
 theta_arr = np.linspace(5, 85, 100) * u.deg
 c = 3.0e8 * u.m / u.s
-
 
 def test_two_fluid_dispersion():
     r"""Test the two fluid analytical dispersion solution"""
 
     sol = two_fluid_dispersion_solution(
-        B=B, k=k, n=n, T_e=T_e, T_i=T_i, theta=theta, z=z
+        B=B, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_mean
     )
 
     sol_theta_0 = two_fluid_dispersion_solution(
-        B=B, k=k, n=n, T_e=T_e, T_i=T_i, theta=0 * u.deg, z=z
+        B=B, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=0 * u.deg, z_mean=z_mean
     )
 
     sol_theta_90 = two_fluid_dispersion_solution(
-        B=B, k=k, n=n, T_e=T_e, T_i=T_i, theta=90 * u.deg, z=z
+        B=B, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=90 * u.deg, z_mean=z_mean
     )
 
     sol_B_0 = two_fluid_dispersion_solution(
-        B=0 * u.T, k=k, n=n, T_e=T_e, T_i=T_i, theta=theta, z=z
+        B=0 * u.T, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_mean
     )
 
     sol_k_0 = two_fluid_dispersion_solution(
-        B=B, k=0 * u.m ** -1, n=n, T_e=T_e, T_i=T_i, theta=theta, z=z
+        B=B, k=0 * u.m ** -1, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_mean
     )
 
     assert np.isclose(sol["fast_mode"].value[0, 0], 56.00705052, rtol=1e-6)
@@ -67,47 +66,47 @@ def test_two_fluid_dispersion():
 
     with pytest.raises(ValueError):
         two_fluid_dispersion_solution(
-            B=B_neg, k=k, n=n, T_e=T_e, T_i=T_i, theta=theta, z=z
+            B=B_neg, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_mean
         )
 
     with pytest.raises(ValueError):
         two_fluid_dispersion_solution(
-            B=B, k=k, n=n_neg, T_e=T_e, T_i=T_i, theta=theta, z=z
+            B=B, k=k, n_i=n_neg, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_mean
         )
 
     with pytest.raises(ValueError):
         two_fluid_dispersion_solution(
-            B=B, k=k, n=n, T_e=T_e_neg, T_i=T_i, theta=theta, z=z
+            B=B, k=k, n_i=n_i, T_e=T_e_neg, T_i=T_i, theta=theta, z_mean=z_mean
         )
 
     with pytest.raises(ValueError):
         two_fluid_dispersion_solution(
-            B=B, k=k, n=n, T_e=T_e, T_i=T_i_neg, theta=theta, z=z
+            B=B, k=k, n_i=n_i, T_e=T_e, T_i=T_i_neg, theta=theta, z_mean=z_mean
         )
 
     with pytest.raises(ValueError):
         two_fluid_dispersion_solution(
-            B=B, k=k, n=n, T_e=T_e, T_i=T_i, theta=theta, z=z_neg
+            B=B, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_neg
         )
 
     # Cases where one or more of the inputs is an array
     sol_k_arr = two_fluid_dispersion_solution(
-        B=B, k=k_arr, n=n, T_e=T_e, T_i=T_i, theta=theta, z=z
+        B=B, k=k_arr, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_mean
     )
     sol_theta_arr = two_fluid_dispersion_solution(
-        B=B, k=k, n=n, T_e=T_e, T_i=T_i, theta=theta_arr, z=z
+        B=B, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta_arr, z_mean=z_mean
     )
     sol_k_arr0 = two_fluid_dispersion_solution(
-        B=B, k=k_arr[0], n=n, T_e=T_e, T_i=T_i, theta=theta, z=z
+        B=B, k=k_arr[0], n_i=n_i, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_mean
     )
     sol_theta_arr0 = two_fluid_dispersion_solution(
-        B=B, k=k, n=n, T_e=T_e, T_i=T_i, theta=theta_arr[0], z=z
+        B=B, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta_arr[0], z_mean=z_mean
     )
     sol_k_arr1 = two_fluid_dispersion_solution(
-        B=B, k=k_arr[-1], n=n, T_e=T_e, T_i=T_i, theta=theta, z=z
+        B=B, k=k_arr[-1], n_i=n_i, T_e=T_e, T_i=T_i, theta=theta, z_mean=z_mean
     )
     sol_theta_arr1 = two_fluid_dispersion_solution(
-        B=B, k=k, n=n, T_e=T_e, T_i=T_i, theta=theta_arr[-1], z=z
+        B=B, k=k, n_i=n_i, T_e=T_e, T_i=T_i, theta=theta_arr[-1], z_mean=z_mean
     )
 
     assert np.isclose(
