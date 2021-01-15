85afbf6735ab948f3ee0bf4e8998bb797ba1d9fb
Erik <eteverson@gmail.com>
2021-01-14 17:37:10 -0800

    create test class TestTwoFluidDispersionSolution and initiate with test method test_raises

--- plasmapy/dispersion/tests/test_two_fluid_dispersion.py
+++ plasmapy/dispersion/tests/test_two_fluid_dispersion.py
@@ -155,3 +155,54 @@ def test_two_fluid_dispersion():
         sol_theta_arr["acoustic_mode"].value[0, -1],
         sol_theta_arr1["acoustic_mode"].value[0, 0],
     )
+
+
+class TestTwoFluidDispersionSolution:
+    _kwargs_single_valued = {
+        "B": 8.3e-9 * u.T,
+        "ion": "p+",
+        "k": 0.0001 * u.rad/u.m,
+        "n_i": 5.0e6 * u.m ** -3,
+        "T_e": 1.6e6 * u.K,
+        "T_i": 4.0e5 * u.K,
+        "theta": 45 * u.deg,
+    }
+
+    @pytest.mark.parametrize(
+        "kwrgs, _error",
+        [
+            ({**_kwargs_single_valued, "B": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "B": [8e-9, 8.5e-9] * u.T}, ValueError),
+            ({**_kwargs_single_valued, "B": -1 * u.T}, ValueError),
+            ({**_kwargs_single_valued, "B": 5 * u.m}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "ion": {"not": "a particle"}}, TypeError),
+            ({**_kwargs_single_valued, "ion": "e-"}, ValueError),
+            ({**_kwargs_single_valued, "ion": "He", "z_mean": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "k": np.ones((3, 2)) * u.rad / u.m}, ValueError),
+            ({**_kwargs_single_valued, "k": 0 * u.rad / u.m}, ValueError),
+            ({**_kwargs_single_valued, "k": -1.0 * u.rad / u.m}, ValueError),
+            ({**_kwargs_single_valued, "k": 5 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "n_i": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "n_i": [5e6, 6e6] * u.m ** -3}, ValueError),
+            ({**_kwargs_single_valued, "n_i": -5e6 * u.m ** -3}, ValueError),
+            ({**_kwargs_single_valued, "n_i": 2 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "T_e": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "T_e": [1.4e6, 1.7e6] * u.K}, ValueError),
+            ({**_kwargs_single_valued, "T_e": -10 * u.eV}, ValueError),
+            ({**_kwargs_single_valued, "T_e": 2 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "T_i": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "T_i": [4e5, 5e5] * u.K}, ValueError),
+            ({**_kwargs_single_valued, "T_i": -1 * u.eV}, ValueError),
+            ({**_kwargs_single_valued, "T_i": 2 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "theta": np.ones((3, 2)) * u.deg}, ValueError),
+            ({**_kwargs_single_valued, "theta": 5 * u.eV}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "gamma_e": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "gamma_i": "wrong type"}, TypeError),
+        ],
+    )
+    def test_raises(self, kwrgs, _error):
+        with pytest.raises(_error):
+            two_fluid_dispersion_solution(**kwrgs)
+
+    def test_warns(self):
+        pass
