ba92d5ca7de6aa50af961c6fbd49eb1a1cbe86b2
Nick Murphy <namurphy@cfa.harvard.edu>
2021-01-23 18:31:16 -0500

    Expand ParticleList

--- plasmapy/particles/particle_list.py
+++ plasmapy/particles/particle_list.py
@@ -5,18 +5,34 @@ __all__ = ["ParticleList"]
 import astropy.units as u
 import numpy as np
 from typing import *
-from plasmapy.particles.particle_class import Particle, particle_like
-from plasmapy.particles.exceptions import InvalidParticleError
+from plasmapy.particles.decorators import particle_input
+from plasmapy.particles.particle_class import Particle, AbstractParticle, particle_like, CustomParticle, DimensionlessParticle
+from plasmapy.particles.exceptions import *
 import collections
+
 
 class ParticleList(collections.UserList):
 
-    pass
+    def __init__(self, *particles):
+        if len(particles) == 1 and isinstance(particles[0], (tuple, list)):
+            particles = particles[0]
+        self.data = list()
+        for particle in particles:
+            if isinstance(particle, DimensionlessParticle):
+                raise (
+                    f"ParticleList instances cannot include dimensionless particles."
+                )
+            if not isinstance(particle, (Particle, CustomParticle)):
+                particle = Particle(particle)
+            self.data.append(particle)
 
+    @particle_input
+    def append(self, particle: Particle):
+        self.data.append(particle)
 
-
-
-
+    @particle_input
+    def insert(self, index, particle: Particle):
+        self.data.insert(index, particle)
 
 
 
--- plasmapy/particles/tests/test_particle_list.py
+++ plasmapy/particles/tests/test_particle_list.py
@@ -2,6 +2,32 @@
 
 import pytest
 
-from plasmapy.particles.particle_class import Particle
+import astropy.units as u
+
+from plasmapy.particles.particle_class import Particle, DimensionlessParticle, CustomParticle
 from plasmapy.particles.particle_list import ParticleList
 from plasmapy.particles.exceptions import *
+from plasmapy.particles import electron, proton, alpha
+
+
+custom_particle = CustomParticle(mass=1e-25 * u.kg, charge=1e-18 * u.C)
+dimensionless_particle = DimensionlessParticle(mass=1.25, charge=1.58)
+
+particle_list_arguments = [
+    (),
+    (electron,),
+    (electron, proton, alpha),
+    ("e-", "e+"),
+    (electron, "e-"),
+    (custom_particle,),
+    (custom_particle, electron, "e-"),
+]
+
+@pytest.mark.parametrize("args", particle_list_arguments)
+def test_particle_list_creation_membership(args):
+    particle_list = ParticleList(*args)
+    for arg, particle in zip(args, particle_list):
+        assert particle == arg
+        assert isinstance(particle, (Particle, CustomParticle))
+
+
