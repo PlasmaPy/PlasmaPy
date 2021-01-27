52fb2395830a8ae4230f2d543b7eb4f8d2da496e
Nick Murphy <namurphy@cfa.harvard.edu>
2021-01-27 16:53:49 -0500

    Clean up code_repr tests
     - Use assert instead of pytest.fail
     - Define namedtuples to provide semantic information in parametrize

--- plasmapy/utils/tests/test_code_repr.py
+++ plasmapy/utils/tests/test_code_repr.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 
 from astropy import units as u
+from collections import namedtuple
 
 from plasmapy.utils.code_repr import (
     _code_repr_of_arg,
@@ -27,65 +28,95 @@ def adams_number():
     return 42
 
 
-# function, args, kwargs, expected
-call_string_table = [
-    (generic_function, (), {}, "generic_function()"),
-    (generic_function, (1), {}, "generic_function(1)"),
-    (generic_function, ("x"), {}, "generic_function('x')"),
-    (generic_function, (1, "b", {}), {}, "generic_function(1, 'b', {})"),
-    (generic_function, (), {"kw": 1}, "generic_function(kw=1)"),
-    (generic_function, (), {"x": "c"}, "generic_function(x='c')"),
-    (
-        generic_function,
-        (1, 2, 3),
-        {"a": 4, "b": 5, "c": 6},
-        "generic_function(1, 2, 3, a=4, b=5, c=6)",
-    ),
-    (
-        generic_function,
-        np.array((1.0, 2.0)),
-        {},
-        "generic_function(np.array([1., 2.]))",
-    ),
-    (
-        generic_function,
-        np.array((3.0, 4.0)) * u.m,
-        {},
-        "generic_function(np.array([3., 4.])*u.m)",
-    ),
-    (
-        generic_function,
-        (ValueError,),
-        {"x": TypeError},
-        "generic_function(ValueError, x=TypeError)",
-    ),
-    (generic_function, (), {"bowl": super}, "generic_function(bowl=super)"),
-    (
-        generic_function,
-        (1, "b"),
-        {"b": 42, "R2": "D2"},
-        "generic_function(1, 'b', b=42, R2='D2')",
-    ),
-]
+function_case = namedtuple("function_case", ("func", "args", "kwargs", "expected"))
 
 
-@pytest.mark.parametrize("function,args,kwargs,expected", call_string_table)
-def test_call_string(function, args, kwargs, expected):
+@pytest.mark.parametrize(
+    "func, args, kwargs, expected",
+    [
+        function_case(
+            func=generic_function, args=(), kwargs={}, expected="generic_function()"
+        ),
+        function_case(
+            func=generic_function, args=(1), kwargs={}, expected="generic_function(1)"
+        ),
+        function_case(
+            func=generic_function,
+            args=("x"),
+            kwargs={},
+            expected="generic_function('x')",
+        ),
+        function_case(
+            func=generic_function,
+            args=(1, "b", {}),
+            kwargs={},
+            expected="generic_function(1, 'b', {})",
+        ),
+        function_case(
+            func=generic_function,
+            args=(),
+            kwargs={"kw": 1},
+            expected="generic_function(kw=1)",
+        ),
+        function_case(
+            func=generic_function,
+            args=(),
+            kwargs={"x": "c"},
+            expected="generic_function(x='c')",
+        ),
+        function_case(
+            func=generic_function,
+            args=(1, 2, 3),
+            kwargs={"a": 4, "b": 5, "c": 6},
+            expected="generic_function(1, 2, 3, a=4, b=5, c=6)",
+        ),
+        function_case(
+            func=generic_function,
+            args=np.array((1.0, 2.0)),
+            kwargs={},
+            expected="generic_function(np.array([1., 2.]))",
+        ),
+        function_case(
+            func=generic_function,
+            args=np.array((3.0, 4.0)) * u.m,
+            kwargs={},
+            expected="generic_function(np.array([3., 4.])*u.m)",
+        ),
+        function_case(
+            func=generic_function,
+            args=(ValueError,),
+            kwargs={"x": TypeError},
+            expected="generic_function(ValueError, x=TypeError)",
+        ),
+        function_case(
+            func=generic_function,
+            args=(),
+            kwargs={"bowl": super},
+            expected="generic_function(bowl=super)",
+        ),
+        function_case(
+            func=generic_function,
+            args=(1, "b"),
+            kwargs={"b": 42, "R2": "D2"},
+            expected="generic_function(1, 'b', b=42, R2='D2')",
+        ),
+    ],
+)
+def test_call_string(func, args, kwargs, expected):
     """
     Tests that call_string returns a string that is equivalent to the
     function call.
     """
-    actual = call_string(function, args, kwargs)
-    if expected != actual:
-        pytest.fail(
-            "When call_string is called with:\n"
-            f"  function: {function.__name__}\n"
-            f"  args:     {args}\n"
-            f"  kwargs:   {kwargs}\n"
-            "the actual result does not match the expected result:\n"
-            f"  expected: {repr(expected)}\n"
-            f"  actual:   {repr(actual)}"
-        )
+    actual = call_string(func, args, kwargs)
+    assert actual == expected, (
+        "When call_string is called with:\n"
+        f"  function: {func.__name__}\n"
+        f"  args:     {args}\n"
+        f"  kwargs:   {kwargs}\n"
+        "the actual result does not match the expected result:\n"
+        f"  expected: {repr(expected)}\n"
+        f"  actual:   {repr(actual)}"
+    )
 
 
 class SampleClass:
@@ -97,91 +128,154 @@ class SampleClass:
         pass
 
 
-class_method_call_string_table = [
-    ((), {}, (), {}, "SampleClass().method()"),
-    (
-        (1, 2),
-        {"a": "x", "b": "y"},
-        (3, 4.2),
-        {"q": UserWarning},
-        "SampleClass(1, 2, a='x', b='y').method(3, 4.2, q=UserWarning)",
-    ),
-    (
-        (Exception),
-        {},
-        (SampleClass, adams_number),
-        {"r": 5.3 * u.m},
-        "SampleClass(Exception).method(SampleClass, adams_number, r=5.3*u.m)",
-    ),
-    (1, {}, 2, {}, "SampleClass(1).method(2)"),
-]
+method_case = namedtuple(
+    "method_case",
+    ("args_to_cls", "kwargs_to_cls", "args_to_method", "kwargs_to_method", "expected",),
+)
 
 
 @pytest.mark.parametrize(
-    "c_args, c_kwargs, m_args, m_kwargs, expected", class_method_call_string_table
+    "args_to_cls, kwargs_to_cls, args_to_method, kwargs_to_method, expected",
+    [
+        method_case(
+            args_to_cls=(),
+            kwargs_to_cls={},
+            args_to_method=(),
+            kwargs_to_method={},
+            expected="SampleClass().method()",
+        ),
+        method_case(
+            args_to_cls=(1, 2),
+            kwargs_to_cls={"a": "x", "b": "y"},
+            args_to_method=(3, 4.2),
+            kwargs_to_method={"q": UserWarning},
+            expected="SampleClass(1, 2, a='x', b='y').method(3, 4.2, q=UserWarning)",
+        ),
+        method_case(
+            args_to_cls=(Exception),
+            kwargs_to_cls={},
+            args_to_method=(SampleClass, adams_number),
+            kwargs_to_method={"r": 5.3 * u.m},
+            expected="SampleClass(Exception).method(SampleClass, adams_number, r=5.3*u.m)",
+        ),
+        method_case(
+            args_to_cls=1,
+            kwargs_to_cls={},
+            args_to_method=2,
+            kwargs_to_method={},
+            expected="SampleClass(1).method(2)",
+        ),
+    ],
 )
-def test_method_call_string(c_args, c_kwargs, m_args, m_kwargs, expected):
+def test_method_call_string(
+    args_to_cls, kwargs_to_cls, args_to_method, kwargs_to_method, expected
+):
     """Test that `method_call_string` returns the expected results."""
     actual = method_call_string(
-        SampleClass, "method", c_args, c_kwargs, m_args, m_kwargs
+        SampleClass,
+        "method",
+        args_to_cls,
+        kwargs_to_cls,
+        args_to_method,
+        kwargs_to_method,
     )
-    if expected != actual:
-        pytest.fail(
-            "When method_call_string is called with:\n"
-            f"  cls:      SampleClass\n"
-            f"  method:   'method'\n"
-            f"  c_args:   {c_args}\n"
-            f"  c_kwargs: {c_kwargs}\n"
-            f"  m_args:   {m_args}\n"
-            f"  m_kwargs: {m_kwargs}\n"
-            "the actual outcome does not match the expected result:\n"
-            f"  expected: {repr(expected)}\n"
-            f"  actual:   {repr(actual)}"
-        )
+    assert actual == expected, (
+        "When method_call_string is called with:\n"
+        f"  cls:      SampleClass\n"
+        f"  method:   'method'\n"
+        f"  c_args:   {args_to_cls}\n"
+        f"  c_kwargs: {kwargs_to_cls}\n"
+        f"  m_args:   {args_to_method}\n"
+        f"  m_kwargs: {kwargs_to_method}\n"
+        "the actual outcome does not match the expected result:\n"
+        f"  expected: {repr(expected)}\n"
+        f"  actual:   {repr(actual)}"
+    )
 
 
-class_attribute_call_string_table = [
-    ((), {}, "SampleClass().attr"),
-    ((1, 2), {"a": "x", "b": "y"}, "SampleClass(1, 2, a='x', b='y').attr"),
-    (1, {}, "SampleClass(1).attr"),
-    ({"key": "value"}, {}, "SampleClass({'key': 'value'}).attr"),
-]
+attribute_case = namedtuple(
+    "attribute_case", ("args_to_cls", "kwargs_to_cls", "expected",)
+)
 
 
 @pytest.mark.parametrize(
-    "c_args, c_kwargs, expected", class_attribute_call_string_table
+    "args_to_cls, kwargs_to_cls, expected",
+    [
+        attribute_case(args_to_cls=(), kwargs_to_cls={}, expected="SampleClass().attr"),
+        attribute_case(
+            args_to_cls=(1, 2),
+            kwargs_to_cls={"a": "x", "b": "y"},
+            expected="SampleClass(1, 2, a='x', b='y').attr",
+        ),
+        attribute_case(args_to_cls=1, kwargs_to_cls={}, expected="SampleClass(1).attr"),
+        attribute_case(
+            args_to_cls={"key": "value"},
+            kwargs_to_cls={},
+            expected="SampleClass({'key': 'value'}).attr",
+        ),
+    ],
 )
-def test_attribute_call_string(c_args, c_kwargs, expected):
+def test_attribute_call_string(args_to_cls, kwargs_to_cls, expected):
     """Test that `attribute_call_string` returns the expected results."""
-    actual = attribute_call_string(SampleClass, "attr", c_args, c_kwargs)
-    if expected != actual:
-        pytest.fail(
-            "When attribute_call_string is called with:\n"
-            f"  cls:      SampleClass\n"
-            f"  attr:     'attr'\n"
-            f"  c_args:   {c_args}\n"
-            f"  c_kwargs: {c_kwargs}\n"
-            "the actual outcome does not match the expected result:\n"
-            f"  expected: {repr(expected)}\n"
-            f"  actual:   {repr(actual)}"
-        )
+    actual = attribute_call_string(SampleClass, "attr", args_to_cls, kwargs_to_cls)
+    assert actual == expected, (
+        "When attribute_call_string is called with:\n"
+        f"  cls:      SampleClass\n"
+        f"  attr:     'attr'\n"
+        f"  c_args:   {args_to_cls}\n"
+        f"  c_kwargs: {kwargs_to_cls}\n"
+        "the actual outcome does not match the expected result:\n"
+        f"  expected: {repr(expected)}\n"
+        f"  actual:   {repr(actual)}"
+    )
+
+
+ndarray_case = namedtuple("ndarray_case", ("array_inputs", "max_items", "expected"))
 
 
 @pytest.mark.parametrize(
     "array_inputs, max_items, expected",
     [
-        ([0], np.inf, "np.array([0])"),
-        ([0.0, 1.0], np.inf, "np.array([0., 1.])"),
-        ([0.0, 1.142], np.inf, "np.array([0., 1.142])"),
-        ([[0, 1, 2], [3, 4, 5]], np.inf, "np.array([[0, 1, 2], [3, 4, 5]])"),
-        ([np.inf], np.inf, "np.array([np.inf])"),
-        ([np.nan], np.inf, "np.array([np.nan])"),
-        ([np.nan, np.inf, -np.inf], np.inf, "np.array([np.nan, np.inf, -np.inf])"),
-        ([1], 1, "np.array([1])"),
-        ([1, 2], 1, "np.array([1, ...])"),
-        ([1, 2, 3, 4], 2, "np.array([1, 2, ...])"),
-        ([[1, 2, 3], [4, 5, 6]], 5, "np.array([[1, 2, 3], [4, 5, ...]])"),
-        ([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], 1, "np.array([[[1, ...]]])"),
+        ndarray_case(array_inputs=[0], max_items=np.inf, expected="np.array([0])"),
+        ndarray_case(
+            array_inputs=[0.0, 1.0], max_items=np.inf, expected="np.array([0., 1.])"
+        ),
+        ndarray_case(
+            array_inputs=[0.0, 1.142],
+            max_items=np.inf,
+            expected="np.array([0., 1.142])",
+        ),
+        ndarray_case(
+            array_inputs=[[0, 1, 2], [3, 4, 5]],
+            max_items=np.inf,
+            expected="np.array([[0, 1, 2], [3, 4, 5]])",
+        ),
+        ndarray_case(
+            array_inputs=[np.inf], max_items=np.inf, expected="np.array([np.inf])"
+        ),
+        ndarray_case(
+            array_inputs=[np.nan], max_items=np.inf, expected="np.array([np.nan])"
+        ),
+        ndarray_case(
+            array_inputs=[np.nan, np.inf, -np.inf],
+            max_items=np.inf,
+            expected="np.array([np.nan, np.inf, -np.inf])",
+        ),
+        ndarray_case(array_inputs=[1], max_items=1, expected="np.array([1])"),
+        ndarray_case(array_inputs=[1, 2], max_items=1, expected="np.array([1, ...])"),
+        ndarray_case(
+            array_inputs=[1, 2, 3, 4], max_items=2, expected="np.array([1, 2, ...])"
+        ),
+        ndarray_case(
+            array_inputs=[[1, 2, 3], [4, 5, 6]],
+            max_items=5,
+            expected="np.array([[1, 2, 3], [4, 5, ...]])",
+        ),
+        ndarray_case(
+            array_inputs=[[[1, 2], [3, 4]], [[5, 6], [7, 8]]],
+            max_items=1,
+            expected="np.array([[[1, ...]]])",
+        ),
     ],
 )
 def test__code_repr_of_ndarray(array_inputs, max_items, expected):
@@ -193,34 +287,40 @@ def test__code_repr_of_ndarray(array_inputs, max_items, expected):
     """
     array = np.array(array_inputs)
     actual = _code_repr_of_ndarray(array, max_items=max_items)
-    if actual != expected:
-        pytest.fail(
-            f"The representation of an ndarray for array_inputs = {array_inputs} "
-            f"and max_items = {max_items} is not the expected result:\n"
-            f"  expected: {repr(expected)}\n"
-            f"  actual:   {repr(actual)}\n"
-        )
+    assert actual == expected, (
+        f"The representation of an ndarray for array_inputs = {array_inputs} "
+        f"and max_items = {max_items} is not the expected result:\n"
+        f"  expected: {repr(expected)}\n"
+        f"  actual:   {repr(actual)}\n"
+    )
 
     if max_items >= array.size:
         recreated_array = eval(actual)
-        if not np.allclose(array, recreated_array, atol=1e-15, equal_nan=True):
-            pytest.fail(
-                f"Evaluating the representation of an ndarray for array_inputs = "
-                f"{array_inputs} and max_items = {max_items} does not recreate "
-                f"the original array, as expected."
-                f"array:           {array}\n"
-                f"recreated_array: {recreated_array}"
-            )
+        assert np.allclose(array, recreated_array, atol=1e-15, equal_nan=True), (
+            f"Evaluating the representation of an ndarray for array_inputs = "
+            f"{array_inputs} and max_items = {max_items} does not recreate "
+            f"the original array, as expected."
+            f"array:           {array}\n"
+            f"recreated_array: {recreated_array}"
+        )
+
+
+quantity_case = namedtuple("QuantityTestCases", ("quantity", "expected"))
 
 
 @pytest.mark.parametrize(
     "quantity, expected",
     [
-        (5.3 * u.m, "5.3*u.m"),
-        (5.4 / u.m, "5.4/u.m"),
-        (5.5 * u.m ** -2, "5.5*u.m**-2"),
-        (5.0 * u.dimensionless_unscaled, "5.0*u.dimensionless_unscaled"),
-        (np.array([3.5, 4.2]) * u.m ** -2.5, "np.array([3.5, 4.2])*u.m**-2.5"),
+        quantity_case(quantity=5.3 * u.m, expected="5.3*u.m"),
+        quantity_case(quantity=5.4 / u.m, expected="5.4/u.m"),
+        quantity_case(quantity=5.5 * u.m ** -2, expected="5.5*u.m**-2"),
+        quantity_case(
+            quantity=u.Quantity(5.0), expected="5.0*u.dimensionless_unscaled"
+        ),
+        quantity_case(
+            quantity=np.array([3.5, 4.2]) * u.m ** -2.5,
+            expected="np.array([3.5, 4.2])*u.m**-2.5",
+        ),
     ],
 )
 def test__code_repr_of_quantity(quantity, expected):
@@ -229,23 +329,12 @@ def test__code_repr_of_quantity(quantity, expected):
     string as expected.
     """
     actual = _code_repr_of_quantity(quantity)
-    if actual != expected:
-        pytest.fail(
-            f"_code_repr_of_quantity for {quantity} is not producing the "
-            f"expected result:\n"
-            f"expected: {repr(expected)}\n"
-            f"actual:   {repr(actual)}"
-        )
-
-
-@pytest.mark.parametrize("not_a_quantity", [3.5, "1.2"])
-def test__code_repr_of_quantity_typeerror(not_a_quantity):
-    """
-    Test that `_code_repr_of_quantity` raises a `TypeError` when
-    supplied with an object other than a `astropy.units.Quantity`.
-    """
-    with pytest.raises(TypeError):
-        _code_repr_of_quantity(not_a_quantity)
+    assert actual == expected, (
+        f"_code_repr_of_quantity for {quantity} is not producing the "
+        f"expected result:\n"
+        f"expected: {repr(expected)}\n"
+        f"actual:   {repr(actual)}"
+    )
 
 
 def test__string_together_warnings_for_printing():
@@ -254,13 +343,12 @@ def test__string_together_warnings_for_printing():
     warning_messages = ["msg1", "msg2"]
     expected = "UserWarning: msg1\n\nDeprecationWarning: msg2"
     actual = _string_together_warnings_for_printing(warnings, warning_messages)
-    if actual != expected:
-        pytest.fail(
-            f"_string_together_warnings_for_printing is not producing the "
-            f"expected result:\n"
-            f"  expected: {repr(expected)}\n"
-            f"  actual: {repr(actual)}"
-        )
+    assert actual == expected, (
+        f"_string_together_warnings_for_printing is not producing the "
+        f"expected result:\n"
+        f"  expected: {repr(expected)}\n"
+        f"  actual: {repr(actual)}"
+    )
 
 
 @pytest.mark.parametrize(
@@ -282,13 +370,12 @@ def test__code_repr_of_arg(arg, expected):
     `str` the represents how the arg would appear in code.
     """
     code_repr_of_arg = _code_repr_of_arg(arg)
-    if code_repr_of_arg != expected:
-        pytest.fail(
-            f"_code_repr_of_arg is not returning the expected result.\n"
-            f"arg = {arg}\n"
-            f"expected:         {expected}\n"
-            f"code_repr_of_arg: {code_repr_of_arg}"
-        )
+    assert code_repr_of_arg == expected, (
+        f"_code_repr_of_arg is not returning the expected result.\n"
+        f"arg = {arg}\n"
+        f"expected:         {expected}\n"
+        f"code_repr_of_arg: {code_repr_of_arg}"
+    )
 
 
 @pytest.mark.parametrize(
@@ -336,11 +423,10 @@ def test__name_with_article(obj, expected):
     contains ``"a "`` or ``"an "`` followed by the name of ``obj``.
     """
     name_with_article = _name_with_article(obj)
-    if name_with_article != expected:
-        pytest.fail(
-            f"When calling _name_with_article for {obj}, expecting "
-            f"{repr(expected)} but got {repr(name_with_article)}."
-        )
+    assert name_with_article == expected, (
+        f"When calling _name_with_article for {obj}, expecting "
+        f"{repr(expected)} but got {repr(name_with_article)}."
+    )
 
 
 @pytest.mark.parametrize(
@@ -359,10 +445,9 @@ def test__name_with_article(obj, expected):
 def test__object_name(obj, showmodule, expected_name):
     """Test that `_object_name` produces the expected output."""
     actual_name = _object_name(obj, showmodule=showmodule)
-    if actual_name != expected_name:
-        pytest.fail(
-            f"For obj = {repr(obj)}, the expected output of _obj_name with "
-            f"showmodule = {showmodule} does not match the actual output:\n"
-            f"  expected: {expected_name}\n"
-            f"  actual:   {actual_name}"
-        )
+    assert actual_name == expected_name, (
+        f"For obj = {repr(obj)}, the expected output of _obj_name with "
+        f"showmodule = {showmodule} does not match the actual output:\n"
+        f"  expected: {expected_name}\n"
+        f"  actual:   {actual_name}"
+    )
