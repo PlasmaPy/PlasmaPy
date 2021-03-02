411bfce1e0a4e3b939f9979328784f7e5555fc6f
Dominik Stańczak <stanczakdominik@gmail.com>
2021-03-02 18:32:43 +0100

    FluxSurface

---
+++ plasmapy/plasma/fluxsurface.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+from dataclasses import dataclass
+from functools import cached_property
+from scipy import integrate
+from typing import Callable
+
+
+@dataclass
+class FluxSurface:
+    R: np.ndarray
+    Z: np.ndarray
+    psi: float
+    Brvals: np.ndarray
+    Bzvals: np.ndarray
+    Bt_func: Callable
+    Bprimervals: np.ndarray
+    Bprimezvals: np.ndarray
+
+    def __post_init__(self):
+        self.Bvectors = np.stack((self.Brvals, self.Bzvals))
+        self.dZ = np.gradient(self.Z)
+        self.dR = np.gradient(self.R)
+        self.dL = np.sqrt(self.dZ ** 2 + self.dR ** 2)
+
+        n = np.stack((self.dZ, -self.dR))
+        self.n = n / np.linalg.norm(n, axis=0)
+        self.nr, self.nz = self.n
+
+        self.lp = np.cumsum(self.dL)
+        self.Lp = np.sum(self.dL)
+        self.Bp = np.linalg.norm(self.Bvectors, axis=0)
+        self.B2 = self.Bp ** 2 + self.Bt_func(self.R, self.Z) ** 2
+        self.Bmag = np.sqrt(self.B2)
+        self.Bmax = self.Bmag.max()
+        self.Bmin = self.Bmag.min()
+
+    @cached_property
+    def Bt(self):
+        return self.Bt_func(self.R, self.Z)
+
+    def plot(self, ax=None, n=False, B=False, **kwargs):
+        if ax is None:
+            fig, ax = plt.subplots()
+            ax.set_aspect("equal")
+            ax.set_xlabel("Radius $R/R_0$")
+            ax.set_ylabel("Height $z/R_0$")
+        ax.plot(self.R, self.Z, label=fr"$\psi = ${self.psi:.2f}")
+        if B:
+            ax.quiver(self.R, self.Z, self.Brvals, self.Bzvals, **kwargs)
+        if n:
+            ax.quiver(self.R, self.Z, self.nr, self.nz, **kwargs)
+        ax.legend()
+        return ax
+
+    def plot_psi(self, ax=None):
+        if ax is None:
+            fig, ax = plt.subplots()
+            ax.set_xlabel("L")
+            ax.set_ylabel(r"$\psi$")
+        ax.plot(self.lp, psi(self.R, self.Z))
+        ax.axhline(self.psi)
+        return ax
+
+    def flux_surface_average(self, quantity):
+        # TODO test the <B dot ... > = 0 property
+        integrand = 1 / self.Bp
+        return integrate.simpson(integrand * quantity, self.lp) / integrate.simpson(
+            integrand, self.lp
+        )
+
+    @cached_property
+    def theta(self):
+        theta = 2 * np.pi * self.dL / self.Lp
+        return theta
+
+    @cached_property
+    def Theta(self):
+        integrand = self.Bmag / self.Bp
+        integral = integrate.cumulative_trapezoid(integrand, self.lp, initial=0)
+        gamma = 2 * np.pi / integral[-1]
+        integral *= gamma
+        return integral
---
+++ plasmapy/plasma/tests/test_fluxsurface.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pytest
+
+from hypothesis.extra.numpy import arrays
+from hypothesis.strategies import floats, integers
+
+from plasmapy.plasma.fluxsurface import FluxSurface
+
+
+def Bt_func(R, Z):
+    return 5.3
+
+
+def FS(psi_value=-0.01, Bt_func=Bt_func):
+    import plasmaboundaries
+
+    from skimage import measure
+
+    # plasma parameters
+    params = plasmaboundaries.ITER
+
+    # compute magnetic flux psi(R, z)
+    psi = plasmaboundaries.compute_psi(params, config="non-null")
+
+    # plot the results
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    rmin, rmax = 0.6, 1.4
+    zmin, zmax = -0.6, 0.6
+    r = np.arange(rmin, rmax, step=0.01)
+    z = np.arange(zmin, zmax, step=0.01)
+    R, Z = np.meshgrid(r, z)
+    PSI = psi(R, Z)  # compute magnetic flux
+    import sympy
+
+    Rsym, Zsym = sympy.symbols("R Z")
+    psisym = psi(Rsym, Zsym, pkg="sp")
+    Br = -psisym.diff(Zsym) / Rsym
+    Bz = psisym.diff(Rsym) / Rsym
+    B = sympy.sqrt(Br ** 2 + Bz ** 2)
+    Bdiff_r = B.diff(Rsym)
+    Bdiff_z = B.diff(Zsym)
+    Brfunc = sympy.lambdify((Rsym, Zsym), Br)
+    Bzfunc = sympy.lambdify((Rsym, Zsym), Bz)
+    Brdifffunc = sympy.lambdify((Rsym, Zsym), Bdiff_r)
+    Bzdifffunc = sympy.lambdify((Rsym, Zsym), Bdiff_z)
+    contours = measure.find_contours(PSI, psi_value, positive_orientation="high")
+    if len(contours) != 1:
+        print(f"Could not find contour for psi = {psi_value} ({len(contours)=})")
+    assert len(contours) == 1, (len(contours), psi_value, i)
+    contour = contours[0]
+    RcontourArrayUnits, ZcontourArrayUnits = contour[:, 1], contour[:, 0]
+
+    Zcontour = ZcontourArrayUnits / PSI.shape[0] * (zmax - zmin) + zmin
+    Rcontour = RcontourArrayUnits / PSI.shape[1] * (rmax - rmin) + rmin
+    Brvals = Brfunc(Rcontour, Zcontour)
+    Bzvals = Bzfunc(Rcontour, Zcontour)
+    Bprimervals = Brdifffunc(Rcontour, Zcontour)
+    Bprimezvals = Bzdifffunc(Rcontour, Zcontour)
+
+    dZ = np.gradient(Zcontour)
+    dR = np.gradient(Rcontour)
+    dL = np.sqrt(dZ ** 2 + dR ** 2)
+    plt.plot(Rcontour, Zcontour)
+    Brvals = Brfunc(Rcontour, Zcontour)
+    Bzvals = Bzfunc(Rcontour, Zcontour)
+    Bprimervals = Brdifffunc(Rcontour, Zcontour)
+    Bprimezvals = Bzdifffunc(Rcontour, Zcontour)
+    fs = FluxSurface(
+        Rcontour, Zcontour, psi_value, Brvals, Bzvals, Bt_func, Bprimervals, Bprimezvals
+    )
+    #     fs.flux_surface_average(fs.Bmag)
+    return fs
+
+
+def test_fs_Bmag(num_regression):
+    num_regression.check({"fs-averaged mod B": fs.flux_surface_average(fs.Bmag)})
+
+
+fs = FS()
+from hypothesis import given, infer, settings
+
+
+@given(
+    array=arrays(
+        float,
+        fs.R.size,
+        elements=floats(-1e60, 1e60, allow_nan=False, allow_infinity=False),
+    )
+)
+def test_fs_flux_surface_average(array):
+    average = fs.flux_surface_average(array)
+    assert average.min() <= average <= average.max()
+
+
+@given(m=integers(-100, 100))
+def test_fs_flux_surface_modes(m: int):
+    mode = np.sin(fs.Theta * m)
+    assert abs(fs.flux_surface_average(mode)) < 0.1
+
+
+@pytest.mark.xfail(reason="This is only really true for periodic functions IIRC")
+@given(
+    array=arrays(
+        float, (fs.R.size, 2), elements=floats(allow_nan=False, allow_infinity=False)
+    )
+)
+def test_fs_flux_surface_average_annihilates_Bdot(array):
+    under_average = np.array([B @ ai for B, ai in zip(fs.Bvectors.T, array)])
+    assert abs(fs.flux_surface_average(under_average)) < 0.01
