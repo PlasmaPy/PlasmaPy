3d947763bbe8cd04f8592cc61167b589563b6678
pheuer <pvheuer@gmail.com>
2021-03-12 09:46:27 -0500

    Reorganized rot_a_to_b tests

--- plasmapy/formulary/tests/test_mathematics.py
+++ plasmapy/formulary/tests/test_mathematics.py
@@ -9,25 +9,35 @@ from plasmapy.formulary import mathematics as mathematics
 # TODO: Move the Fermi integral tests over to this file?
 
 
-def test_rot_a_to_b():
-
-    # Test one rotation
-    a = np.array([1, 0, 0])
-    b = np.array([0, 1, 0])
+@pytest.mark.parametrize(
+    "a, b, correct",
+    [
+        # Test one rotation
+        (
+            np.array([1, 0, 0]),
+            np.array([0, 1, 0]),
+            np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]]),
+        ),
+        # Test the special case where the two vectors are parallel
+        (np.array([1, 0, 0]), np.array([1, 0, 0]), np.identity(3)),
+        # Test the special case where the two vectors are anti-parallel
+        (np.array([1, 0, 0]), np.array([-1, 0, 0]), -np.identity(3)),
+    ],
+)
+def test_rot_a_to_b(a, b, correct):
     R = mathematics.rot_a_to_b(a, b)
-    correct = np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]])
     np.testing.assert_allclose(R, correct, atol=1e-6)
 
-    # Test the special case where the two vectors are parallel
-    a = np.array([1, 0, 0])
-    b = np.array([1, 0, 0])
-    R = mathematics.rot_a_to_b(a, b)
-    correct = np.identity(3)
-    assert np.allclose(R, correct, atol=1e-6)
 
-    # Test the special case where the two vectors are anti-parallel
-    a = np.array([1, 0, 0])
-    b = np.array([-1, 0, 0])
-    R = mathematics.rot_a_to_b(a, b)
-    correct = -np.identity(3)
-    assert np.allclose(R, correct, atol=1e-6)
+@pytest.mark.parametrize(
+    "a, b, _raises",
+    [
+        # a is the wrong length
+        (np.array([1, 0]), np.array([1, 0, 0]), ValueError),
+        # b is the wrong length
+        (np.array([1, 0, 0]), np.array([1, 0]), ValueError),
+    ],
+)
+def test_rot_a_to_b_raises(a, b, _raises):
+    with pytest.raises(_raises):
+        mathematics.rot_a_to_b(a, b)
