73e296f19ed90016c0be125c83b1c4f5ec213757
Erik <eteverson@gmail.com>
2021-03-26 20:54:15 -0700

    update decorator angular_freq_to_hz to match the improved schemed used in validate_quantities

--- plasmapy/utils/decorators/converter.py
+++ plasmapy/utils/decorators/converter.py
@@ -7,6 +7,8 @@ import astropy.units as u
 import functools
 import inspect
 
+from collections import OrderedDict
+
 from plasmapy.utils.decorators.helpers import preserve_signature
 
 
@@ -87,26 +89,32 @@ def angular_freq_to_hz(fn):
 
     """
     # raise exception if fn uses the 'to_hz' kwarg
-    sig = inspect.signature(fn)
-    if "to_hz" in sig.parameters:
+    fsig = inspect.signature(fn)
+    if "to_hz" in fsig.parameters:
         raise ValueError(
             f"Wrapped function '{fn.__name__}' can not use keyword 'to_hz'."
             f" Keyword reserved for decorator functionality."
         )
 
     # make new signature for fn
-    new_params = sig.parameters.copy()
-    new_params["to_hz"] = inspect.Parameter(
-        "to_hz", inspect.Parameter.POSITIONAL_OR_KEYWORD, default=False
+    new_param = inspect.Parameter(
+        "to_hz",
+        inspect.Parameter.KEYWORD_ONLY,
+        default=False,
+        annotation=bool,
     )
+    new_sig = OrderedDict(fsig.parameters)
+    new_sig.update({"to_hz": new_param})
     new_sig = inspect.Signature(
-        parameters=new_params.values(), return_annotation=sig.return_annotation
+        parameters=tuple(new_sig.values()),
+        return_annotation=fsig.return_annotation,
     )
     fn.__signature__ = new_sig
 
     @preserve_signature
     @functools.wraps(fn)
-    def wrapper(*args, to_hz=False, **kwargs):
+    def wrapper(*args, **kwargs):
+        to_hz = kwargs.pop("to_hz", False)
         _result = fn(*args, **kwargs)
         if to_hz:
             return _result.to(u.Hz, equivalencies=[(u.cy / u.s, u.Hz)])
@@ -116,7 +124,7 @@ def angular_freq_to_hz(fn):
     Other Parameters
     ----------------
     to_hz: bool
-        Set `True` to to convert function output from angular frequency to Hz
+        Set `True` to to convert function output from angular frequency to Hz.
     """
     if wrapper.__doc__ is not None:
         wrapper.__doc__ += added_doc_bit
