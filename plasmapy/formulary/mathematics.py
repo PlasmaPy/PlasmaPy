413950a9c33313ec88de959868dfff5068710da9
Dominik Stańczak <stanczakdominik@gmail.com>
2021-03-29 17:37:22 +0200

    Updated docstring

--- plasmapy/formulary/mathematics.py
+++ plasmapy/formulary/mathematics.py
@@ -91,15 +91,51 @@ def Fermi_integral(
         raise TypeError(f"Improper type {type(x)} given for argument x.")
 
 
-def Chandrasekhar_G(x):
+def Chandrasekhar_G(x: float):
+    r"""
+    Calculate the Chandrasekhar G function used in transport theory.
+
+    Parameters
+    ----------
+    x : `float` or `~numpy.ndarray`
+        Usually the ratio of a particle's velocity to its species' thermal
+        velocity.
+
+    Returns
+    -------
+    `float` or `numpy.ndarray`
+
+    Notes
+    -----
+
+    The Chandrasekhar function goes as :math:`2x / 3 \sqrt{π}` at :math:`x \to 0`
+    and :math:`0.5 x^{-2}` at :math:`x \to \infty`. It describes the drag on a
+    particle by collisions with a Maxwellian background.
+
+    Since it goes to zero at infinity, for any applied electric field you can
+    always find electrons for which the field is larger than the friction.
+    These electrons will then enter a feedback loop, accelerating endlessly (in
+    the non-relativistic limit) and becoming runaways.
+
+    Incidentally, if your field is barely strong enough to accelerate thermal
+    electrons to infinity, it's called the Dreicer electric field.
+
+    Examples
+    --------
+    >>> Chandrasekhar_G(1)
+    0.21379664776456
+    >>> Chandrasekhar_G(1e-6)
+    3.7608262858090935e-07
+    >>> Chandrasekhar_G(1e6)
+    5e-13
+    >>> Chandrasekhar_G(-1)
+    -0.21379664776456
+
+    References
+    ----------
+    - Collisional Transport in Magnetized Plasmas"
+    Per Helander & Dieter J. Sigmar, 2005
     """
-    The Chandrasekhar function :math:`G(x)` goes as :math:`2x / 3 \sqrt{π}` at low :math:`x`
-    and :math:`0.5 x^{-2}` describes the drag on a particle by collisions with a Maxwellian
-    background. Because it goes to zero at infinity, there always be electrons
-    for which any applied electric field is larger than the friction force;
-    those will be accelerated into runaway electrons. A sufficiently strong
-    "Dreicer" electric field will be enough to accelerate
-    thermal electrons."""
 
     erf = special.erf(x)
     erf_derivative = 2 * np.exp(-(x ** 2)) / np.sqrt(np.pi)
