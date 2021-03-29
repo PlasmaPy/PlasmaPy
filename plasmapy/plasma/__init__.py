033a9604b628fd8fc414802730c06d910161b91c
Dominik Stańczak <stanczakdominik@gmail.com>
2021-03-29 19:43:39 +0200

    Add FluxSurface, SymbolicEquilibrium to __init__

--- plasmapy/plasma/__init__.py
+++ plasmapy/plasma/__init__.py
@@ -1,4 +1,6 @@
 from plasmapy.plasma import exceptions, sources
 
+from .fluxsurface import FluxSurface
 from .plasma_base import BasePlasma, GenericPlasma
 from .plasma_factory import Plasma
+from .symbolicequilibrium import SymbolicEquilibrium
