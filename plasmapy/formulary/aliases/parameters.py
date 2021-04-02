d335b376cb8ae9a2ac6445f6cfad4cb7db4d9608
Dominik Stańczak <stanczakdominik@gmail.com>
2021-04-02 07:15:00 +0200

    Comment out debug prints for now

--- plasmapy/formulary/aliases/parameters.py
+++ plasmapy/formulary/aliases/parameters.py
@@ -6,10 +6,10 @@ module = importlib.import_module(__name__.replace(".aliases", ""))
 __all__ = []
 for name, function in inspect.getmembers(module):
     if not name.startswith("_") and name.endswith("_"):
-        print(f"{name=} fits!")
+        # print(f"{name=} fits!")
         __all__.append(name)
         setattr(sys.modules[__name__], name, function)
-    else:
-        print(f"\t{name=} SKIPPED!")
+    # else:
+    #     print(f"\t{name=} SKIPPED!")
 __all__.sort()
-print(__all__)
+# print(__all__)
