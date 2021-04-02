7d7f221bc4ff02559aa77005a6f5aa61ab56a899
Dominik Stańczak <stanczakdominik@gmail.com>
2021-04-02 16:58:47 +0200

    draft of generalized helper

--- plasmapy/formulary/aliases/_helper.py
+++ plasmapy/formulary/aliases/_helper.py
@@ -1,14 +1,28 @@
+import functools
 import importlib
 import inspect
 import sys
 
 
-def _generate_aliases(_name_):
-    module = importlib.import_module(_name_.replace(".aliases", ""))
+def _generate_namespace(_name_, func_condition, mod_name_replacer):
+    module = importlib.import_module(mod_name_replacer(_name_))
     _all_ = []
     for name, function in inspect.getmembers(module):
-        if not name.startswith("_") and name.endswith("_"):
+        if func_condition(name):
             _all_.append(name)
             setattr(sys.modules[_name_], name, function)
     _all_.sort()
     return _all_
+
+
+_generate_aliases = functools.partial(
+    _generate_namespace,
+    condition=lambda name: not name.startswith("_") and name.endswith("_"),
+    mod_name_replacer=lambda modname: modname.replace(".aliases", ""),
+)
+
+_generate_lite_versions = functools.partial(
+    _generate_namespace,
+    condition=lambda name: name.endswith("lite"),
+    mod_name_replacer=lambda modname: modname.replace(".lite", ""),
+)
