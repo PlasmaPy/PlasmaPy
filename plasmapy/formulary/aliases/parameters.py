b953561939f18cd90c024f03ec6b5e22c3fcfcd1
Dominik Stańczak <stanczakdominik@gmail.com>
2021-04-02 07:09:44 +0200

    Add aliases/parameters.py module to dynamically load aliases from parameters

---
+++ plasmapy/formulary/aliases/parameters.py
@@ -0,0 +1,15 @@
+import importlib
+import inspect
+import sys
+
+module = importlib.import_module(__name__.replace(".aliases", ""))
+__all__ = []
+for name, function in inspect.getmembers(module):
+    if not name.startswith("_") and name.endswith("_"):
+        print(f"{name=} fits!")
+        __all__.append(name)
+        setattr(sys.modules[__name__], name, function)
+    else:
+        print(f"\t{name=} SKIPPED!")
+__all__.sort()
+print(__all__)
