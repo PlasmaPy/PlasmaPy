bea2557334f88c0f6d0cbf74e307b35597601102
Dominik Stańczak <stanczakdominik@gmail.com>
2021-04-13 11:34:35 +0200

    Refactor out S_pt duplication

--- plasmapy/formulary/neoclassical.py
+++ plasmapy/formulary/neoclassical.py
@@ -376,7 +376,9 @@ if __name__ == "__main__":
     ν_T_ai(1.0495932305582267e-05, 1, hydrogen, all_species)
 
 
-def rbar_sources(a, all_species, flux_surface, beta_coeffs=None) -> u.Quantity:
+def rbar_sources(
+    all_species, flux_surface, density_gradient, temperature_gradient, beta_coeffs=None
+) -> u.Quantity:
     fs = flux_surface
     if beta_coeffs is not None:
         # TODO should be a dict or sth
@@ -385,55 +387,29 @@ def rbar_sources(a, all_species, flux_surface, beta_coeffs=None) -> u.Quantity:
         beta_cx = np.zeros(3)  # TODO
         beta_an = np.zeros(3)  # TODO
         beta_coeffs = np.diag(beta_cx + beta_an) * u.kg / u.m ** 3 / u.s
-    num_charge_states = len(a.integer_charges)
-    temperatures = u.Quantity([a.T_e] * num_charge_states)
 
-    # TODO make these inputs
-    density_gradient = u.Quantity(num_charge_states * [1e18 * u.m ** -3 / u.m])
-    temperature_gradient = u.Quantity(num_charge_states * [10 * u.K / u.m])
+    results = []
+    for a in all_species:
 
-    # but not this, this is dervied
-    pressure_gradient = constants.k_B * (
-        temperatures * density_gradient + a.number_densities * temperature_gradient
-    )
-
-    def gen():
-        for i, ai in enumerate(a):
-            if ξ(a)[i] == 0:
-                continue  # won't add anything to sum anyway, and matrix gets singular
-            Aai = (
-                ξ(a)[i] * M_script(a, all_species)
-                - mu_hat(i, a, all_species, flux_surface)
-                - beta_coeffs
-            )
-            μ = mu_hat(i, a, all_species, fs)
-            Spt = (
-                fs.Fhat
-                / ai.ion.charge
-                / ai.number_density
-                * u.Quantity(
-                    [
-                        pressure_gradient[i] * μ[0, 0]
-                        + a.number_densities[i]
-                        * constants.k_B
-                        * temperature_gradient[i]
-                        * μ[0, 1],
-                        pressure_gradient[i] * μ[1, 0]
-                        + a.number_densities[i]
-                        * constants.k_B
-                        * temperature_gradient[i]
-                        * μ[1, 1],
-                    ]
+        def gen():
+            for i, ai in enumerate(a):
+                if ξ(a)[i] == 0:
+                    continue  # won't add anything to sum anyway, and matrix gets singular
+                Aai = (
+                    ξ(a)[i] * M_script(a, all_species)
+                    - mu_hat(i, a, all_species, flux_surface)
+                    - beta_coeffs
                 )
-            ).si
-            Spt = np.append(Spt, 0)
-            S_matrix = Spt
-            rai_as_rows = np.linalg.solve(Aai, S_matrix)
-            # TODO does not include r_pT, r_E, r_NBI yet
-            rbar_ingredient = ξ(a)[i] * rai_as_rows
-            yield rbar_ingredient
+                μ = mu_hat(i, a, all_species, fs)
+                Spt = S_pt(ai, μ, fs, density_gradient, temperature_gradient)
+                S_matrix = Spt
+                rai_as_rows = np.linalg.solve(Aai, S_matrix)
+                # TODO does not include r_pT, r_E, r_NBI yet
+                rbar_ingredient = ξ(a)[i] * rai_as_rows
+                yield rbar_ingredient
 
-    return sum(gen())
+        results.append(sum(gen()))
+    return np.concatenate(results).si
 
 
 def get_flows(
@@ -445,10 +421,9 @@ def get_flows(
     beta_coeffs=None,
 ):
     fs = flux_surface
-    # rbar_sources ABSOLUTELY needs a bloody rework to do stuff simultaneously
-    rhs = np.concatenate(
-        [rbar_sources(a, all_species, fs, beta_coeffs=beta_coeffs) for a in all_species]
-    ).si
+    rhs = rbar_sources(
+        all_species, fs, density_gradient, temperature_gradient, beta_coeffs=beta_coeffs
+    )
     if beta_coeffs is not None:
         # TODO should be a dict or sth
         raise NotImplementedError
@@ -479,39 +454,41 @@ def get_flows(
                 continue
             μ = mu_hat(i, a, all_species, fs)
             Aai = xi[i] * M - μ - beta_coeffs
-            S_ai = xi[i] * np.diag(Λ)  # TODO np.diag(Δ)?
+            S_ai = xi[i] * np.diag(Λ)
             rai_as_rows = np.linalg.solve(Aai, S_ai)
             order_flow_sum = (
                 (Λ.reshape(-1, 1) * rai_as_rows).sum(axis=0).si.value
             )  # TODO fix units
 
-            temperature = ai.T_i
-            density = a.number_densities[i]  # TODO cleanup
-            ne_grad = density_gradient.get(ai.ionic_symbol, 0 * u.m ** -4)
-            T_grad = temperature_gradient.get(ai.ionic_symbol, 0 * u.K / u.m)
-            pressure_gradient = constants.k_B * (
-                temperature * ne_grad + density * T_grad
-            )
-            Spt = (
-                fs.Fhat
-                / ai.ion.charge
-                / ai.number_density
-                * u.Quantity(
-                    [
-                        pressure_gradient * μ[0, 0]
-                        + density * constants.k_B * T_grad * μ[0, 1],
-                        pressure_gradient * μ[1, 0]
-                        + density * constants.k_B * T_grad * μ[1, 1],
-                    ]
-                )
-            ).si
-            Spt = np.append(Spt, 0)
+            Spt = S_pt(ai, μ, fs, density_gradient, temperature_gradient)
             rpt_row = np.linalg.solve(
                 Aai, Spt
             ).si.value  # TODO units are wrong here too; but I think the mechanics should just about work
             flows = order_flow_sum + rpt_row  # Eq31
             outputs[ai.ionic_symbol] = flows
     return outputs
+
+
+def S_pt(ai, μ, fs, density_gradient, temperature_gradient):
+    # TODO gradients should be attached to ai
+    ne_grad = density_gradient.get(ai.ionic_symbol, 0 * u.m ** -4)
+    T_grad = temperature_gradient.get(ai.ionic_symbol, 0 * u.K / u.m)
+    pressure_gradient = constants.k_B * (ai.T_i * ne_grad + ai.number_density * T_grad)
+    Spt = (
+        fs.Fhat
+        / ai.ion.charge
+        / ai.number_density
+        * u.Quantity(
+            [
+                pressure_gradient * μ[0, 0]
+                + ai.number_density * constants.k_B * T_grad * μ[0, 1],
+                pressure_gradient * μ[1, 0]
+                + ai.number_density * constants.k_B * T_grad * μ[1, 1],
+            ]
+        )
+    ).si
+    Spt = np.append(Spt, 0)
+    return Spt
 
 
 def get_fluxes(flows, all_species):
