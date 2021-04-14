bac010762eacfdd72dcd29c9306b86e95557bc22
Dominik Stańczak <stanczakdominik@gmail.com>
2021-04-14 09:57:18 +0200

    Remove duplicated test

--- plasmapy/formulary/tests/test_Chandrasekhar_G.py
+++ plasmapy/formulary/tests/test_Chandrasekhar_G.py
@@ -7,12 +7,6 @@ from hypothesis import strategies as st
 from plasmapy.formulary.mathematics import Chandrasekhar_G
 
 
-def test_Chandrasekhar(num_regression):
-    x = np.linspace(-5, 5, 100)
-    y = Chandrasekhar_G(x)
-    num_regression.check({"x": x, "y": y})
-
-
 @given(
     x=st.floats(
         min_value=6e-150,
