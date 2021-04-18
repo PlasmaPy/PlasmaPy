d7d0c0dcaf627b5edf82f65a520c9778a0c7081a
Erik <eteverson@gmail.com>
2021-04-18 13:33:25 -0700

    revise main docstring

--- docs/plasmapy_sphinx/__init__.py
+++ docs/plasmapy_sphinx/__init__.py
@@ -1,11 +1,11 @@
 """
-This `sphinx` documentation package was highly influenced by
-`Astropy's <https://www.astropy.org/>`_
-`sphinx_automodapi <https://sphinx-automodapi.readthedocs.io/en/latest/index.html>`_
+This `sphinx` extension package was highly influenced by
+`Astropy's sphinx_automodapi
+<https://sphinx-automodapi.readthedocs.io/en/latest/index.html>`_
 package. In fact,
-the `~automdoapi.automodapi` and `~automodapi.automodsumm` directive defined in
-this package are a adaption/evolutions of `sphinx_automodapi.automodapi`
-and `sphinx_automodapi.automodsumm` directives.
+the :rst::dir:`automodapi` and :rst:dir:`automodsumm` directives defined in
+this package are an adaption/evolutions of the `sphinx_automodapi.automodapi`
+and the `sphinx_automodapi.automodsumm` directives.
 """
 # The code here was adapted from v0.14.0.dev0 of sphinx_automodapi
 
