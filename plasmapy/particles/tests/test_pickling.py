b7ac8b7cd33fdb7ad8bad9b60b424d71270572ef
Dominik Stańczak <stanczakdominik@gmail.com>
2021-04-21 20:43:27 +0200

    Pickleable particles

--- plasmapy/particles/tests/test_pickling.py
+++ plasmapy/particles/tests/test_pickling.py
@@ -19,17 +19,15 @@ class TestPickling:
     Test that different objects in `plasmapy.particles` can be pickled.
     """
 
-    xfail = pytest.mark.xfail(reason="see issue #1011")
-
     @pytest.mark.parametrize(
         "instance",
         [
             CustomParticle(mass=1 * u.kg, charge=1 * u.C),
             DimensionlessParticle(mass=5, charge=5),
-            pytest.param(Particle("p+"), marks=xfail),
-            pytest.param(IonicLevel("p+", 0.1, 1e9 * u.m ** -3), marks=xfail),
-            pytest.param(IonizationState("H", [0.5, 0.5]), marks=xfail),
-            pytest.param(IonizationStateCollection({"H": [0.5, 0.5]}), marks=xfail),
+            pytest.param(Particle("p+")),
+            pytest.param(IonicLevel("p+", 0.1, 1e9 * u.m ** -3)),
+            pytest.param(IonizationState("H", [0.5, 0.5])),
+            pytest.param(IonizationStateCollection({"H": [0.5, 0.5]})),
         ],
     )
     def test_pickling(self, instance, tmp_path):
