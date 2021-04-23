aba5bb5639b2db7ac86287282cbf5f1847682abc
Peter Heuer <pvheuer@gmail.com>
2021-04-22 21:00:13 -0400

    Implemented interferometer

--- plasmapy/diagnostics/line_integrated_diagnostic.py
+++ plasmapy/diagnostics/line_integrated_diagnostic.py
@@ -7,6 +7,7 @@ __all__ = [
 ]
 
 import astropy.units as u
+import astropy.constants as const
 import numpy as np
 
 from typing import Union
@@ -293,4 +294,34 @@ class LineIntegrateScalarQuantities(LineIntegratedDiagnostic):
         return arr
 
 
-# TODO: implement optical interferometry as an example
+class Interferometer(LineIntegrateScalarQuantities):
+    def __init__(self, grid, source, detector, verbose=False):
+        super().__init__(grid, source, detector, quantities='n_e', verbose=verbose)
+    
+    def Interferogram(self, 
+                      probe_freq : u.Hz, 
+                      size=np.array([[-1, 1], [-1, 1]]) * u.cm,
+                      bins=[50, 50],
+                      collimated=True,
+                      num=100,
+                      interference = False,
+                      ):
+    
+        # TODO: implement an actual critical density function for PlasmaPy
+        # Critical density in cm^-3
+        n_c = (const.eps0.si*const.m_e/const.e.si**2)*(2*np.pi)**2*probe_freq**2
+        n_c = n_c.to(u.cm**-3)
+       
+        hax, vax, int_ne = self.line_integral(size=size, 
+                                    bins=bins, 
+                                    collimated=collimated,
+                                    num=num)
+        
+        phase_shift = (-np.pi*probe_freq / (const.c.si * n_c))*int_ne
+        
+        phase_shift = phase_shift.to(u.dimensionless_unscaled).value
+        
+        if interference:
+            phase_shift = (phase_shift  + np.pi) % (2 * np.pi) - np.pi
+        
+        return hax, vax, phase_shift
