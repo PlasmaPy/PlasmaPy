51ad0c5493b1c0bdcc348c1ae09112d903158986
Peter Heuer <pvheuer@gmail.com>
2021-04-22 21:00:44 -0400

    Added n_e to recognized quantities

--- plasmapy/plasma/grids.py
+++ plasmapy/plasma/grids.py
@@ -87,6 +87,7 @@ class AbstractGrid(ABC):
         RecognizedQuantity("y", "y spatial position", u.m),
         RecognizedQuantity("z", "z spatial position", u.m),
         RecognizedQuantity("rho", "Mass density", u.kg / u.m ** 3),
+        RecognizedQuantity("n_e", "Electron number density", 1 / u.m ** 3),
         RecognizedQuantity("E_x", "Electric field (x component)", u.V / u.m),
         RecognizedQuantity("E_y", "Electric field (y component)", u.V / u.m),
         RecognizedQuantity("E_z", "Electric field (z component)", u.V / u.m),
