b2109c4f1a73af5d1ce1702e67297a5417d41b8d
Dominik Stańczak <stanczakdominik@gmail.com>
2021-04-28 07:41:57 +0200

    hypothesis tests

--- plasmapy/formulary/tests/test_Chandrasekhar_G.py
+++ plasmapy/formulary/tests/test_Chandrasekhar_G.py
@@ -1,6 +1,26 @@
+import hypothesis
 import numpy as np
 
+from hypothesis import given, settings
+from hypothesis import strategies as st
+
 from plasmapy.formulary.mathematics import Chandrasekhar_G
+
+
+@given(
+    x=st.floats(
+        min_value=6e-150,
+        max_value=1e90,
+        exclude_min=True,
+        allow_nan=False,
+        allow_infinity=False,
+    )
+)
+@settings(max_examples=500)
+def test_Chandrasekhar_with_hypothesis(x):
+    result = Chandrasekhar_G(x)
+    assert x > 0
+    assert np.isfinite(x)
 
 
 def test_Chandrasekhar_regression(num_regression):
