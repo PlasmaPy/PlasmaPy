ba5cdb0efa412e0ac247d7f33d21499b02e605a7
Nick Murphy <namurphy@cfa.harvard.edu>
2021-05-17 20:46:32 -0400

    Workaround for docs of deprecated decorator
    Co-authored-by: Erik Everson <eteverson@gmail.com>

--- plasmapy/utils/decorators/deprecation.py
+++ plasmapy/utils/decorators/deprecation.py
@@ -1,16 +1,29 @@
 __all__ = ["deprecated"]
 
-import astropy
+import inspect
 
-from functools import partial
+from astropy.utils.decorators import deprecated as astropy_deprecated
 
 from plasmapy.utils.exceptions import PlasmaPyDeprecationWarning
 
-deprecated = partial(
-    astropy.utils.decorators.deprecated,
-    warning_type=PlasmaPyDeprecationWarning,
+
+def deprecated(*args, warning_type=PlasmaPyDeprecationWarning, **kwargs):
+    """
+    A wrapper of `astropy.utils.decorators.deprecated` that by default assumes
+    a warning type of `~plasmapy.utils.exceptions.PlasmaPyDeprecationWarning`.
+    """
+    return astropy_deprecated(*args, warning_type=warning_type, **kwargs)
+
+
+# override deprecated's signature
+asig = inspect.signature(astropy_deprecated)
+new_default = asig.parameters["warning_type"].replace(
+    default=PlasmaPyDeprecationWarning
 )
-"""
-A wrapper of `astropy.utils.decorators.deprecated` that by default assumes
-a warning type of `~plasmapy.utils.exceptions.PlasmaPyDeprecationWarning`.
+new_sig = dict(asig.parameters)
+new_sig["warning_type"] = new_default
+new_sig = inspect.Signature(
+    parameters=tuple(new_sig.values()), return_annotation=asig.return_annotation,
+)
+deprecated.__signature__ = new_sig
 """
