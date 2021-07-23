028e3435745191b56aa31cad71005d123c676583
Sjbrownian <84342922+Sjbrownian@users.noreply.github.com>
2021-07-22 20:48:07 -0400

    Update __init__.py

--- plasmapy/dispersion/numerical/__init__.py
+++ plasmapy/dispersion/numerical/__init__.py
@@ -3,9 +3,7 @@ The `~plasmapy.dispersion` subpackage contains functionality associated with
 plasma dispersion relations, numerical solvers and analytical solutions.
 """
 __all__ = [
-    "plasma_dispersion_func",
-    "plasma_dispersion_func_deriv",
-    "two_fluid",
+    "hollweg",
 ]
 
 from plasmapy.dispersion.dispersionfunction import (
