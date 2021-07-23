a3c83bd5f37d3a4781c638610df846e1176540e3
Ramiz Qudsi <qudsiramiz@gmail.com>
2021-07-23 14:27:11 -0700

    Update __init__.py

--- plasmapy/dispersion/analytical/__init__.py
+++ plasmapy/dispersion/analytical/__init__.py
@@ -3,13 +3,7 @@ The `~plasmapy.dispersion` subpackage contains functionality associated with
 plasma dispersion relations, numerical solvers and analytical solutions.
 """
 __all__ = [
-    "plasma_dispersion_func",
-    "plasma_dispersion_func_deriv",
     "two_fluid",
 ]
 
-from plasmapy.dispersion.dispersionfunction import (
-    plasma_dispersion_func,
-    plasma_dispersion_func_deriv,
-)
 from plasmapy.dispersion.analytical.two_fluid import two_fluid
