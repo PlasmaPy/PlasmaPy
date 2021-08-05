b39a80abf6fe95e77761b67314d1f9f10b41e699
Sjbrownian <84342922+Sjbrownian@users.noreply.github.com>
2021-08-05 13:26:18 -0400

    Update __init__.py

--- plasmapy/dispersion/__init__.py
+++ plasmapy/dispersion/__init__.py
@@ -7,7 +7,9 @@ __all__ = [
     "plasma_dispersion_func",
     "plasma_dispersion_func_deriv",
 ]
-
+# TODO: import plasmapy.dispersion.numerical once it is populated with
+#       functionality (i.e. expose to namespace)
+from plasmapy.dispersion import analytical
 from plasmapy.dispersion.dispersionfunction import (
     plasma_dispersion_func,
     plasma_dispersion_func_deriv,
