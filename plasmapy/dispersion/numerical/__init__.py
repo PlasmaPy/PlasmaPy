5ed12ac8eeb00d1c54a6bfcf9914e8cf63f74312
Sjbrownian <84342922+Sjbrownian@users.noreply.github.com>
2021-08-17 13:55:44 -0400

    Update plasmapy/dispersion/numerical/__init__.py
    Co-authored-by: Erik Everson <eteverson@gmail.com>

--- plasmapy/dispersion/numerical/__init__.py
+++ plasmapy/dispersion/numerical/__init__.py
@@ -2,7 +2,6 @@
 The `~plasmapy.dispersion.numerical` subpackage contains functionality
 associated with numerical dispersion solvers.
 """
-__all__ = [
-  "hollweg",
-]
+__all__ = ["hollweg"]
+
 from plasmapy.dispersion.numerical.hollweg_ import hollweg
