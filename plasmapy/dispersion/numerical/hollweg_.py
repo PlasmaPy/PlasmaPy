c9ed72fec05a0803845c0d679b62b8aa0497596e
Sjbrownian <84342922+Sjbrownian@users.noreply.github.com>
2021-08-17 17:03:35 -0400

    Update hollweg_.py

--- plasmapy/dispersion/numerical/hollweg_.py
+++ plasmapy/dispersion/numerical/hollweg_.py
@@ -128,7 +128,8 @@ def hollweg(
     >>> omegas
     {'fast_mode': <Quantity [2.62911663e-02, 2.27876968e+03] rad / s>,
     'alfven_mode': <Quantity [7.48765909e-04, 2.13800404e+03] rad / s>,
-    'acoustic_mode': <Quantity [0.00043295, 0.07358991] rad / s>}"""
+    'acoustic_mode': <Quantity [0.00043295, 0.07358991] rad / s>}
+    """
 
     # validate argument ion
     if not isinstance(ion, Particle):
