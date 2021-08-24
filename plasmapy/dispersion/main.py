daea0f2244d89201852baac72341826492167fef
etjohnson <77989826+etjohnson@users.noreply.github.com>
2021-08-24 10:34:22 -0400

    Updates

--- plasmapy/dispersion/main.py
+++ plasmapy/dispersion/main.py
@@ -3,10 +3,13 @@ from numerical.cold_plasma_function_solver import cold_plasma_function_solver
 
 inputs = {
    "B": 8.3e-9 * u.T,
-   "k": 0.001 * u.rad / u.m,
-   "omega_p": 1.6e6 * u.rad / u.s,
-   "omega_e": 4.0e5 * u.rad / u.s,
+   "k": [0.001] * u.rad / u.m,
+   "omega_p": [1.6e5]* u.rad / u.s,
+   "omega_e": [4.0e5] * u.rad / u.s,
+   "omega_alpha": [3.0e5] * u.rad / u.s,
    "theta": 30 * u.deg,
 }
 
-cold_plasma_function_solver(**inputs)
+w = cold_plasma_function_solver(**inputs)
+for i in range(len(w[0.001])):
+    print(w[0.001][i])
--- plasmapy/dispersion/numerical/cold_plasma_function_solver.py
+++ plasmapy/dispersion/numerical/cold_plasma_function_solver.py
@@ -5,20 +5,23 @@ import astropy.units as u
 import astropy.constants as const
 from plasmapy.formulary import parameters as pfp
 from plasmapy.utils.decorators import validate_quantities
-from sympy import symbols, solve
+from sympy import Symbol
+from sympy.solvers import solve
 
-@validate_quantities(
-    B={"can_be_negative": False},
-    k={"can_be_negative": False},
-    omega_p={"can_be_negative": False, "equivalencies": u.temperature_energy()},
-    omega_e={"can_be_negative": False, "equivalencies": u.temperature_energy()},
-    theta={},
-)
+#@validate_quantities(
+#    B={"can_be_negative": False},
+#    k={"can_be_negative": False},
+#    omega_p={"can_be_negative": False,},
+#    omega_e={"can_be_negative": False,}
+#    omega_alpha={}
+#    theta={},
+#) equives here
 def cold_plasma_function_solver(
     B: u.T,
     k: u.rad/u.m,
     omega_p: u.rad/u.s,
     omega_e:u.rad/u.s,
+    omega_alpha:u.rad/u.s,
     theta: u.rad,
 ):
     
@@ -39,7 +42,7 @@ def cold_plasma_function_solver(
     k : single value or 1 D array astropy ~astropy.units.Quantity
         Value of the wavenumber in units convertible to :math:'rad/m'.
     
-    omega_ion: single value or 1 D array astropy ~astropy.units.Quantity
+    omega_p,e,alpha: single value or 1 D array astropy ~astropy.units.Quantity
         Frequency value for the associated ion in units convertible to :math:'rad/s'.
         
     theta: single value or 1 D array astropy ~astropy.units.Quantity
@@ -49,13 +52,24 @@ def cold_plasma_function_solver(
         
     Returns
     -------
-    omegas : Single value or 1 D array astropy ~astropy.units.Quantity
-        Value(s) of the frequency (omega) dispersion solution(s).
+    omegas : Dictionary astropy ~astropy.units.Quantity
+        Presents the wavenumber used to find the Value(s) of the frequency (omega)
+        dispersion solution(s) and then the solutions themsevles.
         
-    
     Raises
     ------
-    
+    TypeError
+        If the argument is of an invalid type.
+
+     ~astropy.units.UnitsError
+        If the argument is a `~astropy.units.Quantity` but is not
+        dimensionless.
+        
+    ValueError
+        If the number of frequencies for each ion isn't the same.
+
+    NoConvergence
+        If a solution cannot be found and the convergence failed to root.
     
     Notes
     -----
@@ -103,12 +117,13 @@ def cold_plasma_function_solver(
     Example
     -------
     >>>    from astropy import units as u
-    >>>    from numerical.cold_plasma_function_solution import cold_plasma_function_solution
+    >>>    from numerical.cold_plasma_function_solver import cold_plasma_function_solver
     >>>    inputs = {
     ...       "B": 8.3e-9 * u.T,
     ...       "k": 0.01 * u.rad / u.m,
     ...       "omega_p": 1.6e6 * u.rad / u.s,
     ...       "omega_e": 4.0e5 * u.rad / u.s,
+    ...       "omega_alpha": 3.0e5 * u.rad / u.s,
     ...       "theta": 30 * u.deg,
     ...    }
     >>>    cold_plasma_function_solution(**inputs)
@@ -123,7 +138,7 @@ def cold_plasma_function_solver(
     for arg_name in ("B"):
         value = locals()[arg_name].squeeze()
         if not (value.ndim == 0):
-            raise ValueError(
+            raise TypeError(
                 f"Argument '{arg_name}' must be a float or an integer."
                 f"shape {value.shape}."
                 )
@@ -131,23 +146,34 @@ def cold_plasma_function_solver(
     
     k = k.squeeze()
     if not (k.ndim == 0  or k.ndim == 1):
-        raise ValueError(
+        raise TypeError(
             f"Arguement 'k' needs to be a single value or a 1D array astropy Quantity,"
             f"got a value of shape {k.shpae}."
         )
 
     omega_p = omega_p.squeeze()
     omega_e = omega_e.squeeze()
+    omega_alpha = omega_alpha.squeeze() 
     if not (omega_e.ndim == 0 or omega_e.ndim == 1):
-        raise ValueError(
+        raise TypeError(
             f"Arguement 'omega_e' needs to be a single value or a single valued 1D array astropy Quantity,"   
             f"got value of shape {omega_e.shape}."
+        )
+    if not (omega_p.ndim == 0 or omega_p.ndim == 1):
+        raise TypeError(
+            f"Arguement 'omega_p' needs to be a single value or a single valued 1D array astropy Quantity,"   
+            f"got value of shape {omega_p.shape}."
+        )
+    if not (omega_alpha.ndim == 0 or omega_alpha.ndim == 1):
+        raise TypeError(
+            f"Arguement 'omega_alpha' needs to be a single value or a single valued 1D array astropy Quantity,"   
+            f"got value of shape {omega_alpha.shape}."
         )
         
     theta = theta.squeeze()
     theta = theta.to(u.radian)
     if not (theta.ndim == 0):
-        raise ValueError(
+        raise TypeError(
             f"Arguement 'theta' needs to be a single value astropy Quantity,"
             f"got value of shape {theta.shape}."
         )
@@ -163,87 +189,104 @@ def cold_plasma_function_solver(
         ck[0] =  val.value
         k_int = True
     elif k_dim == 1:
-        ck = np.tile(0*u.rad/u.s,len(k))
+        ck = np.zeros(len(k))
         for i in range(len(k)):
             val = k[i]*const.c
             ck[i] = val.value
         k_int = False
     else:
-       raise ValueError(
+       raise TypeError(
             f"Arguement 'k' needs to be a single value or 1D array astropy Quantity,"
             f"got value of shape {k.shape}."
         )
        
-    component_frequency = np.tile(0*u.rad/u.s, 2)
+    component_frequency = np.tile(0*u.rad/u.s, 3)
     component_frequency[0] = pfp.gyrofrequency(B=B, particle='H+', signed=False)
     component_frequency[1] = pfp.gyrofrequency(B=B, particle='e-', signed=True)
+    component_frequency[2] = pfp.gyrofrequency(B=B, particle='alpha', signed=False)
         
     lengths = []
-    if omega_p.ndim == 0 and omega_e.ndim == 0:
+    if omega_p.ndim == 0 and omega_e.ndim == 0 and omega_alpha.ndim == 0:
         omega_int = True
         lengths.append(1)
-        lengths.append(1)
-    elif omega_p.ndim == 1 and omega_e.ndim == 1:
+    elif omega_p.ndim == 1 and omega_e.ndim == 1 and omega_alpha.ndim == 1:
         omega_int = False
         lengths.append(len(omega_p))
         lengths.append(len(omega_e))
+        lengths.append(len(omega_alpha))
     else:
         raise ValueError(
-            f"Arguement 'omega_p' and 'omega_e' need to be the same quantity type,"
-            f"got value of shape {omega_p.shape} and {omega_e.shape}."
+            f"Arguement 'omega_p', 'omega_alpha' and 'omega_e' need to be the same quantity type,"
+            f"got value of shape {omega_p.shape}, {omega_alpha} and {omega_e.shape}."
         )
     
     sum_len = min(lengths)
-    
+
     plasma_proton = np.zeros(sum_len)
     plasma_electron = np.zeros(sum_len)
+    plasma_alpha = np.zeros(sum_len)
     
     if omega_int == False:
         for i in range(sum_len):
-            plasma_proton[i] = omega_p[i].value
-            plasma_electron[i] = omega_e[i].value
+            plasma_proton[i] = float(omega_p[i].value)
+            plasma_electron[i] = float(omega_e[i].value)
+            plasma_alpha[i] = float(omega_alpha[i].value)
     elif omega_int == True:
         plasma_proton[0] = float(omega_p.value)
         plasma_electron[0] = float(omega_e.value)
+        plasma_alpha[0] = float(omega_alpha.value)
     else:
-        raise ValueError(
+        raise TypeError(
             f"Arguement 'omega_p' and 'omega_e' quantity type could not be deterimined,"
             f"got value of shape {omega_p.shape} and {omega_e.shape}."
         )
-    print(plasma_proton[0])
+
+    #---#
+       
     #---#
     
-    #---#
-    
-    w = symbols('w')
+    w = Symbol('w')
     
     prot_w = (component_frequency[0].value)
     elec_w = (component_frequency[1].value)
-    
-    prot = plasma_proton[0]**2/(w**2+prot_w**2)
-    elec =  (plasma_electron[0]**2)/(w**2+(elec_w**2))
+    alpha_w = (component_frequency[2].value)
 
-    S = 1 - (prot + elec)
-    P = 1# - (plasma_proton[0]**2)/(w**2) - (plasma_electron[0]**2)/(w**2)
+    S = 1
+    P = 1
     D = 0
-    
-    print(S)
+
+    omegas = {}
+
+    for i in range(sum_len):
+        S =+ ((plasma_proton[i]**2)/(w**2+prot_w**2)) + ((plasma_electron[i]**2)/(w**2+elec_w**2)) + ((plasma_alpha[i]**2)/(w**2+alpha_w**2))
+        P =+ ((plasma_proton[i]**2)/(w**2)) + ((plasma_electron[i]**2)/(w**2)) + ((plasma_alpha[i]**2)/(w**2))
+        D =+ ((plasma_proton[i]**2)/(w**2+prot_w**2))*((prot_w)/(w)) + ((plasma_electron[i]**2)/(w**2+elec_w**2))*((elec_w)/(w)) + ((plasma_alpha[i]**2)/(w**2+alpha_w**2))*((alpha_w)/(w))
     
     R = S + D
     L = S - D
     
-    A = S*(np.sin(theta)**2) + P*(np.cos(theta)**2)
-    B = R*L*(np.sin(theta)**2) + P*S*(1 + np.cos(theta)**2)
+    A = S*(np.sin(theta.value)**2) + P*(np.cos(theta.value)**2)
+    B = R*L*(np.sin(theta.value)**2) + P*S*(1 + np.cos(theta.value)**2)
     C = P*R*L
 
-    eq = A*((ck[0]/w)**4) - B*((ck[0]/w)**2) + C
-    print(eq)
-    sol = solve(eq, w)
-    omegas = sol.sort()
+        
+    for i in range(len(ck)):
+        eq = A*((ck[i]/w)**4) - B*((ck[i]/w)**2) + C
+
+        sol = solve(eq,w,warn=True)
+
+        sol_omega = []
     
-    for i in range(len(omegas)):
-        omegas[i] = omegas[i]*u.rad/u.s
+        for j in range(len(sol)):
+            val = complex(sol[j])*u.rad/u.s
+            sol_omega.append(val)
+            
+        omegas[i] = sol_omega
+        val = (ck[i]/const.c.value)
+        omegas[val] = omegas.pop(i)
     
     return omegas
+
+
 
 
