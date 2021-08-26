8f726d8feebf238b1634b4ec733a32ba2bf8f499
Nick Murphy <namurphy@cfa.harvard.edu>
2021-08-25 20:31:47 -0400

    Rename method to average_particle for consistency

--- plasmapy/particles/ionization_state_collection.py
+++ plasmapy/particles/ionization_state_collection.py
@@ -853,7 +853,7 @@ class IonizationStateCollection:
         else:
             raise ValueError("Need 0 <= tol <= 1.")
 
-    def mean_particle(
+    def average_particle(
         self,
         include_neutrals: bool = True,
         use_rms_charge: bool = False,
