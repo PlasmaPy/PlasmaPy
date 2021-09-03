c88363cde1b41b88212f7f91f9032958dca53358
Erik <eteverson@gmail.com>
2021-09-03 13:50:07 -0700

    expose stix() to the plasmapy.dispersion.numerical namespace

--- plasmapy/dispersion/numerical/__init__.py
+++ plasmapy/dispersion/numerical/__init__.py
@@ -2,6 +2,7 @@
 The `~plasmapy.dispersion.numerical` subpackage contains functionality
 associated with numerical dispersion solvers.
 """
-__all__ = ["hollweg"]
+__all__ = ["hollweg","stix"]
 
 from plasmapy.dispersion.numerical.hollweg_ import hollweg
+from plasmapy.dispersion.numerical.stix_ import stix
