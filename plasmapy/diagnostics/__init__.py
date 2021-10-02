e9a6244a552aa93c46098286812f4267d64a1f75
Nick Murphy <namurphy@cfa.harvard.edu>
2021-10-02 16:11:48 -0400

    Auto-format docstrings in diagnostics.__init__

--- plasmapy/diagnostics/__init__.py
+++ plasmapy/diagnostics/__init__.py
@@ -1,7 +1,6 @@
-"""
-The diagnostics subpackage contains functionality for defining diagnostic parameters
-and processing data collected by diagnostics, synthetic or experimental.
-"""
+"""The diagnostics subpackage contains functionality for defining diagnostic
+parameters and processing data collected by diagnostics, synthetic or
+experimental."""
 __all__ = ["langmuir", "thomson"]
 
 from plasmapy.diagnostics import langmuir, thomson
