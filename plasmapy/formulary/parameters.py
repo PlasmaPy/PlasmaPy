e05135b92f33d49f3a17ee7663500e506d5fb0b3
Erik <eteverson@gmail.com>
2021-10-14 20:11:58 -0700

    clean up the plasma_frequency docstring so it is not ion centric

--- plasmapy/formulary/parameters.py
+++ plasmapy/formulary/parameters.py
@@ -1498,37 +1498,38 @@ def plasma_frequency(n: u.m ** -3, particle: Particle, z_mean=None) -> u.rad / u
     Parameters
     ----------
     n : `~astropy.units.Quantity`
-        Particle number density in units convertible to per cubic meter.
+        Particle number density in units convertible to m\ :sup:`-3`.
 
     particle : `~plasmapy.particles.Particle`
-        Representation of the particle species (e.g., 'p' for protons, 'D+'
-        for deuterium, or 'He-4 +1' for singly ionized helium-4). If no
-        charge state information is provided, then the particles are assumed
-        to be singly charged.
+        Representation of the particle species (e.g., ``"p"`` for
+        protons, ``"D+"`` for deuterium, or ``"He-4 +1"`` for singly
+        ionized helium-4). If no charge state information is provided,
+        then the particles are assumed to be singly charged.
 
-    z_mean : `~astropy.units.Quantity`, optional
-        The average ionization (arithmetic mean) for a plasma where the
-        a macroscopic description is valid. If this quantity is not
-        given then the atomic charge state (`int`) of the ion
-        is used. This is effectively an average plasma frequency for the
-        plasma where multiple charge states are present.
+    z_mean : `~numbers.Real`, optional
+        The average ionization (arithmetic mean) for the particle
+        species in the plasma.  Typically the charge state will be
+        dervied from the ``particle`` argument, but this keyword will
+        override that behavior.
 
     Returns
     -------
     omega_p : `~astropy.units.Quantity`
-        The particle plasma frequency in radians per second.
+        The particle plasma frequency in radians per second.  Setting
+        keyword ``to_hz=True`` will apply the factor of :math:`1/2π`
+        and yield a value in Hz.
 
     Raises
     ------
     `TypeError`
-        If ``n_i`` is not a `~astropy.units.Quantity` or particle is not of
-        an appropriate type.
+        If ``n`` is not a `~astropy.units.Quantity` or particle is not
+        of an appropriate type.
 
     `~astropy.units.UnitConversionError`
-        If ``n_i`` is not in correct units.
+        If ``n`` is not in correct units.
 
     `ValueError`
-        If ``n_i`` contains invalid values or particle cannot be used to
+        If ``n`` contains invalid values or particle cannot be used to
         identify an particle or isotope.
 
     Warns
@@ -1541,14 +1542,14 @@ def plasma_frequency(n: u.m ** -3, particle: Particle, z_mean=None) -> u.rad / u
     The particle plasma frequency is
 
     .. math::
-        ω_{pi} = Z e \sqrt{\frac{n_i}{\epsilon_0 m_i}}
+        ω_{p} = Z |e| \sqrt{\frac{n}{\epsilon_0 m}}
 
-    At present, `astropy.units` does not allow direct conversions from
-    radians/second for angular frequency to 1/second or Hz for
-    frequency.  The `~astropy.units.dimensionless_angles` equivalency
-    allows for that conversion, but does not account for the factor of
-    :math:`2π`\ . The alternatives are to convert to cycle/second or to
-    do the conversion manually, as shown in the examples.
+    where :math:`m` is the mass of the particle, :math:`e` is the
+    fundamental unit of charge, :math:`Z` is the average charge state
+    ``z_mean`` of the particle species, :math:`n` is the particle number
+    density.  This form of the plasma frequency has units of
+    radians / s, but using the ``to_hz`` will apply the factor of
+    :math:`1/2π` to give a value in Hz.
 
     Examples
     --------
