620a1ffc94a8e5b8f5d749ac5f4eaf6f9be3448d
Rajagopalan-Gangadharan <g.raju2000@gmail.com>
2021-10-28 13:21:55 +0530

    Move Calculator to utils instead of root

---
+++ plasmapy/utils/calculator.py
@@ -0,0 +1,21 @@
+""" Script file to launch the notebook as a standalone clean app """
+
+import os
+import argparse
+import plasmapy
+
+def main():
+    parser = argparse.ArgumentParser(description='Plasma calculator')
+    parser.add_argument('--port', type=int, default=8866, help='Port to run the notebook')
+    parser.add_argument('--dark', action='store_true', help='Turn on dark mode, reduces eye strain')
+    parser.add_argument('--no-browser', action='store_true', help='Do not open the browser')
+    
+    module_path = plasmapy.__path__[0]
+    calculator_path = os.path.dirname(module_path)
+    computed_calculator_path = os.path.join(calculator_path,'docs','notebooks','plasma_calculator.ipynb')
+    
+    args = parser.parse_args()
+    theme = 'dark' if args.dark else 'light'
+    no_browser = "--no-browser" if args.no_browser else ""
+
+    os.system(f'voila {no_browser} --port={args.port} --theme={theme} {computed_calculator_path} --VoilaConfiguration.file_whitelist="[\'favicon.ico\']"')
