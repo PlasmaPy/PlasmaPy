cab0aa39859b2fc460257834a23d0eafdb1b801d
Shane Brown <ssjbrown@udel.edu>
2021-10-29 12:58:58 -0400

    Update plasmapy/dispersion/numerical/hollweg_.py
    Co-authored-by: Erik Everson <eteverson@gmail.com>

--- plasmapy/dispersion/numerical/hollweg_.py
+++ plasmapy/dispersion/numerical/hollweg_.py
@@ -42,7 +42,7 @@ def hollweg(
     calculates the numerical solution to the two fluid dispersion relation
     presented by Hollweg 1999. This dispersion relation assumes
     :math:`\omega/\omega_{\rm ci} \ll 1`, a uniform magnetic field
-    :math: `\mathbf{B_0}`, and quasi-neutrality.
+    :math:`\mathbf{B_0}`, and quasi-neutrality.
 
     Parameters
     ----------
