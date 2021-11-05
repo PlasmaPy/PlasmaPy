3904e1ca6fda01bb23e4e9bea2f0e03249754881
etjohnson <77989826+etjohnson@users.noreply.github.com>
2021-11-05 11:15:48 -0400

    Updates

--- plasmapy/dispersion/numerical/test_stix_.py
+++ plasmapy/dispersion/numerical/test_stix_.py
@@ -1,89 +1,38 @@
 from astropy import units as u
-
+import numpy as np
+import pytest
 from stix_ import stix
 
-import pytest
+class StixTest:
+	_kwargs_single_valued = {
+		"B": 8.3e-9 * u.T,
+		"k": 0.001* u.rad / u.m,
+		"ions": ['e-','H+'],
+		"omega_ions": [4.0e5, 2.0e5] * u.rad / u.s,
+		"theta": 30 * u.deg,
+	}
 
-B = 8.3e-9 * u.T
-k_int = 0.001 * u.rad / u.m
-k_arr = [0.001, 0.002] * u.rad / u.m
-ions_int = "e-"
-ions_arr = ["e-", "H+"]
-omega_ions_int = 4.0e5 * u.rad / u.s
-omega_ions_arr = [4.0e5, 2.0e5] * u.rad / u.s
-theta = 30 * u.deg
+	@pytest.mark.parametrize(
+		"kwargs, _error",
+		[
+			({**_kwargs_single_valued, "B": "wrong type"}, TypeError),
+			({**_kwargs_single_valued, "B": [8e-9, 8.5e-9] * u.T}, ValueError),
+			({**_kwargs_single_valued, "B": -1 * u.T}, ValueError),
+            		({**_kwargs_single_valued, "B": 5 * u.m}, u.UnitTypeError),
+            		({**_kwargs_single_valued, "k": 0 * u.rad / u.m}, ValueError),
+            		({**_kwargs_single_valued, "k": -1.0 * u.rad / u.m}, ValueError),
+            		({**_kwargs_single_valued, "k": 5 * u.s}, u.UnitTypeError),
+            		({**_kwargs_single_valued, "ions": {"not": "a particle"}}, TypeError),
+           		({**_kwargs_single_valued, "ions": "e-"}, ValueError),
+			({**_kwargs_single_valued, "omega_ions": "wrong type"}, TypeError),
+			({**_kwargs_single_valued, "omega_ions": 6 * u.eV}, u.UnitTypeError),
+			({**_kwargs_single_valued, "theta": np.ones((3, 2)) * u.deg}, ValueError),
+            		({**_kwargs_single_valued, "theta": 5 * u.eV}, u.UnitTypeError)
+		]
+	)
+	def test_raises(self, kwargs, _error):
+		with pytest.raises(_error):
+            		stix(**kwargs)
 
-inputs = {}
-eops = {}
-
-inputs = {
-    "B": B,
-    "k": k_int,
-    "ions": ions_int,
-    "omega_ions": omega_int,
-    "theta": theta,
-}
-
-eops = {
-
-}
-
-def test_confrim(inputs, eops):
-    assert stix(**inputs) == eops
-
-inputs = {
-    "B": B,
-    "k": k_int,
-    "ions": ions_arr,
-    "omega_ions": omega_arr,
-    "theta": theta,
-}
-
-eops = {
-
-}
-
-def test_confirm(inputs, eops):
-    assert stix(**inputs) == eops
-
-inputs = {
-    "B": B,
-    "k": k_arr,
-    "ions": ions_int,
-    "omega_ions": omega_int,
-    "theta": theta,
-}
-
-eops = {
-
-}
-
-def test_confirm(inputs, eops):
-    assert stix(**inputs) == eops
-
-inputs = {
-    "B": B,
-    "k": k_arr,
-    "ions": ions_arr,
-    "omega_ions": omega_arr,
-    "theta": theta,
-}
-
-eops = {
-
-}
-
-def test_confirm(inputs, eops):
-    assert stix(**inputs) == eops
-
-inputs = {
-    "B": B,
-    "k": k_arr,
-    "ions": ions_arr,
-    "omega_ions": omega_int,
-    "theta": theta,
-}
-    
-def test_flag(inputs):
-    with pytest.raises(SystemExit):
-        stix(**inputs)
+       
+print('Out')
