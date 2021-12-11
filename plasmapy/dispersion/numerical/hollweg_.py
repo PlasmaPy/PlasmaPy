a6077829f5051452603bbdbbc6e0d7d9673cf7d5
Sjbrownian <ssjbrown@udel.edu>
2021-12-10 22:13:09 -0500

    example output changes

--- plasmapy/dispersion/numerical/hollweg_.py
+++ plasmapy/dispersion/numerical/hollweg_.py
@@ -183,9 +183,9 @@ def hollweg(
     ... }
     >>> omegas = hollweg(**inputs)
     >>> omegas
-    {'fast_mode': <Quantity [2.62911663e-02, 2.27876968e+03] rad / s>,
-     'alfven_mode': <Quantity [7.48765909e-04, 2.13800404e+03] rad / s>,
-     'acoustic_mode': <Quantity [0.00043295, 0.07358991] rad / s>}
+    {'fast_mode': <Quantity [2.62911663e-02+0.j, 2.27876968e+03+0.j] rad / s>,
+     'alfven_mode': <Quantity [7.48765909e-04+0.j, 2.13800404e+03+0.j] rad / s>,
+     'acoustic_mode': <Quantity [0.00043295+0.j, 0.07358991+0.j] rad / s>}
     """
 
     # validate argument ion
