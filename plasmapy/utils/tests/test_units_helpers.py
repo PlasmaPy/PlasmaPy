0bbdea7c8bfd1b5a924c0c2b2bec647fba453fd1
Nick Murphy <namurphy@cfa.harvard.edu>
2021-12-16 05:20:48 -0500

    First draft of get_physical_type_dict tests

--- plasmapy/utils/tests/test_units_helpers.py
+++ plasmapy/utils/tests/test_units_helpers.py
@@ -1,17 +1,72 @@
 import astropy.units as u
-import astropy.units.physical as physical
 import pytest
+
+from astropy.constants import c, m_e
+from collections import namedtuple
 
 from plasmapy.utils.units_helpers import get_physical_type_dict
 
 
-@pytest.fixture
-def pre_physical_type_dict():
-    units = (u.m, u.m ** -3, u.m * u.s)
-    return {unit.physical_type: 5 * unit for unit in units}
+def test_get_physical_type_dict_specific_example():
+    units = [u.m, u.m ** -3, u.m * u.s]
+    quantities = [5 * unit for unit in units]
+    expected = {quantity.unit.physical_type: quantity for quantity in quantities}
+    new_physical_type_dict = get_physical_type_dict(quantities)
+    assert new_physical_type_dict == expected
 
 
-def test_get_physical_type(pre_physical_type_dict):
-    units = pre_physical_type_dict.values()
-    new_physical_type_dict = get_physical_type_dict(units)
-    assert pre_physical_type_dict == new_physical_type_dict
+velocity = u.get_physical_type("velocity")
+mass = u.get_physical_type("mass")
+dimensionless = u.get_physical_type(1)
+
+test_case = namedtuple("case", ["collection", "kwargs", "expected"])
+
+
+test_cases = [
+    test_case(
+        collection=(c, m_e),
+        kwargs={},
+        expected={velocity: c, mass: m_e},
+    ),
+    test_case(
+        collection=(c, 5),
+        kwargs={"only_quantities": True, "numbers_become_quantities": True},
+        expected={velocity: c, dimensionless: u.Quantity(5)},
+    ),
+    test_case(
+        collection=(c, 5),
+        kwargs={"only_quantities": False, "numbers_become_quantities": True},
+        expected={velocity: c, dimensionless: u.Quantity(5)},
+    ),
+    test_case(
+        collection=(c, 5),
+        kwargs={"only_quantities": True, "numbers_become_quantities": False},
+        expected={velocity: c},
+    ),
+    test_case(
+        collection=(c, 5),
+        kwargs={"only_quantities": False, "numbers_become_quantities": False},
+        expected={velocity: c, dimensionless: 5},
+    ),
+]
+
+
+@pytest.mark.parametrize("collection, kwargs, expected", test_cases)
+def test_get_physical_type_dict(collection, kwargs, expected):
+    physical_type_dict = get_physical_type_dict(collection, **kwargs)
+    assert physical_type_dict == expected
+
+
+test_cases_exceptions = [
+    test_case(
+        collection=(u.m, 5 * u.m),
+        kwargs={},
+        expected=ValueError,
+    ),
+]
+
+
+@pytest.mark.parametrize("collection, kwargs, expected", test_cases_exceptions)
+def test_get_physical_type_dict_exceptions(collection, kwargs, expected):
+    with pytest.raises(expected):
+        get_physical_type_dict(collection, **kwargs)
