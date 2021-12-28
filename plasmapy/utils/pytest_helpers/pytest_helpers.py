52e75e7abf61bda8ddf54e2374ef61bfcaa76fa8
Dominik Stańczak <stanczakdominik@gmail.com>
2021-12-28 18:59:49 +0100

    Hack to allow lists or tuples for *args

--- plasmapy/utils/pytest_helpers/pytest_helpers.py
+++ plasmapy/utils/pytest_helpers/pytest_helpers.py
@@ -218,7 +218,7 @@ def run_test(
     if kwargs is None:
         kwargs = {}
 
-    if not isinstance(args, tuple):
+    if not type(args) in [tuple, list]:
         args = (args,)
 
     if not callable(func):
