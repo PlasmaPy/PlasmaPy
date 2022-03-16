d8a205d82f61147117b110cffa3fe65bdabd8f04
Peter Heuer <pvheuer@gmail.com>
2022-03-16 15:41:30 -0400

    Rename data -> downloader, split file contents test into separate test

--- plasmapy/utils/data/tests/test_downloader.py
+++ plasmapy/utils/data/tests/test_downloader.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import pytest
 
-from plasmapy.utils.data import data
+from plasmapy.utils.data import downloader
 
 test_files = [
     # Test downloading a file
@@ -23,19 +23,18 @@ def test_get_file(filename, expected):
     Test the get_file function
 
     """
-
     # Delete file if it already exists, so the test always downloads it
-    dl_path = os.path.join(data._DOWNLOADS_PATH, filename)
+    dl_path = os.path.join(downloader._DOWNLOADS_PATH, filename)
     if os.path.exists(dl_path):
         os.remove(dl_path)
 
     if expected is not None:
         with pytest.raises(expected):
-            path = data.get_file(filename)
+            path = downloader.get_file(filename)
 
     else:
         # Download data (or check that it already exists)
-        path = data.get_file(filename)
+        path = downloader.get_file(filename)
 
         # For this one file, check that the contents are right manually
         if filename == "NIST_PSTAR_aluminum.txt":
@@ -43,4 +42,23 @@ def test_get_file(filename, expected):
             assert np.allclose(arr[0, :], np.array([1e-3, 1.043e2]))
 
         # Get the file again, already existing so it doesn't download it again
-        path = data.get_file(filename)
+        path = downloader.get_file(filename)
+
+
+def test_get_file_NIST_PSTAR_datafile():
+    """
+    Test the get_file function on a NIST PSTAR datafile
+
+    """
+    filename = "NIST_PSTAR_aluminum.txt"
+
+    # Delete file if it already exists, so the test always downloads it
+    dl_path = os.path.join(downloader._DOWNLOADS_PATH, filename)
+    if os.path.exists(dl_path):
+        os.remove(dl_path)
+
+    # Download data (or check that it already exists)
+    path = downloader.get_file(filename)
+
+    arr = np.loadtxt(path, skiprows=7)
+    assert np.allclose(arr[0, :], np.array([1e-3, 1.043e2]))
