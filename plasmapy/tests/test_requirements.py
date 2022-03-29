052456fc8ee247a8830f4c9651e996b530ee0b43
Nick Murphy <namurphy@cfa.harvard.edu>
2022-03-28 22:36:57 -0400

    Get a dict containing requirements from all sources
    This would have a structure like:
    
      requirements["build"]["requirements/build.txt"]
    
    which would contain a set of the requirements strings. Experimental,
    so I'm not sure if I'm going to use this.

--- plasmapy/tests/test_requirements.py
+++ plasmapy/tests/test_requirements.py
@@ -4,7 +4,7 @@ import os
 import setuptools
 import toml
 
-from typing import Dict, List
+from typing import Dict, Set
 
 import plasmapy
 
@@ -13,7 +13,7 @@ requirements_directory = f"{base_directory}/requirements"
 requirements_prefixes = ("build", "docs", "extras", "install", "tests")
 
 
-def read_requirements_txt_file(prefix: str, requirements_directory: str) -> List[str]:
+def read_requirements_txt_file(prefix: str, requirements_directory: str) -> Set[str]:
     """
     Read in a text file containing requirements.
 
@@ -35,7 +35,7 @@ def read_requirements_txt_file(prefix: str, requirements_directory: str) -> List
     filename = f"{requirements_directory}/{prefix}.txt"
     with open(filename) as file:
         lines_of_file = file.readlines()
-    return [line.strip() for line in lines_of_file if line[0].isalpha()]
+    return {line.strip() for line in lines_of_file if line[0].isalpha()}
 
 
 def get_requirements_from_txt() -> Dict[str, str]:
@@ -49,18 +49,41 @@ def get_requirements_from_txt() -> Dict[str, str]:
     }
 
 
-def get_requirements_from_setup_cfg() -> Dict[str, str]:
+def get_requirements_from_setup_cfg() -> Dict[str, Set[str]]:
     """Get the requirements that are contained in setup.cfg."""
     configuration = setuptools.config.read_configuration(f"{base_directory}/setup.cfg")
     return {
-        "docs": configuration["options"]["extras_require"]["docs"],
-        "extras": configuration["options"]["extras_require"]["extras"],
-        "install": configuration["options"]["install_requires"],
-        "tests": configuration["options"]["extras_require"]["tests"],
+        "docs": set(configuration["options"]["extras_require"]["docs"]),
+        "extras": set(configuration["options"]["extras_require"]["extras"]),
+        "install": set(configuration["options"]["install_requires"]),
+        "tests": set(configuration["options"]["extras_require"]["tests"]),
     }
 
 
-def get_requirements_from_pyproject_toml() -> Dict[str, str]:
+def get_requirements_from_pyproject_toml() -> Dict[str, Set[str]]:
     """Get the requirements that are contained in pyproject.toml."""
     pyproject_toml = toml.load(f"{base_directory}/pyproject.toml")
-    return {"build": pyproject_toml["build-system"]["requires"]}
+    return {"build": set(pyproject_toml["build-system"]["requires"])}
+
+
+def get_requirements_dict() -> Dict[str, Dict[str, Set[str]]]:
+
+    requirements_from_txt = get_requirements_from_txt()
+    requirements_from_pyproject_toml = get_requirements_from_pyproject_toml()
+    requirements_from_setup_cfg = get_requirements_from_setup_cfg()
+
+    requirements = {
+        prefix: {f"requirements/{prefix}.txt": requirements_from_txt[prefix]}
+        for prefix in requirements_from_txt
+    }
+
+    file_requirements_pairs = [
+        ("pyproject.toml", requirements_from_pyproject_toml),
+        ("setup.cfg", requirements_from_setup_cfg),
+    ]
+
+    for file, req in file_requirements_pairs:
+        for prefix in req:
+            requirements[prefix][file] = req[prefix]
+
+    return requirements
