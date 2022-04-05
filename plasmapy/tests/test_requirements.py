00dab5904fa8758e3342a8a715b08ae220b370ed
Nick Murphy <namurphy@cfa.harvard.edu>
2022-04-05 11:41:55 -0400

    Use tomli instead of toml

--- plasmapy/tests/test_requirements.py
+++ plasmapy/tests/test_requirements.py
@@ -3,7 +3,7 @@
 import os
 import pytest
 import setuptools
-import toml
+import tomli
 
 from typing import Dict, Set
 
@@ -63,7 +63,7 @@ def get_requirements_from_setup_cfg() -> Dict[str, Set[str]]:
 
 def get_requirements_from_pyproject_toml() -> Dict[str, Set[str]]:
     """Get the requirements that are contained in pyproject.toml."""
-    pyproject_toml = toml.load(f"{base_directory}/pyproject.toml")
+    pyproject_toml = tomli.load(f"{base_directory}/pyproject.toml")
     return {"build": set(pyproject_toml["build-system"]["requires"])}
 
 
