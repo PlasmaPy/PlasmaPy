dd2994fcac2adf1cf70a18694908550e65b46403
Nick Murphy <namurphy@cfa.harvard.edu>
2022-04-19 13:16:42 -0400

    Continue change to use pathlib
    Co-authored-by: Dominik Stańczak <stanczakdominik@gmail.com>

--- plasmapy/tests/test_requirements.py
+++ plasmapy/tests/test_requirements.py
@@ -34,9 +34,8 @@ def read_requirements_txt_file(prefix: str, requirements_directory: str) -> Set[
         excluding lines that do not start with an alphabetic character
         (like comments).
     """
-    filename = f"{requirements_directory}/{prefix}.txt"
-    with open(filename) as file:
-        lines_of_file = file.readlines()
+    file = (requirements_directory / prefix).with_suffix(".txt")
+    lines_of_file = file.read_text().splitlines()
     return {line.strip() for line in lines_of_file if line[0].isalpha()}
 
 
