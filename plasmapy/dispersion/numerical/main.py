747b8872b7add8916f064829371e837e7bd4cd37
Elliot <ejohn@udel.edu>
2022-04-20 12:28:30 -0400

    Update

--- plasmapy/dispersion/numerical/main.py
+++ plasmapy/dispersion/numerical/main.py
@@ -5,10 +5,10 @@ from plasmapy.particles import Particle
 
 inputs = {
     "B": 8.3e-9 * u.T,
-    "k": 0.001 * u.rad / u.m,
+    "w": 0.001 * u.rad / u.s,
     "ions": [Particle("H+"), Particle("He+")],
     "n_i": [4.0e5, 2.0e5] * u.m ** -3,
     "theta": 30 * u.deg,
 }
 w = stix(**inputs)
-print(w[0.001])
+print(w)
