f8de5827096e5c02f7ee08727eb49285e5d21647
haman80 <70356097+haman80@users.noreply.github.com>
2022-04-20 16:09:49 -0400

    Update plasmapy/analysis/tests/test_nullpoint.py
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>

--- plasmapy/analysis/tests/test_nullpoint.py
+++ plasmapy/analysis/tests/test_nullpoint.py
@@ -424,7 +424,7 @@ class Test_classify_null_point:
         assert uniform_null_point_find(**kwargs)[0].classification == expected
 
 
-# Testing a magnetic filed that violates the divergence constraint
+# Testing a magnetic field that violates the divergence constraint
 def test_null_point_find9():
     nullpoint9_args = {
         "x_range": [-0.1, 0.1],
