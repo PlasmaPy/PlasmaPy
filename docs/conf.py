5814b45229d20cf8b920f9edbc50f417852634ff
Nick Murphy <namurphy@cfa.harvard.edu>
2022-04-26 21:39:32 -0400

    Allow redirects for Wikipedia for make linkcheck
    This is because it showed redirects where the only difference was
    capitalization of the first letter.

--- docs/conf.py
+++ docs/conf.py
@@ -219,6 +219,7 @@ linkcheck_allowed_redirects = {
     r"https://docs.+\.io": r"https://docs.+\.io/en/.+",
     r"https://docs.+\.com": r"https://docs.+\.com/en/.+",
     r"https://docs.+\.dev": r"https://docs.+\.dev/en/.+",
+    r"https://en.wikipedia.org/wiki.+": "https://en.wikipedia.org/wiki.+",
     r"https://.+\.readthedocs\.io": r"https://.+\.readthedocs\.io/en/.+",
     r"https://www\.sphinx-doc\.org": r"https://www\.sphinx-doc\.org/en/.+",
     r"https://.+/github\.io": r"https://.+/github\.io/en/.+",
