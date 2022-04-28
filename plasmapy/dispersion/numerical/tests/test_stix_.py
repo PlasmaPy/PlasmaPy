4fb22d9f43f341ff65ddc84bebca2ef604c21eee
etjohnson <77989826+etjohnson@users.noreply.github.com>
2022-04-28 15:09:05 -0400

    Update test_stix_.py

--- plasmapy/dispersion/numerical/tests/test_stix_.py
+++ plasmapy/dispersion/numerical/tests/test_stix_.py
@@ -41,50 +41,50 @@ class TestStix:
         [
             ({**_kwargs_single_valued, "w": 2 * u.rad / u.s}, {"shape": ()}),
             (
-                {**_kwargs_single_valued, "w": [10] * u.rad / u.s},
-                {"shape": ()},
+                    {**_kwargs_single_valued, "w": [10] * u.rad / u.s},
+                    {"shape": ()},
             ),
             (
-                {**_kwargs_single_valued, "w": [10, 20, 30] * u.rad / u.s},
-                {"shape": (3,)},
+                    {**_kwargs_single_valued, "w": [10, 20, 30] * u.rad / u.s},
+                    {"shape": (3,)},
             ),
-            ({**_kwargs_single_valued, "ions": ["He+", "e-"]}, {"shape": (2,)}),
+            ({**_kwargs_single_valued, "ions": ["He+", "H+"]}, {"shape": (2,)}),
             (
-                {
-                    **_kwargs_single_valued,
-                    "ions": ["He+"],
-                    "n_i": [1] * u.m ** -3,
-                },
-                {"shape": (1,)},
+                    {
+                        **_kwargs_single_valued,
+                        "ions": ["He+"],
+                        "n_i": [1] * u.m ** -3,
+                    },
+                    {"shape": (1,)},
             ),
-            ({**_kwargs_single_valued, "ions": ["He+", "e-"]}, {"shape": (2,)}),
+            ({**_kwargs_single_valued, "ions": ["He+", "H+"]}, {"shape": (2,)}),
             (
-                {
-                    **_kwargs_single_valued,
-                    "ions": ["He+", "H+"],
-                    "n_i": [1, 2] * u.m ** -3,
-                },
-                {"shape": (2,)},
+                    {
+                        **_kwargs_single_valued,
+                        "ions": ["He+", "H+"],
+                        "n_i": [1, 2] * u.m ** -3,
+                    },
+                    {"shape": (2,)},
             ),
         ],
     )
     def test_return_structure(self, kwargs, expected):
 
-        w = stix(**kwargs)
+        k = stix(**kwargs)
 
-        assert isinstance(w, dict)
+        assert isinstance(k, dict)
 
-        for key in w.keys():
-            for val in w[key]:
+        for key in k.keys():
+            for val in k[key]:
                 assert isinstance(val, u.Quantity)
                 assert val.unit == u.rad / u.m
 
-        assert np.shape(w) == expected["shape"]
+        assert np.shape(k) == expected["shape"]
 
     @pytest.mark.parametrize(
         "kwargs, _warning",
         [
-            ({**_kwargs_single_valued, "w": 0 * u.m / u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "w": 0 * u.rad}, u.UnitTypeError),
         ],
     )
     def test_warns(self, kwargs, _warning):
