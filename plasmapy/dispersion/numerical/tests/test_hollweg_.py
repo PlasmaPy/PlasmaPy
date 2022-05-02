59e8ece829965ba886be1491d643ea2b7d02921b
Sjbrownian <ssjbrown@udel.edu>
2022-05-02 12:49:17 -0400

    added test for multiple k/theta vals

--- plasmapy/dispersion/numerical/tests/test_hollweg_.py
+++ plasmapy/dispersion/numerical/tests/test_hollweg_.py
@@ -256,6 +256,14 @@ class TestHollweg:
                 },
                 {"shape": (3,)},
             ),
+            (
+                {
+                    **_kwargs_single_valued,
+                    "k": [1, 2, 3] * u.rad / u.m,
+                    "theta": [50, 77] * u.rad,
+                },
+                {"shape": (3, 2)},
+            ),
         ],
     )
     def test_return_structure(self, kwargs, expected):
