8ac39830752df15409f89d99c98562148702af76
etjohnson <77989826+etjohnson@users.noreply.github.com>
2022-05-03 10:04:31 -0400

    Update

--- plasmapy/dispersion/numerical/__init__.py
+++ plasmapy/dispersion/numerical/__init__.py
@@ -5,4 +5,3 @@ associated with numerical dispersion solvers.
 __all__ = ["hollweg", "stix"]
 
 from plasmapy.dispersion.numerical.hollweg_ import hollweg
-from plasmapy.dispersion.analytical.stix_ import stix
