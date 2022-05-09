41ad898576b97ddcc0bb18295d325a0dd6167513
Sjbrownian <ssjbrown@udel.edu>
2022-05-09 15:44:12 -0400

    bug fix

--- plasmapy/dispersion/numerical/tests/test_hollweg_.py
+++ plasmapy/dispersion/numerical/tests/test_hollweg_.py
@@ -117,7 +117,8 @@ class TestHollweg:
             hollweg(**kwargs)
 
     @pytest.mark.parametrize(
-        "kwargs, expected,"[
+        "kwargs, expected,",
+        [
             (  # array input for k, single value for theta
                 {
                     **_kwargs_single_valued,
