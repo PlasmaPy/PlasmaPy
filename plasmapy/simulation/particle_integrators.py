f81cef3b44f7f77c125644841ed4c95cd4b990ad
MKastek <mkastek8@gmail.com>
2022-05-15 19:10:59 +0200

    added examples in docstring

--- plasmapy/simulation/particle_integrators.py
+++ plasmapy/simulation/particle_integrators.py
@@ -36,6 +36,29 @@ def boris_push(x, v, B, E, q, m, dt):
     dt : `float`
         Timestep, in SI (second) units.
 
+    Examples
+    --------
+    >>> from plasmapy.simulation.particle_integrators import boris_push
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as plt
+    >>>
+    >>> x_0 = np.array([0.0, 0.0, 0.0])
+    >>> v_0 = np.array([5.0, 0.0, 0.0])
+    >>> B = np.array([0.0, 0.0, -5.0])
+    >>> x = []
+    >>> y = []
+    >>>
+    >>> for _ in range(150):
+    >>>     boris_push(x = x_0, v = v_0, B=B, E=0, q=1.0, m=1.0, dt=0.01)
+    >>>     x.append(x_0[0,0])
+    >>>     y.append(x_0[0,1])
+    >>> plt.plot(x,y)
+    >>> plt.xlabel('x [m]')
+    >>> plt.ylabel('y [m]')
+    >>> plt.title(r"Single particle motion without $\vec{E}$ field")
+    >>> plt.grid()
+    >>> plt.gca().set_aspect('equal')
+
     Notes
     ----------
     The Boris algorithm :cite:p:`boris:1970` is the standard energy
