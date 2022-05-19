675543670f940c86b5dcd3712fe6083f4f255635
Isaias <jota33@users.noreply.github.com>
2022-05-19 18:28:01 -0300

    Update plasmapy/plasma/sources/plasmablob.py
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>

--- plasmapy/plasma/sources/plasmablob.py
+++ plasmapy/plasma/sources/plasmablob.py
@@ -134,7 +134,7 @@ class PlasmaBlob(GenericPlasma):
         kinetic energy.
         """
         couple = coupling_parameter(
-            self.T_e, self.n_e, (self.particle, self.particle), self.Z*u.dimensionless_unscaled
+            self.T_e, self.n_e, (self.particle, self.particle), self.Z
         )
         if couple < 0.01:
             warnings.warn(
