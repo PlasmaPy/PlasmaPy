4acea42a6b82316114814d01baf5d9455e9851ec
etjohnson <77989826+etjohnson@users.noreply.github.com>
2022-06-09 13:41:36 -0400

    update

--- plasmapy/dispersion/numerical/kinetic_alfven_.py
+++ plasmapy/dispersion/numerical/kinetic_alfven_.py
@@ -283,17 +283,3 @@ def kinetic_alfven(
             )
 
     return omega
-
-
-inputs = {
-    "k": np.logspace(-7, -2, 2) * u.rad / u.m,
-    "theta": 30 * u.deg,
-    "B": 8.3e-9 * u.T,
-    "n_i": 5 * u.m ** -3,
-    "T_e": 1.6e6 * u.K,
-    "T_i": 4.0e5 * u.K,
-    "ion": Particle("p+"),
-}
-
-omegas = kinetic_alfven(**inputs)
-print(omegas)
