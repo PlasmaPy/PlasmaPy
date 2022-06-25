4584a679ad771d5aecfbe9f6e26d898c1c3021ce
syip1 <shermanyip2015@gmail.com>
2022-06-25 11:31:10 +0800

    fixed warnings in plasmablob.py, checks.py and particle_class.py

--- plasmapy/particles/particle_class.py
+++ plasmapy/particles/particle_class.py
@@ -2267,10 +2267,8 @@ class CustomParticle(AbstractPhysicalParticle):
         return hash(self.__repr__())
 
 
-def molecule(
-    symbol: str, Z: Optional[Integral] = None
-) -> Union[Particle, CustomParticle]:
-    """
+def molecule(symbol: str, Z: Optional[Integral] = None) -> Union[Particle, CustomParticle]:
+    r"""
     Parse a molecule symbol into a |CustomParticle| or |Particle|.
 
     Parameters
