9177be82775c057361c5d70a18ef3c348491149e
Amina Ahmed <aminaahmed1803@gmail.com>
2022-06-27 15:05:43 -0400

    File to start coding magnetics module

---
+++ plasmapy/diagnostics/magnetics.py
@@ -0,0 +1,18 @@
+"""
+Defines the magnetics analysis module as part of `plasmapy.diagnostics`.
+"""
+
+__all__ = [
+    "Characterstic"
+]
+
+#import Packages
+from warnings import warn
+
+# class for magnetics
+
+
+class Magnetics:
+    """
+    Group together values for magnetic analysis
+    """
