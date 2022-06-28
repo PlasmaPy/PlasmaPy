870010bb1744455e325d1feaa6e872e5d3ed84b6
Amina Ahmed <aminaahmed1803@gmail.com>
2022-06-28 12:04:54 -0400

    adding to class

--- plasmapy/diagnostics/magnetics.py
+++ plasmapy/diagnostics/magnetics.py
@@ -10,9 +10,20 @@ __all__ = [
 from warnings import warn
 
 # class for magnetics
+import scipy.io as spio
+import matplotlib.pylab as plt
+import numpy as np
+import scipy.integrate as sp
 
 
 class Magnetics:
     """
     Group together values for magnetic analysis
     """
+
+    def __init__(self):
+        print("Class created")
+
+    # Function to Compute Magnetic Field for Bdots
+    def Bdot_field(Bdot5t, tloop_area, time_s):
+        return sp.cumtrapz(Bdot5t/tloop_area, time_s)*1e4  # Gauss
