7a89d2ec89cd80cfd75de54653213afa49fd9821
Nick Murphy <namurphy@cfa.harvard.edu>
2022-07-01 18:34:38 -0400

    Use pathlib to get calculator path

--- plasmapy/utils/calculator/__init__.py
+++ plasmapy/utils/calculator/__init__.py
@@ -4,7 +4,7 @@ Script and utilities to launch the plasma calculator
 __all__ = ["main"]
 
 import argparse
-import os
+import pathlib
 import shlex
 import subprocess
 
@@ -33,15 +33,13 @@ def main():
         "--no-browser", action="store_true", help="Do not open the browser"
     )
 
-    # module_path = plasmapy.__path__[0]
-    module_path = os.path.dirname(os.path.abspath(__file__))
-    computed_calculator_path = os.path.join(module_path, "plasma_calculator.ipynb")
+    notebook_path = pathlib.Path(__file__).parent.absolute() / "plasma_calculator.ipynb"
 
     args = parser.parse_args()
     theme = "dark" if args.dark else "light"
     no_browser = "--no-browser" if args.no_browser else ""
 
-    command = f"voila {no_browser} --port={args.port} --theme={theme} {computed_calculator_path} \
+    command = f"voila {no_browser} --port={args.port} --theme={theme} {notebook_path} \
         --VoilaConfiguration.file_whitelist favicon.ico"
     try:
         subprocess.call(shlex.split(command))
