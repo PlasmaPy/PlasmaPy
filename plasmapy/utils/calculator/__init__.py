1f7db4882ebbd3c5cdbea2979f4953ba413b8959
Nick Murphy <namurphy@cfa.harvard.edu>
2022-07-01 20:54:24 -0400

    Use `pathlib` to get path to `plasma-calculator` notebook (#1613)
    * Use pathlib to get calculator path
    
    * Make command a list of strings
    
    * Modify how plasma-calculator gets opened
    
    Co-authored-by: Erik Everson <eteverson@gmail.com>
    
    Co-authored-by: Erik Everson <eteverson@gmail.com>

--- plasmapy/utils/calculator/__init__.py
+++ plasmapy/utils/calculator/__init__.py
@@ -4,7 +4,7 @@ Script and utilities to launch the plasma calculator
 __all__ = ["main"]
 
 import argparse
-import os
+import pathlib
 import shlex
 import subprocess
 
@@ -33,17 +33,25 @@ def main():
         "--no-browser", action="store_true", help="Do not open the browser"
     )
 
-    # module_path = plasmapy.__path__[0]
-    module_path = os.path.dirname(os.path.abspath(__file__))
-    computed_calculator_path = os.path.join(module_path, "plasma_calculator.ipynb")
+    module_path = pathlib.Path(__file__).parent.absolute()
+    notebook_path = module_path / "plasma_calculator.ipynb"
+    favicon_path = module_path / "favicon.ico"
 
     args = parser.parse_args()
     theme = "dark" if args.dark else "light"
     no_browser = "--no-browser" if args.no_browser else ""
 
-    command = f"voila {no_browser} --port={args.port} --theme={theme} {computed_calculator_path} \
-        --VoilaConfiguration.file_whitelist favicon.ico"
+    command = [
+        "voila",
+        notebook_path,
+        f"--port={args.port}",
+        f"--theme={theme}",
+        f"--VoilaConfiguration.file_whitelist={favicon_path}",
+    ]
+    if no_browser:
+        command.append(no_browser)
+
     try:
-        subprocess.call(shlex.split(command))
+        subprocess.call(command)
     except KeyboardInterrupt:
         print("Stopping calculator! Bye")
