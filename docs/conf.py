17d7965b03464dfbd84b26bb3068d27273763aaf
Erik <eteverson@gmail.com>
2022-07-30 10:04:50 -0700

    replace sphinx_rtd_theme with plasmapy_theme

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,6 @@ extensions = [
     # plasmapy extensions & setups
     "plasmapy_sphinx.ext.autodoc",
     "plasmapy_sphinx.ext.directives",
-    "plasmapy_sphinx.ext.css",
     # other 3rd party extensions
     "sphinx.ext.autodoc",
     "sphinx.ext.extlinks",
@@ -384,7 +383,8 @@ nitpick_ignore_regex = [
 # html_theme = 'alabaster'
 # html_theme = 'traditional'
 # html_theme = 'agogo'
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "plasmapy_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
