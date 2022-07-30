e540a9f1056157b4777ae7ddd05563f991993ca2
Marcin Kastek <37458996+MKastek@users.noreply.github.com>
2022-07-30 10:15:16 +0200

    Update plasmapy/simulation/particle_integrators.py
    Co-authored-by: Dominik Stańczak <stanczakdominik@gmail.com>

--- plasmapy/simulation/particle_integrators.py
+++ plasmapy/simulation/particle_integrators.py
@@ -53,7 +53,7 @@ def boris_push(x, v, B, E, q, m, dt, inplace=True):
     >>> E = np.array([[0.0, 0.0, 0.0]])
     >>> x_t0 = np.array([[0.0, 0.0, 0.0]])
     >>> v_t0 = np.array([[5.0, 0.0, 0.0]])
-    >>> x_t1, v_t1 = boris_push(x = x_t0, v = v_t0, B = B, E = E, q = 1.0, m = 1.0, dt = 0.01)
+    >>> x_t1, v_t1 = boris_push(x = x_t0, v = v_t0, B = B, E = E, q = 1.0, m = 1.0, dt = 0.01, inplace=False)
     >>> x_t1
     array([[ 0.04993754, -0.00249844,  0.        ]])
     >>> v_t1
