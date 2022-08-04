717463c815ecc5ea4e399d2466c4749fadccffa1
Dominik Stańczak <stanczakdominik@gmail.com>
2022-08-04 11:08:23 +0200

    Minor refactor

--- plasmapy/utils/decorators/checks.py
+++ plasmapy/utils/decorators/checks.py
@@ -566,10 +566,7 @@ class CheckUnits(CheckBase):
                 continue
 
             # grab the checks dictionary for the desired parameter
-            try:
-                param_checks = self.checks[param.name]
-            except KeyError:
-                param_checks = None
+            param_checks = self.checks.get(param.name, default=None)
 
             # -- Determine target units `_units` --
             # target units can be defined in one of three ways (in
