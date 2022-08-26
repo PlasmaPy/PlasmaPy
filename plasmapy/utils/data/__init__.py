c6c467e388a090bacf41eb4b6a59b54b0783d396
Peter Heuer <pvheuer@gmail.com>
2022-08-26 15:38:53 -0400

    Update __init__.py

--- plasmapy/utils/data/__init__.py
+++ plasmapy/utils/data/__init__.py
@@ -1,5 +1,5 @@
 """
-The `plasmapy.data` subpackage contains functionality for downloading and retrieving data files.
+The `plasmapy.utils.data` subpackage contains functionality for downloading and retrieving data files.
 """
 __all__ = ["downloader"]
 
