ba3791a6d43fa79bbf8cecc2b16bf89e19cfad31
Nick Murphy <namurphy@cfa.harvard.edu>
2022-09-08 13:48:20 -0400

    Apply particle_input to dimensionless.py

--- plasmapy/formulary/dimensionless.py
+++ plasmapy/formulary/dimensionless.py
@@ -28,7 +28,7 @@ from typing import Optional
 
 from plasmapy.formulary import frequencies, lengths, misc, speeds
 from plasmapy.formulary.quantum import quantum_theta
-from plasmapy.particles import Particle
+from plasmapy.particles import Particle, particle_input
 from plasmapy.utils.decorators import validate_quantities
 
 __all__ += __aliases__
@@ -111,6 +111,7 @@ nD_ = Debye_number
     n={"can_be_negative": False},
     T={"can_be_negative": False, "equivalencies": u.temperature_energy()},
 )
+@particle_input
 def Hall_parameter(
     n: u.m**-3,
     T: u.K,
@@ -430,6 +431,7 @@ Rm_ = Mag_Reynolds
 """Alias to `~plasmapy.formulary.dimensionless.Mag_Reynolds`."""
 
 
+@particle_input
 def Lundquist_number(
     L: u.m,
     B: u.T,
