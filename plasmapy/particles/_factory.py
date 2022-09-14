56c5a576354a292b7bed076413803e51a99dcd19
Nick Murphy <namurphy@cfa.harvard.edu>
2022-09-14 16:34:17 -0400

    Correct physical type for charge

--- plasmapy/particles/_factory.py
+++ plasmapy/particles/_factory.py
@@ -9,7 +9,7 @@ __all__ = []
 import astropy.units as u
 import contextlib
 
-from astropy.units.physical import charge, mass
+from astropy.units.physical import electrical_charge, mass
 from numbers import Integral
 from typing import Union
 
@@ -100,7 +100,7 @@ def _physical_particle_factory(
 
     if isinstance(args[0], u.Quantity):
         physical_type = u.get_physical_type(args[0])
-        if physical_type not in (charge, mass):
+        if physical_type not in (electrical_charge, mass):
             raise u.UnitConversionError(
                 "Cannot create a particle object with a Quantity with a "
                 f"physical type of {physical_type}."
