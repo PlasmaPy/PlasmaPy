22b37436495feb3951c20e88cda383391c956817
ejohnson-96 <77989826+ejohnson-96@users.noreply.github.com>
2022-10-05 10:43:32 -0400

    update

---
+++ plasmapy/collisions/timescales.py
@@ -0,0 +1,16 @@
+"""
+This module contains functionality for calculating the timescales
+for a range of configurations.
+"""
+
+__all__ = [""]
+
+import astropy.units as u
+import numpy as np
+
+
+
+
+@validate_quantities(
+
+)
