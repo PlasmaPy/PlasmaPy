469ba754b423ab4fe7842c9fa69e3610dcc018f6
Dominik Stańczak <stanczakdominik@gmail.com>
2022-11-11 16:29:01 +0100

    Add tryceratops ignores missed while merging

--- plasmapy/_dev/scm_version.py
+++ plasmapy/_dev/scm_version.py
@@ -6,5 +6,5 @@ try:
     from setuptools_scm import get_version
 
     version = get_version(root=pth.join("..", ".."), relative_to=__file__)
-except Exception:
-    raise ImportError("setuptools_scm broken or not installed")
+except Exception as e:
+    raise ImportError("setuptools_scm broken or not installed") from e
