7d75a972026a4e0ba49d7f147d504aa1ac85262e
ejohnson-96 <77989826+ejohnson-96@users.noreply.github.com>
2022-11-17 15:36:08 -0500

    update

--- plasmapy/formulary/collisions/tests/test_timescales.py
+++ plasmapy/formulary/collisions/tests/test_timescales.py
@@ -27,8 +27,19 @@ class TestTimescales:
         "par_speeds": [30 * u.m / u.s, 50 * u.m / u.s],
     }
 
+    version_2016 = 2016
+    args_2016 = {
+        "T_par": [100*u.K, 500*u.K],
+        "T_perp": [600*u.K, 900*u.K],
+        "n_i": 5000*u.m ** -3,
+        "ions": [Particle("He+"), Particle("H+")],
+        "par_speeds": [600, 700] * u.m / u.s,
+        "perp_speeds": [800, 900] * u.m / u.s,
+    }
+
     _kwargs_2009 = {"version": version_2009, "kwargs": args_2009}
     _kwargs_2010 = {"version": version_2010, "kwargs": args_2010}
+    _kwargs_2016 = {"version": version_2016, "kwargs": args_2016}
 
     @pytest.mark.parametrize(
         "version, kwargs, _error",
