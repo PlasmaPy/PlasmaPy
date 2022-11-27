530fd78928d44e7acb3adfc6dd59d33dc20646b6
gregordecristoforo <gregor.decristoforo@gmail.com>
2022-11-27 14:21:13 +0100

    Implement namurphy's suggestions from review in PR 1803

--- plasmapy/analysis/time_series/running_moments.py
+++ plasmapy/analysis/time_series/running_moments.py
@@ -1,33 +1,38 @@
 """Functionality to calculate running moments of time series"""
+
+__all__ = ["running_mean", "running_moment"]
+
 import numpy as np
+
+from collections import namedtuple
 
 
 def running_mean(signal, radius):
     """
-    Calculate the running mean of a sequence
+    Calculate the running mean of a sequence.
 
     Parameters
     ----------
-    signal: |array_like|
-        signal to be averaged.
+    signal : 1D |array_like|
+        Signal to be averaged.
 
-    radius: int
-        window size is 2*radius+1
+    radius : int
+        The window size is ``2 * radius + 1``.
 
     Returns
     -------
-    |array_like|
-        running mean of signal with length len(signal) - 2*radius
+    1D |array_like|
+        Running mean of signal with length ``len(signal) - 2 * radius``.
 
     Raises
     ------
-    'ValueError'
-        if len(signal <= 2 * radius)
+    `ValueError`
+        If ``len(signal) <= 2 * radius``.
 
     Example
     -------
     >>> from plasmapy.analysis.time_series.running_moments import running_mean
-    >>> running_mean([1,2,3,4], 1)
+    >>> running_mean([1, 2, 3, 4], 1)
     array([2., 3.])
     """
     if len(signal) <= 2 * radius:
@@ -41,62 +46,68 @@ def running_mean(signal, radius):
 
 def running_moment(signal, radius, moment=1, time=None):
     """
-    Calculate either the running mean, standard deviation, skewness or excess kurtosis of a sequence
+        Calculate either the running mean, standard deviation, skewness or
+        excess kurtosis of a sequence.
 
-    Parameters
-    ----------
-    signal: |array_like|
-       signal to be averaged
+        Parameters
+        ----------
+        signal: 1D |array_like|
+           Signal to be averaged.
 
-    radius: int
-        window size is 2*radius +1 for running mean and 4*radius + 1 for higher moments
+        radius: int
+            Window size is ``2 * radius + 1`` for running mean and
+            ``4 * radius + 1`` for higher moments.
 
-    moment: int
-        which running moment to compute
-                1: running mean.
-                2: running standard deviation.
-                3: running skewness
-                4: running excess kurtosis
+        moment: int
+            which running moment to compute
+                    1: running mean.
+                    2: running standard deviation.
+                    3: running skewness
+                    4: running excess kurtosis
 
-    time: |array_like|, optional
-        time base of signal
+        time: 1D |array_like|, optional
+            Time base of ``signal``.
 
-    Returns
-    -------
-    run_moment: |array_like|
-        running moment of signal
-        length is (len(signal) - 2*radius) for running mean
-        length is (len(signal) = 4*signal) for higher moments
+        Returns
+        -------
+        Results are return as a namedtuple
+        Running_Moment(run_moment, time).
+    .
+        run_moment: 1D |array_like|
+            Running moment of signal.
+            length is ``(len(signal) - 2 * radius)`` for running mean
+            length is ``(len(signal) - 4 * signal)`` for higher moments
 
-    time: |array_like|
-        time base corresponding to run_moment if time != None
+        time: 1D |array_like|
+            Time base corresponding to ``run_moment`` if ``time`` is
+            not `None`.
 
-    Raises
-    ------
-    'ValueError'
-        if moment not in (1,2,3,4)
+        Raises
+        ------
+        `ValueError`
+            If ``moment`` is not in (1, 2, 3, 4).
 
-    'ValueError'
-        if signal and time don't have same length
+        `ValueError`
+            If ``signal`` and ``time`` don't have same length.
 
-    'ValueError
-        if len(signal) <= 4*radius if moment > 1
+        `ValueError`
+            If ``len(signal) <= 4 * radius`` and ``moment > 1``.
 
 
-    Notes
-    -----
-    The running rms divides by window, not (window-1)
+        Notes
+        -----
+        The running rms divides by ``window``, not ``(window - 1)``.
 
-    Example
-    -------
-    >>> from plasmapy.analysis.time_series.running_moments import running_moment
-    >>> running_moment([1, 2, 3, 2, 1], 1, 4, [1, 2, 3, 4, 5])
-    (array([3.]), [3])
+        Example
+        -------
+        >>> from plasmapy.analysis.time_series.running_moments import running_moment
+        >>> running_moment([1, 2, 3, 2, 1], 1, 4, [1, 2, 3, 4, 5])
+        Running_Moment(run_moment=array([3.]), time=[3])
     """
     if moment not in range(1, 5):
         raise ValueError("Only first four moments implemented")
 
-    if time and (len(signal) != len(time)):
+    if time is not None and (len(signal) != len(time)):
         raise ValueError("signal and time must have same length")
 
     if moment == 1:
@@ -122,7 +133,8 @@ def running_moment(signal, radius, moment=1, time=None):
             running_mean(tmp**4, radius) / running_mean(tmp**2, radius) ** 2
         )
 
-    if time:
+    if time is not None:
         time = time[2 * radius : -2 * radius]
 
-    return run_moment, time
+    run_moment_tuple = namedtuple("Running_Moment", ["run_moment", "time"])
+    return run_moment_tuple(run_moment=run_moment, time=time)
