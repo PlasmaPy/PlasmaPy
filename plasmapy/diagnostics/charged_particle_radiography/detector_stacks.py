b123f40fe3982ee7a0fd27151365f4cde4103c18
Peter Heuer <pvheuer@gmail.com>
2022-12-07 10:30:14 -0500

    Documentation update

--- plasmapy/diagnostics/charged_particle_radiography/detector_stacks.py
+++ plasmapy/diagnostics/charged_particle_radiography/detector_stacks.py
@@ -104,15 +104,12 @@ class Layer:
 class Stack:
     r"""
     An ordered list of
-    `~plasmapy.diagnostics.charged_particle_radiography.Layer` objects.
+    `~plasmapy.diagnostics.charged_particle_radiography.detector_stacks.Layer` objects.
 
     Parameters
     ----------
-    layers : list of
-    `~plasmapy.diagnostics.charged_particle_radiography.detector_stacks.Layer`
-    objects
-        A list of the
-        `~plasmapy.diagnostics.charged_particle_radiography.detector_stacks.Layer`
+    layers : list of `~plasmapy.diagnostics.charged_particle_radiography.detector_stacks.Layer` objects
+        A list of the `~plasmapy.diagnostics.charged_particle_radiography.detector_stacks.Layer`
         objects that make up the film stack.
     """
 
