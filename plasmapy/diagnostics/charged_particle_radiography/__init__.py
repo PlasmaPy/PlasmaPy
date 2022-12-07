c56e29c77ba0425300d7b5cc7e98aab62253b4c9
Peter Heuer <pvheuer@gmail.com>
2022-12-07 13:17:52 -0500

    Update plasmapy/diagnostics/charged_particle_radiography/__init__.py
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>

--- plasmapy/diagnostics/charged_particle_radiography/__init__.py
+++ plasmapy/diagnostics/charged_particle_radiography/__init__.py
@@ -3,3 +3,6 @@ The charged particle radiography subpackage contains functionality for
 analyzing charged particle radiographs and creating synthetic radiographs.
 """
 __all__ = ["detector_stacks", "synthetic_radiography"]
+
+import detector_stacks
+import synthetic_radiography
