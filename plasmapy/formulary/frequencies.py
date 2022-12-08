23892f014cb6f177302c6e32d76b365fba716a6b
Stephen Vincena <vincena@gmail.com>
2022-12-08 13:36:59 -0800

    Add Buchsbaum frequency calculation to frequencies.py

--- plasmapy/formulary/frequencies.py
+++ plasmapy/formulary/frequencies.py
@@ -4,8 +4,9 @@ __all__ = [
     "lower_hybrid_frequency",
     "plasma_frequency",
     "upper_hybrid_frequency",
+    "Buchsbaum_frequency"
 ]
-__aliases__ = ["oc_", "wc_", "wlh_", "wp_", "wuh_"]
+__aliases__ = ["oc_", "wc_", "wlh_", "wp_", "wuh_", "wbb_", "wii_", "wbi_"]
 __lite_funcs__ = ["plasma_frequency_lite"]
 
 import astropy.units as u
@@ -15,6 +16,7 @@ import numpy as np
 from astropy.constants.si import e, eps0
 from numba import njit
 
+import plasmapy.particles
 from plasmapy import particles
 from plasmapy.formulary import misc
 from plasmapy.particles import ParticleLike
@@ -516,3 +518,102 @@ def upper_hybrid_frequency(B: u.T, n_e: u.m**-3) -> u.rad / u.s:
 
 wuh_ = upper_hybrid_frequency
 """Alias to `~plasmapy.formulary.frequencies.upper_hybrid_frequency`."""
+
+
+@validate_quantities(
+    validations_on_return={
+        "units": [u.rad / u.s, u.Hz],
+        "equivalencies": [(u.cy / u.s, u.Hz)],
+    }
+)
+@angular_freq_to_hz
+def Buchsbaum_frequency(
+    B: u.T,
+    n1: u.m**-3,
+    n2: u.m**-3,
+    ion1: ParticleLike,
+    ion2: ParticleLike,
+    Z1=None,
+    Z2=None,
+) -> u.rad / u.s:
+    r"""
+    Calculate the Buchsbaum frequency for a two-ion-species plasma (defaults to units of radians per second.)
+
+    **Aliases:** `wbb_`, `wii_`, `wbi_`
+
+    Parameters
+    ----------
+    B : `~astropy.units.Quantity`
+        The magnetic field magnitude in units convertible to tesla.
+    n1 : `~astropy.units.Quantity`
+        Particle number density of ion species #1 in units convertible to m\ :sup:`-3`.
+    n2 : `~astropy.units.Quantity`
+        Particle number density of ion species #2 in units convertible to m\ :sup:`-3`.
+    ion1 : `~plasmapy.particles.particle_class.Particle`
+        Representation of ion species #1 (e.g., 'p' for protons, 'D+'
+        for deuterium, or 'He-4 +1' for singly ionized helium-4). If no
+        charge state information is provided, then species #1 is assumed
+        to be singly charged.
+    ion2 : `~plasmapy.particles.particle_class.Particle`
+        Representation of ion species #2 (e.g., 'p' for protons, 'D+'
+        for deuterium, or 'He-4 +1' for singly ionized helium-4). If no
+        charge state information is provided, then species #2 is assumed
+        to be singly charged.
+    Z1 : `float` or `~astropy.units.Quantity`, optional
+        The charge state for ion species #1. If not provided, it defaults to the charge number of ``particle1``.
+    Z2 : `float` or `~astropy.units.Quantity`, optional
+        The charge state for ion species #2. If not provided, it defaults to the charge number of ``particle2``.
+
+    Returns
+    -------
+    omega_BB : `~astropy.units.Quantity`
+        The Buchsbaum frequency of the plasma in units of radians per second.
+        Setting keyword ``to_hz=True`` will apply the factor of :math:`1/2π`
+        and yield a value in Hz.
+
+    Raises
+    ------
+    `TypeError`
+        If the magnetic field is not a `~astropy.units.Quantity` or
+        ``particle`` is not of an appropriate type.
+    `ValueError`
+        If the magnetic field contains invalid values or particle cannot
+        be used to identify a particle or isotope.
+
+    Warns
+    -----
+    : `~astropy.units.UnitsWarning`
+        If units are not provided, SI units are assumed.
+
+    Notes
+    -----
+    In a magnetized plasma, the presence of two ion species allows the
+    perpendicular component of the cold-plasma dielectric coefficient
+    :math:`\epsilon_{\perp}` to vanish at an angular frequency referred
+    to as the Buchsbaum frequency :cite:p:`buchsbaum:1960`, also called
+    the bi-ion frequency or ion-ion hybrid frequency
+    :cite:p:`vincena:2013`.  This frequency can be defined as:
+
+    .. math::
+        \omega_{BB} \equiv \sqrt{\frac{\omega_{p1}^{2}\omega_{c2}^{2}
+            + \omega_{p2}^{2}\omega_{c1}^{2}}{\omega_{p2}^{2}+\omega_{p2}^{2}}}
+
+
+    """
+
+    omega_c1 = gyrofrequency(B, ion1, signed=False, Z=Z1)
+    omega_c2 = gyrofrequency(B, ion2, signed=False, Z=Z2)
+    omega_p1 = plasma_frequency(n1, ion1, z_mean=Z1)
+    omega_p2 = plasma_frequency(n2, ion2, z_mean=Z2)
+
+    return np.sqrt((omega_p1**2 * omega_c2**2 + omega_p2**2 * omega_c1**2) / (omega_p1**2 + omega_p2**2))
+
+
+wbb_ = Buchsbaum_frequency
+"""Alias to `~plasmapy.formulary.frequencies.Buchsbaum_frequency`."""
+
+wii_ = Buchsbaum_frequency
+"""Alias to `~plasmapy.formulary.frequencies.Buchsbaum_frequency`."""
+
+wbi_ = Buchsbaum_frequency
+"""Alias to `~plasmapy.formulary.frequencies.Buchsbaum_frequency`."""
