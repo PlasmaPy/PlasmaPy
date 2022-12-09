d43d8af6a868126ad64de8b7d2a07b94a275f4c2
Stephen Vincena <vincena@gmail.com>
2022-12-09 13:47:39 -0800

    Syntax error fixes

--- plasmapy/formulary/tests/test_frequencies.py
+++ plasmapy/formulary/tests/test_frequencies.py
@@ -199,12 +199,12 @@ def test_Buchsbaum_frequency():
     r"""Test the Buchsbaum_frequency function in frequencies.py."""
 
     with pytest.raises(InvalidParticleError):
-        Buchsbaum_frequency(1.0 * u.T, n1=5e19 * u.m**-3, n2 = 5e19 * u.m**-3,
-        ion1="norwegian jarlsberg", ion2="proton")
+        Buchsbaum_frequency(1.0 * u.T, 5e19 * u.m**-3, 5e19 * u.m**-3,
+        "norwegian jarlsberg","proton")
 
     with pytest.raises(InvalidParticleError):
-        Buchsbaum_frequency(1.0 * u.T, n1=5e19 * u.m**-3, n2 = 5e19 * u.m**-3,
-        ion1="proton", ion2="venezuelan beaver cheese")
+        Buchsbaum_frequency(1.0 * u.T, 5e19 * u.m**-3, 5e19 * u.m**-3,
+        "proton", "venezuelan beaver cheese")
 
     assert np.isclose(Buchsbaum_frequency(0.1*u.T, 1e18*u.m**-3, 1e18*u.m**-3,
         "proton", "He-4 +1").value, 4805575.93140432)
