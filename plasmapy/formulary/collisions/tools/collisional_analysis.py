0264778aacac298303b7a4b15baee11dff8c10e3
ejohnson-96 <77989826+ejohnson-96@users.noreply.github.com>
2023-03-23 15:04:44 -0400

    update

--- plasmapy/formulary/collisions/tools/collisional_analysis.py
+++ plasmapy/formulary/collisions/tools/collisional_analysis.py
@@ -30,32 +30,34 @@ def collisional_thermalization(
     temperature_scale: float = -0.77,
 ):
     r"""
-    Contains the functionality to calculate collisional
+    Contains the functionality to calculate the collisional
     thermalization a plasma will undergo in transit, taken from
-    :cite:t:`maruca:2013`. Coulomb collisions, soft, small angle
-    deflections mediated by the electrostatic force, occur between
-    constituent particles of a plasma :cite:t:`baumjohann:1997`. This
-    causes the plasma to thermalize over distance, i.e. the temperature
-    of the plasma approaches thermal equilibrium. This function
-    allows the thermalization of a plasma to be modeled and predicts
-    the temperature ratio, for ions within the plasma, at a different
-    point in space.
+    :cite:t:`maruca:2013`. Coulomb collisions are soft, small angle
+    deflections mediated by the electrostatic force and occur between
+    constituent particles of a plasma :cite:t:`baumjohann:1997`.
+    These collisions cause the plasma to thermalize over distance,
+    i.e. the temperature of the plasma will approach thermal
+    equilibrium. This function allows this thermalization to be
+    modeled and predicts the temperature ratio, for different ion
+    species within the plasma, at a different point in space.
 
     Parameters
     ----------
     r_0 : `~astropy.units.Quantity`
         Starting position of the plasma in units convertible
-        to meters or astronomical units.
+        to astronomical units.
 
     r_n : `~astropy.units.Quantity`
-        Final position of the plasma in units of meters or
-        astronomical units.
+        Final position of the plasma in units convertible
+        to astronomical units.
 
     n_1 : `~astropy.units.Quantity`
-        The primary ion number density in units convertible to m\ :sup:`-3`.
+        The primary ion number density in units convertible
+        to m\ :sup:`-3`.
 
     n_2 : `~astropy.units.Quantity`
-        The secondary ion number density in units convertible to m\ :sup:`-3`.
+        The secondary ion number density in units convertible
+        to m\ :sup:`-3`.
 
     v_1 : `~astropy.units.Quantity`
         The primary ion speed in units convertible to ms\ :sup:`-1`.
@@ -77,24 +79,24 @@ def collisional_thermalization(
         equation via the Euler method. Must be an int.
 
     density_scale : `float`
-        The value used for scaling parameter of the primary ion
-        density, default value is -1.8 and is taken from
+        The value used as the scaling parameter for the primary ion
+        density, the default value is -1.8 and is taken from
         :cite:t:`hellinger:2011`.
 
     velocity_scale : `float`
-        The value used for scaling parameter of the primary ion
-        velocity, default value is -0.2 and is taken from
+        The value used as the scaling parameter for the primary ion
+        velocity, the default value is -0.2 and is taken from
         :cite:t:`hellinger:2011`.
 
     temperature_scale : `float`
-        The value used for scaling parameter of the primary ion
-        temperature, default value is -0.77 and is taken from
+        The value used as the scaling parameter for the primary ion
+        temperature, the default value is -0.77 and is taken from
         :cite:t:`hellinger:2011`.
 
     Returns
     -------
     theta : `float`
-        The dimensionless alpha-proton temperature ratio prediction
+        The dimensionless ion temperature ratio prediction
         for the distance provided.
 
     Raises
@@ -109,34 +111,60 @@ def collisional_thermalization(
 
     Notes
     -----
-    Equation for collisional thermalization from :cite:t:`maruca:2013`,
-    is given below:
-
-     .. math::
-
-        \frac{d \theta_{ba}}{dr} = k \frac{\left( \mu_{a} \mu_{b} Z_{a}
-        Z_{b} \right )^{1/2} \left( 1 - \theta_{ba} \right ) \left(1 +
-        \eta_{ba}\theta_{ba} \right )}{\left( \frac{\mu_{a}}{\mu_{b}} +
-        \theta_{ba} \right )} \lambda_{ba}
-
-    with
+    The processes by which Coulomb collisions bring ion temperatures
+    into local thermal equilibrium (LTE) has received considerable
+    attention, :cite:t:`verscharen:2019`. The relative temperature
+    between constituent plasma ion species is given as:
 
     .. math::
 
-         \lambda_{ba} = 9 + \ln \left|  \left( \frac{\theta_{ba} +
-         \frac{\mu_{b}}{\mu_a}}{Z_{a}Z_{b}(\mu_{a} + \mu_{b})} \right )
-         \left( \frac{n_{b}Z_{b}^{2}}{n_{a}Z_{a}^{2}} + \theta_{ba}
+        \theta_{21} = \frac{T_{2}}{T_{1}} \, ,
+
+    where :math:`T_{1}` and :math:`T_{2}` are the scalar temperatures
+    for the primary ion of interest and the secondary ion respectively.
+    The scalar temperature defined as:
+
+    .. math::
+
+        T_{\rm i} = \frac{2T_{{\rm i}, \perp} + T_{{\rm i}, \parallel}}{3} \, ,
+
+    where :math:`T_{{\rm i}, \perp}` and :math:`T_{{\rm i}, \parallel}`
+    are the temperature of the :math:`{\rm i}`-particles along the
+    axes perpendicular and parallel to the ambient magnetic field.
+
+    In order to determine how extensively an individual parcel of
+    plasma has been processed by Coulomb collisions, :cite:t:`maruca:2013`
+    introduced an approached called collisional analysis. Which seeks
+    to quantify how collisions affect the plasma’s departures from
+    LTE, the equation for collisional thermalization
+    from :cite:t:`maruca:2013` is given below:
+
+     .. math::
+
+        \frac{d \theta_{21}}{dr} = A \left ( \frac{n_1}{v_1 T_1^{3/2}} \right ) \frac{\left( \mu_{1} \mu_{2}
+        \right )^{1/2} Z_{1} Z_{2} \left( 1 - \theta_{21} \right ) \left(1 +
+        \eta_{21}\theta_{21} \right )}{\left( \frac{\mu_{2}}{\mu_{1}} +
+        \theta_{21} \right )^{3/2}} \lambda_{21}
+
+    and
+
+    .. math::
+
+         \lambda_{21} = 9 + \ln \left| B \left ( \frac{T^{3}_{1}}{n_{1}} \right )^{1/2} \left( \frac{\theta_{21} +
+         \frac{\mu_{2}}{\mu_1}}{Z_{1}Z_{2}(\mu_{1} + \mu_{2})} \right )
+         \left( \frac{n_{2}Z_{2}^{2}}{n_{1}Z_{1}^{2}} + \theta_{21}
          \right)^{1/2}\right |
 
-    With :math:`\eta = \frac{n_{2}}{n_{1}}` and :math:`\theta = \frac{T_{2}}{T_{1}}`
+    With :math:`\eta = \frac{n_{2}}{n_{1}}`,
+    :math:`\theta = \frac{T_{2}}{T_{1}}`, :math:`A = 2.60 \times 10^{7} \, {\rm cm}^{3} \, {\rm km} \, {\rm K}^{3/2} \, {\rm s}^{-1} \, {\rm au}^{-1}`,
+    and :math:`B = 1 \, {\rm cm}^{-3/2}{\rm K}^{-3/2}`.
 
-    Applicable is primarily for the solar wind.
-
-
-    It is assumed that there is no relative drift between the ion
-    species and that it is a mixed ion collision. Thermalization is
-    from Coulomb collisions, which assumes “soft”, small-angle
-    deflections mediated by the electrostatic force.
+    The thermalization is from Coulomb collisions, which assumes
+    “soft”, small-angle deflections mediated by the electrostatic
+    force :cite:t:`baumjohann:1997`. It is assumed that there is no
+    relative drift between the ion species and that it is a mixed ion
+    collision, the Coulomb logarithm for a mixed ion collision is
+    given by :cite:t:`nrlformulary:2019`.
 
     The density, velocity and temperature of the primary ion can be
     radially scaled, as seen below. The values for the scaling can be
@@ -145,8 +173,10 @@ def collisional_thermalization(
 
     .. math::
 
-        n(r) \propto r^{-1.8}\ , \hspace{1cm} v_{r}(r) \propto r^{-0.2}\ , \hspace{0.5cm} {\rm and} \hspace{0.5cm} T(r) \propto r^{-0.74}
+        n(r) \propto r^{-1.8}\ , \hspace{1cm} v_{r}(r) \propto r^{-0.2}\ ,
+        \hspace{0.5cm} {\rm and} \hspace{0.5cm} T(r) \propto r^{-0.74}
 
+    Application is primarily for the solar wind.
 
     Examples
     --------
@@ -204,12 +234,14 @@ def collisional_thermalization(
         d_r = (r_0 - r_n) / (1.0 * n_step)
         d_r = d_r.value
 
-        # Define constant
-        k = 0.174
+        # Define constants
+        A = 2.6 * 10**7 * (u.cm**3 * u.km * u.K**1.5) / (u.s * u.au)
+        B = 1 / (u.cm * u.K) ** 1.5
 
         # Define Coulomb log for mixed ion collisions
         def lambda_ba(
             theta,
+            T_1,
             n_1,
             n_2,
             z_1,
@@ -217,11 +249,10 @@ def collisional_thermalization(
             mu_1,
             mu_2,
         ):
-            return 9 + np.log(
-                np.sqrt(n_2 * z_2**2 / theta * n_1 * z_1**2 + 1)
-                * ((z_1 * z_2 * (mu_1 + mu_2)) / (theta + (mu_2 / mu_1)))
-                * ((theta + mu_2 / mu_1) / (z_1 * z_2 * (mu_1 + mu_2)))
-            )
+            a = np.sqrt(T_1**3 / n_1)
+            b = (theta + mu_2 / mu_1) / (z_1 * z_2 * (mu_1 + mu_2))
+            c = np.sqrt(n_2 * z_2**2 / n_1 * z_1**2 + theta)
+            return 9 + np.log(B * a * b * c)
 
         # Loop.
         for i in range(n_step):
@@ -230,24 +261,28 @@ def collisional_thermalization(
             eta = n_2 / n_1
             theta = T_2 / T_1
 
-            l_ba = lambda_ba(theta, n_1, n_2, z_1, z_2, mu_1, mu_2)
+            l_ba = lambda_ba(theta, T_1, n_1, n_2, z_1, z_2, mu_1, mu_2)
 
-            n_1 = n_1 * (r / r_n) ** density
-            v_1 = v_1 * (r / r_n) ** velocity
-            T_1 = T_1 * (r / r_n) ** temperature
+            n_1 = n_1 * (r / r_n.value) ** density
+            v_1 = v_1 * (r / r_n.value) ** velocity
+            T_1 = T_1 * (r / r_n.value) ** temperature
 
-            d_theta = (
-                d_r
-                * l_ba
-                * k
-                * (np.sqrt(mu_1 * mu_2 * z_1 * z_2) * (1 - theta) * (1 + eta * theta))
-                / (np.sqrt(mu_1 / mu_2 + theta) ** 3)
+            alpha = n_1 / (v_1 * (T_1**1.5))
+            beta = (
+                (np.sqrt(mu_1 * mu_2) * z_1 * z_2 * (1 - theta) * (1 + eta * theta))
+                / (np.sqrt((mu_2 / mu_1) + theta) ** 3)
+                * u.au
+                / u.km
             )
+
+            d_theta = d_r * u.m * alpha * l_ba * A * beta
+
+            # print("dr, A, l_ba", d_r, A, l_ba, alpha, beta)
 
             print(theta, d_theta)
             theta = theta + d_theta
 
-        return theta
+        return theta.value
 
     vars = [r_0, r_n, n_1, n_2, v_1, T_1, T_2]
 
@@ -278,8 +313,7 @@ def collisional_thermalization(
     else:
         if all(len(vars[0]) == len(z) for z in vars[1:]):
             res = []
-            L = 1
-            for i in range(L):
+            for i in range(len(vars[0])):
                 res.append(
                     df_eq(
                         r_0[i],
@@ -304,35 +338,3 @@ def collisional_thermalization(
                 "arguments should be of equal length: 'r_0', 'r_n', "
                 "'n_1', 'n_2', 'v_1', 'T_1' and 'T_2'."
             )
-
-
-import pandas as pd
-
-obj = pd.read_pickle(
-    r"/Users/elliotjohnson/GitHub/Collsionality/data/save/EA/solar_data.pkl"
-)
-
-key_list = list(obj.keys())
-vars = {}
-
-for key in key_list:
-    vars[key] = list(obj[key].keys())
-
-n_a = obj["proton"]["np1"] * u.cm**-3
-n_b = obj["alpha"]["na"] * u.cm**-3
-v_a = obj["proton"]["v_mag"] * u.m / u.s
-T_a = obj["proton"]["Tperp1"] * u.K
-T_b = obj["alpha"]["Trat"] * u.K
-ions = [Particle("p+"), Particle("He-4++")]
-
-l = len(obj["proton"]["np1"])
-r_0 = [0.1] * l * u.au
-r_n = [1.0] * l * u.au
-n_step = 1
-
-x = collisional_thermalization(r_0, r_n, n_a, n_b, v_a, T_a, T_b, ions, n_step)
-
-import matplotlib.pyplot as plt
-
-plt.hist(x, bins=50)
-plt.show()
--- plasmapy/formulary/collisions/tools/tests/test_collisional_analysis.py
+++ plasmapy/formulary/collisions/tools/tests/test_collisional_analysis.py
@@ -1,4 +1,4 @@
-"""Test functionality of `plasmapy.formulary.collisions.collisional_analysis`."""
+"""Test functionality of `plasmapy.formulary.collisions.tools.collisional_analysis`."""
 import pytest
 
 from astropy import units as u
@@ -14,20 +14,40 @@ c_si_unitless = c.value
 
 class Testcollisional_thermalizastion:
     _kwargs_single_valued = {
-        "n_a": [1, 2, 3] * u.cm**-3,
-        "n_b": [0.5, 0.6, 0.4] * u.cm**-3,
-        "v_a": [400, 500, 600] * u.m / u.s,
-        "T_a": [50, 55, 60] * 10**3 * u.K,
-        "T_b": [30, 40, 60] * 10**3 * u.K,
-        "ions": [Particle("p+"), Particle("He-4++")],
         "r_0": [0.1, 0.1, 0.1] * u.au,
         "r_n": [1.0, 1.0, 1.0] * u.au,
+        "n_1": [1, 2, 3] * u.cm**-3,
+        "n_2": [0.5, 0.6, 0.4] * u.cm**-3,
+        "v_1": [400, 500, 600] * u.m / u.s,
+        "T_1": [50, 55, 60] * 10**3 * u.K,
+        "T_2": [30, 40, 60] * 10**3 * u.K,
+        "ions": [Particle("p+"), Particle("He-4++")],
+        "n_step": 1000,
+        "density_scale": -1.8,
+        "velocity_scale": -0.2,
+        "temperature_scale": -0.77,
     }
 
     @pytest.mark.parametrize(
         "kwargs, _error",
         [
-            ({**_kwargs_single_valued, "B": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "n_1": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "n_1": 2 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "n_2": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "n_2": 2 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "v_1": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "v_1": 2 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "T_1": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "T_1": 2 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "T_2": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "T_2": 2 * u.s}, u.UnitTypeError),
+            ({**_kwargs_single_valued, "ions": [Particle("p+"), "He"]}, TypeError),
+            ({**_kwargs_single_valued, "ions": [Particle("p+")]}, ValueError),
+            ({**_kwargs_single_valued, "n_step": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "n_step": 4.5}, TypeError),
+            ({**_kwargs_single_valued, "density_scale": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "velocity_scale": "wrong type"}, TypeError),
+            ({**_kwargs_single_valued, "temperature_scale": "wrong type"}, TypeError),
         ],
     )
     def test_raises(self, kwargs, _error):
