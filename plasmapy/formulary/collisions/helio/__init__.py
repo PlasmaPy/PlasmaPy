411cf1b62e6479382213b322a4e7abfb6154b005
ejohnson-96 <77989826+ejohnson-96@users.noreply.github.com>
2023-03-30 10:00:36 -0400

    update

--- plasmapy/formulary/collisions/helio/__init__.py
+++ plasmapy/formulary/collisions/helio/__init__.py
@@ -2,18 +2,8 @@
 The `~plasmapy.formulary.collisions.helio` subpackage contains
 commonly used heliosphere plasma science, primarily the solar wind.
 """
-__all__ = []
+__all__ = ["thermalization_ratio"]
 
-import inspect
-
-from plasmapy.formulary.collisions.helio.collisional_analysis import *
-
-# auto populate __all__
-for obj_name in list(globals()):
-    if not (
-        obj_name.startswith("__") or obj_name.endswith("__")
-    ) and not inspect.ismodule(globals()[obj_name]):
-        __all__.append(obj_name)
-__all__.sort()
-
-del inspect
+from plasmapy.formulary.collisions.helio.collisional_analysis import (
+    thermalization_ratio,
+)
