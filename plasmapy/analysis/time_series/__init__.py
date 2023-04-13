cf894eb5a056cabb6f12edfd515dd6fdf668c041
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2023-04-13 17:33:23 +0200

    Excess statistics for time series (#1984)
    * Add running_mean function with according tests
    
    * Implement running_moment function
    
    * Implement running_moment function
    
    Co-authored-by:  Audun Theodorsen  <audun.theodorsen@uit.no>
    
    * Fix spelling errors
    
    * Add changelog entry for upcoming pull request
    
    * fix spelling error
    
    * Implement namurphy's suggestions from review in PR 1803
    
    * Implement check if radius is int in running_mean.
    
    * Add api_statis file for time_series.running_moments
    
    * Fix test for TypeError in running_mean
    
    * Make docstring of running_moment function more readable
    
    * Replace namedtuple with tuple in docstring
    
    * Add plasmapy.analysis.time_series.rst
    
    * Implement namurphy's suggestions from second review of PR 1803
    
    * add initial implementation of excess_statistics
    
    * add tests for excess_stat
    
    * rename variable in meaningful way
    
    Co-authored-by: name audunth
    
    * rename variable in meaningful way
    
    Co-authored-by: name <audun.theodorsen@uit.no>
    
    * raise value and type errors
    
    * add documentation to excess_stat
    
    * add excess_statistics.rst file
    
    * run black on files
    
    * add 1984.feature.rst file
    
    * fix length of Title underline
    
    * Exchange function with class
    
    * Remove commented out code
    
    * make thresholds iterable
    
    * add test for threshold of type float
    
    * add documentation for all attributes
    
    * add blank lines after docstring
    
    * fix docstrings of new attributes
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * replace try catch with isinstance check
    
    * replace 'tmp' with meaningful name
    
    * provide better docstring for hist method
    
    * remove outdated info from docstring
    
    * Update changelog/1984.feature.rst
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/tests/test_excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/excess_statistics.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * run black on excess_statistics.py
    
    * increase readability of docstring
    
    * add test for time_step = 0.5
    
    * parametrize exceptions in test_ExcessStatistics_exception function
    
    ---------
    
    Co-authored-by: Audun Theodorsen  <audun.theodorsen@uit.no>
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>

--- plasmapy/analysis/time_series/__init__.py
+++ plasmapy/analysis/time_series/__init__.py
@@ -1,3 +1,4 @@
 """Subpackage containing routines for analyzing time series."""
 
+from plasmapy.analysis.time_series.excess_statistics import ExcessStatistics
 from plasmapy.analysis.time_series.running_moments import running_mean, running_moment
