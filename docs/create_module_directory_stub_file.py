6d9bac1a6f9d0de6a9b99527c7e9af1da013c63d
Nick Murphy <namurphy@cfa.harvard.edu>
2023-05-04 17:20:13 -0400

    Include directories/subpackages in script

--- docs/create_module_directory_stub_file.py
+++ docs/create_module_directory_stub_file.py
@@ -1,10 +1,22 @@
 import glob
+import os
 
 from pathlib import Path
 
 
 def main():
+    directories = [str(d[0]).removeprefix("../") for d in os.walk("../plasmapy")]
+
+    directories = [
+        d.replace("/", ".")
+        for d in directories
+        if not d.endswith(("tests")) and "." not in d and "/_" not in d
+    ]
+
     files = glob.glob("plasmapy/**/[a-zA-Z]*.py", root_dir="..")
+
+    modules = sorted([*files, *directories])
+
     filepath = Path("./modules.rst")
 
     with filepath.open(mode="w") as f:
@@ -12,7 +24,7 @@ def main():
 
         f.writelines(preamble)
 
-        for file in files:
+        for file in modules:
             if "/_" in file:
                 continue
             module = file.strip().removesuffix(".py").replace("/", ".")
