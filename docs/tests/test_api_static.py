2957e7358ab052b2abd15de5e09837cf1201e644
Nick Murphy <namurphy@cfa.harvard.edu>
2023-05-05 19:27:23 -0400

    Try some test api static stuff

--- docs/tests/test_api_static.py
+++ docs/tests/test_api_static.py
@@ -1,60 +1,49 @@
-"""Tests of documentation infrastructure."""
-
-import glob
 import os
 
-from pathlib import Path
-from typing import Optional
+
+def _is_valid_dir(path: str) -> bool:
+    """
+    Check if a directory is valid based on if it contains .py files and
+    if it does not start with '_'.
+    """
+    if any(file.startswith("test_") for file in os.listdir(path)):
+        return False
+    py_files = [
+        file
+        for file in os.listdir(path)
+        if file.endswith(".py")
+        and not file.startswith("_")
+        and file not in ("conftest.py", "test_conftest.py")
+    ]
+    return bool(py_files)
 
 
-def _change_directories_to_namespaces(path: str) -> list[str]:
-    directories = [str(d[0]).removeprefix("../") for d in os.walk(path)]
-
+def _get_py_files(root):
+    """Return a list of .py files in a directory"""
     return [
-        d.replace("/", ".").strip()
-        for d in directories
-        if not d.endswith(("tests")) and "." not in d and "/_" not in d
+        file
+        for file in os.listdir(root)
+        if file.endswith(".py")
+        and not file.startswith("_")
+        and file not in ("conftest.py", "test_conftest.py")
     ]
 
 
-def _change_python_files_to_namespaces(path: str) -> list[str]:
-    modules = list(glob.glob(f"{path}/**/[a-zA-Z]*.py"))
-
-    return [
-        m.removeprefix("../").removesuffix(".py").replace("/", ".")
-        for m in modules
-        if "tests/test_" not in m and "/_" not in m
-    ]
-
-
-def _get_modules(path: str) -> list[str]:
-
-    subpackages = [
-        *_change_directories_to_namespaces("../../plasmapy/"),
-        *_change_directories_to_namespaces("plasmapy_sphinx/"),
-    ]
-
-    modules = [
-        *_change_python_files_to_namespaces("../../plasmapy/"),
-        *_change_python_files_to_namespaces("plasmapy_sphinx/")
-    ]
-
-    return sorted(subpackages + modules)
-
-
-def _get_rst_files(
-        docpath: str = ".",
-        exclude: Optional[set[str]] = None,
-) -> list[Path]:
-
-    globs = glob.glob(f"{docpath}/**/[a-z][A-Z]*.rst")
-    return
-
-
-
-def _get_automodapi_directives(docpath: str = ".") -> list[str]:
-
-
-
-def test_api_static():
-    ...
+def _get_py_namespaces(path):
+    namespaces = []
+    for root, dirs, files in os.walk(path):
+        # Exclude directories that begin with an underscore
+        dirs[:] = [d for d in dirs if not d.startswith("_")]
+        if _is_valid_dir(root):
+            rel_path = os.path.relpath(root, path)
+            namespace = (
+                f"{os.path.basename(path)}.{rel_path.replace('/', '.')}"
+                if rel_path != "."
+                else os.path.basename(path)
+            )
+            namespaces.append(namespace)
+            for file in _get_py_files(root):
+                rel_path = os.path.relpath(os.path.join(root, file), path)
+                namespace = f"{os.path.basename(path)}.{os.path.splitext(rel_path)[0].replace('/', '.')}"
+                namespaces.append(namespace)
+    return sorted(namespaces)
