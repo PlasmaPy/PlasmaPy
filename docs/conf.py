9fc63840d9d72164d1a428ba07e266fb02e8683c
Nick Murphy <namurphy@cfa.harvard.edu>
2023-05-24 00:15:30 -0400

    Call cff_to_rst from docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,12 +6,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
+import cff_to_rst
 import os
 import sys
 
 from datetime import datetime
 from pkg_resources import parse_version
 from sphinx.application import Sphinx
+
+# Generate author list from CITATION.cff
+cff_to_rst.main()
 
 sys.path.insert(0, os.path.abspath(".."))  # noqa: PTH100
 sys.path.insert(0, os.path.abspath("."))  # noqa: PTH100
