4ac2c1f6297ee1d65df7fa5850bd08f6eef254be
Nick Murphy <namurphy@cfa.harvard.edu>
2023-05-24 19:10:12 -0400

    Remove PR comment and only include error message

--- .github/scripts/check_citation.py
+++ .github/scripts/check_citation.py
@@ -1,11 +1,11 @@
-"""Check that the author(s) of a PR are included in ``CITATION.cff``."""
+"""Check that the authors of a PR are included in ``CITATION.cff``."""
 
 import os
 import pathlib
 import requests
 import sys
 
-from typing import NoReturn, Optional
+from typing import Optional
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 PR_NUMBER = os.getenv("PR_NUMBER")
@@ -37,67 +37,39 @@ def check_citation_file(authors: set[str]) -> tuple[bool, Optional[str]]:
     return True, None
 
 
-def comment_exists(message: str) -> bool:
-    """Check if a comment already exists in the pull request."""
-    url = f"https://api.github.com/repos/{REPO}/issues/{PR_NUMBER}/comments"
-    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
-    response = requests.get(url, headers=headers, timeout=15)
-    response.raise_for_status()
-
-    return any(message in comment["body"] for comment in response.json())
-
-
-def post_comment(message: str) -> NoReturn:
-    """Post a comment on a pull request."""
-    marker = "To ensure that you get credit for your contribution to PlasmaPy"
-
-    if not comment_exists(marker):
-        url = f"https://api.github.com/repos/{REPO}/issues/{PR_NUMBER}/comments"
-        headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
-        response = requests.post(
-            url, json={"body": message}, headers=headers, timeout=15
-        )
-        response.raise_for_status()
-
-
 def main():
     """Check that all authors are included in CITATION.cff."""
     authors = get_pr_authors()
-    check_passed, failed_author = check_citation_file(authors)
+    check_passed, missing_github_username = check_citation_file(authors)
     if not check_passed:
         branch_name = os.getenv("GITHUB_HEAD_REF")
 
-        comment = (
-            f"To ensure that you get credit for your contribution to "
-            f"PlasmaPy, please add yourself as an author to:"
-            f"[CITATION.cff](https://github.com/{REPO}/edit/{branch_name}/CITATION.cff)."
-            f"The entry should be of the form:\n\n```"
-            f"- given-names: <add given names>\n"
-            f"  family-names: <family names>\n"
-            f"  affiliation: <affiliation>\n"
-            f"  orcid: https://orcid.org/<ORCiD ID number>\n"
-            f"  alias: <GitHub username>\n```\n"
-            f"All fields except `alias` are optional. "
-            f"Optionally, [sign up for ORCiD](https://orcid.org/register)."
-        )
+        error_message = f"""
+To ensure that you get credit for your contribution to PlasmaPy, please
+add {missing_github_username} as an author to CITATION.cff.
 
-        post_comment(comment)
+The entry should be of the form:
 
-        message = (
-            f"\nTo ensure that you get credit for your contribution to "
-            "PlasmaPy, please add yourself as an author to CITATION.cff "
-            f"at: https://github.com/{REPO}/edit/{branch_name}/CITATION.cff\n\n"
-            "The entry should be of the form:\n\n"
-            "- given-names: <given names>\n"
-            "  family-names: <family names>\n"
-            "  affiliation: <affiliation>\n"
-            "  orcid: https://orcid.org/<ORCiD number>\n"
-            "  alias: <GitHub username>\n\n"
-            "All fields except `alias` are optional. "
-            "Optionally, sign up for ORCiD: https://orcid.org/register\n"
-        )
+- given-names: <given names>
+  family-names: <family names>
+  affiliation: <affiliation>
+  orcid: https://orcid.org/<ORCiD number>
+  alias: {missing_github_username}
 
-        print(message)
+This file can be edited directly on GitHub at:
+
+https://github.com/{REPO}/edit/{branch_name}/CITATION.cff
+
+All fields are optional except "alias" (GitHub username).
+
+We encourage you to sign up for an ORCID iD: a unique, persistent
+identifier used by researchers, authors, and open source
+contributors. You can sign up at: https://orcid.org/register
+
+Thank you for contributing!
+"""
+
+        print(error_message)
 
         sys.exit(1)
 
