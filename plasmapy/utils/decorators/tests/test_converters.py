59f2734d5b0e8d9d53ae56383f6947eee59a27e7
Nick Murphy <namurphy@cfa.harvard.edu>
2023-05-29 17:23:10 -0400

    Add tests to to_hz

--- plasmapy/utils/decorators/tests/test_converters.py
+++ plasmapy/utils/decorators/tests/test_converters.py
@@ -4,20 +4,41 @@ import numpy as np
 from plasmapy.utils.decorators.converter import angular_freq_to_hz
 
 
-def test_conversion():
+def test_to_hz():
     @angular_freq_to_hz
-    def test_function():
+    def func():
         return 2 * np.pi * u.rad / u.s
 
-    assert test_function().unit == (
+    assert func().unit == (
         u.rad / u.s
-    ), f"Unit expected is {(u.rad / u.s)} instead of {test_function().unit}"
+    ), f"Unit expected is {(u.rad / u.s)} instead of {func().unit}"
     assert (
-        test_function().value == 2 * np.pi
-    ), f"Value expected is {2 * np.pi} instead of {test_function().value}"
+        func().value == 2 * np.pi
+    ), f"Value expected is {2 * np.pi} instead of {func().value}"
     assert (
-        test_function(to_hz=True).unit == u.Hz
-    ), f"Unit expected is {u.Hz} instead of {test_function(to_hz=True).unit}"
+        func(to_hz=True).unit == u.Hz
+    ), f"Unit expected is {u.Hz} instead of {func(to_hz=True).unit}"
     assert (
-        test_function(to_hz=True).value == 1
-    ), f"Value expected is 1 instead of {test_function(to_hz=True).value}"
+        func(to_hz=True).value == 1
+    ), f"Value expected is 1 instead of {func(to_hz=True).value}"
+
+
+def test_to_hz_complicated_signature():
+    @angular_freq_to_hz
+    def func2(a, /, b, *args, c, d=2, **kwargs):  # noqa: ARG001
+        return 2 * np.pi * u.rad / u.s
+
+    result_rad_per_s = func2(1, 2, 3, 4, c=5, d=6, e=7)
+    result_hz = func2(1, 2, 3, 4, c=5, d=6, e=7, to_hz=True)
+
+    assert result_rad_per_s.unit == (
+        u.rad / u.s
+    ), f"Unit expected is {(u.rad / u.s)} instead of {result_rad_per_s.unit}"
+    assert (
+        result_rad_per_s.value == 2 * np.pi
+    ), f"Value expected is {2 * np.pi} instead of {result_rad_per_s.value}"
+
+    assert (
+        result_hz.unit == u.Hz
+    ), f"Unit expected is {u.Hz} instead of {result_hz.unit}"
+    assert result_hz.value == 1, f"Value expected is 1 instead of {result_hz.value}"
