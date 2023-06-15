620cf4bb9866d26a42ad7da312bd1f0b79eb6b85
mohawk811 <124110016+mohawk811@users.noreply.github.com>
2023-06-15 13:51:28 -0400

    Update plasmapy/plasma/tests/test_equilibria1d.py
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>

--- plasmapy/plasma/tests/test_equilibria1d.py
+++ plasmapy/plasma/tests/test_equilibria1d.py
@@ -37,7 +37,7 @@ def test_currentDensity():
     hs = HarrisSheet(B0, delta, P0)
     y = [-2, 0, 2] * u.m
     J = hs.current_density(y)
-    correct_J = [56250.656, 796178.343, 56250.656] * u.I
+    correct_J = [56250.656, 796178.343, 56250.656] * u.A / u.m**2
     assert u.allclose(J, correct_J, atol=1e-9 * u.Pa)
 
 
