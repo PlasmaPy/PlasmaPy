b9f85604df9a07892e731ce3f825f79531ec154f
Leo Murphy <85201894+LeoMurphyWM24@users.noreply.github.com>
2023-06-30 15:37:43 -0400

    Correct typo in langmuir.py
    Add "return None" statement to get_unique_bias

--- plasmapy/diagnostics/langmuir.py
+++ plasmapy/diagnostics/langmuir.py
@@ -146,6 +146,7 @@ class Characteristic:
 
         self.bias = bias_unique
         self.current = current_unique
+        return None
 
     def _check_validity(self):
         r"""Check the unit and value validity of the characteristic."""
