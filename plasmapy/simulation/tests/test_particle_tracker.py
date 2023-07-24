99e1170e5d0fb1314f093a7eb2d9f69867a95655
JaydenR2305 <109694185+JaydenR2305@users.noreply.github.com>
2023-07-24 14:41:47 -0400

    Parametrize `test_gyroradius`

--- plasmapy/simulation/tests/test_particle_tracker.py
+++ plasmapy/simulation/tests/test_particle_tracker.py
@@ -3,6 +3,7 @@ Tests for particle_tracker.py
 """
 import astropy.units as u
 import numpy as np
+import pytest
 
 from plasmapy.formulary.lengths import gyroradius
 from plasmapy.particles import CustomParticle
@@ -13,32 +14,36 @@ from plasmapy.simulation.particle_tracker import (
     TimeElapsedStopCondition,
 )
 
+rng = np.random.default_rng()
+
 
 class TestParticleTracker:
-    def test_gyroradius(self):
+    L = 1e6 * u.km
+    num = 2
+    grid = CartesianGrid(-L, L, num=num)
+    grid_shape = (num,) * 3
+
+    B_strength = 1 * u.T
+    Bz = np.full(grid_shape, B_strength) * u.T
+    grid.add_quantities(B_z=Bz)
+
+    point_particle = CustomParticle(1 * u.kg, 1 * u.C)
+
+    @pytest.mark.parametrize("v_x", [rng.integers(0, 100, 10) * u.m / u.s])
+    def test_gyroradius(self, v_x):
         """Test to ensure particles maintain their gyroradius over time"""
-        L = 1 * u.km
-        num = 2
-
-        grid = CartesianGrid(-L, L, num=num)
-
-        grid_shape = (num,) * 3
-        B_strength = 1 * u.T
-        Bz = np.full(grid_shape, B_strength) * u.T
-
-        grid.add_quantities(B_z=Bz)
-
         simulation = ParticleTracker(
-            grid,
-            req_quantities=["E_x", "E_y", "E_z", "B_x", "B_y", "B_z"],
-            verbose=False,
+            self.grid, req_quantities=["E_x", "E_y", "E_z", "B_x", "B_y", "B_z"]
         )
 
-        x = np.array([[1, 0, 0], [2, 0, 0], [3, 0, 0]]) * u.m
-        v = np.array([[0, -1, 0], [0, -2, 0], [0, -3, 0]]) * u.m / u.s
-        point_particle = CustomParticle(1 * u.kg, 1 * u.C)
+        v = np.array([[v_x_element.value, 0, 0] for v_x_element in v_x]) * u.m / u.s
 
-        simulation.load_particles(x, v, point_particle)
+        # Set the initial position to the gyroradius
+        # This means the particle will orbit the origin
+        R_L = gyroradius(self.B_strength, self.point_particle, Vperp=v_x)
+        x = np.array([[0, R_L_element.value, 0] for R_L_element in R_L]) * u.m
+
+        simulation.load_particles(x, v, self.point_particle)
 
         stop_condition = TimeElapsedStopCondition(10 * u.s)
         save_routine = MemoryIntervalSaveRoutine(0.1 * u.s)
@@ -48,6 +53,4 @@ class TestParticleTracker:
         positions = np.asarray(save_routine.x_all) * u.m
         distances = np.linalg.norm(positions, axis=-1)
 
-        gyroradii = gyroradius(B_strength, point_particle, Vperp=v[:, 1])
-
-        assert np.isclose(distances, gyroradii, rtol=1e-3).all()
+        assert np.isclose(distances, R_L, rtol=1e-3).all()
