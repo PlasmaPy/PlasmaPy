59118fbbf7f30be1490d268f0b974b457bebda50
JaydenR2305 <109694185+JaydenR2305@users.noreply.github.com>
2023-07-25 09:22:07 -0400

    Add test for kinetic energies in `ParticleTracker`

--- plasmapy/simulation/tests/test_particle_tracker.py
+++ plasmapy/simulation/tests/test_particle_tracker.py
@@ -32,16 +32,16 @@ class TestParticleTracker:
     @pytest.mark.parametrize("v_x", [rng.integers(0, 100, 10) * u.m / u.s])
     def test_gyroradius(self, v_x):
         """Test to ensure particles maintain their gyroradius over time"""
-        simulation = ParticleTracker(
-            self.grid, req_quantities=["E_x", "E_y", "E_z", "B_x", "B_y", "B_z"]
-        )
-
         v = np.array([[v_x_element.value, 0, 0] for v_x_element in v_x]) * u.m / u.s
 
         # Set the initial position to the gyroradius
         # This means the particle will orbit the origin
         R_L = gyroradius(self.B_strength, self.point_particle, Vperp=v_x)
         x = np.array([[0, R_L_element.value, 0] for R_L_element in R_L]) * u.m
+
+        simulation = ParticleTracker(
+            self.grid, req_quantities=["E_x", "E_y", "E_z", "B_x", "B_y", "B_z"]
+        )
 
         simulation.load_particles(x, v, self.point_particle)
 
@@ -54,3 +54,31 @@ class TestParticleTracker:
         distances = np.linalg.norm(positions, axis=-1)
 
         assert np.isclose(distances, R_L, rtol=1e-3).all()
+
+    @pytest.mark.parametrize("v_x", [rng.integers(0, 100, 10) * u.m / u.s])
+    def test_kinetic_energy(self, v_x):
+        """Test to ensure particles maintain their gyroradius over time"""
+        v = np.array([[v_x_element.value, 0, 0] for v_x_element in v_x]) * u.m / u.s
+        initial_kinetic_energies = 0.5 * self.point_particle.mass * v_x**2
+
+        # Set the initial position to the gyroradius
+        # This means the particle will orbit the origin
+        R_L = gyroradius(self.B_strength, self.point_particle, Vperp=v_x)
+        x = np.array([[0, R_L_element.value, 0] for R_L_element in R_L]) * u.m
+
+        simulation = ParticleTracker(
+            self.grid, req_quantities=["E_x", "E_y", "E_z", "B_x", "B_y", "B_z"]
+        )
+
+        simulation.load_particles(x, v, self.point_particle)
+
+        stop_condition = TimeElapsedStopCondition(10 * u.s)
+        save_routine = MemoryIntervalSaveRoutine(0.1 * u.s)
+
+        simulation.run(stop_condition, save_routine, dt=1e-3 * u.s)
+
+        velocities = np.asarray(save_routine.v_all) * u.m / u.s
+        speeds = np.linalg.norm(velocities, axis=-1)
+        simulation_kinetic_energies = 0.5 * self.point_particle.mass * speeds**2
+
+        assert np.isclose(initial_kinetic_energies, simulation_kinetic_energies).all()
