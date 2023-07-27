4dcddf77e4d56d71873dc6b41de5c03073f9fbce
JaydenR2305 <109694185+JaydenR2305@users.noreply.github.com>
2023-07-27 13:03:35 -0400

    Clean up tests

--- plasmapy/simulation/tests/test_particle_tracker.py
+++ plasmapy/simulation/tests/test_particle_tracker.py
@@ -7,7 +7,6 @@ import pytest
 
 from hypothesis import given, settings
 from hypothesis import strategies as st
-from hypothesis.extra.numpy import arrays
 
 from plasmapy.formulary.lengths import gyroradius
 from plasmapy.particles import CustomParticle
@@ -19,72 +18,59 @@ from plasmapy.simulation.particle_tracker import (
     TimeElapsedStopCondition,
 )
 
+rng = np.random.default_rng()
+
 
 class TestParticleTrackerGyroradius:
+    RESOLUTION = 100
+    v_theta = rng.integers(0, 2 * np.pi * RESOLUTION, size=100) / RESOLUTION
+    speed = rng.integers(1, 10, size=100) * u.m / u.s
+    v_x = rng.integers(1, 10, size=100) * u.m / u.s
+
+    v = np.array([[v_x_element.value, 0, 0] for v_x_element in v_x]) * u.m / u.s
+
+    B_strength = rng.integers(1, 10) * u.T
+
     point_particle = CustomParticle(1 * u.kg, 1 * u.C)
 
-    def instantiate_simulation(self, B_strength=1 * u.T):
-        L = 1e6 * u.km
-        num = 2
-        grid = CartesianGrid(-L, L, num=num)
-        grid_shape = (num,) * 3
+    # Set the initial position to the gyroradius
+    # This means the particle will orbit the origin
+    R_L = gyroradius(B_strength, point_particle, Vperp=v_x)
+    x = np.array([[0, R_L_element.value, 0] for R_L_element in R_L]) * u.m
 
-        Bz = np.full(grid_shape, B_strength) * u.T
-        grid.add_quantities(B_z=Bz)
+    L = 1e6 * u.km
+    num = 2
+    grid = CartesianGrid(-L, L, num=num)
+    grid_shape = (num,) * 3
 
-        return ParticleTracker(
-            grid, req_quantities=["E_x", "E_y", "E_z", "B_x", "B_y", "B_z"]
-        )
+    Bz = np.full(grid_shape, B_strength) * u.T
+    grid.add_quantities(B_z=Bz)
 
-    @pytest.mark.slow()
-    @given(arrays(int, (5,), elements=st.integers(1, 100)), st.integers(1, 100))
-    @settings(deadline=5e4, max_examples=3)
-    def test_gyroradius(self, v_x, B_strength):
+    simulation = ParticleTracker(
+        grid, req_quantities=["E_x", "E_y", "E_z", "B_x", "B_y", "B_z"]
+    )
+    simulation.load_particles(x, v, point_particle)
+
+    stop_condition = TimeElapsedStopCondition(10 * u.s)
+    save_routine = MemoryIntervalSaveRoutine(0.1 * u.s)
+
+    simulation.run(stop_condition, save_routine, dt=1e-3 * u.s)
+
+    def test_gyroradius(self):
         """Test to ensure particles maintain their gyroradius over time"""
-        v_x = v_x * u.m / u.s
-        v = np.array([[v_x_element.value, 0, 0] for v_x_element in v_x]) * u.m / u.s
-
-        # Set the initial position to the gyroradius
-        # This means the particle will orbit the origin
-        R_L = gyroradius(B_strength, self.point_particle, Vperp=v_x)
-        x = np.array([[0, R_L_element.value, 0] for R_L_element in R_L]) * u.m
-
-        simulation = self.instantiate_simulation(B_strength=B_strength)
-        simulation.load_particles(x, v, self.point_particle)
-
-        stop_condition = TimeElapsedStopCondition(10 * u.s)
-        save_routine = MemoryIntervalSaveRoutine(0.1 * u.s)
-
-        simulation.run(stop_condition, save_routine, dt=1e-3 * u.s)
-
-        positions = np.asarray(save_routine.x_all) * u.m
+        positions = np.asarray(self.save_routine.x_all) * u.m
         distances = np.linalg.norm(positions, axis=-1)
 
-        assert np.isclose(distances, R_L, rtol=1e-2).all()
+        is_close = np.isclose(distances, self.R_L, rtol=1e-2)
 
-    @pytest.mark.slow()
-    @given(arrays(int, (5,), elements=st.integers(1, 100)))
-    @settings(deadline=1e5, max_examples=3)
-    def test_kinetic_energy(self, v_x):
+        assert is_close.all()
+
+    def test_kinetic_energy(self):
         """Test to ensure particles maintain their gyroradius over time"""
-        v_x = v_x * u.m / u.s
-        v = np.array([[v_x_element.value, 0, 0] for v_x_element in v_x]) * u.m / u.s
-        initial_kinetic_energies = 0.5 * self.point_particle.mass * v_x**2
 
-        # Set the initial position to the gyroradius
-        # This means the particle will orbit the origin
-        R_L = gyroradius(self.B_strength, self.point_particle, Vperp=v_x)
-        x = np.array([[0, R_L_element.value, 0] for R_L_element in R_L]) * u.m
+        initial_kinetic_energies = 0.5 * self.point_particle.mass * self.v_x**2
 
-        simulation = self.instantiate_simulation()
-        simulation.load_particles(x, v, self.point_particle)
-
-        stop_condition = TimeElapsedStopCondition(10 * u.s)
-        save_routine = MemoryIntervalSaveRoutine(0.1 * u.s)
-
-        simulation.run(stop_condition, save_routine, dt=1e-3 * u.s)
-
-        velocities = np.asarray(save_routine.v_all) * u.m / u.s
+        velocities = np.asarray(self.save_routine.v_all) * u.m / u.s
         speeds = np.linalg.norm(velocities, axis=-1)
         simulation_kinetic_energies = 0.5 * self.point_particle.mass * speeds**2
 
