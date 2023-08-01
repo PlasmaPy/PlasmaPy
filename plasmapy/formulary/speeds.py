c8e54127f67db5df21b39891f642a3f2bfd855b3
Nick Murphy <namurphy@cfa.harvard.edu>
2023-08-01 14:51:02 -0400

    Update plasmapy/formulary/speeds.py

--- plasmapy/formulary/speeds.py
+++ plasmapy/formulary/speeds.py
@@ -163,7 +163,7 @@ def Alfven_speed(
     if density.unit.physical_type == u.physical.number_density and ion is None:
         raise ValueError(
             "When calculating the Alfvén speed, the ion must be specified "
-            "when the 'density' parameter is provided an argument with a "
+            "when the 'density' argument has a "
             "physical type of number density."
         )
 
