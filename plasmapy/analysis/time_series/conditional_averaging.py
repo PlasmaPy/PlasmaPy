0f83bbf7d9bd6bb0bc31abba3ed03a373ed93321
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-08-02 11:16:58 +0200

    use Rolf's version of cond_av

--- plasmapy/analysis/time_series/conditional_averaging.py
+++ plasmapy/analysis/time_series/conditional_averaging.py
@@ -4,157 +4,128 @@ __all__ = ["ConditionlEvents"]
 
 
 import numpy as np
-
+from scipy.signal import find_peaks, peak_prominences
 
 class ConditionlEvents:
     def __init__(
-        self,
-        signal,
-        time,
-        lower_threshold,
-        upper_threshold=None,
-        reference_signal=None,
-        minimal_distance=None,
-        window=False,
-        print_verbose=True,
+        self, S, T, smin=None, smax=None, Sref=None, delta=None, window=False,  prominence=None, threshold = None, weight='amplitude'
     ):
-        if reference_signal is None:
-            reference_signal = signal
-        assert len(reference_signal) == len(signal) and len(signal) == len(time)
+    
+    if all(i is None for i in[smin, prominence]):
+        raise TypeError('Missing 1 required positional argument: \'smin\' '
+                        'or \'prominence\'')
+    
+    if Sref is None:
+        Sref = S.copy()
+    assert len(Sref) == len(S) and len(S) == len(T)
 
-        normalizes_signal = (reference_signal - np.mean(reference_signal)) / np.std(
-            reference_signal
-        )
-        time_step = sum(np.diff(time)) / (len(time) - 1)
-
-        places = np.where(normalizes_signal > lower_threshold)[0]
-
-        assert len(places) > 0, "No conditional events"
-
-        if print_verbose:
-            print("places to check:{}".format(len(places)), flush=True)
-
-        distance_between_places = np.diff(places)
-        _split = np.where(distance_between_places != 1)[0]
+    sgnl = (Sref - np.mean(Sref)) / np.std(Sref)
+    dt = np.diff(T).sum() / (len(T) - 1)
+    
+    #Estimating delta.
+    if delta is None:
+        if smin is None:
+            tmpmin = prominence
+        else:
+            tmpmin = smin
+            
+        places = np.where(sgnl > tmpmin)[0]
+        dplaces = np.diff(places)
+        split = np.where(dplaces != 1)[0]
         # (+1 since dplaces is one ahead with respect to places)
-        conditional_events = np.split(places, _split + 1)
+        lT = np.split(places, split + 1)
+        delta = len(sgnl) / len(lT) * dt
+    
+    distance = None
+    # Ensure distance delta between peaks.
+    if window:
+        distance = int(delta / dt)
+        
+    
+    
+    # Find peak indices.
+    gpl_array, properties = find_peaks(sgnl, height = [smin, smax], distance = distance,
+                              prominence = prominence, threshold = threshold)
+    
+    if not prominence and not window:
+        places = np.where(sgnl > smin)[0]
+        if smax:
+            higher = np.where(sgnl<smax)[0]
+            places = np.intersect1d(places, higher)
+        assert len(places) > 0, "No conditional events"
+   
+        dplaces = np.diff(places)
+        split = np.where(dplaces != 1)[0]
+        lT = np.split(places, split + 1)
+        #Ensure one peak for every threshold crossing. Largest peak is chosen.
+        #Ties are broken my earliest time.
+        for i, lTi in enumerate(lT): 
+            peak_check = np.isin(gpl_array, lTi)
+            if peak_check.sum()>1:
+                peak_ind = gpl_array[peak_check]
+                highest_local_peak = Sref[peak_ind].argmax()
+                not_highest_local_peaks = np.delete(peak_ind, highest_local_peak)
+                gpl_array = np.delete(gpl_array, np.isin(gpl_array, not_highest_local_peaks))
+        
+    
+    # Use arange instead of linspace to guarantee 0 in the middle of the array.
+    t_av = np.arange(-int(delta / (dt * 2)), int(delta / (dt * 2)) + 1) * dt
 
-        if minimal_distance is None:
-            minimal_distance = len(normalizes_signal) / len(conditional_events) * time_step
-        if upper_threshold is not None:
-            too_high = []
-            for i in range(len(conditional_events)):
-                if max(normalizes_signal[conditional_events[i]]) > upper_threshold:
-                    too_high.append(i)
-            removeset = set(too_high)
-            conditional_events = [event for i, event in enumerate(conditional_events) if i not in removeset]
+    
+    peaks = S[gpl_array]
+    prominences = peak_prominences(S, gpl_array)[0]
+    wait = np.append(np.array([T[0]]), T[gpl_array])
+    wait = np.diff(wait)
 
-        # Use arange instead of linspace to guarantee 0 in the middle of the array.
-        return_time = (
-            np.arange(-int(minimal_distance / (time_step * 2)), int(minimal_distance / (time_step * 2)) + 1)
-            * time_step
-        )
+    Svals = np.zeros(len(sgnl))
+    Svals[:] = np.nan
 
-        # diagnostics
-        lplcount = 0
+    badcount = 0
 
-        gpl_array = np.array([])
-        if window:
-            conditional_events.sort(key=lambda t: max(normalizes_signal[t]), reverse=True)
+    t_half_len = int((len(t_av) - 1) / 2)
+    s_tmp = np.zeros([len(t_av), len(gpl_array)])
 
-        # Find local and global peak values
-        for i in range(len(conditional_events)):
-            local_peak_loc = np.where(
-                reference_signal[conditional_events[i]] == max(reference_signal[conditional_events[i]])
-            )[0]
+    for i, global_peak_loc in enumerate(gpl_array):
 
-            # Troubleshooting in case there are more than one unique peak
-            if len(local_peak_loc) > 1:
-                lplcount += 1
-                # Prefer the peak closest to the mean of the candidates
-                # (earliest time breaks ties)
-                local_peak_loc = local_peak_loc[
-                    abs(local_peak_loc - np.mean(local_peak_loc)).argmin()
-                ]
-
-            gpl_array = np.append(gpl_array, local_peak_loc + conditional_events[i][0])
-
-        # Ensure distance delta between peaks.
-        if window:
-            index = 0
-            while index < len(gpl_array):
-                t_to_close = np.where(
-                    abs(gpl_array[index + 1 :] - gpl_array[index])
-                    < int(minimal_distance / time_step)
-                )[0] + (index + 1)
-                gpl_array = np.delete(gpl_array, t_to_close)
-                removeset = set(t_to_close)
-                conditional_events = [v for i, v in enumerate(conditional_events) if i not in removeset]
-                index += 1
-            conditional_events.sort(key=lambda t: max(t))
-            gpl_array.sort()
-
-        gpl_array = gpl_array.astype(int)
-        peaks = signal[gpl_array]
-        wait = np.append(np.array([time[0]]), time[gpl_array])
-        wait = np.diff(wait)
-
-        Svals = np.zeros(len(normalizes_signal))
-        Svals[:] = np.nan
-
-        badcount = 0
-
-        t_half_len = int((len(return_time) - 1) / 2)
-        s_tmp = np.zeros([len(return_time), len(conditional_events)])
-
-        for i in range(len(conditional_events)):
-            global_peak_loc = gpl_array[i]
-
-            # Find the average values and their variance
-            low_ind = int(max(0, global_peak_loc - t_half_len))
-            high_ind = int(
-                min(len(normalizes_signal), global_peak_loc + t_half_len + 1)
+        # Find the average values and their variance
+        low_ind = int(max(0, global_peak_loc - t_half_len))
+        high_ind = int(min(len(sgnl), global_peak_loc + t_half_len + 1))
+        tmp_sn = S[low_ind:high_ind]
+        Svals[low_ind:high_ind] = S[low_ind:high_ind]
+        if low_ind == 0:
+            tmp_sn = np.append(np.zeros(-global_peak_loc + t_half_len), tmp_sn)
+        if high_ind == len(S):
+            tmp_sn = np.append(
+                tmp_sn, np.zeros(global_peak_loc + t_half_len + 1 - len(S))
             )
-            tmp_sn = signal[low_ind:high_ind]
-            Svals[low_ind:high_ind] = signal[low_ind:high_ind]
-            if low_ind == 0:
-                tmp_sn = np.append(np.zeros(-global_peak_loc + t_half_len), tmp_sn)
-            if high_ind == len(signal):
-                tmp_sn = np.append(
-                    tmp_sn, np.zeros(global_peak_loc + t_half_len + 1 - len(signal))
-                )
-            if max(tmp_sn) != tmp_sn[t_half_len]:
-                badcount += 1
+        if tmp_sn.max() != tmp_sn[t_half_len]:
+            badcount += 1
+        
+        s_tmp[:, i] = tmp_sn
+        if weight == 'equal':
+            if smin is None:
+                s_tmp[:, i] /= prominences[i]
+                s_tmp[:, i] += (1-s_tmp[t_half_len, i])
+                
+            else:
+                s_tmp[:, i] /= tmp_sn[t_half_len]
+    s_av = np.mean(s_tmp, axis=1)
 
-            s_tmp[:, i] = tmp_sn
-        s_av = np.mean(s_tmp, axis=1)
+    # The conditional variance of the conditional event f(t) is defined as
+    # CV = <(f-<f>)^2>/<f^2> = 1 - <f>^2/<f^2>
+    # at each time t.
+    # For a highly reproducible signal, f~<f> and CV = 0.
+    # For a completely random signal, <f^2> >> <f>^2 and CV = 1.
+    # OBS: We return 1-CV = <f>^2/<f^2>.
+    s_var = s_av ** 2 / np.mean(s_tmp ** 2, axis=1)
+    print("conditional events:{}".format(len(peaks)), flush=True)
+    if badcount > 0:
+        print("bursts where the recorded peak is not the largest:" + str(badcount))
 
-        # The conditional variance of the conditional event f(t) is defined as
-        # CV = <(f-<f>)^2>/<f^2> = 1 - <f>^2/<f^2>
-        # at each time t.
-        # For a highly reproducible signal, f~<f> and CV = 0.
-        # For a completely random signal, <f^2> >> <f>^2 and CV = 1.
-        # Note: We return 1-CV = <f>^2/<f^2>.
-        s_var = s_av**2 / np.mean(s_tmp**2, axis=1)
-        if print_verbose:
-            print("conditional events:{}".format(len(peaks)), flush=True)
-            if badcount > 0:
-                print(
-                    "bursts where the recorded peak is not the largest:" + str(badcount)
-                )
-            if lplcount > 0:
-                print(
-                    "There were problems locating unique peaks in {0} bursts, {1:.3f} of all bursts".format(
-                        lplcount, lplcount / len(conditional_events)
-                    )
-                )
-
-                print("In all cases, the first peak per burst was used.")
-
-        self.return_time = return_time
-        self.s_av = s_av
-        return Svals, s_av, s_var, return_time, peaks, wait
-
+    self.return_time = t_av
+    self.s_av = s_av
+    # return Svals, s_av, s_var, t_av, peaks, wait, prominences
+i
     @property
     def time(self):
         return self.return_time
@@ -180,5 +151,5 @@ class ConditionlEvents:
         pass
 
     @property
-    def number_of_unrecorded_events(self):
+    def number_of_unused_peaks(self):
         pass
