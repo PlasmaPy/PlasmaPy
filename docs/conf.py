372010c91094f8155db4457023db113e1fc76322
Nick Murphy <namurphy@cfa.harvard.edu>
2023-08-04 18:17:28 -0400

    Fix configuration for sphinx-codeautolink

--- docs/conf.py
+++ docs/conf.py
@@ -116,6 +116,9 @@ bibtex_default_style = "plain"
 bibtex_reference_style = "author_year"
 bibtex_cite_id = "{key}"
 
+
+codeautolink_concat_default = True
+
 # Intersphinx generates automatic links to the documentation of objects
 # in other packages. When mappings are removed or added, please update
 # the section in docs/doc_guide.rst on references to other packages.
