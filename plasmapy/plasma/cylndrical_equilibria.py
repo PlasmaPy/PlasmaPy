c3be2fd946e32c67bb71b0a7826d83f33a7aa91a
Mohammed Haque <arnob811@gmail.com>
2023-08-17 16:57:17 -0400

    Cylndrical solutions to Spheromaks

--- plasmapy/plasma/cylndrical_equilibria.py
+++ plasmapy/plasma/cylndrical_equilibria.py
@@ -1,12 +1,7 @@
-import math
-import numpy as np
 import scipy.special
-import sympy.functions.special.bessel
 
-from sympy import Derivative  
-  
+
 class ForceFreeFluxRope:
-  
     def __init__(self, B0, a, lamb):
         self.B0 = B0
         self.a = a
@@ -14,16 +9,11 @@ class ForceFreeFluxRope:
         self.r = r
         self.z = z
 
-
-
     def B_radial_cyln():
-
-            return (0)
+        return 0
 
     def B_phi_cyln(self, B, r):
-
-            return (B*scipy.special.j1(self.lamb * r))
+        return B * scipy.special.j1(self.lamb * r)
 
     def B_z_cyln(self, B, r):
-
-            return(B*scipy.special.j1(self.lamb * r))
+        return B * scipy.special.j1(self.lamb * r)
