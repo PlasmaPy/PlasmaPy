8da0c2a286b1d500ded18f56d57795743e644709
Nick Murphy <namurphy@cfa.harvard.edu>
2023-08-30 13:18:00 -0400

    Apply suggestions from code review

--- plasmapy/plasma/cylindrical_equilibria.py
+++ plasmapy/plasma/cylindrical_equilibria.py
@@ -6,22 +6,24 @@ import scipy.special
 
 class ForceFreeFluxRope:
     r"""
-    Define Analytical solution for lundquist solution :cite:p:`lundquist:1950`.
+    Representation of the analytical Lundquist solution for 
+    force-free magnetic flux ropes :cite:p:`lundquist:1950`.
 
     Parameters
     ----------
     B0 : `~astropy.units.Quantity`
         Magnetic field strength in units convertible to tesla.
 
-    a : `~astropy.units.Quantity`
+    alpha : `~astropy.units.Quantity`
         Eigenvalue to make :math:`\mathbf{J} × \mathbf{B} = 0`, in units
         convertible to inverse length.
 
     Notes
     -----
-    Lundquist solutions describe the flow of a conducting fluid in an electric field
-
-
+    The Lundquist solution [also known as the Bessel Function Model (BFM)]
+    is a cylindrically symmetric force-free equilibrium which is often used
+    to approximate the magnetic structure of interplanetary coronal mass
+    ejections (ICMEs).
     """
 
     def __init__(self, B0, alpha):
@@ -30,31 +32,48 @@ class ForceFreeFluxRope:
 
     def B_theta(self, r):
         r"""
-        Compute the magnetic field in the theta direction.
+        Compute the component of the magnetic field in the azimuthal
+        direction.
 
         .. math::
 
-            (self.B0*scipy.special.j1(self.a * r))
+            B_θ(r) = B_0 J_1(α r)
+            
+        where :math:`α` is the eigenvalue and :math:`J_1` is the Bessel
+        function of the first kind of order 1.
 
         Parameters
         ----------
         r : `~astropy.units.Quantity`
-             A distance from the flux rope.
+             Radial distance from flux rope axis in units convertible
+             to meters.
+
+        Returns
+        -------
+        `~astropy.units.Quantity`
         """
         return self.B0 * scipy.special.j1(self.alpha * r)
 
     def B_z(self, r):
         r"""
-        Compute the magnetic field in the z direction.
+        Compute the axial component of the magnetic field.
 
         .. math::
 
-            self.B0*scipy.special.j0(self.a * r)
+            B_z(r) = B_0 J_0(α r)
+
+        where :math:`α` is the eigenvalue and :math:`J_0` is the Bessel
+        function of the first kind of order 0.
 
         Parameters
         ----------
         r : `~astropy.units.Quantity`
-          distance from equilibria
+             Radial distance from flux rope axis in units convertible
+             to meters.
+
+         Returns
+         -------
+         `~astropy.units.Quantity`
 
         """
         return self.B0 * scipy.special.j0(self.alpha * r)
@@ -65,12 +84,16 @@ class ForceFreeFluxRope:
 
         .. math::
 
-            np.sqrt(self.B_z(r)**2 + self.B_theta(r)**2)
+            B(r) = \sqrt{B_z(r)^2 + B_θ(r)^2}.
 
         Parameters
         ----------
         r : `~astropy.units.Quantity`
-            distance from equilibria
+             Radial distance from flux rope axis in units convertible
+             to meters.
 
+        Returns
+        -------
+        `~astropy.units.Quantity`
         """
         return np.sqrt(self.B_z(r) ** 2 + self.B_theta(r) ** 2)
