3e601cc957f76f64948cb27d2225b82e582f5f85
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-09-25 14:58:52 -0500

    Add function for calculating the derivative of chi

--- plasmapy/diagnostics/brl/poisson_equation_integration.py
+++ plasmapy/diagnostics/brl/poisson_equation_integration.py
@@ -1,4 +1,5 @@
 """Evaluation and integration of Poisson's equation."""
+import numpy as np
 
 
 def evaluate_K0_term(eta_net, gamma, x, spherical=True):
@@ -75,6 +76,46 @@ def evaluate_K2_term(K1_term, chi_0, integration_matrix):
 
     Notes
     -----
-    This is the term :math:`K_2(s)` as defined in equations (D.6). This is the same for both the spherical and cylindrical probes.
+    This is the term :math:`K_2(s)` as defined in equation (D.6). This is the same for both the spherical and cylindrical probes.
     """
     return chi_0 + integration_matrix @ K1_term
+
+
+def dchi_dx_at_probe(
+    K1_term, K2_term, x, dx_ds, spherical=True, zero_T_repelled_particles=False
+):
+    r"""The derivative of the normalized potential in terms of :math:`x` at the probe.
+
+    Parameters
+    ----------
+    K1_term, K2_term : `numpy.ndarray`
+        The :math:`K_1(s)` and :math:`K_2(s)` terms calculated from `~plasmapy.diagnostics.brl.poisson_equation_integration.evaluate_K1_term` and `~plasmapy.diagnostics.brl.poisson_equation_integration.evaluate_K2_term` respectively.
+    x, dx_ds : `numpy.ndarray`
+        The the normalized inverse radius and it's `s` derivative calculated from `~plasmapy.diagnostics.brl.net_spacing.get_x_and_dx_ds`.
+    spherical : `bool`, optional
+        If `True` the probe will be treated as spherical. If `False` then the probe is cylindrical. Default is `True`.
+    zero_T_repelled_particles : `bool`, optional
+        If `True` then the repelled particles have zero temperature which requires a slightly modified calculation. Default is `False`.
+
+    Returns
+    -------
+    `float`
+
+    Notes
+    -----
+    This is the term :math:`(d\chi / dx)_{s=0}` as defined in equations (D.10), (D.11), (D.18), and (D.19).
+    """
+    if zero_T_repelled_particles:
+        if spherical:
+            # Equation (D.11).
+            return K2_term[-1] / (1 - x[-1])
+        else:
+            # Equation (D.19).
+            return -K2_term[-1] / np.log(x[-1])
+    else:  # noqa: PLR5501
+        if spherical:
+            # Equation (D.10).
+            return (2 * K2_term[-1] - x[-1] * K1_term[-1] * dx_ds[-1]) / (2 - x[-1])
+        else:
+            # Equation (D.18).
+            return (K2_term[-1] - x[-1] * K1_term[-1] / dx_ds[-1]) / (1 - np.log(x[-1]))
