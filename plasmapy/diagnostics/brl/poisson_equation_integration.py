ae687d14c6cd52b7c5aa1dcd9b77068e6e89c7cc
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-09-25 14:37:07 -0500

    Add function for calculating K2

--- plasmapy/diagnostics/brl/poisson_equation_integration.py
+++ plasmapy/diagnostics/brl/poisson_equation_integration.py
@@ -55,3 +55,26 @@ def evaluate_K1_term(K0_term, x, dx_ds, integration_matrix, spherical=True):
         return dx_ds * (integration_matrix @ (K0_term * dx_ds))
     else:
         return (dx_ds / x) * (integration_matrix @ (K0_term * dx_ds))
+
+
+def evaluate_K2_term(K1_term, chi_0, integration_matrix):
+    r"""The integral of :math:`K_1(s)` with the addition of the probe potential.
+
+    Parameters
+    ----------
+    K1_term : `numpy.ndarray`
+        The :math:`K_1(s)` term calculated from `~plasmapy.diagnostics.brl.poisson_equation_integration.evaluate_K1_term`.
+    chi_0 : `float`
+        The normalized potential of the probe.
+    integration_matrix : `numpy.ndarray`
+        The matrix used for integrating functions from `~plasmapy.diagnostics.brl.integration.construct_integration_matrix`.
+
+    Returns
+    -------
+    `numpy.ndarray`
+
+    Notes
+    -----
+    This is the term :math:`K_2(s)` as defined in equations (D.6). This is the same for both the spherical and cylindrical probes.
+    """
+    return chi_0 + integration_matrix @ K1_term
