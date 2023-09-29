4da8525aaf987e7e726b568bca440f65da9df4b3
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-09-28 19:56:48 -0500

    Move derivative calculation out of each function

--- plasmapy/diagnostics/brl/tests/test_net_spacing.py
+++ plasmapy/diagnostics/brl/tests/test_net_spacing.py
@@ -31,38 +31,56 @@ class Test__private_x_and_dx_ds:
     s_points = net_spacing.get_s_points(1000, 0.8)
     ds = s_points[1] - s_points[0]
 
+    def approximate_derivative(self, x_points):
+        r"""Approximate the value of dx/ds."""
+        return (x_points[2:] - x_points[:-2]) / (2 * self.ds)
+
     def test_large_probe_x_and_dx_ds(self):
         r"""Test large probe x and dx/ds."""
 
         x_points, dx_ds_points = net_spacing._large_probe_x_and_dx_ds(self.s_points, 80)
-        approximate_derivative = (x_points[2:] - x_points[:-2]) / (2 * self.ds)
+        approximate_derivative = self.approximate_derivative(x_points)
 
         assert x_points[0] == 1
-        assert np.allclose(dx_ds_points[1:-1], approximate_derivative)
+        assert np.allclose(
+            dx_ds_points[1:-1], approximate_derivative
+        ), f"The derivative, dx/ds, with value {dx_ds_points} should be close to the approximate derivative with value {approximate_derivative}."
 
     def test_medium_probe_x_and_dx_ds(self):
         r"""Test medium probe x and dx/ds."""
 
         x_points, dx_ds_points = net_spacing._medium_probe_x_and_dx_ds(self.s_points)
-        approximate_derivative = (x_points[2:] - x_points[:-2]) / (2 * self.ds)
+        approximate_derivative = self.approximate_derivative(x_points)
 
         assert x_points[0] == 1
-        assert np.allclose(dx_ds_points[1:-1], approximate_derivative)
+        assert np.allclose(
+            dx_ds_points[1:-1], approximate_derivative
+        ), f"The derivative, dx/ds, with value {dx_ds_points} should be close to the approximate derivative with value {approximate_derivative}."
 
     def test_small_probe_x_and_dx_ds(self):
         r"""Test small probe x and dx/ds."""
 
         x_points, dx_ds_points = net_spacing._small_probe_x_and_dx_ds(self.s_points)
-        approximate_derivative = (x_points[2:] - x_points[:-2]) / (2 * self.ds)
+        approximate_derivative = self.approximate_derivative(x_points)
 
         assert x_points[0] == 1
-        assert np.allclose(dx_ds_points[1:-1], approximate_derivative)
+        assert np.allclose(
+            dx_ds_points[1:-1], approximate_derivative
+        ), f"The derivative, dx/ds, with value {dx_ds_points} should be close to the approximate derivative with value {approximate_derivative}."
 
     def test_zero_T_repelled_x_and_dx_ds(self):
         r"""Test zero temperature repelled particles x and dx/ds calculation."""
 
-        x_points, dx_ds_points = net_spacing._zero_T_repelled_x_and_dx_ds(self.s_points, 10, 25)
-        approximate_derivative = (x_points[2:] - x_points[:-2]) / (2 * self.ds)
+        x_points, dx_ds_points = net_spacing._zero_T_repelled_x_and_dx_ds(
+            self.s_points, 10, 25
+        )
+        approximate_derivative = self.approximate_derivative(x_points)
 
         assert x_points[0] == 1
-        assert np.allclose(dx_ds_points[1:-1], approximate_derivative)
+        assert np.allclose(
+            dx_ds_points[1:-1], approximate_derivative
+        ), f"The derivative, dx/ds, with value {dx_ds_points} should be close to the approximate derivative with value {approximate_derivative}."
+
+
+class Test__get_x_and_dx_ds:
+    r"""Test the get_x_and_dx_ds function in net_spacing.py"""
