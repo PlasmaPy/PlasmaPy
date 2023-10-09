731cde514cb7d8368b195323595795e0e25d63ec
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-10-09 16:21:02 -0500

    Add calculation for zero temperature particle charge density contribution

--- plasmapy/diagnostics/brl/charge_density.py
+++ plasmapy/diagnostics/brl/charge_density.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 from scipy.integrate import quad_vec
-from scipy.special import erfc
+from scipy.special import erfc, erfi
 
 
 def _g(x):
@@ -120,3 +120,46 @@ def eta_2(A, chi, chi_p, x, spherical=True):
             * np.arctan((x**2 / (1 - x**2) * (A - chi_p) / (A - kappa)) ** 0.5)
             + x / (1 - x**2) ** 0.5 * (chi_p - chi) / (2 * np.pi) * H1
         )
+
+
+def eta_3(A, chi, x, x_B, spherical=True):
+    r"""Calculate the contribution to the charge density when there is a finite radius at which the potential is 0.
+
+    This only appears when the temperature of the attracted particles is zero.
+
+    Parameters
+    ----------
+    A : `numpy.ndarray`
+        Lower bound of the integral. Also, `A >= kappa` as defined in (E.3).
+    chi : `numpy.ndarray`
+        The normalized potential.
+    x : `numpy.ndarray`
+        Normalized inverse radius calculated from `~plasmapy.diagnostics.brl.net_spacing.get_x_and_dx_ds`.
+    x_B : `float`
+        Normalized inverse radius at which the potential is 0.
+    spherical : `bool`, optional
+        If `True` the probe will be treated as spherical. If `False` then the probe is cylindrical. Default is `True`.
+
+    Returns
+    -------
+    `numpy.ndarray`
+
+    Notes
+    -----
+    This is the term :math:`\eta_3(A)` that appears in various formula in appendix E.
+    """
+    # Equation (E.5).
+    beta_B = -chi / (x**2 / x_B**2 - 1)
+    if spherical:
+        # Equation (E.27) where we've simplified the last integral.
+        return -(((x**2 / x_B**2 - 1) / np.pi) ** 0.5) * (
+            (beta_B - A) ** 0.5 * np.exp(-A)
+            - np.exp(-beta_B) * np.pi**0.5 / 2 * erfi((beta_B - A) ** 0.5)
+        )
+    else:
+        # TODO: See if this should be converted to using the approximations given in equations (E.66) - (E.85).
+        # Equation (E.11).
+        def integrand(beta):
+            np.exp(-beta) * np.arcsin((x**2 / x_B**2 * beta / (beta - chi)) ** 0.5)
+
+        return 1 / np.pi * quad_vec(integrand, A, beta_B)[0]
