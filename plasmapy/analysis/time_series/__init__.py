4d8b30baae23239051386eb13695de5ad25dc7ae
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2023-10-18 00:52:08 +0200

    Add class for calculating conditional average of time series. (#2275)
    * add original implementation
    
    * started renaming variables
    
    * remove unnecessary variables
    
    * make ConditionalEvents class testable
    
    * use Rolf's version of cond_av
    
    * extract max peak finding for every event
    
    * refactor conditional averaging methods
    
    * add assert statements
    
    * turn assert statements into ValueErrors
    
    * add first tests
    
    * add test for list compability
    
    * get 100% code coverage
    
    * add test for reference_signal
    
    * implement astropy units option
    
    * fix bug for signal.unit != reference.unit
    
    * add provisional docstrings
    
    * format docstring
    
    * update docstring
    
    * fix ValueError formatting
    
    * Try different docs
    
    Co-authored-by: audunth
    Co-authored-by: RolfNi
    
    * Try different docs
    
    Co-authored-by: audunth <audun.theodorsen@uit.no>
    Co-authored-by: RolfNi <rni062@post.uit.no>
    
    * add rst file
    
    * update docstring of ConditionalEvents class
    
    * add changelog file
    
    * remove unused variable
    
    * fix spelling mistake in docstring
    
    * Update docs/api_static/plasmapy.analysis.time_series.conditional_averaging.rst
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/conditional_averaging.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/conditional_averaging.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/conditional_averaging.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/conditional_averaging.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * Update plasmapy/analysis/time_series/conditional_averaging.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * simplify removing of units
    
    * Update plasmapy/analysis/time_series/conditional_averaging.py
    
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    
    * adjust tests for keyword-only parameters
    
    * return results as numpy arrays
    
    * add checks for threshold with unit and signal without unit
    
    * check whether largest value per event is peak
    
    Co-authored-by: RolfNi <rni062@uit.no>
    
    * add option for deleting events where peak is not largest value inside window
    
    * add unit checks for time related variables
    
    * remove unused variables
    
    * sort imports
    
    * remove collapsible if statements
    
    * update black to 23.9.1
    
    * separate value checks to separate methode
    
    * remove duplicate tests
    
    * run isort on __init__.py
    
    * add test for reference signal
    
    * Capitalize error messages
    
    * allow reference signal together with the remove_non_max_peaks
    
    * provide linked reference for master thesis
    
    ---------
    
    Co-authored-by: audunth <audun.theodorsen@uit.no>
    Co-authored-by: RolfNi <rni062@post.uit.no>
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>
    Co-authored-by: RolfNi <rni062@uit.no>

--- plasmapy/analysis/time_series/__init__.py
+++ plasmapy/analysis/time_series/__init__.py
@@ -6,5 +6,6 @@ Subpackage containing routines for analyzing time series.
    |expect-api-changes|
 """
 
+from plasmapy.analysis.time_series.conditional_averaging import ConditionalEvents
 from plasmapy.analysis.time_series.excess_statistics import ExcessStatistics
 from plasmapy.analysis.time_series.running_moments import running_mean, running_moment
