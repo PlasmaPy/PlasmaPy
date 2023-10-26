24a792e2a2186c6c1b311ffaa5806c5b0c8e8fd0
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-10-26 18:48:16 -0500

    Add some attempt at a vectorization of eta4

--- plasmapy/diagnostics/brl/integration.py
+++ plasmapy/diagnostics/brl/integration.py
@@ -36,6 +36,8 @@ def construct_integration_matrix(num_points, point_spacing):
     >>> y_points = x_points**2
     >>> integrated_y_points = integration_matrix @ y_points
     """
+    # TODO: Change this so that it takes in an array of points and then integrates instead of passing around this integration object.
+    # TODO: This should be a slightly different matrix as the last term is -1 but should actually follow the last equation of (D.22) for all lines.
     if num_points < 3:
         raise ValueError("Can't create integration matrix for fewer than 3 points.")
 
@@ -64,3 +66,46 @@ def construct_integration_matrix(num_points, point_spacing):
 
     integration_matrix *= point_spacing
     return integration_matrix
+
+
+def integrate(integrand, start, end, point_spacing):
+    f"""Integrate an array where the start and end indeces can be floats.
+    
+    This will only calculate the integrand where it is real.
+    The integration is defined in the `CAL` function on 
+    page 37 - 39 of the computer program listing, appendix I.
+
+    Parameters
+    ----------
+    integrand : `numpy.ndarray`
+        2D-array to integrate. Of shape `(num_x_points, num_y_points)`.
+    starts, ends : `numpy.ndarray`
+        1D-array of indeces of the `integrand` array to integrate between. Of 
+        shape `(num_x_points,)`. Additionally, `0 <= starts <= num_y_points - 1` 
+        and `0 <= ends <= num_y_points - 1`.
+    point_spacing : `float`
+        Distance between each sample point.
+
+    Notes
+    -----
+    This operates by using the integration described in appendix D for 
+    integration between the indeces closest to the `starts` and `ends`. If the 
+    start or end point is not an integer then a Taylor expansion is done around 
+    either the closest integer index or one away from that depending on if the 
+    integrand is a complex number.
+    """
+    start_float = np.where(start <= end, start, end)
+    end_float = np.where(start <= end, end, start)
+    reverse_integration = np.where(start <= end, 1, -1)
+
+    start_index = np.ceil(start_float)
+    end_index = np.floor(end_float)
+
+    integral = np.zeros(integrand.shape[0], dtype=float)
+
+    # Integrate up to the start index.
+    interpolated_value = np.zeros_like(integral)
+    # Line 231.
+    interpolated_value[start_float == start_index] = integrand[start_float == start_index]
+    np.logical_and(integrand[])
+    integral += np.where(start_float != start_index, )
