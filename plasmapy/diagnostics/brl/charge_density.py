0a1e41ce86b0f67e478b10c18f0fd1ac223423b4
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-10-27 20:58:08 -0500

    Add calculation for cylindrical and move integration out

--- plasmapy/diagnostics/brl/charge_density.py
+++ plasmapy/diagnostics/brl/charge_density.py
@@ -1,6 +1,6 @@
 """Using the potential, calculate the charge density."""
 import numpy as np
-
+from diagnostics.brl.integration import integrate
 from scipy.integrate import quad_vec
 from scipy.special import erfc, erfi
 
@@ -166,8 +166,10 @@ def eta_3(A, chi, x, x_B, spherical=True):
 
 
 def eta_4(
-        s1_indeces, 
+        s1_indeces,
         s2_indeces, 
+        complex_integrand_at_s1,
+        complex_integrand_at_s2,
         s_indeces, 
         ds, 
         chi, 
@@ -186,6 +188,8 @@ def eta_4(
     ----------
     s1_indeces, s2_indeces : `numpy.ndarray[float]`
         Lower and upper indeces of `s` to integrate between where `s1_indeces <= s2_indeces`.
+    complex_integrand_at_s1, complex_integrand_at_s2 : `numpy.ndarray[bool]`
+        Whether the integrand is complex at s1 or s2.
     s_indeces : `numpy.ndarray[int]`
         The `s` index at which `eta_4` is calculated.
     ds : `float`
@@ -209,94 +213,33 @@ def eta_4(
 
     Notes
     -----
+    All the terms that enter the integrand (`chi`, `x`, `eta_net`, `omega_G`, 
+    and `beta_G`) should be 1D-arrays that contain the value for all points in 
+    `s`.
+
     This is the term :math:`\eta_4(\beta_1, \beta_2)` that appears in various 
     formula in appendix E. The integration is defined in the `CAL` function on 
     page 36 of the computer program listing, appendix I.
     """
-    # TODO: Figure out how Laframboise takes care of the end point integration.
-    num_s_points = chi.size
-    s1_indeces_int = np.ceil(s1_indeces)
-    s2_indeces_int = np.minimum(np.floor(s2_indeces), num_s_points - 1)
-    max_num_s_prime_points = np.max(s2_indeces_int - s1_indeces_int)
+    if spherical:
+        # Equation (E.30).
+        alpha_G = dchi_ds / 2 + gamma * eta_net / (2 * x**3) * dx_ds
+        # Equation (E.31).
+        epsilon_G = alpha_G * np.exp(-beta_G)
+        # Equation (E.32).
+        psi_G = (beta_G[np.newaxis, :] - chi[:, np.newaxis] - omega_G[np.newaxis, :] * x[:, np.newaxis]**2)**0.5
 
-    num_points = s1_indeces_int.size
-    # Populate the integration matrix with equation (D.22) for each `s_index`.
-    integration_matrix = np.zeros((num_points, max_num_s_prime_points))
-    index_mask = np.zeros_like(integration_matrix)
-    s_index_mask = np.zeros_like(integration_matrix)
-    for point_index, (s1_index, s2_index, s_index) in enumerate(zip(s1_indeces_int, s2_indeces_int, s_indeces)):
-        index_difference = s2_index - s1_index
-        index_mask[point_index, :index_difference + 1] = np.arange(s1_index, s2_index + 1)
-        s_index_mask[point_index, :index_difference + 1] = s_index
+        all_integrands = -1 / np.pi**0.5 * epsilon_G[np.newaxis, :] * psi_G # Indexed as `[s, s']` in (E.33).
+    else:
+        # Equation (E.86).
+        alpha_G = dchi_ds + gamma * eta_net / (2 * x**3) * dx_ds
+        # Equation (E.31).
+        epsilon_G = alpha_G * np.exp(-beta_G)
+        # Equation (E.87).
+        psi_G = np.arctan((omega_G[np.newaxis, :] * x[:, np.newaxis]**2 / (beta_G[np.newaxis, :] - chi[:, np.newaxis] - omega_G[np.newaxis, :] * x[:, np.newaxis]**2))**0.5)
 
-        if index_difference == 0:
-            continue
-        elif index_difference == 1:
-            integration_matrix[point_index, 0] = 1
-        elif index_difference == 2:
-            integration_matrix[point_index, :index_difference + 1] = np.array([1, 4, 1]) / 3
-        elif index_difference == 3:
-            integration_matrix[point_index, :index_difference + 1] = np.array([3, 9, 9, 3]) / 8
-        elif index_difference == 4:
-            integration_matrix[point_index, :index_difference + 1] = np.array([9, 28, 22, 28, 9]) / 24
-        else:
-            integration_matrix[point_index, :index_difference + 1] = np.concatenate(
-                (np.array([9, 28, 23]), 24 * np.ones(index_difference - 3), np.array([23, 28, 9]))
-            ) / 24
-    integration_matrix *= ds
-
-    # Equation (E.30).
-    # alpha_G = np.zeros_like(integration_matrix)
-    # alpha_G[index_mask] = dchi_ds[index_mask] / 2 + gamma * eta_net[index_mask] / (2 * x[index_mask]**3) * dx_ds[index_mask]
-    alpha_G = dchi_ds / 2 + gamma * eta_net / (2 * x**3) * dx_ds
-    # Equation (E.31).
-    # epsilon_G = np.zeros_like(integration_matrix)
-    # epsilon_G[index_mask] = alpha_G[index_mask] * np.exp(-beta_G[index_mask])
-    epsilon_G = alpha_G * np.exp(-beta_G)
-    # Equation (E.32).
-    # psi_G = np.zeros_like(integration_matrix)
-    # psi_G[index_mask] = (beta_G[index_mask] - chi[s_index_mask] - omega_G[index_mask] * x[s_index_mask]**2)**0.5
-    psi_G = (beta_G[:, np.newaxis] - chi[np.newaxis, :] - omega_G[:, np.newaxis] * x[np.newaxis, :]**2)**0.5
-
-    integrand = epsilon_G[:, np.newaxis] * psi_G
-
-    # Equation (E.33).
-    # TODO: Fix this integration as it can't use the index_mask or s_index_mask.
-    integral = -1 / np.pi**0.5 * np.sum(integration_matrix * integrand, axis=-1)
-
-    # Now include the edges of the integral that we have not yet accounted for.
-    # TODO: Use the methods of taylor expanding near the edge points for better inclusion of the edges during integration.
-
-    # The non integer `s1_indeces > 0` because at `s[0] = 1` we are at the probe.
-    non_integer_starts = True ^ np.isclose(s1_indeces, s1_indeces_int)
-    # If the `integrand` is imaginary at the first index before `s1` then assume that the integration starts where the integrand is 0.
-    # Otherwise just do a linear interpolation between `s1 - 1` and `s1`.
-    integrand_at_start_index = integrand[s1_indeces_int[non_integer_starts]]
-    integrand_before_start_index = integrand[s1_indeces_int[non_integer_starts] - 1]
-    integrand_start = np.where(
-        np.iscomplex(integrand_before_start_index),
-        0,
-        (integrand_before_start_index + integrand_at_start_index) / 2
-    )
-    # Use the trapezoidal rule to add this contribution.
-    integral[non_integer_starts] += 0.5 * (integrand_start + integrand_at_start_index) * (s1_indeces_int - s1_indeces)[non_integer_starts] * ds
-
-    non_integer_ends = True ^ np.isclose(s2_indeces, s2_indeces_int)
-    integrand_end = np.zeros_like(non_integer_ends)
-    # `s2` may be greater than the largest `s` in our grid. Thus we'll linearly interpolate the integrand using the two previous points.
-    s2_larger_than_grid = s2_indeces > num_s_points - 1
-    integrand_end[s2_larger_than_grid] = (integrand[-1, s2_larger_than_grid] - integrand[-2, s2_larger_than_grid]) / ds * \
-        (s2_indeces[s2_larger_than_grid] - ds * (num_s_points - 1)) + integrand[-1, s2_larger_than_grid]
-
-    # If the `integrand` is imaginary at the first index after `s2` then assume that the integration starts where the integrand is 0.
-    # Otherwise just do a linear interpolation between `s2` and `s2 + 1`.
-    integrand_at_end_index = integrand[s2_indeces_int[non_integer_ends]]
-    integrand_after_end_index = integrand[s2_indeces_int[non_integer_ends] + 1]
-    integrand_end = np.where(
-        np.iscomplex(integrand_after_end_index),
-        0,
-        (integrand_after_end_index + integrand_at_end_index) / 2
-    )
-    # Use the trapezoidal rule to add this contribution.
-    integral[non_integer_ends] += 0.5 * (integrand_end + integrand_at_end_index) * (s2_indeces - s2_indeces_int)[non_integer_ends] * ds
+        all_integrands = 1 / np.pi * epsilon_G[np.newaxis, :] * psi_G
+    all_integrands = all_integrands[s_indeces]
+    
+    return integrate(all_integrands, s1_indeces, s2_indeces, complex_integrand_at_s1, complex_integrand_at_s2, ds)
 
