918d1d87545a78aea933f0c0c97ce354daa82da2
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-11-09 11:34:40 -0700

    Add initial test for complex integrand and add more options to real integral

--- plasmapy/diagnostics/brl/tests/test_integration.py
+++ plasmapy/diagnostics/brl/tests/test_integration.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pytest
 
+from itertools import product
+
 from plasmapy.diagnostics.brl import integration
 
 
@@ -59,33 +61,29 @@ class Test__integrate:
         )
 
     @pytest.mark.parametrize(
-        ("start_index", "end_index", "error"),
-        [
-            # Test many combinations of spacing between points as different formulas are used for each.
-            (2, 2, 10**-6),
-            (2, 4, 10**-6),
-            (2, 5, 10**-6),
-            (2, 6, 10**-6),
-            (2, 7, 10**-6),
-            (2, 8, 10**-6),
-            (1.3, 2, 10**-6),
-            (1.3, 3, 10**-6),
-            (1.3, 4, 10**-6),
-            (2, 2.3, 10**-6),
-            (2, 3.3, 10**-6),
-            (2, 4.3, 10**-6),
-            (1.3, 1.7, 10**-6),
-            (1.3, 2.7, 10**-6),
-            (1.3, 3.7, 10**-6),
-            (1.3, 4.7, 10**-6),
-            # The large error for the starting and ending points being one apart is due to using the trapezoidal rule for the integration.
-            (2, 3, 2 * 10**-1),
-        ],
+        ("start_index", "integration_distance"),
+        product(
+            [0, 1, 2, 7, 8, 9, 0.3, 1.3, 2.3, 6.3, 7.3, 8.3],
+            [0, 1, 2, 3, 4, 5, 6, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5],
+        ),
     )
     def test_non_complex_start_and_end_combinations(
-        self, start_index, end_index, error
+        self, start_index, integration_distance
     ):
         r"""Test integration for start and end points that aren't complex."""
+        end_index = min(self.num_points - 1, start_index + integration_distance)
+
+        # The large error for the starting and ending points being one apart is due to using an integration method of lower order than the integrand.
+        if (
+            np.isclose(end_index - start_index, 1)
+            or self.num_points - 1 - end_index < 1
+        ) and np.isclose(start_index, int(start_index)):
+            absolute_error = 2 * 10**-1
+            relative_error = 10**-2
+        else:
+            absolute_error = 10**-6
+            relative_error = 10**-5
+
         start = np.array([start_index])
         end = np.array([end_index])
 
@@ -99,7 +97,6 @@ class Test__integrate:
                 self.sample_spacing,
             ),
             (self.constant_integral_func(end) - self.constant_integral_func(start)),
-            atol=error,
         )
         assert np.isclose(
             integration.integrate(
@@ -111,5 +108,50 @@ class Test__integrate:
                 self.sample_spacing,
             ),
             (self.real_integral_func(end) - self.real_integral_func(start)),
-            atol=error,
+            atol=absolute_error,
+            rtol=relative_error,
+        )
+
+    @pytest.mark.parametrize(
+        ("start_index", "end_index", "complex_before_start", "complex_after_end"), []
+    )
+    def test_complex_start_and_end_combinations(
+        self, start_index, end_index, complex_before_start, complex_after_end
+    ):
+        r"""Test integration for start and end points that may be complex."""
+        start = np.array([start_index])
+        end = np.array([end_index])
+
+        # Construct the integrand.
+        if complex_before_start and complex_after_end:
+            integrand = (
+                (self.sample_points - start_index) ** 0.5
+                + (end_index - self.sample_points) ** 0.5
+            )[np.newaxis, :]
+
+            def integral_func(x):
+                return 2 / 3 * ((x - start_index) ** 1.5 - (end_index - x) ** 1.5)
+
+        elif complex_before_start:
+            integrand = ((self.sample_points - start_index) ** 0.5)[np.newaxis, :]
+
+            def integral_func(x):
+                return 2 / 3 * (x - start_index) ** 1.5
+
+        elif complex_after_end:
+            integrand = ((end_index - self.sample_points) ** 0.5)[np.newaxis, :]
+
+            def integral_func(x):
+                return 2 / 3 * -((end_index - x) ** 1.5)
+
+        assert np.isclose(
+            integration.integrate(
+                integrand,
+                start,
+                end,
+                np.array([complex_before_start]),
+                np.array([complex_after_end]),
+                self.sample_spacing,
+            ),
+            (integral_func(end) - integral_func(start)),
         )
