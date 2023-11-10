5e04dc81bc7972146cbc4610f7d60a0bdd283c7b
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-11-09 18:54:41 -0700

    Add all tests for complex integration

--- plasmapy/diagnostics/brl/tests/test_integration.py
+++ plasmapy/diagnostics/brl/tests/test_integration.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pytest
+import warnings
 
 from itertools import product
 
@@ -113,37 +114,72 @@ class Test__integrate:
         )
 
     @pytest.mark.parametrize(
-        ("start_index", "end_index", "complex_before_start", "complex_after_end"), []
+        ("start_index", "integration_distance", "is_complex_tuple"),
+        product(
+            [0, 1, 2, 7, 8, 9, 0.3, 1.3, 2.3, 6.3, 7.3, 8.3],
+            [0, 1, 2, 3, 4, 5, 6, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5],
+            [(True, True), (True, False), (False, True)],
+        ),
     )
     def test_complex_start_and_end_combinations(
-        self, start_index, end_index, complex_before_start, complex_after_end
+        self, start_index, integration_distance, is_complex_tuple
     ):
         r"""Test integration for start and end points that may be complex."""
+        end_index = min(self.num_points - 1, start_index + integration_distance)
+        complex_before_start, complex_after_end = is_complex_tuple
+
+        # When there are very few points between the start and end there is larger error due to the method of integration.
+        if (
+            end_index - start_index - complex_before_start - complex_after_end <= 1
+            or self.num_points - 1 - end_index < 1
+        ):
+            absolute_error = 5 * 10**-1
+            relative_error = 10**-1
+        else:
+            absolute_error = 10**-1
+            relative_error = 10**-1
+
         start = np.array([start_index])
         end = np.array([end_index])
 
         # Construct the integrand.
-        if complex_before_start and complex_after_end:
-            integrand = (
-                (self.sample_points - start_index) ** 0.5
-                + (end_index - self.sample_points) ** 0.5
-            )[np.newaxis, :]
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
 
-            def integral_func(x):
-                return 2 / 3 * ((x - start_index) ** 1.5 - (end_index - x) ** 1.5)
+            if complex_before_start and complex_after_end:
+                integrand = (
+                    (self.sample_points - start_index) ** 0.5
+                    * (end_index - self.sample_points) ** 0.5
+                )[np.newaxis, :]
 
-        elif complex_before_start:
-            integrand = ((self.sample_points - start_index) ** 0.5)[np.newaxis, :]
+                def integral_func(x):
+                    if start_index == end_index:
+                        return 0
+                    else:
+                        return (
+                            (x - start_index) ** 0.5
+                            * (end_index - x) ** 0.5
+                            * (start_index + end_index - 2 * x)
+                            - (start_index - end_index) ** 2
+                            * np.arcsin(
+                                (end_index - x) ** 0.5
+                                / (end_index - start_index) ** 0.5
+                            )
+                        ) / 4
 
-            def integral_func(x):
-                return 2 / 3 * (x - start_index) ** 1.5
+            elif complex_before_start:
+                integrand = ((self.sample_points - start_index) ** 0.5)[np.newaxis, :]
 
-        elif complex_after_end:
-            integrand = ((end_index - self.sample_points) ** 0.5)[np.newaxis, :]
+                def integral_func(x):
+                    return 2 / 3 * (x - start_index) ** 1.5
 
-            def integral_func(x):
-                return 2 / 3 * -((end_index - x) ** 1.5)
+            elif complex_after_end:
+                integrand = ((end_index - self.sample_points) ** 0.5)[np.newaxis, :]
 
+                def integral_func(x):
+                    return 2 / 3 * -((end_index - x) ** 1.5)
+
+        correct_result = integral_func(end) - integral_func(start)
         assert np.isclose(
             integration.integrate(
                 integrand,
@@ -153,5 +189,7 @@ class Test__integrate:
                 np.array([complex_after_end]),
                 self.sample_spacing,
             ),
-            (integral_func(end) - integral_func(start)),
+            correct_result,
+            atol=absolute_error,
+            rtol=relative_error,
         )
