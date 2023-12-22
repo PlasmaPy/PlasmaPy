2aae218d3497ff8ca436bc9c0c6ffc0680a49ebf
ironwod <89990448+ironwod@users.noreply.github.com>
2023-12-22 20:49:31 +0100

    Function mass_density moved from misc.py to densities.py (#2410)
    * mass_density moved to densities.py
    
    * changelog entry and minor fixes
    
    * broken link change to literal
    
    * Include alias in move
    
    ---------
    
    Co-authored-by: ironwod <ironwod@proton.me>
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>

--- plasmapy/formulary/tests/test_densities.py
+++ plasmapy/formulary/tests/test_densities.py
@@ -3,8 +3,10 @@ import astropy.units as u
 import numpy as np
 import pytest
 
-from plasmapy.formulary.densities import critical_density
+from plasmapy.formulary.densities import critical_density, mass_density
 from plasmapy.formulary.frequencies import plasma_frequency
+from plasmapy.particles import Particle
+from plasmapy.utils._pytest_helpers import assert_can_handle_nparray
 
 
 class TestCriticalDensity:
@@ -33,3 +35,56 @@ class TestCriticalDensity:
         """
 
         assert np.isclose(n_c.value, self.n_i.value)
+
+
+class Test_mass_density:
+    r"""Test the mass_density function in densities.py."""
+
+    @pytest.mark.parametrize(
+        ("args", "kwargs", "conditional"),
+        [
+            ((-1 * u.kg * u.m**-3, "He"), {}, pytest.raises(ValueError)),
+            ((-1 * u.m**-3, "He"), {}, pytest.raises(ValueError)),
+            (("not a Quantity", "He"), {}, pytest.raises(TypeError)),
+            ((1 * u.m**-3,), {}, pytest.raises(TypeError)),
+            ((1 * u.J, "He"), {}, pytest.raises(u.UnitTypeError)),
+            ((1 * u.m**-3, None), {}, pytest.raises(TypeError)),
+            (
+                (1 * u.m**-3, "He"),
+                {"z_ratio": "not a ratio"},
+                pytest.raises(TypeError),
+            ),
+        ],
+    )
+    def test_raises(self, args, kwargs, conditional):
+        with conditional:
+            mass_density(*args, **kwargs)
+
+    @pytest.mark.parametrize(
+        ("args", "kwargs", "expected"),
+        [
+            ((1.0 * u.g * u.m**-3, ""), {}, 1.0e-3 * u.kg * u.m**-3),
+            ((5.0e12 * u.cm**-3, "He"), {}, 3.32323849e-8 * u.kg * u.m**-3),
+            (
+                (5.0e12 * u.cm**-3, Particle("He")),
+                {},
+                3.32323849e-8 * u.kg * u.m**-3,
+            ),
+            (
+                (5.0e12 * u.cm**-3, "He"),
+                {"z_ratio": 0.5},
+                1.66161925e-08 * u.kg * u.m**-3,
+            ),
+            (
+                (5.0e12 * u.cm**-3, "He"),
+                {"z_ratio": -0.5},
+                1.66161925e-08 * u.kg * u.m**-3,
+            ),
+        ],
+    )
+    def test_values(self, args, kwargs, expected):
+        assert np.isclose(mass_density(*args, **kwargs), expected)
+
+    def test_handle_nparrays(self):
+        """Test for ability to handle numpy array quantities"""
+        assert_can_handle_nparray(mass_density)
