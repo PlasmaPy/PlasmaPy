ac7befa4caf3946df26a734d40e3b3e247e73b3c
Nick Murphy <namurphy@cfa.harvard.edu>
2023-12-27 13:58:04 -0500

    Update tests of particle_input with varpos args

--- plasmapy/particles/tests/test_decorators.py
+++ plasmapy/particles/tests/test_decorators.py
@@ -656,14 +656,41 @@ def test_particle_input_warning_for_float_z_mean():
     assert u.isclose(Z, z_mean)
 
 
-@particle_input
-def function_with_positional_arguments(*args, particle: ParticleLike = None):
-    return args, particle
+def test_particle_input_with_var_positional_arguments():
+    """
+    Test that |particle_input| works with functions that accept
+    variadic positional arguments and keyword arguments.
+    """
 
+    @particle_input
+    def function_with_var_positional_arguments(*args, particle: ParticleLike = None):
+        return args, particle
 
-def test_particle_input_with_variadic_positional_arguments():
-    args = [5, 6, 7]
+    args = (5, 6, 7)
     particle = "p+"
-    expected = args.append(particle)
-    actual = function_with_positional_arguments(*args, particle=particle)
+    expected = (args, Particle(particle))
+
+    actual = function_with_var_positional_arguments(*args, particle=particle)
+
+    assert actual == expected
+
+
+@pytest.mark.xfail(reason="See issue #2150.")
+def test_particle_input_with_pos_and_var_positional_arguments():
+    """
+    Test that |particle_input| works with functions that accept
+    positional followed by variadic positional arguments.
+    """
+
+    @particle_input
+    def function_with_pos_and_var_positional_arguments(
+        a, *args, particle: ParticleLike = None
+    ):
+        return args, particle
+
+    a = 1
+    args = (5, 6, 7)
+    particle = "p+"
+    expected = (a, args, Particle(particle))
+    actual = function_with_pos_and_var_positional_arguments(a, *args, particle=particle)
     assert actual == expected
