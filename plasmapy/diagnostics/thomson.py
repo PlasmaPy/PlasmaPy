b3052feb91dd98b4ccf4abf7ba41aeecd2fc4425
bryancfoo <68606269+bryancfoo@users.noreply.github.com>
2024-01-03 14:44:31 -0500

    Update thomson.py

--- plasmapy/diagnostics/thomson.py
+++ plasmapy/diagnostics/thomson.py
@@ -763,7 +763,7 @@ def spectral_density_model(  # noqa: C901, PLR0912, PLR0915
         - ``"instr_func"`` : A function that takes a wavelength
           |Quantity| array and returns a spectrometer instrument
           function as an `~numpy.ndarray`.
-        - ``"notch"`` : A wavelength range or array of several wavelength
+        - ``"notch"`` : A wavelength range or array of multiple wavelength
           ranges over which the spectral density is set to 0.
 
         These quantities cannot be varied during the fit.
