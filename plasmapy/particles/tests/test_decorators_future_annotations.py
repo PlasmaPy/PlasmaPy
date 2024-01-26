fd89bda6206bd8b1368e6f442745eda7f64203c0
Nick Murphy <namurphy@cfa.harvard.edu>
2024-01-26 17:48:03 -0500

    Make failing tests fail the way they're supposed to

--- plasmapy/particles/tests/test_decorators_future_annotations.py
+++ plasmapy/particles/tests/test_decorators_future_annotations.py
@@ -1,30 +1,43 @@
+"""Tests for ``@particle_input`` with delayed evaluation of annotations."""
+
+# These tests must be in their own file because the `from __future__`
+# import must be at the top.
 from __future__ import annotations
 
 from plasmapy.particles import Particle, ParticleLike, particle_input
 
 
 @particle_input
-def function_decorated_with_particle_input(x, particle: ParticleLike):
+def function_decorated_with_particle_input(particle: ParticleLike):
     return particle
 
 
-class SomeClass:
+class DecoratedClass:
     @particle_input
     def __init__(self, particle: ParticleLike):
         self.particle = particle
 
+
+class UndecoratedClass:
     @particle_input
-    def f(x, particle: ParticleLike):
+    def decorated_method(self, particle: ParticleLike):
         return particle
 
 
 def test_particle_input_from_future_import_annotations_function():
-    particle = f(None, "p+")
+    particle = function_decorated_with_particle_input("p+")
     assert particle == Particle("p+")
 
 
-def test_particle_input_from_future_import_annotations_method():
-    instance = SomeClass(1, "p+")
+def test_particle_input_from_future_import_annotations_instantiation():
+    instance = DecoratedClass("p+")
 
     assert isinstance(instance.particle, Particle)
-    assert instance.particle == "p+"
+    assert instance.particle == Particle("p+")
+
+
+def test_particle_input_from_future_import_annotations_method():
+    instance = UndecoratedClass()
+    result = instance.decorated_method(particle="p+")
+    assert isinstance(result, Particle)
+    assert result == Particle("p+")
