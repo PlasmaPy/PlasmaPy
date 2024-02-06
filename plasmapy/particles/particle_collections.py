9313eb1cbe058fa9c90636295662c098430640c4
Nick Murphy <namurphy@cfa.harvard.edu>
2024-02-05 20:51:51 -0500

    Upgrade more type hints

--- plasmapy/particles/particle_collections.py
+++ plasmapy/particles/particle_collections.py
@@ -5,7 +5,6 @@ __all__ = ["ParticleList", "ParticleListLike"]
 import collections
 import contextlib
 from collections.abc import Callable, Iterable, Sequence
-from typing import Union
 
 import astropy.units as u
 import numpy as np
@@ -574,7 +573,7 @@ Raises
 
 ParticleList.reverse.__doc__ = """Reverse the |ParticleList| in place."""
 
-ParticleListLike = Union[ParticleList, Sequence[ParticleLike]]
+ParticleListLike = ParticleList | Sequence[ParticleLike]
 
 ParticleListLike.__doc__ = r"""
 An `object` is |particle-list-like| if it can be identified as a
