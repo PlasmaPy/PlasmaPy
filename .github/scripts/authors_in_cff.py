ce7e488f35796b3adafc46c634c64371ab3a28d6
Nick Murphy <namurphy@cfa.harvard.edu>
2024-02-22 19:01:58 -0500

    Update info message

--- .github/scripts/authors_in_cff.py
+++ .github/scripts/authors_in_cff.py
@@ -16,21 +16,21 @@ REPO = os.getenv("GITHUB_REPOSITORY")
 excluded_authors = {"dependabot[bot]", "pre-commit-ci[bot]", "sourcery-ai"}
 
 
-def get_pr_authors() -> set[str]:
+def get_pr_authors() -> list[str]:
     """Get the GitHub usernames of the pull request."""
     url = f"https://api.github.com/repos/{REPO}/pulls/{PR_NUMBER}/commits"
     headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
     response = requests.get(url, headers=headers, timeout=15)
     response.raise_for_status()
 
-    authors = {
+    authors = [
         commit["author"]["login"] for commit in response.json() if commit["author"]
-    }
+    ]
 
     return authors - excluded_authors
 
 
-def find_missing_github_usernames(authors: set[str]) -> list[str]:
+def find_missing_github_usernames(authors: list[str]) -> list[str]:
     """Verify that all authors of a PR are included in :file:`CITATION.cff`."""
     with pathlib.Path("CITATION.cff").open() as file:
         lines = file.read()
@@ -51,21 +51,17 @@ def main():
         logging.info(msg)
         sys.exit(0)
 
-    branch_name = os.getenv("GITHUB_HEAD_REF")
-
-    if len(missing_github_usernames) == 1:
-        alias_field = missing_github_usernames[0]
-    else:
-        alias_field = "<GitHub username>"
+    branch = os.getenv("GITHUB_HEAD_REF")
+    username = missing_github_usernames[0]
 
     instructions_to_add_author = f"""
 
 To ensure that you get credit for your contribution, please add the
 following authors to CITATION.cff: {", ".join(missing_github_usernames)!r}
 
-This file can be edited directly on GitHub at:
+This file can be edited at:
 
-https://github.com/{REPO}/edit/{branch_name}/CITATION.cff
+https://github.com/{username}/PlasmaPy/edit/{branch}/CITATION.cff
 
 Each entry should be of the form:
 
@@ -73,7 +69,7 @@ Each entry should be of the form:
   family-names: <family names>
   affiliation: <affiliation>
   orcid: https://orcid.org/<ORCiD number>
-  alias: {alias_field}
+  alias: {username}
   email: <email address>
 
 All fields are optional except "alias", which is the GitHub username.
