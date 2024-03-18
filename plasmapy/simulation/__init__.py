31d95300986f37f80f469807625f65682b78101a
JaydenR2305 <109694185+jaydenr2305@users.noreply.github.com>
2024-03-17 21:12:33 -0400

    Documentation adjustments

--- plasmapy/simulation/__init__.py
+++ plasmapy/simulation/__init__.py
@@ -1,11 +1,3 @@
-"""
-Module containing plasma simulation tools.
-
-.. attention::
-
-   |expect-api-changes|
-"""
-
 __all__ = ["AbstractSimulation", "AbstractTimeDependentSimulation", "particle_tracker"]
 
 from plasmapy.simulation import particle_tracker
