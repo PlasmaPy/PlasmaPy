507793fcc8d614fa4f1670c226c7a98143bc924f
Nick Murphy <namurphy@cfa.harvard.edu>
2024-03-27 18:24:16 -0400

    Consolidate conftest.py in tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 """Adds custom functionality to pytest."""
 
+import os
+
+from hypothesis import Verbosity, settings
+
+
 # Force MPL to use non-gui backends for testing.
 try:
     import matplotlib as mpl
@@ -22,3 +27,9 @@ def pytest_configure(config) -> None:  # coverage: ignore
             "or exclusively executed with 'pytest -m slow'."
         ),
     )
+
+
+settings.register_profile("ci", max_examples=1000)
+settings.register_profile("dev", max_examples=10)
+settings.register_profile("debug", max_examples=10, verbosity=Verbosity.verbose)
+settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
