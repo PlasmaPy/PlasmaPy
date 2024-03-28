7e770b5c900e5200ea0183432bfe96076b98e5be
Nick Murphy <namurphy@cfa.harvard.edu>
2024-03-27 21:28:41 -0400

    Define @pytest.mark.slow in pyproject.toml

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,6 @@ import os
 
 from hypothesis import Verbosity, settings
 
-
-def pytest_configure(config) -> None:  # coverage: ignore
-    """Adds @pytest.mark.slow annotation for marking slow tests for optional skipping."""
-    config.addinivalue_line(
-        "markers",
-        (
-            "slow: mark test as slow to run. Used to mark tests that execute in more than 1000x the "
-            "time of the median test. Tests marked slow may be skipped with 'pytest -m 'not slow'' "
-            "or exclusively executed with 'pytest -m slow'."
-        ),
-    )
-
-
 settings.register_profile("ci", max_examples=1000)
 settings.register_profile("dev", max_examples=10)
 settings.register_profile("debug", max_examples=10, verbosity=Verbosity.verbose)
