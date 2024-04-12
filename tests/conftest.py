877d9026229c87f441218a4ede3aefaac3e1da64
Nick Murphy <namurphy@cfa.harvard.edu>
2024-04-12 11:45:05 -0700

    Put back using Agg for matplotlib in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,14 @@ settings.register_profile("ci", max_examples=1000)
 settings.register_profile("dev", max_examples=10)
 settings.register_profile("debug", max_examples=10, verbosity=Verbosity.verbose)
 settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
+
+# Force MPL to use non-gui backends for testing.
+try:
+    import matplotlib as mpl
+except ImportError:
+    pass
+else:
+    import os
+
+    if "PLASMAPY_PLOT_TESTS" not in os.environ:
+        mpl.use("Agg")
