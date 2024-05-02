89bfa76ac0f4441a20cd13910fdc56fbfb156b7e
Nick Murphy <namurphy@cfa.harvard.edu>
2024-05-02 15:30:45 -0400

    Move type: ignore comment

--- src/plasmapy/__init__.py
+++ src/plasmapy/__init__.py
@@ -48,8 +48,8 @@ try:
     try:
         from plasmapy._dev.scm_version import version as __version__
     except ImportError:
-        from plasmapy._version import (
-            version as __version__,  # type: ignore[import-not-found,no-redef,unused-ignore]
+        from plasmapy._version import (  # type: ignore[import-not-found,no-redef,unused-ignore]
+            version as __version__,
         )
 except Exception:  # coverage: ignore  # noqa: BLE001
     __version__ = "0.0.0"  # package is not installed
