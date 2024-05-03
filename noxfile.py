5cddbadf213c26c716425f6f113b130b8a896c99
Nick Murphy <namurphy@cfa.harvard.edu>
2024-05-03 14:28:25 -0400

    Use nox for some testing environments in GitHub workflows (#2656)
    * Allow session posargs in nox envs
    
    * Build docs with nox in CI
    
    * Add temporary intentional failure to test printing of doc troubleshooting
    
    * Update doc troubleshooting info
    
    * Remove intentional failure
    
    * Remove steps to test troubleshooting message
    
    * Revert "Remove steps to test troubleshooting message"
    
    This reverts commit fac1f475df05530e975808c5e4c4dc7c5829d589.
    
    * Use option for echo
    
    * Add `man echo`
    
    * Revert some temporary changes
    
    * Use nox env for mypy
    
    * Ignore _version
    
    * Add nox as a global substitution
    
    * Edit changelog: 2656.internal.rst
    
    * Update mypy env
    
    * Add type: ignore comment
    
    * Move type: ignore comment
    
    * Make name of action consistent with required workflow
    
    * Add noxfile.py to MANIFEST.in
    
    * Update linkcheck action
    
    * Use quiet option for linkcheck
    
    * Fix some hyperlinks
    
    * Update linkcheck settings
    
    * Fix allowed redirect
    
    For nox.thea.codes

--- noxfile.py
+++ noxfile.py
@@ -19,7 +19,7 @@ sphinx_commands = (
 )
 
 html = ("-b", "html")
-check_hyperlinks = ("-b", "linkcheck")
+check_hyperlinks = ("-b", "linkcheck", "-q")
 
 
 @nox.session(python=maxpython)
@@ -27,7 +27,7 @@ def docs(session):
     """Build documentation with Sphinx."""
     session.install(*doc_requirements)
     session.install(".")
-    session.run(*sphinx_commands, *html)
+    session.run(*sphinx_commands, *html, *session.posargs)
 
 
 @nox.session(python=maxpython)
@@ -35,22 +35,20 @@ def linkcheck(session):
     """Check hyperlinks in documentation."""
     session.install(*doc_requirements)
     session.install(".")
-    session.run(*sphinx_commands, *check_hyperlinks)
+    session.run(*sphinx_commands, *check_hyperlinks, *session.posargs)
 
 
 @nox.session(python=maxpython)
 def mypy(session):
-    """Perform static type checking."""
-    session.install("mypy >= 1.9.0", "pip")
-    session.install("-r", "requirements.txt")
-    session.run(
-        "mypy",
-        ".",
+    """Perform static type checking with mypy."""
+    mypy_command = ("mypy", ".")
+    mypy_options = (
         "--install-types",
         "--non-interactive",
         "--show-error-context",
         "--show-error-code-links",
         "--pretty",
-        "--exclude",
-        "build",
     )
+    session.install("mypy >= 1.9.0", "pip")
+    session.install("-r", "requirements.txt")
+    session.run(*mypy_command, *mypy_options, *session.posargs)
