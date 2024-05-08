336cb600083dc1f4bb199af257aef80f035b949c
Chase Davies <chase.davies@sproutsocial.com>
2024-05-08 13:11:07 -0600

    Revert pkgutil format to match others, and fix copy paste error in comment

--- src/plasmapy/particles/_ionization_energy.py
+++ src/plasmapy/particles/_ionization_energy.py
@@ -17,8 +17,8 @@ def ionization_energy_obj_hook(obj):
     """Provide an ``object_hook`` designed for `json.load` and `json.loads`."""
     return (obj["ionization energy"] * u.eV).to(u.J) if "ionization energy" in obj else obj
 
-#: Dictionary of isotope data.
+#: Dictionary of ionization energy data.
 data_about_ionization_energy = json.loads(
-    pkgutil.get_data("plasmapy.particles.data", "ionization_energy.json"),
+    pkgutil.get_data("plasmapy", "particles/data/ionization_energy.json"),
     object_hook=ionization_energy_obj_hook
 )
