0599fe8e251fcc4b4039561a5940a677d704a7dc
Nick Murphy <namurphy@cfa.harvard.edu>
2024-05-09 15:25:35 -0400

    Begin nox sessions for running tests

--- noxfile.py
+++ noxfile.py
@@ -52,3 +52,48 @@ def mypy(session):
     session.install("mypy >= 1.9.0", "pip")
     session.install("-r", "requirements.txt")
     session.run(*mypy_command, *mypy_options, *session.posargs)
+
+
+pytest = (
+    "pytest",
+    "--pyargs",
+    "--durations=5",
+    "--tb=short",
+    "-n=auto",
+    "--dist=loadfile",
+)
+
+slow = ("-m", "slow")
+skipslow = ("-m", "not slow")
+all = ()
+
+coverage = (
+    "--cov=plasmapy",
+    "--cov-config=pyproject.toml",
+    "--cov-append",
+    "--cov-report xml:coverage.xml",
+)
+
+
+@nox.session
+@nox.parametrize(
+    "test_selection",
+    [
+        nox.param(all, id="all"),
+        nox.param(skipslow, id="skipslow"),
+        nox.param(slow, id="slow"),
+    ],
+)
+@nox.parametrize(
+    "doctest_option",
+    [
+        nox.param((), id="no-doctest"),
+    ],
+)
+def tests(session, test_selection, doctest_option):
+    """Run pytest for PlasmaPy."""
+    session.install("-r", "requirements.txt")
+    session.run(*pytest, *session.posargs, *test_selection, *doctest_option)
+
+    # Make it so that doctests are run if and only if python=maxpython
+    # and the requirements files are
