f454ee08a3aa501d55cc9f4d6dad1330eeb99bb0
Nick Murphy <namurphy@cfa.harvard.edu>
2024-05-08 22:37:17 -0400

    Make it so that a session needs to be defined when running nox
    Running `nox` will now make it print out a help message along with
    the list of sessions.

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,7 @@ Documentation for nox is at: https://nox.thea.codes
 import nox
 
 nox.options.default_venv_backend = "uv|virtualenv"
-nox.options.sessions = ["requirements"]
+nox.options.sessions = []
 
 supported_python_versions = ("3.10", "3.11", "3.12")
 
