22d88841ce60f8a05f04242617d5a908ae9deb6e
Nick Murphy <namurphy@cfa.harvard.edu>
2024-05-10 16:10:07 -0400

    Update docstring & reorganize file

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,21 @@
 """
-nox is an automation tool that allows us to configure and perform tasks
-using programmable Python sessions. Each nox session is defined via a
-function decorated with ``@nox.session``. To list available sessions,
-run `nox -l`. To invoke a nox session, run `nox -s <session>`, where
-<session> is replaced with the name of the session.
+Running `nox` without arguments will run tests with the version of
+Python that `nox` is installed under, skipping slow tests. To invoke a
+nox session, run `nox -s <session>`, where <session> is replaced with
+the name of the session. To list available sessions, run `nox -l`.
 
-Documentation for nox is at: https://nox.thea.codes
+The tests can be run with the following options:
+
+* "all": run all tests
+* "skipslow": run tests, except tests decorated with `@pytest.mark.slow`
+* "cov": run all tests with code coverage checks
+* "lowest-direct" : run all tests with lowest version of direct dependencies
+
+Doctests are run only for the most recent versions of Python and
+PlasmaPy dependencies, and not when code coverage checks are performed.
 """
+
+# nox documentation: https://nox.thea.codes
 
 import sys
 
@@ -34,21 +43,9 @@ def get_requirements_filepath(
 
     Parameters
     ----------
-    category : str
-        The category for determining requirements: "docs", "tests", or
-        "all".
-
-    version : str
-        The version of Python to get the requirements for.
-
-    resolution : str, default: "highest"
-        The resolution strategy to be used by ``uv pip compile``. Other
-        options include ``"lowest-direct"`` or ``"lowest"``.
-
-    Returns
-    -------
-    str
-        The path to the requirements file.
+    category : "docs" | "tests" | "all"
+    version : "3.10" | "3.11" | "3.12"
+    resolution : "highest" (default) | "lowest-direct" | "lowest"
     """
     requirements_directory = "ci_requirements"
     specifiers = [category, version]
@@ -59,18 +56,14 @@ def get_requirements_filepath(
 
 @nox.session
 def requirements(session):
-    """Regenerate pinned requirements files used during CI."""
+    """Regenerate pinned requirements files."""
 
     session.install("uv >= 0.1.39")
 
     category_version_resolution = [
-        ("all", maxpython, "highest"),
-        ("docs", maxpython, "highest"),
-        ("tests", minpython, "lowest-direct"),
-    ]
-
-    category_version_resolution += [
-        ("tests", version, "highest") for version in supported_python_versions
+        ("tests", version, resolution)
+        for version in supported_python_versions
+        for resolution in ("highest", "lowest-direct")
     ]
 
     category_flags = {
@@ -106,6 +99,69 @@ def requirements(session):
         )
 
 
+pytest_command = (
+    "pytest",
+    "--pyargs",
+    "--durations=5",
+    "--tb=short",
+    "-n=auto",
+    "--dist=loadfile",
+)
+
+with_doctests = ("--doctest-modules", "--doctest-continue-on-failure")
+
+with_coverage = (
+    "--cov=plasmapy",
+    "--cov-config=pyproject.toml",
+    "--cov-append",
+    "--cov-report xml:coverage.xml",
+)
+
+skipslow = ("-m", "not slow")
+
+test_selections = [
+    nox.param("all", id="all"),
+    nox.param("with code coverage", id="cov"),
+    nox.param("skip slow tests", id="skipslow"),
+    nox.param("lowest-direct", id="lowest-direct"),
+]
+
+
+@nox.session(python=supported_python_versions)
+@nox.parametrize("test_selection", test_selections)
+# @nox.parametrize(
+#    ["test_selection", "coverage"],
+# )
+def tests(session, test_selection):
+    """Run tests with pytest."""
+
+    resolution = "lowest-direct" if test_selection == "lowest-direct" else "highest"
+
+    requirements = get_requirements_filepath(
+        category="tests",
+        version=session.python,
+        resolution=resolution,
+    )
+
+    options = []
+
+    if test_selection == "skipslow":
+        options += skipslow
+
+    if test_selection == "cov":
+        options += with_coverage
+
+    # Doctests are only run with the most recent versions of Python and
+    # other dependencies because there may be subtle differences in the
+    # output between different versions of Python, NumPy, and Astropy.
+    if session.python == maxpython and test_selection in {"all", "skipslow"}:
+        options += with_doctests
+
+    session.install("-r", requirements)
+    session.install(".")
+    session.run(*pytest_command, *options, *session.posargs)
+
+
 sphinx_commands = (
     "sphinx-build",
     "docs/",
@@ -122,7 +178,7 @@ docs_requirements = get_requirements_filepath(category="docs", version=maxpython
 
 @nox.session(python=maxpython)
 def docs(session):
-    """Build documentation with Sphinx."""
+    """Build documentation with most recent supported version of Python."""
     session.install("-r", docs_requirements)
     session.install(".")
     session.run(*sphinx_commands, *html, *session.posargs)
@@ -144,7 +200,7 @@ def linkcheck(session):
 
 @nox.session
 def mypy(session):
-    """Perform static type checking with mypy."""
+    """Perform static type checking."""
     mypy_command = (
         "mypy",
         ".",
@@ -157,66 +213,3 @@ def mypy(session):
     session.install("mypy >= 1.10.0", "pip")
     session.install("-r", "requirements.txt")
     session.run(*mypy_command, *session.posargs)
-
-
-pytest_command = (
-    "pytest",
-    "--pyargs",
-    "--durations=5",
-    "--tb=short",
-    "-n=auto",
-    "--dist=loadfile",
-)
-
-with_doctests = ("--doctest-modules", "--doctest-continue-on-failure")
-
-with_coverage = (
-    "--cov=plasmapy",
-    "--cov-config=pyproject.toml",
-    "--cov-append",
-    "--cov-report xml:coverage.xml",
-)
-
-all_skipslow = [nox.param((), id="all"), nox.param(("-m", "not slow"), id="skipslow")]
-
-
-@nox.session(python=supported_python_versions)
-@nox.parametrize("test_selection", all_skipslow)
-def tests(session, test_selection):
-    """
-    Run tests with pytest.
-
-    Slow tests are skipped during sessions that include `skipslow`. All
-    tests are run with code coverage during sessions that include `all`.
-    Doctests are run only for the most recent version of Python.
-    """
-
-    options = []
-    if session.python == maxpython:
-        options += with_doctests
-
-    if test_selection == "all":
-        options += with_coverage
-
-    requirements = get_requirements_filepath(
-        category="tests",
-        version=session.python,
-        resolution="highest",
-    )
-
-    session.install("-r", requirements)
-    session.install(".")
-    session.run(*pytest_command, *test_selection, *options, *session.posargs)
-
-
-@nox.session(python=minpython)
-def tests_lowest_direct(session):
-    """Run tests of Python with the lowest version of direct dependencies."""
-    requirements = get_requirements_filepath(
-        category="tests",
-        version=session.python,
-        resolution="lowest-direct",
-    )
-    session.install("-r", requirements)
-    session.install(".")
-    session.run(*pytest_command, *session.posargs)
