5b719e7b78ebfd9428df88db4038fb4541c9b516
Nick Murphy <namurphy@cfa.harvard.edu>
2024-05-28 16:05:49 -0400

    Move files to new locations

--- src/plasmapy/simulation/particle_tracker/__init__.py
+++ src/plasmapy/simulation/particle_tracker/__init__.py
@@ -1,3 +1,14 @@
-"""Particle tracking tools."""
+"""
+The particle_tracker subpackage contains functionality related to the
+particle tracker class. These include the definition of the particle
+tracker, as well as the definitions of save routines and termination
+conditions.
+"""
 
-from plasmapy.simulation.particle_tracker import particle_tracker
+__all__ = ["particle_tracker", "save_routines", "termination_conditions"]
+
+from plasmapy.simulation.particle_tracker import (
+    particle_tracker,
+    save_routines,
+    termination_conditions,
+)
