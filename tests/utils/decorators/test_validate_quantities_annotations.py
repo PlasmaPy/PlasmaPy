3a4e204547ad9f734739903feba751acda7d86e5
Nick Murphy <namurphy@cfa.harvard.edu>
2024-07-03 15:45:44 -0400

    Update type hints & type ignore comments

--- tests/utils/decorators/test_validate_quantities_annotations.py
+++ tests/utils/decorators/test_validate_quantities_annotations.py
@@ -7,11 +7,11 @@ import astropy.units as u
 from plasmapy.utils.decorators.validators import validate_quantities
 
 
-@validate_quantities
+@validate_quantities  # type: ignore[misc]
 def annotated_function(mass: u.Quantity[u.g]) -> u.Quantity[u.kg]:
     return mass
 
 
-def test_validate_quantities_postponed_annotations():
+def test_validate_quantities_postponed_annotations() -> None:
     result = annotated_function(1 * u.g)
     assert result.unit == u.kg
