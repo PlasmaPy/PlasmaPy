3aeebd26fb2b77efe22b9635e9d0636cde1cd305
Nick Murphy <namurphy@cfa.harvard.edu>
2024-07-10 18:55:59 -0400

    Update docstrings

--- docs/_changelog_index.py
+++ docs/_changelog_index.py
@@ -1,11 +1,16 @@
-"""Regenerate :file:`docs/changelog/index.rst`."""
+"""
+Regenerate the index of changelogs for releases.
+
+Only include unreleased changes via `dev.rst` if there are changelog
+entries corresponding to changes that have not been released.
+"""
 
 import pathlib
 
 repo = pathlib.Path(__file__).parent.parent
 changelog_directory = repo / "changelog"
 changelog_docs_directory = repo / "docs" / "changelog"
-changelog_index_file = changelog_docs_directory / "index2.rst"
+changelog_index_file = changelog_docs_directory / "index.rst"
 
 preamble = r""".. _changelog:
 
@@ -23,11 +28,20 @@ PlasmaPy.
 
 
 def there_are_unreleased_changes() -> bool:
+    """
+    Return `True` if there are any news fragments in the top-level
+    `changelog` directory corresponding to unreleased changes, and
+    `False` otherwise.
+    """
     path = pathlib.Path(changelog_directory)
     return bool(path.glob("[1-9]*[0-9].*.rst"))
 
 
-def get_release_notes_stems():
+def get_release_notes_stems() -> list[str]:
+    """
+    Find the stems of filenames for the changelog files corresponding to
+    releases.
+    """
     files = changelog_docs_directory.glob("[0-9]*.[1-9]*.[0-9]*.rst")
 
     stems = [file.stem for file in files]
@@ -39,7 +53,11 @@ def get_release_notes_stems():
     return sorted(stems, key=sort_key, reverse=True)
 
 
-def main():
+def main() -> None:
+    """
+    Regenerate the index file for release changelog files. Include
+    unreleased changes only when there are unreleased changes.
+    """
     changelog_index_file.open(mode="w", encoding="utf-8")
 
     with changelog_index_file.open("w") as f:
