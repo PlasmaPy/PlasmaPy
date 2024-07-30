7262c948c7a605970e9f8de2a35040dbd69de543
dschaffner <dschaffner@brynmawr.edu>
2024-07-30 16:26:29 -0400

    Create magnetic_pickup_probe.py

---
+++ src/plasmapy/diagnostics/magnetic_pickup_probe.py
@@ -0,0 +1,87 @@
+"""Defines the basic magnetic pickup probe (Bdot probe) analysis
+   
+"""
+__all__ = []
+
+from warnings import warn
+import scipy.integrate as sp
+from typing import Tuple
+import numpy as np
+
+
+import astropy.units as u
+from plasmapy.utils.decorators import validate_quantities
+from typing import Tuple
+
+
+def compute_bfield(bdot_data: np.ndarray, times: np.ndarray, loop_area: float) -> np.ndarray:
+    """
+    Returns array of the magnetic field and the corresponding time array
+
+    Parameters
+    ----------
+    bdot_data: `numpy.ndarray`
+        A data array containing voltage fluctuations from a bdot probe. The
+        array values are proportional to time changing magnetic field via Faradays Law
+
+    times: `numpy.ndarray`
+        The time series to the data collection.
+
+    loop_area: float
+        The area through which the changing flux is measured. 
+
+    Returns
+    -------
+    field_arr: `numpy.ndarray`
+        The array containing the magnetic field fluctuations. 
+
+    new_time: `numpy.ndarray`
+        The corresponding time series.   
+    """
+    if len(bdot_data) != len(times):
+        raise Exception("length of time and voltage arrays in not equal\n")
+
+    bdot_data /= loop_area
+    field_arr = sp.cumtrapz(bdot_data, times, initial=0)  # Tesla
+
+    return field_arr
+
+class Bdot:
+    """Class representing a basic magnetic pickup probe (Bdot probe)
+
+    """
+    _settings = {}
+    _signal = None
+    _time = None
+    _area = None
+
+    @validate_quantities
+    def __init__(
+        self,
+        signal: u.volt,
+        time: u.s,
+        area: u.m**2,
+        *,
+        nloops,
+        gain,
+    ):
+        self._signal = signal.value
+        self._time = time.value
+        self._area = area.value
+        self._settings = {
+            "nloops": nloops,
+            "gain": gain,
+        }
+
+    @validate_quantities
+    def calc_bfield(self) -> u.T:
+        """_summary_
+
+        """
+        bfield = compute_bfield(
+            self._signal,
+            self._time,
+            self._area,
+            #    ** self._settings,
+        )
+        return bfield * u.T
