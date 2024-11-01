b2cf3fd8670b79b91af2c1c725c474ec9c673582
Nick Murphy <namurphy@cfa.harvard.edu>
2024-10-31 20:33:00 -0400

    Update docstrings for analysis.swept_langmuir

--- src/plasmapy/analysis/swept_langmuir/__init__.py
+++ src/plasmapy/analysis/swept_langmuir/__init__.py
@@ -1,7 +1,4 @@
-"""
-Subpackage containing routines for analyzing swept Langmuir probe
-traces.
-"""
+"""Functionality for analyzing swept Langmuir probe traces."""
 
 __all__ = [
     "check_sweep",
