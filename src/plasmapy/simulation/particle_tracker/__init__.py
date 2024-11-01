b9eac988b4e9ff74b62e75b37a139bdee0487515
Nick Murphy <namurphy@cfa.harvard.edu>
2024-11-01 18:23:50 -0400

    Update docstrings in plasmapy.simulation.particle_tracker

--- src/plasmapy/simulation/particle_tracker/__init__.py
+++ src/plasmapy/simulation/particle_tracker/__init__.py
@@ -1,10 +1,9 @@
 """
-Functionality to track charged particles in electromagnetic fields.
+Tracks charged particles in electromagnetic fields.
 
 This subpackage contains functionality related to the
 |ParticleTracker| class. This functionality include the definition of
-the particle tracker as well as the definitions of save routines and
-termination conditions.
+the particle tracker, including save routines and termination conditions.
 """
 
 __all__ = ["particle_tracker", "save_routines", "termination_conditions"]
