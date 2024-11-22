6fd2d8f42cb572f67c8e786c346ed9b4ca37c829
Peter Heuer <pvheuer@gmail.com>
2024-11-22 14:02:23 -0500

    Only show detailed progressbar when verbose

--- src/plasmapy/simulation/particle_tracker/particle_tracker.py
+++ src/plasmapy/simulation/particle_tracker/particle_tracker.py
@@ -711,14 +711,15 @@ class ParticleTracker:
 
             self._push()
 
-            desc = (
-                f"Iter. {self.iteration_number},  "
-                f"Avg. Pos.=({np.nanmean(self.x[:,0]):.1e},{np.nanmean(self.x[:,1]):.1e},{np.nanmean(self.x[:,2]):.1e}) m, "
-                f"Avg. Vel.=({np.nanmean(self.v[:,0]):.1e},{np.nanmean(self.v[:,1]):.1e},{np.nanmean(self.v[:,2]):.1e}) m/s, "
-                f"dt range = ({np.min(self.dt):.1e},{np.max(self.dt):.1e}) s, "
-                f"{self.termination_condition.progress_description}"
-            )
-            pbar.set_description(desc)
+            if self.verbose:
+                desc = (
+                    f"Iter. {self.iteration_number},  "
+                    f"Avg. Pos.=({np.nanmean(self.x[:,0]):.1e},{np.nanmean(self.x[:,1]):.1e},{np.nanmean(self.x[:,2]):.1e}) m, "
+                    f"Avg. Vel.=({np.nanmean(self.v[:,0]):.1e},{np.nanmean(self.v[:,1]):.1e},{np.nanmean(self.v[:,2]):.1e}) m/s, "
+                    f"dt range = ({np.min(self.dt):.1e},{np.max(self.dt):.1e}) s, "
+                    f"{self.termination_condition.progress_description}"
+                )
+                pbar.set_description(desc)
 
             # The state of a step is saved after each time step by calling `post_push_hook`
             # The save routine may choose to do nothing with this information
