3b40963f789623a9de80b0ff28adad0c68cc093f
Cameron Kuchta <cameronkuchta97@yahoo.com>
2025-04-11 15:11:43 -0500

    Most progress on calculating eta_2 from E.45 onwards. Stuck on boundary cases.

--- src/plasmapy/diagnostics/brl/charge_density.py
+++ src/plasmapy/diagnostics/brl/charge_density.py
@@ -1,7 +1,9 @@
 """Using the potential, calculate the charge density."""
 
+import logging
+
 import numpy as np
-from scipy.integrate import quad_vec
+from scipy.integrate import quad, quad_vec
 from scipy.special import erfc, erfi
 
 from plasmapy.diagnostics.brl.integration import integrate
@@ -23,7 +25,16 @@ def _g(x):
     -------
     `numpy.ndarray`
     """
-    return np.pi**0.5 / 2 * np.exp(x**2) * erfc(x)
+    if np.any(x < 0):
+        # TODO: Should this be a `logging.warning` or a `warnings.warn`?
+        # According to this stack exchange post,
+        # https://stackoverflow.com/questions/9595009/warnings-warn-vs-logging-warning,
+        # maybe `logging.warning` is correct?
+        logging.warning(
+            "The argument `x` is negative. The result may be incorrect and negative `x` is not expected theoretically."
+        )
+
+    return np.where(erfc(x) == 0, 0, np.pi**0.5 / 2 * np.exp(x**2) * erfc(x))
 
 
 def eta_1(A, chi, spherical=True):
@@ -50,32 +61,49 @@ def eta_1(A, chi, spherical=True):
     -----
     This is the term :math:`\eta_1(A)` that appears in various formula in appendix E.
     """
+    if np.any(A < chi):
+        raise ValueError("`A` must be greater than or equal to `chi`.")
+
+    if np.any(A < 0):
+        raise ValueError("`A` must be greater than or equal to 0.")
+
     if spherical:
         # Equation (E.23).
-        return -np.exp(-A) / np.pi**0.5 * ((A - chi) ** 0.5 + _g((A - chi) ** 0.5))
+        return np.where(
+            A == np.inf,
+            0,
+            -np.exp(-A) / np.pi**0.5 * ((A - chi) ** 0.5 + _g((A - chi) ** 0.5)),
+        )
     else:
         # Equation (E.10).
-        return 0
+        return np.zeros_like(A)
 
 
-def eta_2(A, chi, chi_p, x, spherical=True):
+def eta_2(A, chi, chi_p, x, spherical=True):  # noqa: C901, PLR0912
     r"""
-    Calculate the contribution to the charge density between the region where particles are absorbed by the probe and non-striking particles exist.
+    Calculate the contribution to the charge density between the region where
+    particles are absorbed by the probe and non-striking particles exist.
 
     This is the straight line given by the equation :math:`\beta = \chi_p + \Omega`.
+    :math:`\kappa` is the value of :math:`\beta` where
+    :math:`\beta = \chi_p + \Omega` and :math:`\beta = \chi + x^2 \Omega` meet.
 
     Parameters
     ----------
     A : `numpy.ndarray`
         Lower bound of the integral. Also, `A >= kappa` as defined in (E.3).
     chi : `numpy.ndarray`
-        The normalized potential as defined in `~plasmapy.diagnostics.brl.normalizations.get_normalized_potential`.
+        The normalized potential as defined in
+        `~plasmapy.diagnostics.brl.normalizations.get_normalized_potential`.
     chi_p : `float`
-        The normalized potential of the probe as defined in `~plasmapy.diagnostics.brl.normalizations.get_normalized_potential`.
+        The normalized potential of the probe as defined in
+        `~plasmapy.diagnostics.brl.normalizations.get_normalized_potential`.
     x : `numpy.ndarray`
-        Normalized inverse radius calculated from `~plasmapy.diagnostics.brl.net_spacing.get_x_and_dx_ds`.
+        Normalized inverse radius calculated from
+        `~plasmapy.diagnostics.brl.net_spacing.get_x_and_dx_ds`.
     spherical : `bool`, optional
-        If `True` the probe will be treated as spherical. If `False` then the probe is cylindrical. Default is `True`.
+        If `True` the probe will be treated as spherical. If `False` then the
+        probe is cylindrical. Default is `True`.
 
     Returns
     -------
@@ -84,49 +112,123 @@ def eta_2(A, chi, chi_p, x, spherical=True):
     Notes
     -----
     This is the term :math:`\eta_2(A)` that appears in various formula in appendix E.
+
+    If `x == 1` then `eta_2` is the same as `eta_5`.
     """
-    kappa = (chi - x**2 * chi_p) / (1 - x**2)
+    if np.any(x == 1):
+        x_is_one_indices = np.nonzero(x == 1)[0]
+        x_is_not_one_indices = np.nonzero(x != 1)[0]
+    else:
+        x_is_one_indices = np.array([])
+        x_is_not_one_indices = np.arange(x.size)
+
+    chi_subset = chi[x_is_not_one_indices]
+    A_subset = A[x_is_not_one_indices]
+    x_subset = x[x_is_not_one_indices]
+
+    kappa = (chi_subset - x_subset**2 * chi_p) / (1 - x_subset**2)
+
+    if np.any(A_subset < kappa):
+        raise ValueError("`A` must be greater than or equal to `kappa`.")
+    if np.any(A_subset < chi_p):
+        raise ValueError("`A` must be greater than or equal to `chi_p`.")
+
     if spherical:
         # Equation (E.25)
-        return (
-            -((1 - x**2) ** 0.5)
-            * np.exp(-A)
+        eta_2_subset = (
+            -((1 - x_subset**2) ** 0.5)
+            * np.exp(-A_subset)
             / np.pi**0.5
-            * ((A - kappa) ** 0.5 + _g(A - kappa**0.5))
+            * ((A_subset - kappa) ** 0.5 + _g((A_subset - kappa) ** 0.5))
         )
     else:
         # TODO: Change this to use the approximations given by equations (E.45) - (E.65).
+        # TODO: Currently this is facing issues when chi == chi_p == 0. This will only happen when the probe potential is the same as the plasma potential.
+        if np.any(chi_subset == chi_p) and chi_p == 0:
+            raise ValueError(
+                "The probe potential is the same as the plasma potential. The current implementation of eta_2 breaks down in this case."
+            )
 
         # Variables defined in (E.45).
         tau = (kappa - chi_p) / 2
-        mu = np.max(kappa, chi_p) - tau
-        theta = chi - tau
-        B = A - tau
-
-        # Integrand from (E.59). (E.59) is a formula that is ready to integrate numerically.
-        def integrand(omega):
-            return 1 / ((-np.log(omega) - theta) * (np.log(omega) ** 2 - mu**2) ** 0.5)
+        mu = np.maximum(kappa, chi_p) - tau
+        theta = chi_subset - tau
+        B = A_subset - tau
 
         # Evaluate H1 from (E.59).
-        H1 = (
-            np.exp(-tau)
-            * quad_vec(
-                lambda omega: (
-                    (-np.log(omega) - theta) * ((np.log(omega)) ** 2 - mu**2) ** 0.5
+        H1 = np.zeros_like(A_subset)
+
+        # At this point we don't need to worry about `x == 1` because we've already handled it.
+        arctan_term = np.arctan(
+            (
+                x_subset**2
+                / (1 - x_subset**2)
+                * np.where(
+                    A_subset == kappa, np.inf, (A_subset - chi_p) / (A_subset - kappa)
                 )
-                ** -1,
-                0,
-                np.exp(-B),
-            )[0]
+            )
+            ** 0.5
         )
 
+        for i, (tau_value, B_value, theta_value, mu_value) in enumerate(
+            zip(tau, B, theta, mu, strict=False)
+        ):
+            if A_subset[i] == chi_p:
+                # Equation (E.60) is ready to integrate.
+                if mu_value == 0 and theta_value == 0:
+                    args = ()
+
+                    def integrand(z):
+                        lambda_value = 1
+                        return 1 / (np.cosh(z) - 1 + 1 / lambda_value)
+                else:
+                    args = (mu_value, theta_value)
+
+                    def integrand(z, mu_value, theta_value):
+                        lambda_value = mu_value / (mu_value - theta_value)
+                        return np.exp(
+                            -mu_value * (np.cosh(z) - 1 + 1 / lambda_value)
+                        ) / (np.cosh(z) - 1 + 1 / lambda_value)
+
+                # I think the issue is that when mu == 0 then the division gives inf.
+                # Also I need to double check that my interpretation of the equations are correct by using Mathematica.
+                # I think that the mu stuff may be figured out if we do the division later on when we are summing stuff.
+                H1[i] = (
+                    np.exp(-chi_subset[i])
+                    / mu_value
+                    * quad(integrand, 0, np.inf, args=args)[0]
+                )
+                # From the sentence between (E.59) and (E.60) the arctan term drops out.
+                # TODO: Check if this is correct.
+                arctan_term[i] = 0
+            else:
+                # Integrand from (E.59). (E.59) is a formula that is ready to integrate numerically and
+                # works for large values of `mu`.
+                args = (mu_value, theta_value)
+
+                def integrand(omega, mu_value, theta_value):
+                    return 1 / (
+                        (-np.log(omega) - theta_value)
+                        * abs(np.log(omega) ** 2 - mu_value**2) ** 0.5
+                    )
+
+                H1[i] = (
+                    np.exp(-tau_value) * quad(integrand, 0, np.exp(-B_value), args)[0]
+                )
+
         # Equation (E.44)
-        return (
-            np.exp(-A)
-            / np.pi
-            * np.arctan((x**2 / (1 - x**2) * (A - chi_p) / (A - kappa)) ** 0.5)
-            + x / (1 - x**2) ** 0.5 * (chi_p - chi) / (2 * np.pi) * H1
+        eta_2_subset = np.exp(-A_subset) / np.pi * arctan_term + x_subset / (
+            1 - x_subset**2
+        ) ** 0.5 * (chi_p - chi_subset) / (2 * np.pi) * np.where(
+            chi_subset == chi_p, 0, H1
         )
+
+    eta_2 = np.zeros_like(A)
+    eta_2[x_is_not_one_indices] = eta_2_subset
+    if x_is_one_indices.size > 0:
+        eta_2[x_is_one_indices] = eta_5(A[x_is_one_indices], spherical=spherical)
+
+    return eta_2
 
 
 def eta_3(A, chi, x, x_B, spherical=True):
@@ -176,11 +278,11 @@ def eta_3(A, chi, x, x_B, spherical=True):
 
 
 def eta_4(
-    s1_indeces,
-    s2_indeces,
+    s1_indices,
+    s2_indices,
     complex_integrand_at_s1,
     complex_integrand_at_s2,
-    s_indeces,
+    s_indices,
     ds,
     chi,
     dchi_ds,
@@ -196,11 +298,11 @@ def eta_4(
 
     Parameters
     ----------
-    s1_indeces, s2_indeces : `numpy.ndarray[float]`
-        Lower and upper indices of `s` to integrate between where `s1_indeces <= s2_indeces`.
+    s1_indices, s2_indices : `numpy.ndarray[float]`
+        Lower and upper indices of `s` to integrate between where `s1_indices <= s2_indices`.
     complex_integrand_at_s1, complex_integrand_at_s2 : `numpy.ndarray[bool]`
         Whether the integrand is complex at s1 or s2.
-    s_indeces : `numpy.ndarray[int]`
+    s_indices : `numpy.ndarray[int]`
         The `s` index at which `eta_4` is calculated.
     ds : `float`
         Spacing between any two points in `s`.
@@ -266,12 +368,12 @@ def eta_4(
         )
 
         all_integrands = 1 / np.pi * epsilon_G[np.newaxis, :] * psi_G
-    all_integrands = all_integrands[s_indeces]
+    all_integrands = all_integrands[s_indices]
 
     return integrate(
         all_integrands,
-        s1_indeces,
-        s2_indeces,
+        s1_indices,
+        s2_indices,
         complex_integrand_at_s1,
         complex_integrand_at_s2,
         ds,
@@ -432,12 +534,12 @@ def delta_function_charge_density(chi, x, omega_G, beta_G, spherical=True):
     # just before crossing. We only need crossings in which
     # `beta_G[index] < beta_M < beta_G[index + 1]`. This is because these
     # crossings are the ones that will globally limit omega.
-    crossing_indeces = np.nonzero(
+    crossing_indices = np.nonzero(
         np.logical_and(beta_G[:-1] < beta_M, beta_G[1:] > beta_M)
     )[0]
     # If there are any crossings then find the omega that corresponds to each of these crossings.
-    if crossing_indeces.size > 0:
-        for crossing_index in crossing_indeces:
+    if crossing_indices.size > 0:
+        for crossing_index in crossing_indices:
             crossed_omega_G = estimate_omega_M(beta_M, crossing_index, beta_G, omega_G)
             # Apply the global boundary of the extrema.
             no_particle_omega_boundary[: crossing_index + 1] = np.min(
