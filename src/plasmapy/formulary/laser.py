acddef2e2794330c2794d1fa5f60aafd51def017
Mahlet Getahun <212391704+Getahun-yw-Mahl@users.noreply.github.com>
2025-06-02 10:24:17 -0400

    rename and add alias to function

--- src/plasmapy/formulary/laser.py
+++ src/plasmapy/formulary/laser.py
@@ -7,8 +7,9 @@ Functions for calculating quantities associated with laser pulses.
 """
 
 __all__ = [
-    "E0",
+    "electric_field_amplitude",
 ]
+__aliases__ = ["E0_"]
 
 import astropy.units as u
 import numpy as np
@@ -20,14 +21,14 @@ from plasmapy.utils.decorators import validate_quantities
 @validate_quantities(
     intensity={"can_be_negative": False},
 )
-def E0(
+def electric_field_amplitude(
     intensity: u.Quantity[u.watt / u.m**2],
 ) -> u.Quantity[u.V / u.m]:
     r"""
     Calculate the electric field amplitude :math:`E_0` from the intensity :math:`I`
     of a laser.
 
-    The electric field amplitude of an electromagnetic wave in vacuum
+    The electric field amplitude of an electromagnetic plane wave in vacuum
     is calculated using:
 
     .. math::
@@ -36,10 +37,12 @@ def E0(
     where :math:`c` is the speed of light and
     :math:`ε_0` is the permittivity of free space.
 
+    **Aliases:** `E0_`
+
     Parameters
     ----------
     intensity : `~astropy.units.Quantity`
-        Intensity of the laser pulse (convertible to u.Watts/u.m**2).
+        Intensity of the laser pulse (convertible to W / m\:sup:`2`).
 
     Returns
     -------
@@ -53,9 +56,13 @@ def E0(
     Examples
     --------
     >>> import astropy.units as u
-    >>> E0(1e-3 * u.watt / u.m**2)  # Electric Field Strength
+    >>> electric_field_ampluitude(1e-3 * u.watt / u.m**2)  # Electric Field Strength
     <Quantity 0.8680211 V / m>
     """
 
     E = np.sqrt((2 * intensity) / (c * eps0))
     return E.to(u.V / u.m)
+
+
+E0_ = electric_field_amplitude
+"""Alias to `~plasmapy.formulary.laser.electric_field_amplitude`."""
--- tests/formulary/test_laser.py
+++ tests/formulary/test_laser.py
@@ -5,9 +5,7 @@ import numpy as np
 import pytest
 from astropy.tests.helper import assert_quantity_allclose
 
-from plasmapy.formulary.laser import (
-    E0,
-)
+from plasmapy.formulary.laser import E0_, electric_field_amplitude
 
 
 @pytest.mark.parametrize(
@@ -18,9 +16,21 @@ from plasmapy.formulary.laser import (
         (np.nan * u.watt / u.m**2, np.nan * u.V / u.m),
     ],
 )
+
+@pytest.mark.parametrize(
+    ("alias", "parent"),
+    [
+        (E0_, electric_field_amplitude),
+    ],
+)
+
+def test_aliases(alias, parent) -> None:
+    """Test all aliases defined in laser.py"""
+    assert alias is parent
+
 @pytest.mark.filterwarnings("ignore::astropy.units.UnitsWarning")
-def test_E0(intensity, expected) -> None:
-    result = E0(intensity=intensity)
+def test_electric_field_amplitude(intensity, expected) -> None:
+    result = electric_field_amplitude(intensity=intensity)
     assert_quantity_allclose(result, expected, rtol=1e-6, equal_nan=True, verbose=True)
     assert result.unit == u.V / u.m
 
@@ -32,9 +42,9 @@ def test_E0(intensity, expected) -> None:
         (1 * u.kg, u.UnitTypeError),
     ],
 )
-def test_E0_errors(intensity, expected) -> None:
+def test_electric_field_amplitude_errors(intensity, expected) -> None:
     with pytest.raises(expected):
-        E0(intensity=intensity)
+        electric_field_amplitude(intensity=intensity)
 
 
 @pytest.mark.parametrize(
@@ -43,6 +53,6 @@ def test_E0_errors(intensity, expected) -> None:
         (5, u.UnitsWarning),
     ],
 )
-def test_E0_warnings(intensity, expected_warning) -> None:
+def test_electric_field_amplitude_warnings(intensity, expected_warning) -> None:
     with pytest.warns(expected_warning):
-        E0(intensity=intensity)
+        electric_field_amplitude(intensity=intensity)
