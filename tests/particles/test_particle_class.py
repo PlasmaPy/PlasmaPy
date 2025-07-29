9964dc3d16d7d71483d24abc34aadedeffecd141
Nick Murphy <namurphy@cfa.harvard.edu>
2025-07-29 14:12:24 -0400

    Add test case for np.integer supplied to Particle

--- tests/particles/test_particle_class.py
+++ tests/particles/test_particle_class.py
@@ -510,6 +510,14 @@ test_Particle_table = [
             "ionic_symbol": "C-14 3+",
         },
     ),
+    (
+        np.array([26])[0],  # see issue #3044
+        {},
+        {
+            "symbol": "Fe",
+            "atomic_number": 26,
+        }
+    )
 ]
 
 
