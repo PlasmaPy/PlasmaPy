7f4a3371e7d25580685c27d0c6779f9426cdc235
Erik Everson <eeverson@gmail.com>
2025-07-30 12:21:01 -0700

    force data_size to be at least 3 elements

--- src/plasmapy/analysis/swept_langmuir/plasma_potential.py
+++ src/plasmapy/analysis/swept_langmuir/plasma_potential.py
@@ -198,10 +198,10 @@ def find_didv_peak(  # noqa: C901, PLR0912
     _slice = _condition_voltage_window(voltage, voltage_window)
     rtn_extras["data_slice"] = _slice
     data_size = len(_slice.indices(voltage.size))
-    if data_size <= 1:
+    if data_size < 3:
         raise ValueError(
             f"The specified voltage_window ({voltage_window}) would result "
-            f"in a null window or a 1-element window."
+            f"in a null window or a window less than 3-elements."
         )
 
     # define starting savgol windows
