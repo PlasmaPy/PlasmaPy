86ddd77559bd5e5c5e277cb6de44735d9644a56a
Nick Murphy <namurphy@cfa.harvard.edu>
2025-07-30 14:43:16 -0400

    Make plasma calculator main() raise error

---
+++ src/plasmapy/utils/calculator/__init__.py
@@ -0,0 +1,14 @@
+"""
+Script and utilities to launch the plasma calculator.
+
+.. important::
+   The plasma calculator has been removed from PlasmaPy
+   so that it can be included in a standalone package.
+"""
+
+
+def main():
+    """Plasma calculator."""
+    raise RuntimeError(
+        "The plasma calculator has been removed."
+    )
