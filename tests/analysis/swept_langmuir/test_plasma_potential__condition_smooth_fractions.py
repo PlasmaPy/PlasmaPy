65255afea27d3265811cad4c0a3f9f19b6f0265b
Erik <eteverson@gmail.com>
2025-09-24 15:57:07 -0700

    create test_plasma_potential__condition_smooth_fractions.py and initialize with raises tests

---
+++ tests/analysis/swept_langmuir/test_plasma_potential__condition_smooth_fractions.py
@@ -0,0 +1,41 @@
+"""
+Tests for
+`plasmapy.analysis.swept_langmuir.plasma_potential._condition_smooth_fractions`.
+"""
+
+import numpy as np
+import pytest
+
+from plasmapy.analysis.swept_langmuir.plasma_potential import _condition_smooth_fractions
+
+
+class TestConditionSmoothFractions:
+    @pytest.mark.parametrize(
+        ("_raises", "smooth_fractions", "data_size"),
+        [
+            # smooth_fractions is not a list-like or None
+            (pytest.raises(TypeError), {"one": 1, "two": 2}, 10),
+            (pytest.raises(TypeError), "invalid fraction", 10),
+            (pytest.raises(TypeError), 5.0, 10),
+            # smooth_fractions is not 1D
+            (pytest.raises(ValueError), [[1, 2], [1, 2]], 10),
+            (pytest.raises(ValueError), np.zeros((2, 5)), 10),
+            # smooth_fractions elements are NOT floats
+            (pytest.raises(ValueError), [1, 2, 3], 10),
+            (pytest.raises(ValueError), ["one", 2., "three"], 10),
+            # smooth_fractions is out of range.. (0, 1]
+            (pytest.raises(ValueError), [-5.0], 10),
+            (pytest.raises(ValueError), [0.0], 10),
+            (pytest.raises(ValueError), [5.0], 10),
+            (pytest.raises(ValueError), [0, 0.1, .9, 1.0], 10),
+            (pytest.raises(ValueError), [-0.5, 0.1, .9, 1.0], 10),
+            (pytest.raises(ValueError), [-0.5, 0, .9, 1.0], 10),
+            (pytest.raises(ValueError), [0.1, .9, 1.1], 10),
+            (pytest.raises(ValueError), [0.1, .9, 20.0], 10),
+            (pytest.raises(ValueError), [0, 0.1, .9, 1.1], 10),
+            (pytest.raises(ValueError), [-20.0, 0, 0.1, .9, 1.1], 10),
+        ],
+    )
+    def test_raises(self, _raises, smooth_fractions, data_size):
+        with _raises:
+            _condition_smooth_fractions(smooth_fractions, data_size)
