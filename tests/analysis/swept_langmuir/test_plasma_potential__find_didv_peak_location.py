0d8612eeeae639becc69d9dd0c5b0d932722b7f0
Erik <eteverson@gmail.com>
2025-09-25 10:04:22 -0700

    initialized TestFinddIdVPeakLocation

--- tests/analysis/swept_langmuir/test_plasma_potential__find_didv_peak_location.py
+++ tests/analysis/swept_langmuir/test_plasma_potential__find_didv_peak_location.py
@@ -51,3 +51,81 @@ def test_didv_namedtuple_index_field_mapping(index, field_name):
 
     assert extras[index] == getattr(extras, field_name)
 
+
+class TestFinddIdVPeakLocation:
+
+    @pytest.mark.parametrize(
+        ("helper", "used_callable"),
+        [
+            (sla.helpers.check_sweep, sla.plasma_potential.check_sweep),
+            (
+                sla.helpers._condition_voltage_window,
+                sla.plasma_potential._condition_voltage_window,
+            ),
+        ],
+    )
+    def test_helper_callable_consistency(self, helper, used_callable):
+        assert helper is used_callable
+
+    def test_call_of_check_sweep(self) -> None:
+        varr = np.linspace(-20.0, 20.0, 100)
+        carr = np.linspace(-20.0, 20.0, 100)
+
+        with mock.patch(f"{sla.plasma_potential.__name__}.check_sweep") as mock_cs:
+            mock_cs.return_value = varr, carr
+            find_didv_peak_location(voltage=varr, current=carr)
+
+            assert mock_cs.call_count == 1
+
+            # passed args
+            assert len(mock_cs.call_args[0]) == 2
+            assert np.array_equal(mock_cs.call_args[0][0], varr)
+            assert np.array_equal(mock_cs.call_args[0][1], carr)
+
+            # passed kwargs
+            assert mock_cs.call_args[1] == {"strip_units": True}
+
+    def test_call_of_merge_voltage_clusters(self) -> None:
+        varr = np.linspace(-20.0, 20.0, 100)
+        carr = np.linspace(-20.0, 20.0, 100)
+
+        with (
+            mock.patch(f"{sla.plasma_potential.__name__}.merge_voltage_clusters")
+            as mock_mvc
+        ):
+            mock_mvc.return_value = varr, carr
+            find_didv_peak_location(voltage=varr, current=carr)
+
+            assert mock_mvc.call_count == 1
+
+            # passed args
+            assert len(mock_mvc.call_args[0]) == 2
+            assert np.array_equal(mock_mvc.call_args[0][0], varr)
+            assert np.array_equal(mock_mvc.call_args[0][1], carr)
+
+            # passed kwargs
+            assert mock_mvc.call_args[1] == {"voltage_step_size": 0}
+
+    def test_call_of_condition_voltage_window(self) -> None:
+        varr = np.linspace(-20.0, 20.0, 100)
+        carr = np.linspace(-20.0, 20.0, 100)
+
+        with (
+            mock.patch(
+                f"{sla.plasma_potential.__name__}._condition_voltage_window",
+                side_effect=sla.plasma_potential._condition_voltage_window,
+            )
+            as mock_cvw
+        ):
+            # mock_cvw.return_value = varr, carr
+            find_didv_peak_location(voltage=varr, current=carr)
+
+            assert mock_cvw.call_count == 1
+
+            # passed args
+            assert len(mock_cvw.call_args[0]) == 2
+            assert np.array_equal(mock_cvw.call_args[0][0], varr)
+            assert mock_cvw.call_args[0][1] is None  # voltage_window arg
+
+            # passed kwargs
+            assert mock_cvw.call_args[1] == {}
