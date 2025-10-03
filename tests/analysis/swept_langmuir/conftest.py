0f259e25bd073c5dc68ec542bd0ce072de5d9297
Erik <eteverson@gmail.com>
2025-10-02 18:09:49 -0700

    create func arch_current

--- tests/analysis/swept_langmuir/conftest.py
+++ tests/analysis/swept_langmuir/conftest.py
@@ -10,3 +10,21 @@ def simple_voltage():
 @pytest.fixture(scope="module")
 def simple_current(simple_voltage):
     return simple_voltage.copy()
+
+
+def arch_current(voltage, *, radius, centroid, positive_arch=True):
+    # radius: float
+    #     radius of the arch/circle to be generated
+    #
+    # centroid: 2 element array
+    #     center (x, y) location of the circle
+    #
+    # positive_arch: bool
+    #     if True draw the positive (upper) arch
+    #     if False draw the negative (lower) arch
+    #
+
+    sign = 1.0 if positive_arch else -1.0
+    return sign * np.sqrt(
+        radius ** 2 - (voltage - centroid[0]) ** 2
+    ) + centroid[1]
