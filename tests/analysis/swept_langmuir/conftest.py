7f59cadd3a9efc0672f05026fcbb0dc3cbddbe43
Erik <eteverson@gmail.com>
2025-10-02 18:08:46 -0700

    increase size of simple_voltage, make simple_current just be a compy of simple_voltage

--- tests/analysis/swept_langmuir/conftest.py
+++ tests/analysis/swept_langmuir/conftest.py
@@ -4,9 +4,9 @@ import pytest
 
 @pytest.fixture(scope="module")
 def simple_voltage():
-    return np.linspace(-20.0, 20.0, 100)
+    return np.linspace(-20.0, 20.0, 200)
 
 
 @pytest.fixture(scope="module")
-def simple_current():
-    return np.linspace(-20.0, 20.0, 100)
+def simple_current(simple_voltage):
+    return simple_voltage.copy()
