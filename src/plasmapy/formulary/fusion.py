a07a417f5a40b045446f45be089b8b10a7cb07b0
Sean Clark <spc222@cornell.edu>
2026-07-24 15:09:27 -0400

    correcting a comment style

--- src/plasmapy/formulary/fusion.py
+++ src/plasmapy/formulary/fusion.py
@@ -23,9 +23,7 @@ import numpy as np
 from plasmapy.particles import Particle
 from plasmapy.utils.decorators import validate_quantities
 
-"""
-Opening Bosch and Hale Tables IV and Json Files
-"""
+# Opening Bosch and Hale Tables IV and Json Files
 
 _DATA_DIR = files("plasmapy.utils.data")
 
